365727a326fc9a0489b013253e3f0f4b6873a899
Dimitri Glazkov <dimitri@glazkov.com>
2023-06-14 16:36:49 -0700

    [graph-playground] mess some more with invocations.

--- seeds/graph-playground/src/index.ts
+++ seeds/graph-playground/src/index.ts
@@ -51,7 +51,7 @@ interface Edge {
 
 interface GraphDescriptor {
   edges: Edge[];
-  nodes: NodeDescriptor[];
+  nodes: Record<NodeIdentifier, NodeDescriptor>;
 }
 
 const graph: GraphDescriptor = {
@@ -66,16 +66,37 @@ const graph: GraphDescriptor = {
       to: { node: "console-output", input: "text" },
     },
   ],
-  nodes: [
-    { id: "user-input", outputs: ["text"], inputs: [] },
-    { id: "text-completion", inputs: ["text"], outputs: ["completion"] },
-    { id: "console-output", inputs: ["text"], outputs: [] },
-  ],
+  nodes: {
+    "user-input": { id: "user-input", outputs: ["text"], inputs: [] },
+    "text-completion": {
+      id: "text-completion",
+      inputs: ["text"],
+      outputs: ["completion"],
+    },
+    "console-output": { id: "console-output", inputs: ["text"], outputs: [] },
+  },
+};
+
+const invokeNode = (
+  node: NodeDescriptor,
+  input: InputIdentifier | null
+): OutputIdentifier[] => {
+  console.log(
+    `invoke node "${node.id}" with ${input ? `input "${input}"` : "no input"}`
+  );
+  if (!node.outputs.length) {
+    console.log("node produces no further outputs");
+  } else {
+    console.log(
+      `node produces outputs:${node.outputs.map((output) => `\n- ${output}`)}`
+    );
+  }
+  return node.outputs;
 };
 
 const invoke = (edge: Edge) => {
   console.log(
-    `invoke node: "${edge.from.node}", get "${edge.from.output}" output, feed to input "${edge.to.input}" of node "${edge.to.node}"`
+    `[invoke node: "${edge.from.node}", get "${edge.from.output}" output, feed to input "${edge.to.input}" of node "${edge.to.node}"]`
   );
 };
 
@@ -86,14 +107,21 @@ const invoke = (edge: Edge) => {
 const follow = (graph: GraphDescriptor) => {
   let edge = graph.edges.find((edge) => edge.entry);
   let next: NodeIdentifier | null = null;
+  let input: InputIdentifier | null = null;
+  let outputs: OutputIdentifier[] | null = null;
 
   while (edge) {
+    const current = graph.nodes[edge.from.node];
+    outputs = invokeNode(current, input);
+
+    input = edge.to.input;
     invoke(edge);
     next = edge.to.node;
     edge = graph.edges.find((edge) => edge.from.node == next);
   }
   if (next) {
-    console.log(`invoke node: ${next}`);
+    const last = graph.nodes[next];
+    invokeNode(last, input);
   }
 };
 
