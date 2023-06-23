05c99e828782946c91c1d42fba89596609debf72
Dimitri Glazkov <dimitri@glazkov.com>
2023-06-23 12:16:40 -0700

    [graph-playground] Use node VM for code execution.

--- seeds/graph-playground/src/nodes/run-javascript.ts
+++ seeds/graph-playground/src/nodes/run-javascript.ts
@@ -5,15 +5,25 @@
  */
 
 import type { InputValues } from "../graph.js";
+import vm from "node:vm";
 
 // https://regex101.com/r/PeEmEW/1
 const stripCodeBlock = (code: string) =>
   code.replace(/(?:```(?:js|javascript)?\n+)(.*)(?:\n+```)/gms, "$1");
 
+const runCode = async (code: string) => {
+  const context = vm.createContext({ console });
+  const script = new vm.Script(code);
+  const result = await script.runInNewContext(context);
+  return String(result);
+};
+
 export default async (inputs?: InputValues) => {
   if (!inputs) throw new Error("Running JavaScript requires inputs");
   const code = inputs["code"] as string;
-  if (!code) throw new Error("Running JavaScript requires code");
+  if (!code) throw new Error("Running JavaScript requires `code` input");
+  const name = (inputs["name"] as string) || "run";
   const clean = stripCodeBlock(code);
-  return { result: String(eval(`${clean}\ncompute();`)) };
+  const result = await runCode(`${clean}\n${name}();`);
+  return { result };
 };
