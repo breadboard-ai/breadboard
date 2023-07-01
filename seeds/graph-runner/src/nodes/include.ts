e9b53b7db6851ad8faf76bdaf5c108a8b3d22b8d
Dimitri Glazkov <dimitri@glazkov.com>
2023-06-30 17:59:11 -0700

    [graphs] Non-working slotted include.

--- seeds/graph-runner/src/nodes/include.ts
+++ seeds/graph-runner/src/nodes/include.ts
@@ -6,6 +6,7 @@
 
 import { readFile } from "fs/promises";
 import type {
+  GraphDescriptor,
   GraphTraversalContext,
   InputValues,
   NodeHandlers,
@@ -13,14 +14,28 @@ import type {
 } from "../types.js";
 import { traverseGraph } from "../traversal.js";
 
+type SlottedGraphs = Record<string, GraphDescriptor>;
+
+type IncludeInput = {
+  path: string;
+  slotted?: SlottedGraphs;
+  args: InputValues;
+};
+
 class IncludeContext implements GraphTraversalContext {
   log: (s: string) => void;
   values: OutputValues = {};
   handlers: NodeHandlers;
+  slotted: SlottedGraphs;
 
-  constructor(private inputs: InputValues, context: GraphTraversalContext) {
+  constructor(
+    private inputs: InputValues,
+    context: GraphTraversalContext,
+    slotted?: SlottedGraphs
+  ) {
     this.handlers = context.handlers;
     this.log = context.log;
+    this.slotted = slotted || {};
   }
 
   async requestExternalInput(_inputs: InputValues): Promise<OutputValues> {
@@ -32,27 +47,23 @@ class IncludeContext implements GraphTraversalContext {
   }
 
   async requestSlotOutput(
-    _slot: string,
-    _args: InputValues
+    slot: string,
+    args: InputValues
   ): Promise<OutputValues> {
-    throw new Error("Not implemented yet");
+    if (!this.slotted) throw new Error("No slotted sub-graphs were provided");
+    const graph = this.slotted[slot];
+    if (!graph) throw new Error(`No graph found for slot "${slot}"`);
+    const includeContext = new IncludeContext(args, this, {});
+    await traverseGraph(includeContext, graph);
+    return includeContext.values;
   }
 }
 
 export default async (context: GraphTraversalContext, inputs: InputValues) => {
-  const { path, slotted, ...args } = inputs;
+  const { path, slotted, ...args } = inputs as IncludeInput;
   if (!path) throw new Error("To include, we need a path");
-  const traverseSubgraph = async (path: string) => {
-    const graph = JSON.parse(await readFile(path, "utf-8"));
-    const includeContext = new IncludeContext(args, context);
-    await traverseGraph(includeContext, graph);
-    return includeContext.values;
-  };
-
-  if (slotted) {
-    // We have slotted subgraphs, so we need to traverse them in addition to
-    // the included graph.
-    // TODO: Implement handling slotted subgraphs.
-  }
-  return await traverseSubgraph(path as string);
+  const graph = JSON.parse(await readFile(path, "utf-8"));
+  const includeContext = new IncludeContext(args, context, slotted);
+  await traverseGraph(includeContext, graph);
+  return includeContext.values;
 };
