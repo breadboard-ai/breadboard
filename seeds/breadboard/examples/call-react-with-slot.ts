456e8742cf864d8623fe32f8857c2f5a8404e9aa
Dimitri Glazkov <dimitri@glazkov.com>
2023-07-09 14:10:01 -0500

    [breadboard] Add comments.

--- seeds/breadboard/examples/call-react-with-slot.ts
+++ seeds/breadboard/examples/call-react-with-slot.ts
@@ -12,35 +12,62 @@ import { config } from "dotenv";
 
 config();
 
+// A program can contain multiple breadboards. Here's an example that uses four
+// of them.
+
+// A URL to a repository containing various saved breadboard layouts.
 const REPO_URL =
   "https://raw.githubusercontent.com/google/labs-prototypes/main/seeds/graph-playground/graphs/";
 
+/**
+ * Creates a breadboard that wires up a bunch of useful tools for ReAct recipe
+ * to use.
+ * @returns a `tools` breadboard
+ */
 const getTools = () => {
   const tools = new Breadboard();
   const kit = new Starter(tools);
 
+  // Include a a `search-summarize` breadboard from a URL.
+  // The `$id` and `description` are important, because they help ReAct recipe
+  // figure out the purpose of each tool.
   const search = kit.include(`${REPO_URL}/search-summarize.json`, {
     $id: "search",
     description:
       "Useful for when you need to find facts. Input should be a search query.",
   });
+
+  // Include `math` breadboard from a URL.
   const math = kit.include(`${REPO_URL}/math.json`, {
     $id: "math",
     description:
       "Useful for when you need to solve math problems. Input should be a math problem to be solved.",
   });
 
+  // Wire the breadboard:
+  // - Wire input to the `search` and `math` include nodes.
+  // - Additionally, wire input to `reflect` node, which allows the ReAct recipe
+  // to introspect the graph (and get access to `description` and `$id`
+  // properties above)
+  // - Finally, wire the all of the nodes to outputs.
   kit
     .input()
     .wire("graph", kit.reflect().wire("graph", kit.output()))
     .wire("math->text", math.wire("text", kit.output()))
     .wire("search->text", search.wire("text", kit.output()));
+
+  // The `tools` breadboard is ready to go!
   return tools;
 };
 
+// This is the main breadboard that controls the rest of the breadboards.
 const main = new Breadboard();
 const kit = new Starter(main);
 
+// Include the `react-with-slot` breadboard from a URL, wiring input to it.
+// Slot the `tools` breadboard into the `tools` slot.
+// This is how the ReAct recipe will consume the `tools` breadboard we created
+// above.
 kit.input("Ask ReAct").wire(
   "text",
   kit
@@ -50,26 +77,30 @@ kit.input("Ask ReAct").wire(
     .wire("text", kit.output())
 );
 
-// Save breadboard
+// We can save the resulting breadboard
 await writeFile(
   "examples/call-react-with-slot.json",
   JSON.stringify(main, null, 2)
 );
 
-// Make it into a diagram
+// .. or turn it into a diagram.
 await writeFile(
   "examples/call-react-with-slot.md",
   `# Google News Diagram\n\n\`\`\`mermaid\n${toMermaid(main)}\n\`\`\``
 );
 
-// Run breadboard
+// To run the whole thing:
+
+// Add the inputs.
 main.addInputs({
   text: "What's the square root of the number of holes on a typical breadboard?",
 });
 
+// Add the output event handler.
 main.on("output", (event) => {
   const { detail } = event as CustomEvent;
   console.log(detail.text);
 });
 
+// Run the breadboard.
 await main.run();
--- seeds/breadboard/examples/google-news.ts
+++ seeds/breadboard/examples/google-news.ts
@@ -12,64 +12,86 @@ import { config } from "dotenv";
 
 config();
 
-/** Just playing with the API for now */
-
-// Instance of a graph.
+// Create an new breadboard.
 const breadboard = new Breadboard();
-// Core handlers.
-const n = new Starter(breadboard);
 
-const summarizeResults = n.textTemplate(
+// Get a starter kit.
+// Starter kit contains helper methods to create useful nodes.
+// To work, a starter kit needs to be associated with the instance of
+// the breadboard on which we're wiring the nodes.
+const kit = new Starter(breadboard);
+
+// Create a new text template node.
+const summarizeResults = kit.textTemplate(
   "Use the news headlines below to write one or two sentences to summarize the latest news on this topic:\n\n##Topic:\n{{topic}}\n\n## Headlines {{headlines}}\n\\n## Summary:\n"
 );
 
-const newsUrl = n.urlTemplate(
+// Create a new url template node.
+const newsUrl = kit.urlTemplate(
   "https://news.google.com/rss/search?q={{query}}&hl=en-US&gl=US&ceid=US:en"
 );
 
-n.input("What news topic would you like to have summarized?")
+// Wire input to the text template and the url template.
+kit
+  .input("What news topic would you like to have summarized?")
   .wire("text->topic", summarizeResults)
   .wire("text->query", newsUrl);
 
-const fetchHeadlines = n.fetch();
+// Create a new fetch node.
+const fetchHeadlines = kit.fetch();
 
+// Wire the url template to the fetch node.
 newsUrl.wire("url", fetchHeadlines);
 
-const parseHeadlines = n.jsonata(
+// Create a new JSONata node.
+const parseHeadlines = kit.jsonata(
   "$join((rss.channel.item.title.`$t`)[[1..20]], `\n`)"
 );
 
+// Wire a whole series of nodes together:
+// - fetch node to the XML-to-JSON node
+// - XML-to-JSON to the JSONata node.
+// - JSONata node to the text template node.
 fetchHeadlines.wire(
   "response->xml",
-  n
+  kit
     .xmlToJson()
     .wire("json", parseHeadlines.wire("result->headlines", summarizeResults))
 );
 
-const textCompletion = n.textCompletion().wire("completion->text", n.output());
+// Create a new text completion node and wire it to the output.
+const textCompletion = kit
+  .textCompletion()
+  .wire("completion->text", kit.output());
 
-n.secrets(["API_KEY"]).wire("API_KEY", textCompletion);
+// Wire secrets node (containing API_KEY) to the text completion node.
+kit.secrets(["API_KEY"]).wire("API_KEY", textCompletion);
 
+// Wire the text template node to the text completion node.
 summarizeResults.wire("prompt->text", textCompletion);
 
-// Save breadboard
+// Save resulting breadboard
 await writeFile(
   "examples/google-news.json",
   JSON.stringify(breadboard, null, 2)
 );
 
-// Make it into a diagram
+// .. or turn it into a diagram
 await writeFile(
   "examples/google-news.md",
   `# Google News Diagram\n\n\`\`\`mermaid\n${toMermaid(breadboard)}\n\`\`\``
 );
 
-// Run breadboard
+// Run the breadboard:
+
+// Add the inputs.
 breadboard.addInputs({ text: "Breadboards" });
 
+// Add the output event handler
 breadboard.on("output", (event) => {
-  const customEvent = event as CustomEvent;
-  console.log(customEvent.detail.text);
+  const { detail } = event as CustomEvent;
+  console.log(detail.text);
 });
 
+// ... and run!
 await breadboard.run();
