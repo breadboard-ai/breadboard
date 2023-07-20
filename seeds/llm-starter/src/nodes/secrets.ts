b8f4f3f4228f1d5128ac32ee7c685cdd92ab3f25
Dimitri Glazkov <dimitri@glazkov.com>
2023-07-19 20:29:30 -0700

    [llm-starter] Be explicit about secrets.
    Fixes  #23

--- seeds/llm-starter/src/nodes/secrets.ts
+++ seeds/llm-starter/src/nodes/secrets.ts
@@ -11,6 +11,16 @@
 
 import type { InputValues, OutputValues } from "@google-labs/graph-runner";
 
-export default async (_inputs: InputValues) => {
-  return process.env as OutputValues;
+type SecretInputs = {
+  keys: string[];
+};
+
+export default async (inputs: InputValues) => {
+  const { keys = [] } = inputs as SecretInputs;
+  return keys
+    .map((key) => [key, process.env[key]])
+    .reduce((acc, [key, value]) => {
+      if (value) acc[key as string] = value;
+      return acc;
+    }, {} as OutputValues);
 };
