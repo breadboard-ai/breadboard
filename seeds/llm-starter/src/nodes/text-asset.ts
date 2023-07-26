24739a03965e1f0cdd9086a1c8a65b835482b277
Bernhard Seefeld <seefeld@gmail.com>
2023-07-26 15:35:59 -0700

    Fix warnings.

--- seeds/llm-starter/src/nodes/text-asset.ts
+++ seeds/llm-starter/src/nodes/text-asset.ts
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { InputValues, OutputValues } from "@google-labs/graph-runner";
-import * as fs from 'fs';
+import type { InputValues } from "@google-labs/graph-runner";
+import * as fs from "fs";
 
 export default async (inputs: InputValues) => {
-  var f = fs.readFileSync(inputs.filename as string,'utf8');
-  return await {'text': f};
+  const f = fs.readFileSync(inputs.filename as string, "utf8");
+  return await { text: f };
 };
