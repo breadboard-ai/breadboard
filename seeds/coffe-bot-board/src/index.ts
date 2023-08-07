1b3829d0e54763a6a231cdeee3e60985126c5ebc
Dimitri Glazkov <dimitri@glazkov.com>
2023-08-07 16:18:33 -0700

    [coffee-bot-board] Teach bot to make a template.

--- seeds/coffe-bot-board/src/index.ts
+++ seeds/coffe-bot-board/src/index.ts
@@ -4,4 +4,43 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-console.log("code goes here");
+import { Board, BreadboardNode } from "@google-labs/breadboard";
+import { Starter } from "@google-labs/llm-starter";
+
+import { readFile, writeFile } from "fs/promises";
+
+const board = new Board();
+const kit = board.addKit(Starter);
+
+const makeTemplate = async (
+  board: Board,
+  kit: Starter
+): Promise<BreadboardNode> => {
+  const text = await readFile("./prompts/prompt-template.txt", "utf-8");
+  const template = kit.promptTemplate(text);
+
+  // For now, just read them as static files.
+  const readPart = async (name: string) => {
+    const part = await readFile(`./prompts/${name}.txt`, "utf-8");
+    template.wire(`<-${name}`, board.passthrough({ [name]: part }));
+  };
+
+  await Promise.all(
+    ["modifier_list", "hours", "menu", "prices", "modifiers", "moves"].map(
+      (name) => readPart(name)
+    )
+  );
+
+  return template;
+};
+
+const template = await makeTemplate(board, kit);
+board.input().wire("user->", template.wire("prompt->", board.output()));
+
+await writeFile("./graphs/coffee-bot.json", JSON.stringify(board, null, 2));
+
+const result = await board.runOnce({
+  user: "I'd like a latte",
+});
+
+console.log(result);
