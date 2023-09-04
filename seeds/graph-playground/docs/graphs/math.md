4a5c2d46787655b2368265a77dc6816c4ea58a2b
Dimitri Glazkov <dimitri@glazkov.com>
2023-09-03 21:05:24 -0700

    [graph-playground] Add schema to `math` pattern.

--- seeds/graph-playground/docs/graphs/math.md
+++ seeds/graph-playground/docs/graphs/math.md
@@ -6,13 +6,15 @@
   %%{init: 'themeVariables': { 'fontFamily': 'Fira Code, monospace' }}%%
 graph TD;
 compute["runJavascript <br> id='compute'"] -- "result->text" --> print{{"output <br> id='print'"}}:::output
-mathfunctioncompletion["generateText <br> id='math-function-completion'"] -- "completion->code" --> compute["runJavascript <br> id='compute'"]
-secrets1("secrets <br> id='secrets-1'"):::secrets -- "PALM_KEY->PALM_KEY" --> mathfunctioncompletion["generateText <br> id='math-function-completion'"]
-mathfunction["promptTemplate <br> id='math-function'"] -- "prompt->text" --> mathfunctioncompletion["generateText <br> id='math-function-completion'"]
+mathfunctiongenerator["generateText <br> id='math-function-generator'"] -- "completion->code" --> compute["runJavascript <br> id='compute'"]
+secrets1("secrets <br> id='secrets-1'"):::secrets -- "PALM_KEY->PALM_KEY" --> mathfunctiongenerator["generateText <br> id='math-function-generator'"]
+mathfunction["promptTemplate <br> id='math-function'"] -- "prompt->text" --> mathfunctiongenerator["generateText <br> id='math-function-generator'"]
 mathquestion[/"input <br> id='math-question'"/]:::input -- "text->question" --> mathfunction["promptTemplate <br> id='math-function'"]
 messagemathquestion[message]:::config -- "message->message" --o mathquestion
+schemamathquestion[schema]:::config -- "schema->schema" --o mathquestion
 templatemathfunction[template]:::config -- "template->template" --o mathfunction
 namecompute[name]:::config -- "name->name" --o compute
+schemaprint[schema]:::config -- "schema->schema" --o print
 keyssecrets1[keys]:::config -- "keys->keys" --o secrets1
 classDef default stroke:#ffab40,fill:#fff2ccff,color:#000
 classDef input stroke:#3c78d8,fill:#c9daf8ff,color:#000
--- seeds/graph-playground/src/boards/math.ts
+++ seeds/graph-playground/src/boards/math.ts
@@ -10,25 +10,54 @@ import { Starter } from "@google-labs/llm-starter";
 const math = new Board();
 const kit = math.addKit(Starter);
 
-math.input("Ask a math question", { $id: "math-question" }).wire(
-  "text->question",
-  kit
-    .promptTemplate(
-      "Translate the math problem below into a JavaScript function named `compute` that can be executed to provide the answer to the problem\nMath Problem: {{question}}\nSolution:",
-      { $id: "math-function" }
-    )
-    .wire(
-      "prompt->text",
-      kit
-        .generateText({ $id: "math-function-completion" })
-        .wire(
-          "completion->code",
-          kit
-            .runJavascript("compute", { $id: "compute" })
-            .wire("result->text", math.output({ $id: "print" }))
-        )
-        .wire("<-PALM_KEY", kit.secrets(["PALM_KEY"]))
-    )
-);
+math
+  .input("Ask a math question", {
+    $id: "math-question",
+    schema: {
+      type: "object",
+      properties: {
+        text: {
+          type: "string",
+          title: "Math problem",
+          description: "Ask a math question",
+        },
+      },
+      required: ["text"],
+    },
+  })
+  .wire(
+    "text->question",
+    kit
+      .promptTemplate(
+        "Translate the math problem below into a JavaScript function named `compute` that can be executed to provide the answer to the problem\nMath Problem: {{question}}\nSolution:",
+        { $id: "math-function" }
+      )
+      .wire(
+        "prompt->text",
+        kit
+          .generateText({ $id: "math-function-generator" })
+          .wire(
+            "completion->code",
+            kit.runJavascript("compute", { $id: "compute" }).wire(
+              "result->text",
+              math.output({
+                $id: "print",
+                schema: {
+                  type: "object",
+                  properties: {
+                    text: {
+                      type: "string",
+                      title: "Answer",
+                      description: "The answer to the math problem",
+                    },
+                  },
+                  required: ["text"],
+                },
+              })
+            )
+          )
+          .wire("<-PALM_KEY", kit.secrets(["PALM_KEY"]))
+      )
+  );
 
 export default math;
