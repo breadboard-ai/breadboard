2e6b22fcb5b52580c69e497f3c34f1f25b4a2dbf
Dimitri Glazkov <dimitri@glazkov.com>
2023-09-05 21:37:41 -0700

    [breadboard-server] Write tests for `server`.

--- seeds/breadboard-server/tests/server.ts
+++ seeds/breadboard-server/tests/server.ts
@@ -6,6 +6,76 @@
 
 import test from "ava";
 
-test("test", (t) => {
-  t.pass();
+import { runResultLoop } from "../src/server.js";
+import { Board, RunResult } from "@google-labs/breadboard";
+import { Writer, WriterResponse } from "../src/writer.js";
+
+class MockResponse implements WriterResponse {
+  written = "";
+
+  write(chunk: unknown, _encoding?: unknown, _callback?: unknown): boolean {
+    this.written += chunk as string;
+    return true;
+  }
+}
+
+test("runResultLoop correctly handles finite graph", async (t) => {
+  const board = new Board();
+  board
+    .input("in", { $id: "in" })
+    .wire(
+      "*->",
+      board
+        .passthrough({ $id: "noop" })
+        .wire("*->", board.output({ $id: "out" }))
+    );
+
+  const response = new MockResponse();
+  let intermediateState = "";
+  let count = 0;
+  const writer = new Writer(response, async (state) => {
+    intermediateState = state;
+    return (count++).toString();
+  });
+
+  {
+    response.written = "";
+    await runResultLoop(writer, board, {}, undefined);
+    t.is(
+      response.written,
+      '{"type":"input","data":{"message":"in"},"state":"0"}\n'
+    );
+    t.is(
+      intermediateState,
+      '{"state":{"descriptor":{"id":"in","type":"input","configuration":{"message":"in"}},"inputs":{"message":"in"},"missingInputs":[],"opportunities":[],"newOpportunities":[{"from":"in","to":"noop","out":"*"}],"state":{"state":{"$type":"Map","value":[]},"constants":{"$type":"Map","value":[]}}},"type":"input"}'
+    );
+    t.is(count, 1);
+  }
+
+  {
+    response.written = "";
+    const restoredState = RunResult.load(intermediateState);
+    await runResultLoop(writer, board, { text: "hello" }, restoredState);
+    t.is(
+      response.written,
+      '{"type":"beforehandler","data":{"id":"noop","type":"passthrough"}}\n{"type":"output","data":{"text":"hello"},"state":"1"}\n'
+    );
+    t.is(
+      intermediateState,
+      '{"state":{"descriptor":{"id":"out","type":"output"},"inputs":{"text":"hello"},"missingInputs":[],"opportunities":[],"newOpportunities":[],"state":{"state":{"$type":"Map","value":[["out",{"$type":"Map","value":[["noop",{"text":"hello"}]]}]]},"constants":{"$type":"Map","value":[]}}},"type":"output"}'
+    );
+    t.is(count, 2);
+  }
+
+  {
+    response.written = "";
+    const restoredState = RunResult.load(intermediateState);
+    await runResultLoop(writer, board, { text: "hello" }, restoredState);
+    t.is(response.written, '{"type":"done","data":{}}\n');
+    t.is(
+      intermediateState,
+      '{"state":{"descriptor":{"id":"out","type":"output"},"inputs":{"text":"hello"},"missingInputs":[],"opportunities":[],"newOpportunities":[],"state":{"state":{"$type":"Map","value":[["out",{"$type":"Map","value":[["noop",{"text":"hello"}]]}]]},"constants":{"$type":"Map","value":[]}}},"type":"output"}'
+    );
+    t.is(count, 2);
+  }
 });
