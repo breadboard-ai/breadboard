085a6eb4ca50f0371ca2de407975c1744751831c
Dimitri Glazkov <dimitri@glazkov.com>
2023-09-15 15:49:04 -0700

    [cloud-function] Add more secrets to deploy ReAct board.

--- seeds/cloud-function/scripts/deploy.sh
+++ seeds/cloud-function/scripts/deploy.sh
@@ -10,5 +10,5 @@ gcloud functions deploy ${filename%.*} \
 --entry-point=board \
 --trigger-http \
 --allow-unauthenticated \
---set-secrets 'PALM_KEY=PALM_KEY:latest' \
+--set-secrets PALM_KEY=PALM_KEY:latest,GOOGLE_CSE_ID=GOOGLE_CSE_ID:latest \
 --update-env-vars BOARD_URL=https://raw.githubusercontent.com/google/labs-prototypes/main/seeds/graph-playground/graphs/${filename}
