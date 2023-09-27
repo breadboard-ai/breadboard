7c4d9621e493a5fa2246b64852ce32d3c9d1c382
Dimitri Glazkov <dimitri@glazkov.com>
2023-09-27 09:31:19 -0700

    [breadboard-web] Sketch out a mailbox pattern.

--- seeds/breadboard-web/src/worker.ts
+++ seeds/breadboard-web/src/worker.ts
@@ -1,6 +1,36 @@
 import { Board } from "@google-labs/breadboard";
 import { Starter } from "@google-labs/llm-starter";
 
+type MailboxMessage = {
+  id?: string;
+};
+
+export class Mailbox {
+  boxes: Record<string, (value: unknown) => void> = {};
+  worker: Worker;
+
+  constructor(worker: Worker) {
+    this.worker = worker;
+    this.worker.addEventListener("message", this.#onMessage.bind(this));
+  }
+
+  #onMessage(e: MessageEvent) {
+    const message = e.data as MailboxMessage;
+    if (!message.id) return;
+    const resolve = this.boxes[message.id];
+    if (!resolve) return;
+    resolve(message);
+  }
+
+  async ask(data: unknown) {
+    const id = Math.random().toString(36).substring(2, 9);
+    this.worker.postMessage({ id, data });
+    return new Promise((resolve) => {
+      this.boxes[id] = resolve;
+    });
+  }
+}
+
 const BOARD_URL =
   "https://raw.githubusercontent.com/google/labs-prototypes/main/seeds/graph-playground/graphs/math.json";
 
