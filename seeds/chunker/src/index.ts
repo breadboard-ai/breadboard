f94117c20954f459b142c8109430c61ba93f6d43
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-03 16:32:34 -0700

    [chunker] Sketch out a basic chunker.

--- seeds/chunker/src/index.ts
+++ seeds/chunker/src/index.ts
@@ -4,28 +4,91 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export type TextRun = {
-  text: {
-    textRun: string;
-  };
+export type TypedNode = {
+  type: string;
 };
 
-export type StructuralElementType = "paragraph" | "bodySection" | "listItem";
-
-export type StructuralElement = {
-  elements: {
-    [Type in StructuralElementType]: StructuralElement | TextRun;
-  };
+export type TextNode = TypedNode & {
+  type: "text";
+  text: string;
 };
 
-export type DataToChunk = {
-  body: {
-    content: StructuralElement;
-  };
+export type StructuralNode = TypedNode & {
+  children: TypedNode[];
+};
+
+export type DocumentNode = TextNode | StructuralNode;
+
+// TODO: Make it a BasicChunker option
+const MAX_WORDS_PER_PASSAGE = 10;
+
+class AggregateNode {
+  chunked = false;
+  isText = false;
+  wordCount = 0;
+  chunks: string[] = [];
+
+  constructor(node: TypedNode) {
+    this.isText = node.type === "text";
+  }
+
+  aggregate(child: AggregateNode) {
+    this.wordCount += child.wordCount;
+    this.chunks = [...this.chunks, ...child.chunks];
+  }
+
+  fits(child: AggregateNode): boolean {
+    return this.wordCount + child.wordCount <= MAX_WORDS_PER_PASSAGE;
+  }
+}
+
+const addToPassages = (node: AggregateNode, passages: string[]) => {
+  const text = node.chunks.join(" ");
+  if (text.length > 0) passages.push(text);
 };
 
 export class BasicChunker {
-  chunk(data: DataToChunk) {
-    return data.body.content;
+  chunk(data: TypedNode) {
+    const passages: string[] = [];
+    const node = this.processNode(data, passages);
+    console.log("passages", passages);
+    return node;
+  }
+
+  processNode(docNode: TypedNode, passages: string[]): AggregateNode {
+    const node = new AggregateNode(docNode);
+    if (!node.isText) {
+      const text = (docNode as TextNode).text;
+      if (text) {
+        const words = text.split(" ");
+        node.wordCount = words.length;
+        node.chunks.push(text);
+      }
+      return node;
+    }
+    const aggregatingNode = new AggregateNode(docNode);
+
+    let shouldAggregate = false;
+    const unchunkedNodes: AggregateNode[] = [];
+    const children = (docNode as StructuralNode).children;
+    if (children) {
+      for (const child of children) {
+        const childNode = this.processNode(child, passages);
+        if (childNode.chunked) {
+          shouldAggregate = false;
+        } else {
+          aggregatingNode.aggregate(childNode);
+          unchunkedNodes.push(childNode);
+        }
+      }
+    }
+    if (!shouldAggregate || !node.fits(aggregatingNode)) {
+      unchunkedNodes.forEach((unchunkedNode) => {
+        addToPassages(unchunkedNode, passages);
+      });
+    }
+    addToPassages(node, passages);
+    node.chunked = true;
+    return node;
   }
 }
