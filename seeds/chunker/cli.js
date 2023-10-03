8611454ba584572574df44cbd27c25357695b91c
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-03 15:25:23 -0700

    [chunker] Stub out basic CLI plumbing.

--- seeds/chunker/cli.js
+++ seeds/chunker/cli.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import process, { exit } from "process";
+import { readFile } from "fs/promises";
+
+import { BasicChunker } from "./dist/src/index.js";
 
 /**
  * @license
@@ -6,4 +10,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-console.log("Command line utility goes here");
+const filename = process.argv[2];
+if (!filename) {
+  console.log("Give me a path to a file with structured data (JSON) to chunk");
+  exit(1);
+}
+
+const data = JSON.parse(await readFile(filename));
+
+console.log(`Chunking ${filename} ...`);
+
+const chunker = new BasicChunker();
+const result = chunker.chunk(data);
+
+console.log("DONE", result);
--- seeds/chunker/src/index.ts
+++ seeds/chunker/src/index.ts
@@ -4,4 +4,28 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-console.log("code goes here");
+export type TextRun = {
+  text: {
+    textRun: string;
+  };
+};
+
+export type StructuralElementType = "paragraph" | "bodySection" | "listItem";
+
+export type StructuralElement = {
+  elements: {
+    [Type in StructuralElementType]: StructuralElement | TextRun;
+  };
+};
+
+export type DataToChunk = {
+  body: {
+    content: StructuralElement;
+  };
+};
+
+export class BasicChunker {
+  chunk(data: DataToChunk) {
+    return data.body.content;
+  }
+}
