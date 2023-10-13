37998ef1f25637e0c71297104bc62b23b577d32f
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-13 15:29:57 -0700

    [llm-starter] Teach `jsonata` to describe itself.

--- seeds/llm-starter/src/nodes/jsonata.ts
+++ seeds/llm-starter/src/nodes/jsonata.ts
@@ -4,7 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { InputValues } from "@google-labs/graph-runner";
+import {
+  InputValues,
+  NodeDescriberFunction,
+  NodeHandlerFunction,
+  Schema,
+} from "@google-labs/graph-runner";
 
 import jsonata from "jsonata";
 
@@ -18,13 +23,90 @@ export type JsonataInputs = {
   json?: unknown;
 };
 
+export const jsonataHandler: NodeHandlerFunction<object> = async (
+  inputs: InputValues
+) => {
+  const { expression, raw, ...rest } = inputs as JsonataInputs;
+  if (!expression) throw new Error("Jsonata node requires `expression` input");
+  const json = rest.json || rest;
+  const result = await jsonata(expression).evaluate(json);
+  return raw ? result : { result };
+};
+
+export const generateOutputSchema = async (
+  inputs: InputValues
+): Promise<Schema> => {
+  if (!inputs || !inputs.raw) {
+    return {
+      type: "object",
+      properties: {
+        result: {
+          title: "result",
+          description: "The result of the Jsonata expression",
+          type: "string",
+        },
+      },
+      required: ["result"],
+    };
+  }
+
+  try {
+    const result = await jsonataHandler(inputs);
+    if (!result) return {};
+    const properties: Schema["properties"] = {};
+    const outputSchema = {
+      type: "object",
+      properties,
+      additionalProperties: false,
+    };
+    Object.entries(result).forEach(([key, value]) => {
+      properties[key] = {
+        type: typeof value,
+        title: key,
+      };
+    });
+    return outputSchema;
+  } catch (e) {
+    return {
+      type: "object",
+      additionalProperties: false,
+      properties: {},
+    };
+  }
+};
+
+export const jsonataDescriber: NodeDescriberFunction = async (
+  inputs?: InputValues
+) => {
+  const outputSchema = await generateOutputSchema(inputs || {});
+  return {
+    inputSchema: {
+      type: "object",
+      properties: {
+        expression: {
+          title: "expression",
+          description: "The Jsonata expression to evaluate",
+          type: "string",
+        },
+        raw: {
+          title: "raw",
+          description:
+            "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
+          type: "boolean",
+        },
+        json: {
+          title: "json",
+          description: "The JSON object to evaluate",
+          type: ["object", "string"],
+        },
+      },
+      required: ["expression"],
+    },
+    outputSchema,
+  };
+};
+
 export default {
-  invoke: async (inputs: InputValues) => {
-    const { expression, raw, ...rest } = inputs as JsonataInputs;
-    if (!expression)
-      throw new Error("Jsonata node requires `expression` input");
-    const json = rest.json || rest;
-    const result = await jsonata(expression).evaluate(json);
-    return raw ? result : { result };
-  },
+  describe: jsonataDescriber,
+  invoke: jsonataHandler,
 };
