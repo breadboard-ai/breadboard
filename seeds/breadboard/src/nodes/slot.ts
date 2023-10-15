a7a0a0129bc938a0c4714256c79ae7189f4e311b
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-15 12:01:18 -0700

    [breadboard] Teach `slot` node to describe itself.

--- seeds/breadboard/src/nodes/slot.ts
+++ seeds/breadboard/src/nodes/slot.ts
@@ -11,15 +11,30 @@ import type {
   SlotNodeInputs,
 } from "../types.js";
 import { BoardRunner } from "../runner.js";
+import { SchemaBuilder } from "../schema.js";
 
-export default async (
-  inputs: InputValues,
-  context: NodeHandlerContext
-): Promise<OutputValues> => {
-  const { slot, ...args } = inputs as SlotNodeInputs;
-  if (!slot) throw new Error("To use a slot, we need to specify its name");
-  const graph = context.slots[slot];
-  if (!graph) throw new Error(`No graph found for slot "${slot}"`);
-  const slottedBreadboard = await BoardRunner.fromGraphDescriptor(graph);
-  return await slottedBreadboard.runOnce(args, context);
+export default {
+  describe: async (inputs?: InputValues) => ({
+    inputSchema: new SchemaBuilder()
+      .setAdditionalProperties(true)
+      .addInputs(inputs)
+      .addProperty("slot", {
+        title: "slot",
+        description: "The slot to run.",
+        type: "string",
+      })
+      .build(),
+    outputSchema: new SchemaBuilder().setAdditionalProperties(true).build(),
+  }),
+  invoke: async (
+    inputs: InputValues,
+    context: NodeHandlerContext
+  ): Promise<OutputValues> => {
+    const { slot, ...args } = inputs as SlotNodeInputs;
+    if (!slot) throw new Error("To use a slot, we need to specify its name");
+    const graph = context.slots[slot];
+    if (!graph) throw new Error(`No graph found for slot "${slot}"`);
+    const slottedBreadboard = await BoardRunner.fromGraphDescriptor(graph);
+    return await slottedBreadboard.runOnce(args, context);
+  },
 };
