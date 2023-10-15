f99482820c341737a6aa9d2babd09d42e49a7eab
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-15 11:59:44 -0700

    [breadboard] Teach `lambda` node to describe itself.

--- seeds/breadboard/src/nodes/lambda.ts
+++ seeds/breadboard/src/nodes/lambda.ts
@@ -11,17 +11,40 @@ import {
   LambdaNodeOutputs,
 } from "../types.js";
 import { Board } from "../board.js";
+import { SchemaBuilder } from "../schema.js";
 
-export default async (inputs: InputValues): Promise<LambdaNodeOutputs> => {
-  const { board, ...args } = inputs as LambdaNodeInputs;
-  if (!board || board.kind !== "board" || !board.board)
-    throw new Error(`Lambda node requires a BoardCapability as "board" input`);
-  const runnableBoard = {
-    ...(await Board.fromBreadboardCapability(board)),
-    args,
-  };
+export default {
+  describe: async (inputs?: InputValues) => ({
+    inputSchema: new SchemaBuilder()
+      .setAdditionalProperties(true)
+      .addInputs(inputs)
+      .addProperty("board", {
+        title: "board",
+        description: "The board to run.",
+        type: "object",
+      })
+      .build(),
+    outputSchema: new SchemaBuilder()
+      .addProperty("board", {
+        title: "board",
+        description: "The now-runnable board.",
+        type: "object",
+      })
+      .build(),
+  }),
+  invoke: async (inputs: InputValues): Promise<LambdaNodeOutputs> => {
+    const { board, ...args } = inputs as LambdaNodeInputs;
+    if (!board || board.kind !== "board" || !board.board)
+      throw new Error(
+        `Lambda node requires a BoardCapability as "board" input`
+      );
+    const runnableBoard = {
+      ...(await Board.fromBreadboardCapability(board)),
+      args,
+    };
 
-  return {
-    board: { ...board, board: runnableBoard as GraphDescriptor },
-  };
+    return {
+      board: { ...board, board: runnableBoard as GraphDescriptor },
+    };
+  },
 };
