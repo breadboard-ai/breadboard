f3a37909ba76fa67752b99714ef3af53cc1643c6
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-15 11:47:55 -0700

    [breadboard] Teach `include` to describe itself.

--- seeds/breadboard/src/nodes/include.ts
+++ seeds/breadboard/src/nodes/include.ts
@@ -12,35 +12,67 @@ import type {
   NodeHandlerContext,
 } from "../types.js";
 import { Board } from "../board.js";
+import { SchemaBuilder } from "../schema.js";
 
-export default async (
-  inputs: InputValues,
-  context: NodeHandlerContext
-): Promise<OutputValues> => {
-  const { path, $ref, board, graph, slotted, ...args } =
-    inputs as IncludeNodeInputs;
+export default {
+  describe: async (inputs?: InputValues) => ({
+    inputSchema: new SchemaBuilder()
+      .setAdditionalProperties(true)
+      .addInputs(inputs)
+      .addProperties({
+        path: {
+          title: "path",
+          description: "The path to the board to include.",
+          type: "string",
+        },
+        $ref: {
+          title: "$ref",
+          description: "The $ref to the board to include.",
+          type: "string",
+        },
+        graph: {
+          title: "graph",
+          description: "The graph descriptor of the board to include.",
+          type: "object",
+        },
+        slotted: {
+          title: "slotted",
+          description: "The slotted graphs to include.",
+          type: "object",
+        },
+      })
+      .build(),
+    outputSchema: new SchemaBuilder().setAdditionalProperties(true).build(),
+  }),
+  invoke: async (
+    inputs: InputValues,
+    context: NodeHandlerContext
+  ): Promise<OutputValues> => {
+    const { path, $ref, board, graph, slotted, ...args } =
+      inputs as IncludeNodeInputs;
 
-  // Add the current graph's URL as the url of the slotted graph,
-  // if there isn't an URL already.
-  const slottedWithUrls: BreadboardSlotSpec = {};
-  if (slotted) {
-    for (const key in slotted) {
-      slottedWithUrls[key] = { url: context.board.url, ...slotted[key] };
+    // Add the current graph's URL as the url of the slotted graph,
+    // if there isn't an URL already.
+    const slottedWithUrls: BreadboardSlotSpec = {};
+    if (slotted) {
+      for (const key in slotted) {
+        slottedWithUrls[key] = { url: context.board.url, ...slotted[key] };
+      }
     }
-  }
 
-  // TODO: Please fix the $ref/path mess.
-  const source = path || $ref || "";
+    // TODO: Please fix the $ref/path mess.
+    const source = path || $ref || "";
 
-  const runnableBoard = board
-    ? await Board.fromBreadboardCapability(board)
-    : graph
-    ? await Board.fromGraphDescriptor(graph)
-    : await Board.load(source, {
-        slotted: slottedWithUrls,
-        base: context.board.url,
-        outerGraph: context.parent,
-      });
+    const runnableBoard = board
+      ? await Board.fromBreadboardCapability(board)
+      : graph
+      ? await Board.fromGraphDescriptor(graph)
+      : await Board.load(source, {
+          slotted: slottedWithUrls,
+          base: context.board.url,
+          outerGraph: context.parent,
+        });
 
-  return await runnableBoard.runOnce(args, context);
+    return await runnableBoard.runOnce(args, context);
+  },
 };
