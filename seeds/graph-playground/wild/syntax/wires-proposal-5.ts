17defc946dc10dbfb8d22b9ce47b08c6c1819608
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-15 22:21:28 -0700

    [graph-playground] Tweak proposal 5 some more.

--- seeds/graph-playground/wild/syntax/wires-proposal-5.ts
+++ seeds/graph-playground/wild/syntax/wires-proposal-5.ts
@@ -1,14 +1,43 @@
 {
   /**
    * The Board only has two nodes: input and output.
-   * @see BoardNode
+   * @see PlacedNode
+   * You can wire nodes by calling the `wire` method on the board.
+   *
+   * ```js
+   * board.wire(input.out.text, prompt.in.question);
+   * ```
+   *
+   * You can also wire nodes in bunches:
+   *
+   * ```js
+   * board.wire(input.out.text, [prompt.in.question, conversationMemory.in.user]);
+   * ```
+   *
+   * This is the only way to wire nodes in a queue-ey way.
+   *
+   * You can also connect nodes in a standard function-call-ey way:
+   *
+   * ```js
+   * kit.promptTemplate({
+   *  PALM_KEY: kit.secrets({ keys: ["PALM_KEY"] }).out.PALM_KEY,
+   * });
+   *
    */
   type Board = {
-    addInput(options?: unknown): PlacedNode;
-    addOutput(options?: unknown): PlacedNode;
-    add(node: UnplacedNode): PlacedNode;
+    input(options?: Options): PlacedNode;
+    output(options?: Options): PlacedNode;
+    /**
+     * Takes an unplaced node and places it on the board.
+     * This is useful for kit-less nodes.
+     * @param node The node to place on the board
+     */
+    place(node: UnplacedNode): PlacedNode;
+    addKit(kit: unknown): void;
     wire(inPort: InputPort, outPort: OutputPort): Board;
   };
+
+  type Options = Record<string, unknown>;
 
   type InputPort = object;
   type OutputPort = object;
@@ -16,41 +45,45 @@
   type InputPorts = Record<string, InputPort>;
   type OutputPorts = Record<string, OutputPort>;
 
+  /**
+   * Each node, when placed on the board, has two sets of ports:
+   * - `in` ports
+   * - `out` ports
+   */
   type PlacedNode = {
     in: InputPorts;
     out: OutputPorts;
   };
 
-  type UnplacedNode = (options: unknown) => UnplacedNode;
+  type UnplacedNode = (options: Options) => PlacedNode;
 
   const board = {} as Board;
-  const promptTemplate = {} as UnplacedNode;
-  const generateText = {} as UnplacedNode;
-  const runJavascript = {} as UnplacedNode;
-  const secrets = {} as UnplacedNode;
-  const passthrough = {} as UnplacedNode;
-  const append = {} as UnplacedNode;
+  const kit = {} as {
+    promptTemplate: UnplacedNode;
+    generateText: UnplacedNode;
+    runJavascript: UnplacedNode;
+    secrets: UnplacedNode;
+    passthrough: UnplacedNode;
+    append: UnplacedNode;
+  };
 
   {
     // math.ts
-    const mathFunction = board.add(
-      promptTemplate({
-        $id: "math-function",
-        template:
-          "Translate the math problem below into a JavaScript function named `compute` that can be executed to provide the answer to the problem\nMath Problem: {{question}}\nSolution:",
-      })
-    );
-    const mathFunctionGenerator = board.add(
-      generateText({
-        $id: "math-function-generator",
-        PALM_KEY: board.add(secrets(["PALM_KEY"])).out.PALM_KEY,
-      })
-    );
-    const compute = board.add(runJavascript({ $id: "compute" }));
+    board.addKit(kit);
+    const mathFunction = kit.promptTemplate({
+      $id: "math-function",
+      template:
+        "Translate the math problem below into a JavaScript function named `compute` that can be executed to provide the answer to the problem\nMath Problem: {{question}}\nSolution:",
+    });
+    const mathFunctionGenerator = kit.generateText({
+      $id: "math-function-generator",
+      PALM_KEY: kit.secrets({ keys: ["PALM_KEY"] }).out.PALM_KEY,
+    });
+    const compute = kit.runJavascript({ $id: "compute" });
 
     board
       .wire(
-        board.addInput({
+        board.input({
           $id: "math-question",
           schema: {
             type: "object",
@@ -70,7 +103,7 @@
       .wire(mathFunctionGenerator.out.completion, compute.in.code)
       .wire(
         compute.out.result,
-        board.addOutput({
+        board.output({
           $id: "print",
           schema: {
             type: "object",
@@ -89,9 +122,10 @@
 
   {
     // accumulating-context.ts
+    board.addKit(kit);
     // Store the input node so that we can refer back
     // to it to create a conversation loop.
-    const input = board.addInput({
+    const input = board.input({
       $id: "userRequest",
       schema: {
         type: "object",
@@ -108,24 +142,20 @@
 
     // Use the `append` node to accumulate the conversation history.
     // Populate it with initial context.
-    const conversationMemory = board.add(
-      append({
-        accumulator: "\n== Conversation History",
-        $id: "conversationMemory",
-      })
-    );
+    const conversationMemory = kit.append({
+      accumulator: "\n== Conversation History",
+      $id: "conversationMemory",
+    });
 
     // Store the promptTemplate for the same reason.
-    const prompt = board.add(
-      promptTemplate({
-        $id: "assistant",
-        context: "",
-        template:
-          "This is a conversation between a friendly assistant and their user. You are the assistant and your job is to try to be helpful, empathetic, and fun.\n{{context}}\n\n== Current Conversation\nuser: {{question}}\nassistant:",
-      })
-    );
+    const prompt = kit.promptTemplate({
+      $id: "assistant",
+      context: "",
+      template:
+        "This is a conversation between a friendly assistant and their user. You are the assistant and your job is to try to be helpful, empathetic, and fun.\n{{context}}\n\n== Current Conversation\nuser: {{question}}\nassistant:",
+    });
 
-    const output = board.addOutput({
+    const output = board.output({
       $id: "assistantResponse",
       schema: {
         type: "object",
@@ -141,19 +171,17 @@
       },
     });
 
-    const generator = board.add(
-      generateText({
-        $id: "generator",
-        PALM_KEY: board.add(secrets(["PALM_KEY"])).out.PALM_KEY,
-      })
-    );
+    const generator = kit.generateText({
+      $id: "generator",
+      PALM_KEY: kit.secrets({ keys: ["PALM_KEY"] }).out.PALM_KEY,
+    });
 
     board
       .wire(conversationMemory.out.accumulator, [
         conversationMemory.in.accumulator,
         prompt.in.context,
       ])
-      .wire(board.add(passthrough({ $id: "start" })).out, input.in)
+      .wire(kit.passthrough({ $id: "start" }).out, input.in)
       .wire(input.out.text, [prompt.in.question, conversationMemory.in.user])
       .wire(generator.out.completion, [
         output.in.text,
