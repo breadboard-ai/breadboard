9ebfad509d83506479bfd07569c289a5b120d3d8
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-18 14:22:55 -0700

    [graph-playground] Add `infer-query` graph.

---
+++ seeds/graph-playground/docs/graphs/infer-query.md
@@ -0,0 +1,28 @@
+# infer-query
+  - Original: [`infer-query.ts`](../../src/boards/infer-query.ts)
+  - Graph: [`infer-query.json`](../../graphs/infer-query.json)
+  
+  ```mermaid
+  %%{init: 'themeVariables': { 'fontFamily': 'Fira Code, monospace' }}%%
+graph TD;
+questionGenerator ["generateText <br> id='questionGenerator '"] -- "completion->text" --> printResults{{"output <br> id='printResults'"}}:::output
+secrets1("secrets <br> id='secrets-1'"):::secrets -- "PALM_KEY->PALM_KEY" --> questionGenerator ["generateText <br> id='questionGenerator '"]
+inferringPrompt["promptTemplate <br> id='inferringPrompt'"] -- "prompt->text" --> questionGenerator ["generateText <br> id='questionGenerator '"]
+promptStuffer["jsonata <br> id='promptStuffer'"] -- "result->result" --> inferringPrompt["promptTemplate <br> id='inferringPrompt'"]
+promptToInfer["promptTemplate <br> id='promptToInfer'"] -- "prompt->json" --> promptStuffer["jsonata <br> id='promptStuffer'"]
+askForTemplate[/"input <br> id='askForTemplate'"/]:::input -- "text->template" --> promptToInfer["promptTemplate <br> id='promptToInfer'"]
+schemaaskForTemplate[schema]:::config -- "schema->schema" --o askForTemplate
+schemaprintResults[schema]:::config -- "schema->schema" --o printResults
+contextpromptToInfer[context]:::config -- "context->context" --o promptToInfer
+templateinferringPrompt[template]:::config -- "template->template" --o inferringPrompt
+expressionpromptStuffer[expression]:::config -- "expression->expression" --o promptStuffer
+keyssecrets1[keys]:::config -- "keys->keys" --o secrets1
+classDef default stroke:#ffab40,fill:#fff2ccff,color:#000
+classDef input stroke:#3c78d8,fill:#c9daf8ff,color:#000
+classDef output stroke:#38761d,fill:#b6d7a8ff,color:#000
+classDef passthrough stroke:#a64d79,fill:#ead1dcff,color:#000
+classDef slot stroke:#a64d79,fill:#ead1dcff,color:#000
+classDef config stroke:#a64d79,fill:#ead1dcff,color:#000
+classDef secrets stroke:#db4437,fill:#f4cccc,color:#000
+classDef slotted stroke:#a64d79
+  ```
---
+++ seeds/graph-playground/src/boards/infer-query.ts
@@ -0,0 +1,98 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Board } from "@google-labs/breadboard";
+import { Starter } from "@google-labs/llm-starter";
+
+const board = new Board({
+  title: "Inferring a query for the RAG pattern",
+  description:
+    "This board is a prototype to infer the query from the original prompt for  retrieval-augmented generation (RAG). The basic idea is that the user provides a template with a placeholder for the retrieved data, and the query to retrieve this data is inferred from the template itself.",
+  version: "0.0.1",
+});
+const starter = board.addKit(Starter);
+
+const askForTemplate = board.input({
+  $id: "askForTemplate",
+  schema: {
+    type: "object",
+    properties: {
+      text: {
+        type: "string",
+        title: "Template",
+        format: "multiline",
+        description:
+          "Provide the text of the template, using {{context}} to specify location of context to be retrieved.",
+      },
+    },
+  },
+});
+
+const printResults = board.output({
+  $id: "printResults",
+  schema: {
+    type: "object",
+    properties: {
+      text: {
+        type: "string",
+        title: "Result",
+        description: "The final result of RAG with inferrred query.",
+      },
+    },
+  },
+});
+
+const contextPlaceholder = "CONTEXT GOES HERE";
+
+const promptToInfer = starter.promptTemplate(undefined, {
+  $id: "promptToInfer",
+  context: `\n\n${contextPlaceholder}\n\n`,
+});
+
+const inferringPrompt = starter.promptTemplate(
+  `
+The JSON below represents a prompt that the user wants to populate with more context to better answer questions within the prompt. The context will supplied by a search engine and will be inserted in place of the "${contextPlaceholder}" placeholder.
+
+Your job is to analyze the prompt and summarize all of the information within it, then formulate a query that would allow retrieving the relevant context from a search engine.
+
+Reply as valid JSON in the following format:
+{
+  "summary: [
+    "the list of points in the prompt that will be used to formulate the query"
+  ]
+  "query": "the detailed, comprehensive query that may consist of several questions"
+}
+
+JSON:
+{{result}}
+
+Question:`,
+  { $id: "inferringPrompt" }
+);
+
+const promptStuffer = starter.jsonata('{ "prompt": $ }', {
+  $id: "promptStuffer",
+});
+
+const questionGenerator = starter.generateText({ $id: "questionGenerator " });
+
+askForTemplate.wire(
+  "text->template",
+  promptToInfer.wire(
+    "prompt->json",
+    promptStuffer.wire(
+      "result->",
+      inferringPrompt.wire(
+        "prompt->text",
+        questionGenerator
+          .wire("completion->text", printResults)
+          .wire("<-PALM_KEY", starter.secrets(["PALM_KEY"]))
+      )
+    )
+  )
+);
+
+export default board;
