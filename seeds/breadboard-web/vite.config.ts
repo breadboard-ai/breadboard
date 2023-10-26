d8346752e45bcef50ba1dfb91b788e2a21f31092
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-26 13:34:36 -0700

    [breadboard] Teach vite to have configurable "allow" dirs.

--- seeds/breadboard-web/vite.config.ts
+++ seeds/breadboard-web/vite.config.ts
@@ -1,16 +1,33 @@
-export default {
-  build: {
-    lib: {
-      entry: {
-        worker: "src/worker.ts",
-        sample: "./index.html",
-      },
-      name: "Breadboard Web Runtime",
-      formats: ["es"],
-    },
-    target: "esnext",
-  },
-  test: {
-    include: ["tests/**/*.ts"],
-  },
+import { config } from "dotenv";
+import { defineConfig } from "vitest/config";
+
+export const buildCustomAllowList = (value?: string) => {
+  if (!value) return {};
+  return { fs: { allow: [value] } };
 };
+
+export default defineConfig(async ({ mode }) => {
+  config();
+
+  console.log(mode, process.env.VITE_FS_ALLOW, process.cwd());
+
+  return {
+    build: {
+      lib: {
+        entry: {
+          worker: "src/worker.ts",
+          sample: "./index.html",
+        },
+        name: "Breadboard Web Runtime",
+        formats: ["es"],
+      },
+      target: "esnext",
+    },
+    server: {
+      ...buildCustomAllowList(process.env.VITE_FS_ALLOW),
+    },
+    test: {
+      include: ["tests/**/*.ts"],
+    },
+  };
+});
