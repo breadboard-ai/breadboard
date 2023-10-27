4a5f136c7940975cfed54ff09574c4175e6b21f5
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-26 22:51:50 -0700

    [breadboard-web] Add Pinecone kit.

--- seeds/breadboard-web/src/worker.ts
+++ seeds/breadboard-web/src/worker.ts
@@ -11,6 +11,7 @@ import {
 import { Board } from "@google-labs/breadboard";
 import { Starter } from "@google-labs/llm-starter";
 import { Core } from "@google-labs/core-kit";
+import { Pinecone } from "@google-labs/pinecone-kit";
 
 const controller = new MessageController(self as unknown as Worker);
 const runtime = new WorkerRuntime(controller);
@@ -18,4 +19,4 @@ const runtime = new WorkerRuntime(controller);
 const url = await runtime.onload();
 const runner = await Board.load(url);
 
-await runtime.run(runner, [Starter, Core]);
+await runtime.run(runner, [Starter, Core, Pinecone]);
