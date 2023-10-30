8ef88a126de547c5a6825242566b288b756ca849
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-30 08:54:14 -0700

    [breadboard-web] Add one more asyncGen team.

--- seeds/breadboard-web/tests/async-gen.ts
+++ seeds/breadboard-web/tests/async-gen.ts
@@ -9,6 +9,7 @@ import { asyncGen } from "../src/async-gen";
 
 test("async-gen", async () => {
   const results = [];
+  const yields = [];
 
   async function* foo() {
     yield 1;
@@ -24,7 +25,30 @@ test("async-gen", async () => {
 
   for await (const val of foo()) {
     results.push(val);
+    yields.push(val);
   }
 
   expect(results).toEqual([1, 1.5, 2, 2.5, 3, 3.5, 4]);
+  expect(yields).toEqual([1, 2, 3, 4]);
+});
+
+test("async-gen call to next is optional", async () => {
+  const results = [];
+  const yields = [];
+
+  async function* foo() {
+    yield 1;
+    yield* asyncGen(async () => {
+      results.push(1.5);
+    });
+    yield 2;
+  }
+
+  for await (const val of foo()) {
+    results.push(val);
+    yields.push(val);
+  }
+
+  expect(results).toEqual([1, 1.5, 2]);
+  expect(yields).toEqual([1, 2]);
 });
