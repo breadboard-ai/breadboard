80357421af32e38b040f4b864dec1a17386048e0
Dimitri Glazkov <dimitri@glazkov.com>
2023-10-31 21:21:08 -0700

    [breadboard-web] Naive implementation of preserving access token.

--- seeds/node-nursery-web/src/nodes/credentials.ts
+++ seeds/node-nursery-web/src/nodes/credentials.ts
@@ -5,7 +5,14 @@
  */
 
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithCredential,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 import {
   InputValues,
   NodeHandler,
@@ -23,11 +30,12 @@ export type CredentialsOutputs = OutputValues & {
   accessToken: string;
 };
 
+const CREDENTIALS_KEY = "google:credentials:accessToken";
+
 export default {
   invoke: async (inputs: InputValues): Promise<OutputValues> => {
     const { API_KEY, AUTH_DOMAIN, PROJECT_ID, scopes } =
       inputs as CredentialsInputs;
-    console.log("CREDENTIALS:", inputs, globalThis);
     if (!API_KEY) throw "API Key is required to get credentials.";
     if (!AUTH_DOMAIN)
       throw "The domain for the popup is required to get credentials.";
@@ -37,12 +45,21 @@ export default {
       authDomain: AUTH_DOMAIN,
       projectId: PROJECT_ID,
     });
+
+    const auth = getAuth();
+
     const provider = new GoogleAuthProvider();
     (scopes || []).forEach((scope) => provider.addScope(scope));
 
-    const auth = getAuth();
-    const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    return { accessToken: credential?.accessToken };
+    let accessToken =
+      globalThis.sessionStorage.getItem(CREDENTIALS_KEY) || undefined;
+    if (!accessToken) {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      accessToken = credential?.accessToken;
+      accessToken &&
+        globalThis.sessionStorage.setItem(CREDENTIALS_KEY, accessToken);
+    }
+    return { accessToken };
   },
 } satisfies NodeHandler;
