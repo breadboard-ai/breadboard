6211050b660f4176f72ee7fd6d4f58116183fc66
Paul Lewis <617438+paullewis@users.noreply.github.com>
2023-11-22 09:37:49 +0000

    [breadboard -> breadboard-web] move UI

--- seeds/breadboard-web/src/ui/input.ts
+++ seeds/breadboard-web/src/ui/input.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { type Schema } from "../types.js";
+import { type Schema } from "@google-labs/breadboard";
 import {
   createMultipartInput,
   getMultipartValue,
