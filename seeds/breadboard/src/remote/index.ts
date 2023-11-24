1efd36b3ad3dd13948205b3f5962e5609a454816
Dimitri Glazkov <dimitri@glazkov.com>
2023-11-24 11:13:13 -0800

    [breadboard] Export `remote` submodule.

---
+++ seeds/breadboard/src/remote/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { HTTPServerTransport, HTTPClientTransport } from "./http.js";
+export { ProxyServer, ProxyClient } from "./proxy.js";
+export { RunServer, RunClient } from "./run.js";
+export type * from "./protocol.js";
