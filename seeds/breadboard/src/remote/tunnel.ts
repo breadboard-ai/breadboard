a2f1e0c8a9a7c57f3bd059f367091e8f08ec92cf
Dimitri Glazkov <dimitri@glazkov.com>
2023-11-26 10:36:05 -0800

    [breadboard/remote] Continue sketching out tunnels.

--- seeds/breadboard/src/remote/tunnel.ts
+++ seeds/breadboard/src/remote/tunnel.ts
@@ -4,40 +4,57 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { callHandler } from "../handler.js";
 import {
   InputValues,
+  Kit,
+  NodeHandlerContext,
+  NodeHandlers,
   NodeTypeIdentifier,
   NodeValue,
   OutputValues,
 } from "../types.js";
+import {
+  ProxyServerConfig,
+  TunnelDestinations,
+  TunnelInputs,
+  TunnelSpec,
+} from "./config.js";
 
-export type TunnelInputs = {
-  [inputName: string]: string | RegExp;
+/**
+ * All tunnels defined for a particular node (the tunnel entry).
+ */
+export type NodeTunnels = {
+  [outputName: string]: NodeTunnel[];
 };
 
-export type TunnelDestinations = {
-  to: NodeTypeIdentifier;
-  inputs: TunnelInputs;
+/**
+ * The map of the entire network of tunnels for all nodes.
+ */
+export type TunnelMap = {
+  [node: NodeTypeIdentifier]: NodeTunnels;
 };
 
-export type TunnelSpec = {
-  [outputName: string]:
-    | TunnelDestinations
-    | TunnelDestinations[]
-    | string[]
-    | string;
+export type DestinationTunnels = {
+  [inputName: string]: NodeTunnel;
 };
 
-export type Tunnels = {
-  [outputName: string]: Tunnel[];
+export type TunnelDestinationMap = {
+  [destinationNode: NodeTypeIdentifier]: DestinationTunnels;
 };
 
-export class Tunnel implements TunnelDestinations {
+export class NodeTunnel implements TunnelDestinations {
   constructor(
     readonly outputName: string,
+    readonly from: NodeTypeIdentifier,
     readonly to: NodeTypeIdentifier,
     readonly inputs: TunnelInputs = {}
   ) {}
+
+  getInputNames() {
+    const inputNames = Object.keys(this.inputs);
+    return inputNames.length === 0 ? [this.outputName] : inputNames;
+  }
 
   matches(inputs: InputValues) {
     return Object.entries(this.inputs).every(([inputName, value]) => {
@@ -52,23 +69,44 @@ export class Tunnel implements TunnelDestinations {
   }
 }
 
-export const readSpec = (spec: TunnelSpec): Tunnels => {
+export const readConfig = (config: ProxyServerConfig): TunnelMap => {
+  if (!config.proxy) return {};
+  return Object.fromEntries(
+    config.proxy
+      .map((spec) => {
+        if (typeof spec === "string") {
+          return undefined;
+        }
+        if (!spec.tunnel) return undefined;
+        return [spec.node, readNodeSpec(spec.node, spec.tunnel)];
+      })
+      .filter(Boolean) as [NodeTypeIdentifier, NodeTunnels][]
+  );
+};
+
+export const readNodeSpec = (
+  node: NodeTypeIdentifier,
+  spec: TunnelSpec
+): NodeTunnels => {
   return Object.fromEntries(
     Object.entries(spec).map(([outputName, value]) => {
       if (typeof value === "string") {
-        return [outputName, [new Tunnel(outputName, value)]];
+        return [outputName, [new NodeTunnel(outputName, node, value)]];
       } else if (Array.isArray(value)) {
         return [
           outputName,
           value.map((v) => {
             if (typeof v === "string") {
-              return new Tunnel(outputName, v);
+              return new NodeTunnel(outputName, node, v);
             }
-            return new Tunnel(outputName, v.to, v.inputs);
+            return new NodeTunnel(outputName, node, v.to, v.inputs);
           }),
         ];
       } else {
-        return [outputName, [new Tunnel(outputName, value.to, value.inputs)]];
+        return [
+          outputName,
+          [new NodeTunnel(outputName, node, value.to, value.inputs)],
+        ];
       }
     })
   );
@@ -77,13 +115,14 @@ export const readSpec = (spec: TunnelSpec): Tunnels => {
 type OutputReplacer = (outputName: string, outputValue: NodeValue) => NodeValue;
 
 export const replaceOutputs = (
-  outputs: OutputValues,
-  matches: Tunnels,
+  outputs: void | OutputValues,
+  tunnels: NodeTunnels,
   replacer: OutputReplacer
-): OutputValues => {
+): void | OutputValues => {
+  if (!outputs) return;
   return Object.fromEntries(
     Object.entries(outputs).map(([outputName, value]) => {
-      return outputName in matches
+      return outputName in tunnels
         ? [outputName, replacer(outputName, value)]
         : [outputName, value];
     })
@@ -93,16 +132,15 @@ export const replaceOutputs = (
 type InputReplacer = (inputName: string, inputValue: NodeValue) => NodeValue;
 
 export const replaceInputs = (
-  node: NodeTypeIdentifier,
   inputs: InputValues,
-  matches: Tunnels,
+  tunnels: DestinationTunnels,
   replacer: InputReplacer
 ) => {
   return Object.fromEntries(
     Object.entries(inputs).map(([inputName, value]) => {
-      const match = matches[inputName]?.find((match) => match.to === node);
-      if (!match) return [inputName, value];
-      if (!match.matches(inputs)) return [inputName, value];
+      const tunnel = tunnels[inputName];
+      if (!tunnel) return [inputName, value];
+      if (!tunnel.matches(inputs)) return [inputName, value];
       return [inputName, replacer(inputName, value)];
     })
   );
@@ -110,36 +148,133 @@ export const replaceInputs = (
 
 // Compute a simple hash that expires every 7 days.
 // The point of this hash is not protect anything, but rather to have
-// a simple way to identify a string that represents a protected value.
+// a simple way to identify a tunnelled value.
 // It is also rotating so that the users of the node proxy don't accidentally
 // adopt bad practices of hard-coding the values.
-// Note: the rotation is not window-based, so it will occasionaly cause errors
-// at the break of the week.
+// Note: the rotation will occasionaly cause errors at the break of the week.
 // TODO: Fix the rotation to be window-based or come up with an even better
 // solution.
 const expirationHash = Math.round(Date.now() / 1000 / 60 / 60 / 7).toString(36);
-const PROTECTED_PREFIX = `VAULT-${expirationHash}-`;
+const TUNNEL_PREFIX = `T-${expirationHash}-`;
+const TUNNEL_SUFFIX = `-${expirationHash}-T`;
 
-export const getProtectedValue = (
+export const getTunnelValue = (
   nodeType: NodeTypeIdentifier,
-  outputName: string
+  outputName: string,
+  inputs: InputValues
 ) => {
-  return `${PROTECTED_PREFIX}${nodeType}-${outputName}`;
+  const memoize = JSON.stringify(inputs);
+  return `${TUNNEL_PREFIX}${nodeType}$${outputName}$${memoize}${TUNNEL_SUFFIX}`;
+};
+
+export const createDestinationMap = (map: TunnelMap): TunnelDestinationMap => {
+  // pivot the map of tunnel entries to create a map of tunnel destinations
+  const entries = Object.entries(map).flatMap(([_, nodeTunnels]) => {
+    return Object.entries(nodeTunnels).flatMap(([_, tunnels]) => {
+      return tunnels.map((tunnel) => {
+        return [tunnel.to, tunnel];
+      });
+    });
+  }) as [NodeTypeIdentifier, NodeTunnel][];
+  // collate entries by destination node
+  return entries.reduce((acc, [to, tunnel]) => {
+    if (!acc[to]) acc[to] = {};
+    for (const inputName of tunnel.getInputNames()) {
+      // TODO: What to do about tunnel destination collisions?
+      // At this point, let's just throw an exception.
+      if (acc[to][inputName]) {
+        throw new Error(
+          `Tunnel destination collision: ${to}.${inputName} is already occupied by ${acc[to][inputName].from}.${acc[to][inputName].outputName}`
+        );
+      }
+      acc[to][inputName] = tunnel;
+    }
+    return acc;
+  }, {} as Record<NodeTypeIdentifier, Record<NodeTypeIdentifier, NodeTunnel>>);
+};
+
+/**
+ * A special kit that provides tunneling of outputs and inputs as specified
+ * by the Tunnels spec.
+ *
+ * This kit is constructed from existing NodeHandlers and the Tunnels spec.
+ * It reads the spec and wraps the node handlers to add the tunneling logic.
+ *
+ * The tunnel entries, or the outputs of nodes that are tunneled, are replaced
+ * with a special value that is computed from the node type and the output name.
+ *
+ * The tunnel destinations, or the inputs of the nodes to which a tunnel leads, are
+ * replaced with the original value of the tunnel entry.
+ */
+export const createTunnelKit = (
+  map: TunnelMap,
+  handlers: NodeHandlers
+): Kit => {
+  // wrap handlers to tunnel outputs (tunnel entries)
+  handlers = Object.fromEntries(
+    Object.entries(handlers).map(([nodeType, handler]) => {
+      const nodeTunnels = map[nodeType];
+      if (!nodeTunnels) return [nodeType, handler];
+      return [
+        nodeType,
+        async (inputs: InputValues, context: NodeHandlerContext) => {
+          const outputs = await callHandler(handler, inputs, context);
+          return replaceOutputs(outputs, nodeTunnels, (name) =>
+            getTunnelValue(nodeType, name, inputs)
+          );
+        },
+      ];
+    })
+  );
+
+  // wrap handlers to connect the tunnel to the inputs (tunnel destinations)
+  const destinations = createDestinationMap(map);
+  handlers = Object.fromEntries(
+    Object.entries(handlers).map(([nodeType, handler]) => {
+      const destinationTunnels = destinations[nodeType];
+      if (!destinationTunnels) return [nodeType, handler];
+      return [
+        nodeType,
+        async (inputs: InputValues, context: NodeHandlerContext) => {
+          return callHandler(
+            handler,
+            replaceInputs(inputs, destinationTunnels, (name, value) => {
+              // TODO: Implement replacement here.
+              // scan for tunneled values in `value`.
+              // for each found `tunnel value`,
+              // - extract the node type and output name of the tunnel entry.
+              // - call the handler of the node type for the tunnel entry
+              //   with the inputs that are decoded from the tunnel value
+              // - from the outputs, extract the inputs that are tunneled
+              //   and replace the `tunnel value` with them.
+              return value;
+            }),
+            context
+          );
+        },
+      ];
+    })
+  );
+
+  return {
+    url: "tunnel-kit",
+    handlers,
+  };
 };
 
 export class Vault {
-  #spec: Tunnels;
+  #spec: NodeTunnels;
   #nodeType: NodeTypeIdentifier;
 
   constructor(nodeType: NodeTypeIdentifier, spec: TunnelSpec) {
-    this.#spec = readSpec(spec);
+    this.#spec = {};
     this.#nodeType = nodeType;
   }
 
   protectOutputs(outputs: void | OutputValues) {
     if (!outputs) return outputs;
     return replaceOutputs(outputs, this.#spec, (name) =>
-      getProtectedValue(this.#nodeType, name)
+      getTunnelValue(this.#nodeType, name, {})
     );
   }
 
--- seeds/breadboard/tests/remote/tunnel.ts
+++ seeds/breadboard/tests/remote/tunnel.ts
@@ -6,16 +6,21 @@
 
 import test from "ava";
 import {
-  Vault,
-  Tunnel,
-  getProtectedValue,
-  readSpec,
+  NodeTunnel,
+  getTunnelValue,
+  readNodeSpec,
   replaceInputs,
   replaceOutputs,
+  readConfig,
+  createTunnelKit,
+  createDestinationMap,
 } from "../../src/remote/tunnel.js";
+import { BoardRunner } from "../../src/runner.js";
+import { callHandler } from "../../src/handler.js";
+import { NodeHandlers } from "../../src/types.js";
 
-test("readSpec works as advertised", (t) => {
-  const output = readSpec({
+test("readNodeSpec works as advertised", (t) => {
+  const output = readNodeSpec("secrets", {
     foo: "bar",
     bar: ["foo", "baz"],
     baz: { to: "foo", inputs: { foo: "bar" } },
@@ -25,13 +30,39 @@ test("readSpec works as advertised", (t) => {
     ],
   });
   t.deepEqual(output, {
-    foo: [new Tunnel("foo", "bar")],
-    bar: [new Tunnel("bar", "foo"), new Tunnel("bar", "baz")],
-    baz: [new Tunnel("baz", "foo", { foo: "bar" })],
-    qux: [
-      new Tunnel("qux", "foo", { foo: "bar" }),
-      new Tunnel("qux", "baz", { foo: "foo" }),
+    foo: [new NodeTunnel("foo", "secrets", "bar")],
+    bar: [
+      new NodeTunnel("bar", "secrets", "foo"),
+      new NodeTunnel("bar", "secrets", "baz"),
     ],
+    baz: [new NodeTunnel("baz", "secrets", "foo", { foo: "bar" })],
+    qux: [
+      new NodeTunnel("qux", "secrets", "foo", { foo: "bar" }),
+      new NodeTunnel("qux", "secrets", "baz", { foo: "foo" }),
+    ],
+  });
+});
+
+test("readConfig works as advertised", (t) => {
+  const output = readConfig({
+    board: {} as BoardRunner,
+    proxy: [
+      "fetch",
+      {
+        node: "secrets",
+        tunnel: {
+          PALM_KEY: ["palm-generateText", "palm-embedText"],
+        },
+      },
+    ],
+  });
+  t.deepEqual(output, {
+    secrets: {
+      PALM_KEY: [
+        new NodeTunnel("PALM_KEY", "secrets", "palm-generateText"),
+        new NodeTunnel("PALM_KEY", "secrets", "palm-embedText"),
+      ],
+    },
   });
 });
 
@@ -42,7 +73,7 @@ test("replaceOutputs works as advertised", (t) => {
       bar: "baz",
     },
     {
-      foo: [new Tunnel("foo", "bar")],
+      foo: [new NodeTunnel("foo", "secrets", "bar")],
     },
     (name, value) => `${name}=${value}`
   );
@@ -55,13 +86,14 @@ test("replaceOutputs works as advertised", (t) => {
 test("replaceInputs works as advertised", (t) => {
   {
     const result = replaceInputs(
-      "fetch",
       {
         url: "https://example.com",
         foo: "bar",
       },
       {
-        foo: [new Tunnel("foo", "fetch", { url: "https://example2.com" })],
+        foo: new NodeTunnel("foo", "fetch", "fetch", {
+          url: "https://example2.com",
+        }),
       },
       (name, value) => `${name}=${value}`
     );
@@ -72,13 +104,14 @@ test("replaceInputs works as advertised", (t) => {
   }
   {
     const result = replaceInputs(
-      "fetch",
       {
         url: "https://example.com",
         foo: "bar",
       },
       {
-        foo: [new Tunnel("foo", "fetch", { url: "https://example.com" })],
+        foo: new NodeTunnel("foo", "fetch", "fetch", {
+          url: "https://example.com",
+        }),
       },
       (name, value) => `${name}=${value}`
     );
@@ -89,13 +122,12 @@ test("replaceInputs works as advertised", (t) => {
   }
   {
     const result = replaceInputs(
-      "fetch",
       {
         url: "https://example.com",
         foo: "bar",
       },
       {
-        foo: [new Tunnel("foo", "fetch", { url: /example\.com/ })],
+        foo: new NodeTunnel("foo", "fetch", "fetch", { url: /example\.com/ }),
       },
       (name, value) => `${name}=${value}`
     );
@@ -106,13 +138,12 @@ test("replaceInputs works as advertised", (t) => {
   }
   {
     const result = replaceInputs(
-      "fetch",
       {
         url: "https://example.com",
         foo: "bar",
       },
       {
-        url: [new Tunnel("url", "fetch", { url: /example\.com/ })],
+        url: new NodeTunnel("url", "fetch", "fetch", { url: /example\.com/ }),
       },
       (name, value) => `${name}=${value}`
     );
@@ -123,11 +154,65 @@ test("replaceInputs works as advertised", (t) => {
   }
 });
 
-test("Vault correctly protects outputs", (t) => {
-  const vault = new Vault("secrets", {
-    test: [new Tunnel("test", "bar")],
+test("createTunnelKit creates a kit that tunnels outputs", async (t) => {
+  const handlers = {
+    secrets: async () => {
+      return { API_KEY: "<secret value>" };
+    },
+  } satisfies NodeHandlers;
+  const kit = createTunnelKit(
+    {
+      secrets: {
+        API_KEY: [
+          new NodeTunnel("API_KEY", "secrets", "someNodeThatUsesAPIKey"),
+        ],
+      },
+    },
+    handlers
+  );
+  t.deepEqual(await callHandler(handlers.secrets, {}, {}), {
+    API_KEY: "<secret value>",
   });
-  const protectedValue = getProtectedValue("secrets", "test");
-  const result = vault.protectOutputs({ test: "value", foo: "bar" });
-  t.deepEqual(result, { test: protectedValue, foo: "bar" });
+  t.deepEqual(await callHandler(kit.handlers.secrets, {}, {}), {
+    API_KEY: getTunnelValue("secrets", "API_KEY", {}),
+  });
+});
+
+test("createDestinationMap works as advertised", (t) => {
+  {
+    const output = createDestinationMap({
+      secrets: {
+        API_KEY: [
+          new NodeTunnel("API_KEY", "secrets", "someNodeThatUsesAPIKey"),
+        ],
+      },
+    });
+    t.deepEqual(output, {
+      someNodeThatUsesAPIKey: {
+        API_KEY: new NodeTunnel("API_KEY", "secrets", "someNodeThatUsesAPIKey"),
+      },
+    });
+  }
+  {
+    const output = createDestinationMap({
+      secrets: {
+        API_KEY: [
+          new NodeTunnel("API_KEY", "secrets", "someNodeThatUsesAPIKey"),
+        ],
+        ANOTHER_KEY: [
+          new NodeTunnel("ANOTHER_KEY", "secrets", "someNodeThatUsesAPIKey"),
+        ],
+      },
+    });
+    t.deepEqual(output, {
+      someNodeThatUsesAPIKey: {
+        API_KEY: new NodeTunnel("API_KEY", "secrets", "someNodeThatUsesAPIKey"),
+        ANOTHER_KEY: new NodeTunnel(
+          "ANOTHER_KEY",
+          "secrets",
+          "someNodeThatUsesAPIKey"
+        ),
+      },
+    });
+  }
 });
