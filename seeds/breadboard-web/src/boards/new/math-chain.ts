a0da47626ad5edeca998bc4cf84e15dc94ca3b05
Bernhard Seefeld <seefeld@gmail.com>
2023-11-28 14:34:23 -0800

    [syntax] add zod types to math examples

--- seeds/breadboard-web/src/boards/new/math-chain.ts
+++ seeds/breadboard-web/src/boards/new/math-chain.ts
@@ -4,27 +4,38 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { z } from "zod";
 import { recipe } from "@google-labs/breadboard";
 
 import { starter } from "@google-labs/llm-starter";
 import { palm } from "@google-labs/palm-kit";
 
-export const graph = recipe(async (inputs) => {
-  return starter
-    .promptTemplate({
-      template:
-        "Write a Javascript function called `run` to compute the result for this question:\nQuestion: {{question}}\nCode: ",
-      question: inputs.question,
-    })
-    .prompt.as("text")
-    .to(
-      palm.generateText({
-        PALM_KEY: starter.secrets({ keys: ["PALM_KEY"] }).PALM_KEY,
+export const graph = recipe(
+  {
+    input: z.object({
+      question: z.string().describe("Query: What is your math question?"),
+    }),
+    output: z.object({
+      result: z.string().describe("Answer: The answer to the query"),
+    }),
+  },
+  async (inputs) => {
+    return starter
+      .promptTemplate({
+        template:
+          "Write a Javascript function called `run` to compute the result for this question:\nQuestion: {{question}}\nCode: ",
+        question: inputs.question,
       })
-    )
-    .completion.as("code")
-    .to(starter.runJavascript());
-});
+      .prompt.as("text")
+      .to(
+        palm.generateText({
+          PALM_KEY: starter.secrets({ keys: ["PALM_KEY"] }).PALM_KEY,
+        })
+      )
+      .completion.as("code")
+      .to(starter.runJavascript());
+  }
+);
 
 export const example = { question: "1+1" };
 
--- seeds/breadboard-web/src/boards/new/math-imperative.ts
+++ seeds/breadboard-web/src/boards/new/math-imperative.ts
@@ -4,24 +4,35 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { z } from "zod";
 import { recipe } from "@google-labs/breadboard";
 
 import { starter } from "@google-labs/llm-starter";
 import { palm } from "@google-labs/palm-kit";
 
-export const graph = recipe((inputs) => {
-  const { prompt } = starter.promptTemplate({
-    template:
-      "Write a Javascript function called `run` to compute the result for this question:\nQuestion: {{question}}\nCode: ",
-    question: inputs.question,
-  });
-  const { completion } = palm.generateText({
-    text: prompt,
-    PALM_KEY: starter.secrets({ keys: ["PALM_KEY"] }).PALM_KEY,
-  });
-  const result = starter.runJavascript({ code: completion });
-  return result;
-});
+export const graph = recipe(
+  {
+    input: z.object({
+      question: z.string().describe("Query: What is your math question?"),
+    }),
+    output: z.object({
+      result: z.string().describe("Answer: The answer to the query"),
+    }),
+  },
+  (inputs) => {
+    const { prompt } = starter.promptTemplate({
+      template:
+        "Write a Javascript function called `run` to compute the result for this question:\nQuestion: {{question}}\nCode: ",
+      question: inputs.question,
+    });
+    const { completion } = palm.generateText({
+      text: prompt,
+      PALM_KEY: starter.secrets({ keys: ["PALM_KEY"] }).PALM_KEY,
+    });
+    const result = starter.runJavascript({ code: completion });
+    return result;
+  }
+);
 
 export const example = { question: "1+1" };
 
