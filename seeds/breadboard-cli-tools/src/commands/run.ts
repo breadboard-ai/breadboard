a48cfbb702f6718a13801bf450e126794a67d14d
Paul Lewis <617438+paullewis@users.noreply.github.com>
2023-12-07 13:12:26 +0000

    [breadboard-cli-tools] Fix lint issue

--- seeds/breadboard-cli-tools/src/commands/run.ts
+++ seeds/breadboard-cli-tools/src/commands/run.ts
@@ -1,15 +1,24 @@
-import { BoardRunner, InputValues, Kit, asRuntimeKit } from '@google-labs/breadboard';
-import { watch as fsWatch } from 'fs';
-import { loadBoard, parseStdin, resolveFilePath } from './lib/utils.js';
+import {
+  BoardRunner,
+  InputValues,
+  Kit,
+  asRuntimeKit,
+} from "@google-labs/breadboard";
+import { watch as fsWatch } from "fs";
+import { loadBoard, parseStdin, resolveFilePath } from "./lib/utils.js";
 
-async function runBoard(board: BoardRunner, inputs: InputValues, kitDeclarations: string[] | undefined) {
-  let kits: Kit[] = [];
+async function runBoard(
+  board: BoardRunner,
+  inputs: InputValues,
+  kitDeclarations: string[] | undefined
+) {
+  const kits: Kit[] = [];
 
   if (kitDeclarations != undefined) {
     // We should warn if we are importing code and the associated risks
     for (const kitDetail of kitDeclarations) {
       const kitImport = await import(kitDetail);
-      kits.push(asRuntimeKit(kitImport.default));  
+      kits.push(asRuntimeKit(kitImport.default));
     }
   }
 
@@ -23,7 +32,7 @@ async function runBoard(board: BoardRunner, inputs: InputValues, kitDeclarations
 }
 
 export const run = async (file: string, options: Record<string, any>) => {
-  console.log(options)
+  console.log(options);
   const kitDeclarations = options.kit as string[] | undefined;
 
   if (file != undefined) {
@@ -36,34 +45,44 @@ export const run = async (file: string, options: Record<string, any>) => {
     await runBoard(board, input, kitDeclarations);
 
     // If we are watching, we need to run the board again when the file changes.
-    if ('watch' in options) {
+    if ("watch" in options) {
       const controller = new AbortController();
 
-      fsWatch(file, { signal: controller.signal }, async (eventType: string, filename: string | Buffer | null) => {
-        if (typeof (filename) != 'string') return;
+      fsWatch(
+        file,
+        { signal: controller.signal },
+        async (eventType: string, filename: string | Buffer | null) => {
+          if (typeof filename != "string") return;
 
-        if (eventType === 'change') {
-          // Now the board has changed, we need to reload it and run it again.
-          board = await loadBoard(filePath);
-          // We might want to clear the console here.
-          await runBoard(board, input, kitDeclarations);
+          if (eventType === "change") {
+            // Now the board has changed, we need to reload it and run it again.
+            board = await loadBoard(filePath);
+            // We might want to clear the console here.
+            await runBoard(board, input, kitDeclarations);
+          } else if (eventType === "rename") {
+            console.error(
+              `File ${filename} has been renamed. We can't manage this yet. Sorry!`
+            );
+            controller.abort();
+          }
         }
-        else if (eventType === 'rename') {
-          console.error(`File ${filename} has been renamed. We can't manage this yet. Sorry!`);
-          controller.abort();
-        }
-      });
+      );
     }
-  }
-  else {
+  } else {
     const stdin = await parseStdin();
-    const url = URL.createObjectURL(new Blob([stdin], { type: 'application/json' }));
+    const url = URL.createObjectURL(
+      new Blob([stdin], { type: "application/json" })
+    );
 
     // We should validate it looks like a board...
     const board = await BoardRunner.fromGraphDescriptor(JSON.parse(stdin));
 
-    await runBoard(board, <InputValues> <unknown> options["input"], kitDeclarations);
+    await runBoard(
+      board,
+      <InputValues>(<unknown>options["input"]),
+      kitDeclarations
+    );
 
     URL.revokeObjectURL(url);
   }
-}
+};
