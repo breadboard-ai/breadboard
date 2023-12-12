a48d057c8404e69444ba4de79943fd8b1fc83266
Alexander Marks <aomarks@google.com>
2023-12-12 12:19:15 -0800

    Remove non breadboard packages

--- seeds/palm-lite/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-## [0.0.2] - 2023-07-31
-
-- started a changelog
-- updated link to homepage in `package.json`
--- seeds/pythonboard/src/util.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-def wrap_future(value):
-  future = asyncio.Future()
-  if isinstance(value, Exception):
-    future.set_exception(value)
-  else:
-    future.set_result(value)
-  return future
