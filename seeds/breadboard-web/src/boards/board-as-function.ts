7294369a7b80648503f494c48f0b4c64e7ead240
Dimitri Glazkov <dimitri@glazkov.com>
2023-12-14 22:13:41 -0800

    [breadboard-web] Convert "Board as Function".

--- seeds/breadboard-web/src/boards/board-as-function.ts
+++ seeds/breadboard-web/src/boards/board-as-function.ts
@@ -4,84 +4,80 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Board } from "@google-labs/breadboard";
-import Starter from "@google-labs/llm-starter";
+import { Schema, V, base, recipe } from "@google-labs/breadboard";
+import { starter } from "@google-labs/llm-starter";
 
-const board = new Board({
+const metadata = {
   title: "Board as Function",
   description: "Take a board URL and turn it into an LLM function call",
-  version: "0.0.1",
-});
+  version: "0.0.2",
+};
 
-const starter = board.addKit(Starter);
-
-const input = board.input({
-  $id: "input",
-  schema: {
-    type: "object",
-    properties: {
-      boardURL: {
-        type: "string",
-        title: "Board URL",
-        description: "The URL of the board to convert to a function call",
-        default: "/graphs/board-as-function.json",
-      },
+const inputSchema = {
+  type: "object",
+  properties: {
+    boardURL: {
+      type: "string",
+      title: "Board URL",
+      description: "The URL of the board to convert to a function call",
+      default: "/graphs/board-as-function.json",
     },
   },
-});
+} satisfies Schema;
 
-const output = board.output({
-  $id: "output",
-  schema: {
-    type: "object",
-    properties: {
-      function: {
-        type: "object",
-        title: "Call",
-        description: "The function call to make",
-      },
-      returns: {
-        type: "object",
-        title: "Returns",
-        description: "Schema of the return value(s) of the function",
-      },
+const outputSchema = {
+  type: "object",
+  properties: {
+    function: {
+      type: "object",
+      title: "Call",
+      description: "The function call to make",
+    },
+    returns: {
+      type: "object",
+      title: "Returns",
+      description: "Schema of the return value(s) of the function",
     },
   },
-});
+} satisfies Schema;
 
-const getFunctionSignature = starter.jsonata({
-  $id: "getFunctionSignature",
-  expression: `
-  (
-    $adjustType := function ($type) {
-        $type = "object" or $type = "array" ? "string" : $type
-    };
+export default await recipe(async () => {
+  const input = base.input({ $id: "input", schema: inputSchema });
+  const output = base.output({ $id: "output", schema: outputSchema });
 
-    { 
-    "function": {
-        "name": $replace(title, /\\W/, "_"),
-        "description": description,
-        "parameters": {
-            "type": "object",
-            "properties": nodes[type="input"][0].configuration.schema.properties ~> $each(function($v, $k) {
-            { $k: {
-                "type": $v.type ~> $adjustType,
-                "description": $v.description
-            } }
-            }) ~> $merge
-        }
-    }, 
-    "returns": nodes[type="output"][0].configuration.schema ~> | ** | {}, 'title' |
-    }
-)`,
-  raw: true,
-});
+  const getFunctionSignature = starter.jsonata({
+    $id: "getFunctionSignature",
+    expression: `
+    (
+      $adjustType := function ($type) {
+          $type = "object" or $type = "array" ? "string" : $type
+      };
+  
+      { 
+      "function": {
+          "name": $replace(title, /\\W/, "_"),
+          "description": description,
+          "parameters": {
+              "type": "object",
+              "properties": nodes[type="input"][0].configuration.schema.properties ~> $each(function($v, $k) {
+              { $k: {
+                  "type": $v.type ~> $adjustType,
+                  "description": $v.description
+              } }
+              }) ~> $merge
+          }
+      }, 
+      "returns": nodes[type="output"][0].configuration.schema ~> | ** | {}, 'title' |
+      }
+  )`,
+    raw: true,
+  });
 
-starter
-  .fetch({
-    $id: "getBoard",
-  })
-  .wire("url<-boardURL", input)
-  .wire("response->json", getFunctionSignature.wire("*->", output));
+  starter
+    .fetch({ $id: "getBoard", url: input.boardURL as V<string> })
+    .response.as("json")
+    .to(getFunctionSignature)
+    .to(output);
 
-export default board;
+  return output;
+}).serialize(metadata);
