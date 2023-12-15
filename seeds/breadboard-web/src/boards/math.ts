dd9450cb2ae3405da24ecf20fc951c0bc82ef3af
Dimitri Glazkov <dglazkov@google.com>
2023-12-14 21:54:59 -0800

    [breadboard-web] Convert "The Calculator Recipe" to new syntax (#192)
    - start on maths
    - Polish.

--- seeds/breadboard-web/src/boards/math.ts
+++ seeds/breadboard-web/src/boards/math.ts
@@ -4,81 +4,73 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Board, Schema } from "@google-labs/breadboard";
-import Core from "@google-labs/core-kit";
-import { Starter } from "@google-labs/llm-starter";
+import { Schema, V, base, recipe } from "@google-labs/breadboard";
+import { core } from "@google-labs/core-kit";
+import { starter } from "@google-labs/llm-starter";
 
-const recipe = new Board({
+const metadata = {
   title: "The Calculator Recipe",
   description:
     "A simple AI pattern that leans on the power of the LLMs to generate language to solve math problems.",
   version: "0.0.2",
-});
-const kit = recipe.addKit(Starter);
-const core = recipe.addKit(Core);
+};
 
-const inputs = recipe.input({
-  $id: "math-question",
-  schema: {
-    type: "object",
-    properties: {
-      text: {
-        type: "string",
-        title: "Math problem",
-        description: "Ask a math question",
-        examples: ["What is the square root of pi?"],
-      },
-      generator: {
-        type: "string",
-        title: "Generator",
-        description: "The URL of the generator to call",
-        default: "/graphs/text-generator.json",
-      },
+const inputSchema = {
+  type: "object",
+  properties: {
+    question: {
+      type: "string",
+      title: "Math problem",
+      description: "Ask a math question",
+      examples: ["What is the square root of pi?"],
     },
-    required: ["text"],
-  } satisfies Schema,
-});
+    generator: {
+      type: "string",
+      title: "Generator",
+      description: "The URL of the generator to call",
+      default: "/graphs/text-generator.json",
+    },
+  },
+  required: ["text"],
+} satisfies Schema;
 
-inputs.wire(
-  "text->question",
-  kit
-    .promptTemplate({
-      template:
-        "Translate the math problem below into a self-contained, zero-argument JavaScript function named `compute` that can be executed to provide the answer to the problem. Do not use any dependencies or libraries.\nMath Problem: {{question}}\nSolution:",
-      $id: "math-function",
+const outputSchema = {
+  type: "object",
+  properties: {
+    result: {
+      type: "string",
+      title: "Answer",
+      description: "The answer to the math problem",
+    },
+  },
+  required: ["text"],
+} satisfies Schema;
+
+export default await recipe(async () => {
+  const input = base.input({ $id: "math-question", schema: inputSchema });
+  const template = starter.promptTemplate({
+    template: `Translate the math problem below into a self-contained,
+zero-argument JavaScript function named \`compute\` that can be executed
+to provide the answer to the problem. 
+
+Do not use any dependencies or libraries.
+
+Math Problem: {{question}}
+
+Solution:`,
+    $id: "math-function",
+    question: input.question,
+  });
+  const generator = core.invoke({
+    $id: "generator",
+    path: input.generator as V<string>,
+    text: template.prompt,
+  });
+  return starter
+    .runJavascript({
+      $id: "compute",
+      name: "compute",
+      code: generator.text as V<string>,
     })
-    .wire(
-      "prompt->text",
-      core
-        .invoke({ $id: "generator" })
-        .wire("path<-generator", inputs)
-        .wire(
-          "text->code",
-          kit
-            .runJavascript({
-              name: "compute",
-              $id: "compute",
-            })
-            .wire(
-              "result->text",
-              recipe.output({
-                $id: "print",
-                schema: {
-                  type: "object",
-                  properties: {
-                    text: {
-                      type: "string",
-                      title: "Answer",
-                      description: "The answer to the math problem",
-                    },
-                  },
-                  required: ["text"],
-                },
-              })
-            )
-            .wire("<-PALM_KEY", kit.secrets({ keys: ["PALM_KEY"] }))
-        )
-    )
-);
-
-export default recipe;
+    .to(base.output({ $id: "answer", schema: outputSchema }));
+}).serialize(metadata);
