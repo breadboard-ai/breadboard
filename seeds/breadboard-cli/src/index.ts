bafe996d4b71a810b694e652a69e49631e77999f
Paul Kinlan <paul.kinlan@gmail.com>
2023-12-18 13:21:53 +0000

    [breadboard-cli] Run debug with custom kits (#210)
    This PR was originally about `npx breadboard debug echo.json --kit <some
    kit>`, however there's a lot of unresolved pieces. This PR is the first
    step of cleaning it up so it's a bit easier to introduce at a later
    date.
    
    + Fixes `sh Permission Denied` after a 2nd build.
    + Moves boards to 0.0.2 so they are visible
    + Cleaned up logic to return list of boards
    + Enabled transpilation of ts and js boards (watching these is not yet
    tested)
    + Refactor of code to make `debug` cleaner

--- seeds/breadboard-cli/src/index.ts
+++ seeds/breadboard-cli/src/index.ts
@@ -1,4 +1,6 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+
+// Note: We might hit this bug - https://github.com/npm/cli/issues/4149
 
 /**
  * @license
@@ -13,38 +15,58 @@ import { run } from "./commands/run.js";
 
 import { program } from "commander";
 
-program
-  .version("0.0.1")
+program.version("0.0.1");
 
 program
   .command("debug [file]")
-  .description("Starts a simple HTTP server that serves the breadboard-web app, and outputs a URL that contains a link to a breadboard file that the user provided.")
-  .option("-o, --output <path>", "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph.")
+  .description(
+    "Starts a simple HTTP server that serves the breadboard-web app, and outputs a URL that contains a link to a breadboard file that the user provided."
+  )
+  .option(
+    "-o, --output <path>",
+    "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph."
+  )
   .option("-w, --watch", "Watch the file for changes.")
   .action(debug);
 
 program
   .command("mermaid [file]")
-  .description("Watch a breadboard file and output the mermaid diagram when it changes.")
-  .option("-o, --output <path>", "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph.")
+  .description(
+    "Watch a breadboard file and output the mermaid diagram when it changes."
+  )
+  .option(
+    "-o, --output <path>",
+    "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph."
+  )
   .option("-w, --watch", "Watch the file for changes.")
-  .action(mermaid)
+  .action(mermaid);
 
 program
   .command("make [file]")
-  .description("Make a graph from a javascript file. Note:all the imports have to be resolvable from the current directory.")
-  .option("-o, --output <path>", "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph.")
+  .description(
+    "Make a graph from a javascript file. Note:all the imports have to be resolvable from the current directory."
+  )
+  .option(
+    "-o, --output <path>",
+    "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph."
+  )
   .option("-w, --watch", "Watch the file for changes.")
-  .action(makeGraph)
+  .action(makeGraph);
 
 program
   .command("run [file]")
   .description("Run a graph.")
   .option("-w, --watch", "Watch the file for changes.")
-  .option("-o, --output <path>", "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph.")
+  .option(
+    "-o, --output <path>",
+    "If compiling a graph in Typescript (.ts), you MUST specific a location to output the compiled graph."
+  )
   .option("-k, --kit <kit...>", "The kit to use.")
-  .option("-i, --input <input>", "The JSON that represents the input to the graph.")
-  .action(run)
+  .option(
+    "-i, --input <input>",
+    "The JSON that represents the input to the graph."
+  )
+  .action(run);
 
 program.parse();
 
