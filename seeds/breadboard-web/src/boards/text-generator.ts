504e80c8bd17195267ae5cf0df3ecd7ac10777d9
Dimitri Glazkov <dglazkov@google.com>
2023-12-18 15:00:31 -0800

    [breadboard-web] Convert text-generator to new syntax. (#223)
    - Teach text-generator about palm-text-generator.
    - Convert text-generator to a recipe.
    - Remove now-unnecessary imports.
    - A much simpler text generator.
    
    Progress on #193.

--- seeds/breadboard-web/src/boards/text-generator.ts
+++ seeds/breadboard-web/src/boards/text-generator.ts
@@ -4,151 +4,96 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Board, Schema } from "@google-labs/breadboard";
-import { Core } from "@google-labs/core-kit";
-import { Starter } from "@google-labs/llm-starter";
-import { PaLMKit } from "@google-labs/palm-kit";
+import { Schema, base, recipe, recipeAsCode } from "@google-labs/breadboard";
+import { core } from "@google-labs/core-kit";
 
-const board = new Board({
+const metadata = {
   title: "Text Generator",
   description:
     "This is a text generator. It can generate text using various LLMs. Currently, it supports the follwogin models: Google Gemini Pro, Google PaLM text-bison-001, OpenAI GPT-3.5 Turbo, and a mock model.",
-  version: "0.0.1",
-});
-const palm = board.addKit(PaLMKit);
-const starter = board.addKit(Starter);
-const core = board.addKit(Core);
+  version: "0.0.2",
+};
 
-const input = board.input({
-  $id: "input",
-  schema: {
-    type: "object",
-    properties: {
-      text: {
-        type: "string",
-        title: "Text",
-        description: "The text to generate",
-      },
-      useStreaming: {
-        type: "boolean",
-        title: "Stream",
-        description: "Whether to stream the output",
-        default: "false",
-      },
-      MODEL: {
-        type: "string",
-        title: "Model",
-        description: "The model to use for generation",
-        enum: ["Gemini Pro", "GPT 3.5 Turbo", "PaLM", "mock"],
-        examples: ["Gemini Pro"],
-      },
+const inputSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      title: "Text",
+      description: "The text to generate",
     },
-    required: ["text"],
-  } satisfies Schema,
-});
-
-function switchModel({
-  MODEL,
-  useStreaming,
-}: {
-  MODEL: string;
-  useStreaming: boolean;
-}) {
-  switch (MODEL) {
-    case "Gemini Pro":
-      return { gemini: true };
-    case "PaLM":
-      if (useStreaming) {
-        return { other: `Streaming is not supported for ${MODEL}` };
-      }
-      return { palm: true };
-    case "mock":
-      return { mock: true };
-    case "GPT 3.5 Turbo":
-      return { gpt35: true };
-    default:
-      return { other: `Unsupported model: ${MODEL}` };
-  }
-}
-
-const textOutput = board.output({
-  $id: "textOutput",
-  schema: {
-    type: "object",
-    properties: {
-      text: {
-        type: "string",
-        title: "Text",
-        description: "The generated text",
-      },
+    useStreaming: {
+      type: "boolean",
+      title: "Stream",
+      description: "Whether to stream the output",
+      default: "false",
+    },
+    MODEL: {
+      type: "string",
+      title: "Model",
+      description: "The model to use for generation",
+      enum: ["Gemini Pro", "GPT 3.5 Turbo", "PaLM", "mock"],
+      examples: ["Gemini Pro"],
     },
   },
-});
+  required: ["text"],
+} satisfies Schema;
 
-const streamOutput = board.output({
-  $id: "streamOutput",
-  schema: {
-    type: "object",
-    properties: {
-      stream: {
-        type: "object",
-        title: "Stream",
-        description: "The generated text",
-        format: "stream",
-      },
+const textOutputSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      title: "Text",
+      description: "The generated text",
     },
   },
-});
+} satisfies Schema;
 
-const switcher = starter.runJavascript({
-  $id: "switchModel",
-  name: "switchModel",
-  code: switchModel.toString(),
-  raw: true,
-});
+const streamOutputSchema = {
+  type: "object",
+  properties: {
+    stream: {
+      type: "object",
+      title: "Stream",
+      description: "The generated text",
+      format: "stream",
+    },
+  },
+} satisfies Schema;
 
-const gemini = core
-  .invoke({
-    $id: "gemini",
-    path: "gemini-generator.json",
-  })
-  .wire("<-useStreaming", input)
-  .wire("<-text", input)
-  .wire("text->", textOutput)
-  .wire("stream->", streamOutput);
+export default await recipe(async () => {
+  const parameters = base.input({ $id: "input", schema: inputSchema });
 
-const generateText = palm
-  .generateText()
-  .wire("<-PALM_KEY", starter.secrets({ keys: ["PALM_KEY"] }));
+  const textOutput = base.output({
+    $id: "textOutput",
+    schema: textOutputSchema,
+  });
 
-const gpt35 = core.invoke({
-  $id: "gpt35",
-  path: "openai-gpt-35-turbo.json",
-});
+  const streamOutput = base.output({
+    $id: "streamOutput",
+    schema: streamOutputSchema,
+  });
 
-input.wire("MODEL->", switcher);
-input.wire("useStreaming->", switcher);
+  const switchModel = recipeAsCode(({ MODEL }: { MODEL: string }) => {
+    const models: Record<string, string> = {
+      "Gemini Pro": "gemini-generator.json",
+      PaLM: "palm-text-generator.json",
+      mock: "mock-text-generator.json",
+      "GPT 3.5 Turbo": "openai-gpt-35-turbo.json",
+    };
+    const path = models[MODEL];
+    if (!path) throw new Error(`Unsupported model: ${MODEL}`);
+    return { path };
+  })(parameters.MODEL);
 
-input.wire("useStreaming->", gpt35);
-input.wire("text->", gpt35.wire("text->", textOutput));
-gpt35.wire("stream->", streamOutput);
+  const invoke = core.invoke({
+    $id: "invoke",
+    path: switchModel.path,
+  });
+  parameters.to(invoke);
+  invoke.text.to(textOutput);
+  invoke.stream.to(streamOutput);
 
-input.wire("text->", generateText.wire("completion->text", textOutput));
-
-const mockModel = core.invoke({
-  $id: "mockModel",
-  path: "mock-text-generator.json",
-});
-
-input.wire("useStreaming->", mockModel);
-input.wire("text->", mockModel.wire("text->", textOutput));
-switcher
-  .wire("other->text", textOutput)
-  .wire("gemini->", gemini)
-  .wire("palm->", generateText)
-  .wire("gpt35->", gpt35)
-  .wire("mock->", mockModel);
-
-mockModel.wire("stream->", streamOutput);
-
-export default board;
+  return textOutput;
+}).serialize(metadata);
