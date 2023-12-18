a54ba35cc5087da8f530da49437524a6b7668491
Dimitri Glazkov <dimitri@glazkov.com>
2023-12-18 15:44:43 -0800

    convert search-summarize.

--- seeds/breadboard-web/docs/graphs/search-summarize.md
+++ seeds/breadboard-web/docs/graphs/search-summarize.md
@@ -3,16 +3,15 @@
 ```mermaid
 %%{init: 'themeVariables': { 'fontFamily': 'Fira Code, monospace' }}%%
 graph TD;
-generator["invoke <br> id='generator'"] -- "text->text" --> output1{{"output <br> id='output-1'"}}:::output
+generator["invoke <br> id='generator'"] -- "text->text" --> output4{{"output <br> id='output-4'"}}:::output
+parameters[/"input <br> id='parameters'"/]:::input -- "text->query" --> customSearchURL["urlTemplate <br> id='customSearchURL'"]
+parameters[/"input <br> id='parameters'"/]:::input -- "text->question" --> summarizingtemplate["promptTemplate <br> id='summarizing-template'"]
+parameters[/"input <br> id='parameters'"/]:::input -- "generator->path" --> generator["invoke <br> id='generator'"]
 summarizingtemplate["promptTemplate <br> id='summarizing-template'"] -- "prompt->text" --> generator["invoke <br> id='generator'"]
-jsonata4["jsonata <br> id='jsonata-4'"] -- "result->context" --> summarizingtemplate["promptTemplate <br> id='summarizing-template'"]
-fetch3["fetch <br> id='fetch-3'"] -- "response->json" --> jsonata4["jsonata <br> id='jsonata-4'"]
-urlTemplate2["urlTemplate <br> id='urlTemplate-2'"] -- "url->url" --> fetch3["fetch <br> id='fetch-3'"]
-secrets5("secrets <br> id='secrets-5'"):::secrets -- "API_KEY->API_KEY" --> urlTemplate2["urlTemplate <br> id='urlTemplate-2'"]
-secrets5("secrets <br> id='secrets-5'"):::secrets -- "GOOGLE_CSE_ID->GOOGLE_CSE_ID" --> urlTemplate2["urlTemplate <br> id='urlTemplate-2'"]
-input[/"input <br> id='input'"/]:::input -- "generator->path" --> generator["invoke <br> id='generator'"]
-input[/"input <br> id='input'"/]:::input -- "text->question" --> summarizingtemplate["promptTemplate <br> id='summarizing-template'"]
-input[/"input <br> id='input'"/]:::input -- "text->query" --> urlTemplate2["urlTemplate <br> id='urlTemplate-2'"]
+customSearchURL["urlTemplate <br> id='customSearchURL'"] -- "url->url" --> search["fetch <br> id='search'"]
+getSnippets["jsonata <br> id='getSnippets'"] -- "result->context" --> summarizingtemplate["promptTemplate <br> id='summarizing-template'"]
+secrets3("secrets <br> id='secrets-3'"):::secrets -- all --> customSearchURL["urlTemplate <br> id='customSearchURL'"]
+search["fetch <br> id='search'"] -- "response->json" --> getSnippets["jsonata <br> id='getSnippets'"]
 classDef default stroke:#ffab40,fill:#fff2ccff,color:#000
 classDef input stroke:#3c78d8,fill:#c9daf8ff,color:#000
 classDef output stroke:#38761d,fill:#b6d7a8ff,color:#000
--- seeds/breadboard-web/src/boards/search-summarize.ts
+++ seeds/breadboard-web/src/boards/search-summarize.ts
@@ -4,89 +4,89 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Board } from "@google-labs/breadboard";
-import Core from "@google-labs/core-kit";
-import { Starter } from "@google-labs/llm-starter";
+import {
+  GraphMetadata,
+  Schema,
+  V,
+  base,
+  recipe,
+} from "@google-labs/breadboard";
+import { core } from "@google-labs/core-kit";
+import { starter } from "@google-labs/llm-starter";
 
-const searchSummarize = new Board({
+const metadata = {
   title: "The Search Summarizer Recipe",
   description:
     "A simple AI pattern that first uses Google Search to find relevant bits of information and then summarizes them using LLM.",
-  version: "0.1.0",
-});
-const kit = searchSummarize.addKit(Starter);
-const core = searchSummarize.addKit(Core);
+  version: "0.1.1",
+} satisfies GraphMetadata;
 
-const input = searchSummarize.input({
-  $id: "input",
-  schema: {
-    type: "object",
-    properties: {
-      text: {
-        type: "string",
-        title: "Query",
-        description: "What would you like to search for?",
-      },
-      generator: {
-        type: "string",
-        title: "Generator",
-        description: "The URL of the generator to call",
-        default: "/graphs/text-generator.json",
-      },
+const inputSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      title: "Query",
+      description: "What would you like to search for?",
     },
-    required: ["text"],
-  },
-});
-
-const output = searchSummarize.output({
-  schema: {
-    type: "object",
-    properties: {
-      text: {
-        type: "string",
-        title: "Answer",
-        description: "The answer to the query",
-      },
+    generator: {
+      type: "string",
+      title: "Generator",
+      description: "The URL of the generator to call",
+      default: "/graphs/text-generator.json",
     },
-    required: ["text"],
   },
-});
+  required: ["text"],
+} satisfies Schema;
 
-const generator = core.invoke({ $id: "generator" }).wire("text->", output);
+const outputSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      title: "Answer",
+      description: "The answer to the query",
+    },
+  },
+  required: ["text"],
+} satisfies Schema;
 
-const summarizingTemplate = kit
-  .promptTemplate({
-    template:
-      "Use context below to answer this question:\n\n##Question:\n{{question}}\n\n## Context {{context}}\n\\n## Answer:\n",
-    $id: "summarizing-template",
-  })
-  .wire("prompt->text", generator);
+export default await recipe(async () => {
+  const parameters = base.input({ $id: "parameters", schema: inputSchema });
 
-const searchURLTemplate = kit
-  .urlTemplate({
-    template:
-      "https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={GOOGLE_CSE_ID}&q={query}",
-  })
-  .wire(
-    "url",
-    kit
-      .fetch()
-      .wire(
-        "response->json",
-        kit
-          .jsonata({ expression: "$join(items.snippet, '\n')" })
-          .wire("result->context", summarizingTemplate)
-      )
-  );
-
-kit
-  .secrets({ keys: ["API_KEY", "GOOGLE_CSE_ID"] })
-  .wire("API_KEY", searchURLTemplate)
-  .wire("GOOGLE_CSE_ID", searchURLTemplate);
-
-input
-  .wire("generator->path", generator)
-  .wire("text->question", summarizingTemplate)
-  .wire("text->query", searchURLTemplate);
-
-export default searchSummarize;
+  return starter
+    .secrets({ keys: ["API_KEY", "GOOGLE_CSE_ID"] })
+    .to(
+      starter.urlTemplate({
+        $id: "customSearchURL",
+        template:
+          "https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={GOOGLE_CSE_ID}&q={query}",
+        query: parameters.text,
+      })
+    )
+    .url.to(starter.fetch({ $id: "search" }))
+    .response.as("json")
+    .to(
+      starter.jsonata({
+        $id: "getSnippets",
+        expression: "$join(items.snippet, '\n')",
+      })
+    )
+    .result.as("context")
+    .to(
+      starter.promptTemplate({
+        template:
+          "Use context below to answer this question:\n\n##Question:\n{{question}}\n\n## Context {{context}}\n\\n## Answer:\n",
+        $id: "summarizing-template",
+        question: parameters.text,
+      })
+    )
+    .prompt.as("text")
+    .to(
+      core.invoke({
+        $id: "generator",
+        path: parameters.generator as V<string>,
+      })
+    )
+    .text.to(base.output({ schema: outputSchema }));
+}).serialize(metadata);
