617d3bdab82d7e537f730b9eb82e79c402315360
Paul Kinlan <paul.kinlan@gmail.com>
2023-12-18 14:20:21 +0000

    [breadboard-cli] Debug can now hot-reload

--- seeds/breadboard-cli/src/commands/debug.ts
+++ seeds/breadboard-cli/src/commands/debug.ts
@@ -10,8 +10,11 @@ import handler from "serve-handler";
 import { fileURLToPath, pathToFileURL } from "url";
 import { BoardMetaData, loadBoards, watch } from "./lib/utils.js";
 import { stat } from "fs/promises";
+import { createReadStream } from "fs";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const clients: Record<string, http.ServerResponse> = {};
 
 export const debug = async (file: string, options: Record<string, any>) => {
   const distDir = join(__dirname, "..", "..", "ui");
@@ -31,6 +34,11 @@ export const debug = async (file: string, options: Record<string, any>) => {
         // Refresh the list of boards that are passed in at the start of the server.
         console.log(`${filename} changed. Refreshing boards...`);
         boards = await loadBoards(file);
+
+        // Notify all the clients that the board has changed.
+        Object.values(clients).forEach((clientResponse) => {
+          clientResponse.write(`event: update\ndata:na\nid:${Date.now()}\n\n`);
+        });
       },
       onRename: async () => {
         // Refresh the list of boards that are passed in at the start of the server.
@@ -41,10 +49,9 @@ export const debug = async (file: string, options: Record<string, any>) => {
   }
 
   const server = http.createServer(async (request, response) => {
-    // You pass two more arguments for config and middleware
-    // More details here: https://github.com/vercel/serve-handler#options
     const requestURL = new URL(request.url ?? "", "http://localhost:3000");
 
+    // We should think about a simple router here.. Right now we share a lot of state.
     if (requestURL.pathname === "/local-boards.json") {
       // Generate a list of boards that are valid at runtime.
       // Cache until things change.
@@ -64,6 +71,46 @@ export const debug = async (file: string, options: Record<string, any>) => {
       });
 
       return response.end(boardsData);
+    }
+
+    if ("watch" in options) {
+      if (
+        requestURL.pathname === "/" ||
+        requestURL.pathname === "/index.html"
+      ) {
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+
+        // Need to check this doesn't include "../" and other escape characters.
+        const fileStream = createReadStream(`${distDir}/index.html`);
+        fileStream.pipe(response, { end: false });
+        fileStream.on("end", () => {
+          response.write(`<!-- Added by Debug command --><script>
+const evtSource = new EventSource("/~~debug");
+evtSource.addEventListener("update", () => { window.location.reload(); });</script>`);
+          response.end();
+        });
+        return;
+      }
+
+      if (requestURL.pathname === "/~~debug") {
+        response.writeHead(200, {
+          "Content-Type": "text/event-stream",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+        });
+        const clientId =
+          Date.now() +
+          Buffer.from(crypto.getRandomValues(new Uint32Array(10))).toString(
+            "hex"
+          );
+        response.on("close", () => {
+          delete clients[clientId];
+        });
+        clients[clientId] = response;
+        return;
+      }
     }
 
     const board = boards.find((board) => board.url == requestURL.pathname);
