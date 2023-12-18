7989d6e395ed28e70d16df8a8474449fb437b142
Paul Kinlan <paul.kinlan@gmail.com>
2023-12-18 22:35:19 +0000

    [breadboard-cli] make now knows about yaml.
    + Added randomness into the typescript process
    + Removed the requirement for `-o` - it will still be used, but will default to `cwd`

--- seeds/breadboard-cli/src/commands/make-graph.ts
+++ seeds/breadboard-cli/src/commands/make-graph.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { stat } from "fs/promises";
 import path, { extname } from "path";
 import { Loaders } from "./lib/loaders/index.js";
 import { resolveFilePath, watch } from "./lib/utils.js";
@@ -17,30 +16,13 @@ export const makeGraph = async (
 
   if (
     file != undefined &&
-    path.extname(file) == ".ts" &&
-    "output" in options == false
+    path.extname(file) != ".js" &&
+    path.extname(file) != ".ts" &&
+    path.extname(file) != ".yaml"
   ) {
     throw new Error(
-      `File ${file} is not a TypeScript file. You must specify the output directory with --output.`
+      `File ${file} must be JavaScript, TypeScript or YAML file.`
     );
-  }
-
-  if (file != undefined && path.extname(file) == ".ts" && options["output"]) {
-    const fullDirPath = path.resolve(process.cwd(), options["output"]);
-    const dirStat = await stat(fullDirPath);
-    if (dirStat.isDirectory() == false) {
-      throw new Error(
-        `The path specified by ${options["output"]} is not a directory.`
-      );
-    }
-  }
-
-  if (
-    file != undefined &&
-    path.extname(file) != ".js" &&
-    path.extname(file) != ".ts"
-  ) {
-    throw new Error(`File ${file} must be JavaScript or a TypeScript file.`);
   }
 
   if (file != undefined) {
