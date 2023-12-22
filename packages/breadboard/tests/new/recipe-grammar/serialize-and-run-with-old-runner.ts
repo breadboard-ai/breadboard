8022810282b263aefd9728c2da2812c98a5de862
Bernhard Seefeld <seefeld@gmail.com>
2023-12-22 18:05:42 -0500

    [breadboard/new] More tests for wiring

--- packages/breadboard/tests/new/recipe-grammar/serialize-and-run-with-old-runner.ts
+++ packages/breadboard/tests/new/recipe-grammar/serialize-and-run-with-old-runner.ts
@@ -68,7 +68,7 @@ test("two nodes, spread", async (t) => {
   t.deepEqual(result, { foo: "rab" });
 });
 
-test("simple inline action", async (t) => {
+test("simple inline code", async (t) => {
   const graph = recipe<{ a: number; b: number }, { result: number }>(
     (inputs) => {
       return code<{ a: number; b: number }, { result: number }>(
@@ -82,6 +82,66 @@ test("simple inline action", async (t) => {
 
   const result = await serializeAndRunGraph(graph, { a: 1, b: 2 });
   t.deepEqual(result, { result: 3 });
+});
+
+test("simple inline code, single parameter", async (t) => {
+  const graph = recipe<{ number: number }, { result: number }>((inputs) => {
+    return code<{ number: number }>((inputs) => {
+      return { result: -inputs.number };
+    })(inputs);
+  });
+
+  const result = await serializeAndRunGraph(graph, { number: 3 });
+  t.deepEqual(result, { result: -3 });
+});
+
+test("simple inline code, single parameter, pick", async (t) => {
+  const graph = recipe<{ number: number }, { result: number }>((inputs) => {
+    return code<{ number: number }>((inputs) => {
+      return { result: -inputs.number };
+    })(inputs.number);
+  });
+
+  const result = await serializeAndRunGraph(graph, { number: 3 });
+  t.deepEqual(result, { result: -3 });
+});
+
+test("simple inline code, explicit input and output, single parameter", async (t) => {
+  const graph = recipe((_, base) => {
+    const inputs = base.input({
+      schema: {
+        properties: { foo: { type: "string" } },
+        required: ["foo"],
+      },
+    });
+    const neg = code(({ foo }) => {
+      return { result: `${foo}!!` };
+    })(inputs);
+    const outputs = base.output({});
+    neg.result.to(outputs);
+  });
+
+  const result = await serializeAndRunGraph(graph, { foo: "bar" });
+  t.like(result, { result: "bar!!" });
+});
+
+test("simple inline code, explicit input and output, single parameter, pick", async (t) => {
+  const graph = recipe((_, base) => {
+    const inputs = base.input({
+      schema: {
+        properties: { foo: { type: "string" } },
+        required: ["foo"],
+      },
+    });
+    const neg = code(({ foo }) => {
+      return { result: `${foo}!!` };
+    })(inputs.foo);
+    const outputs = base.output({});
+    neg.result.to(outputs);
+  });
+
+  const result = await serializeAndRunGraph(graph, { foo: "bar" });
+  t.like(result, { result: "bar!!" });
 });
 
 test("code recipe called from another recipe", async (t) => {
