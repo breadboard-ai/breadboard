413bf9499c735ab8b4dc8598e30eff9b2edf0fb4
Paul Lewis <617438+paullewis@users.noreply.github.com>
2024-01-03 12:27:01 +0000

    [breadboard-ui] Add nicer JSON representation

--- packages/breadboard-ui/src/index.ts
+++ packages/breadboard-ui/src/index.ts
@@ -17,6 +17,7 @@ export { Start } from "./start.js";
 export { Progress } from "./progress.js";
 export { UI } from "./ui.js";
 export { BoardList } from "./board-list.js";
+export { JSONTree } from "./json-tree.js";
 
 import { Output } from "./output.js";
 import { UIController as DeprecatedUIController } from "./deprecated-ui-controller.js";
