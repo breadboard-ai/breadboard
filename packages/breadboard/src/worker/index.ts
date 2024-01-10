353acafd81039d46b417d620d18215a027ab63d3
Dimitri Glazkov <dimitri@glazkov.com>
2024-01-09 21:17:35 -0800

    Trim `MessageController` to size.

--- packages/breadboard/src/worker/index.ts
+++ packages/breadboard/src/worker/index.ts
@@ -8,15 +8,6 @@ export { MessageController, WorkerTransport } from "./controller.js";
 export { WorkerRuntime } from "./worker-runtime.js";
 export type {
   ControllerMessage,
-  NodeStartMessage,
-  EndMessage,
   ErrorMessage,
-  InputRequestMessage,
-  InputResponseMessage,
   LoadRequestMessage,
-  LoadResponseMessage,
-  ProxyRequestMessage,
-  ProxyResponseMessage,
-  OutputMessage,
-  StartMesssage,
 } from "./protocol.js";
