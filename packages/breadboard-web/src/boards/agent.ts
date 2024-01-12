f98c712ba2ce3062130c4b2ba1672073b6f4dfa8
Dimitri Glazkov <dimitri@glazkov.com>
2024-01-12 15:36:46 -0800

    Teach agent to assemble full context.

--- packages/breadboard-web/src/boards/agent.ts
+++ packages/breadboard-web/src/boards/agent.ts
@@ -8,20 +8,16 @@ import { recipe } from "@google-labs/breadboard";
 import { core } from "@google-labs/core-kit";
 import { starter } from "@google-labs/llm-starter";
 
-export default await recipe(({ topic, template, generator }) => {
+export default await recipe(({ topic, template, generator, context }) => {
   const prompt = starter.promptTemplate({
     $id: "prompt",
-    template: template
-      .title("Template")
-      .isString()
-      .examples(
-        `You are the most amazing poet who specializes in two-line rhyming poems.
-  Given any topic, you can quickly whip up a two-line rhyming poem about it.
-  Ready?
+    template: template.title("Template").isString().examples(`
+You are a brilliant poet who specializes in two-line rhyming poems.
+Given any topic, you can quickly whip up a two-line rhyming poem about it.
+Ready?
 
-  The topic is: {{topic}}`
-      ),
-    topic: topic.title("Instruction").examples("The universe within us"),
+The topic is: {{topic}}`),
+    topic: topic.title("Topic").examples("The universe within us"),
   });
   const generate = core.invoke({
     $id: "generate",
@@ -32,7 +28,24 @@ export default await recipe(({ topic, template, generator }) => {
       .examples("gemini-generator.json"),
   });
 
-  return { result: generate.text, context: generate.context };
+  const assemble = starter.jsonata({
+    $id: "assemble",
+    expression: `$append(context ? context, $append([
+      {
+          "role": "user",
+          "parts": [
+              {
+                  "text": text
+              }
+          ]
+      }
+  ], [generated]))`,
+    generated: generate.context,
+    text: prompt.prompt,
+    context: context.title("Context").isArray().examples("[]"),
+  });
+
+  return { context: assemble.result };
 }).serialize({
   title: "Agent",
   description: "A prototype of an agent-like board",
