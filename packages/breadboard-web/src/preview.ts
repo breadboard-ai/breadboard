8ab13eccdae65d9bf9dc9bc909ffa8b7e96d84bf
Paul Lewis <617438+paullewis@users.noreply.github.com>
2024-01-19 16:07:53 +0000

    [breadboard-web] Add a little history read-out to the Preview (#435)
    Fixes #431

--- packages/breadboard-web/src/preview.ts
+++ packages/breadboard-web/src/preview.ts
@@ -16,8 +16,10 @@ import {
   clone,
   StreamCapabilityType,
   OutputValues,
+  NodeStartResponse,
 } from "@google-labs/breadboard";
 import { until } from "lit/directives/until.js";
+import { classMap } from "lit/directives/class-map.js";
 
 type ChunkOutputs = OutputValues & { chunk: string };
 
@@ -45,6 +47,7 @@ export class Preview extends LitElement {
   #url: string | null = null;
   #hasOutputs = false;
   #outputs: HTMLTemplateResult[] = [];
+  #nodesVisited: Array<{ data: NodeStartResponse }> = [];
 
   static styles = css`
     :host {
@@ -67,18 +70,17 @@ export class Preview extends LitElement {
     }
 
     main {
-      padding: 32px;
       flex: 1 0 auto;
-      max-width: 960px;
-      margin: 0 auto;
-      width: 100%;
+      display: flex;
+      flex-direction: column;
     }
 
     h1 {
       font-size: 24px;
       margin: 0;
-      padding: 24px calc(var(--bb-grid-size) * 2);
+      padding: 42px calc(var(--bb-grid-size) * 2) 24px;
       font-weight: normal;
+      text-align: center;
       text-transform: uppercase;
       color: var(--bb-accent-color);
     }
@@ -123,57 +125,201 @@ export class Preview extends LitElement {
       color: #cc0011;
     }
 
+    .ui {
+      padding: 0 32px;
+      max-width: 960px;
+      margin: 0 auto;
+      width: 100%;
+    }
+
+    .outputs {
+      padding: 32px;
+      flex: 1 0 auto;
+      padding-bottom: 100px;
+      overflow-y: scroll;
+      mask-image: linear-gradient(
+        rgba(0, 0, 0, 0),
+        rgba(0, 0, 0, 1) 32px,
+        rgba(0, 0, 0, 1) calc(100% - 32px),
+        rgba(0, 0, 0, 0)
+      );
+      /* Set so we get the overflow scroll behavior */
+      height: 0px;
+    }
+
     .output-item {
-      margin-bottom: 32px;
+      line-height: 1.5;
       font-size: var(--bb-text-medium);
+      max-width: 960px;
+      margin: 0 auto;
+      margin-bottom: 32px;
+      width: 100%;
     }
 
     .output-item h1 {
       padding: 0;
       margin: 0 0 8px;
       font-size: var(--bb-text-large);
+      text-align: left;
     }
 
     .working {
       display: flex;
       flex-direction: column;
       align-items: center;
-      padding-bottom: 48px;
+      margin: 0 auto;
+      margin-bottom: 60px;
       position: relative;
+      width: 60%;
+      box-sizing: border-box;
+      padding: 0 32px;
+
+      --size: 16px;
     }
 
-    .working::after {
-      content: "";
-      display: block;
-      width: 12px;
-      border-radius: 50%;
-      background: var(--bb-accent-color);
-      height: 12px;
-      position: absolute;
-      bottom: 23px;
-      animation: sideToSide 0.6s cubic-bezier(0.75, 0, 0.25, 1) infinite
-        alternate;
+    .working h1 {
+      font-size: var(--bb-text-default);
+      margin: 0;
+      padding: 0 0 16px 0;
     }
 
-    .working::before {
-      content: "";
-      display: block;
-      width: 100px;
-      border-radius: 32px;
+    .working .container {
+      height: 80px;
+      width: 100%;
       background: #fff;
-      border: 1px solid rgb(209, 209, 209);
-      height: 16px;
-      position: absolute;
-      bottom: 20px;
+      border-radius: 50px;
+      box-shadow: 0 30px 32px rgba(0, 0, 0, 0.1), 0 10px 12px rgba(0, 0, 0, 0.1),
+        0 2px 2px rgba(0, 0, 0, 0.1);
     }
 
-    @keyframes sideToSide {
-      0% {
-        translate: calc(-50px + 6px + 2px) 0;
+    .working ol {
+      list-style: none;
+      padding: 0;
+      margin: 20px 40px;
+      overflow: hidden;
+      font-size: var(--bb-text-pico);
+      white-space: nowrap;
+      display: flex;
+      justify-content: flex-end;
+      mask-image: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0),
+        rgb(0, 0, 0) 40px
+      );
+    }
+
+    .working ol li {
+      display: inline-block;
+      flex: 0 0 auto;
+      position: relative;
+      width: 20%;
+      height: 42px;
+      color: rgb(119, 119, 119);
+      overflow-x: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      padding-top: 25px;
+    }
+
+    .working ol li:before {
+      content: "";
+      position: absolute;
+      top: calc((var(--size) - 4px) * 0.5);
+      left: 0;
+      height: 4px;
+      background: rgb(218, 218, 218);
+      width: 100%;
+    }
+
+    .working ol li:last-of-type::before {
+      width: 50%;
+    }
+
+    .working ol li:first-child:before {
+      width: 50%;
+      left: 50%;
+    }
+
+    .working ol li:only-child:before {
+      display: none;
+    }
+
+    .working ol li:after {
+      box-sizing: border-box;
+      font-size: var(--bb-text-small);
+      font-weight: bold;
+      content: "";
+      width: var(--size);
+      height: var(--size);
+      display: block;
+      border-radius: 50%;
+      position: absolute;
+      left: 50%;
+      top: 0;
+      translate: -50% 0;
+      border: 1px solid hsl(33.6, 100%, 52.5%);
+      background: hsl(44.7, 100%, 80%);
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
+    }
+
+    .working ol li.error::after {
+      background: #cc0000;
+      border: 1px solid #cc0000;
+    }
+
+    .working ol li.result::after {
+      background: #ffa500;
+      border: 1px solid #ffa500;
+    }
+
+    .working ol li.input::after {
+      background: #c9daf8ff;
+      border: 1px solid #3c78d8;
+    }
+
+    .working ol li.secrets::after {
+      background: #f4cccc;
+      border: 1px solid #db4437;
+    }
+
+    .working ol li.output::after {
+      background: #b6d7a8ff;
+      border: 1px solid #38761d;
+    }
+
+    .working ol li.load::after,
+    .working ol li.end::after {
+      background: var(--bb-done-color);
+      border: 1px solid var(--bb-done-color);
+    }
+
+    .working ol li:last-of-type::after {
+      background: radial-gradient(
+          var(--bb-progress-color) 0%,
+          var(--bb-progress-color) 30%,
+          var(--bb-progress-color-faded) 30%,
+          var(--bb-progress-color-faded) 50%,
+          transparent 50%,
+          transparent 100%
+        ),
+        conic-gradient(transparent 0deg, var(--bb-progress-color) 360deg),
+        linear-gradient(
+          var(--bb-progress-color-faded),
+          var(--bb-progress-color-faded)
+        );
+
+      box-shadow: none;
+      border: none;
+      animation: rotate 0.5s linear infinite;
+    }
+
+    @keyframes rotate {
+      from {
+        transform: rotate(0);
       }
 
-      100% {
-        translate: calc(50px - 6px - 2px) 0;
+      to {
+        transform: rotate(360deg);
       }
     }
   `;
@@ -196,6 +342,7 @@ export class Preview extends LitElement {
     // For non-debug cases, like the preview, ensure that the diagnostics are
     // switched off.
     this.#config.diagnostics = false;
+    this.#nodesVisited.length = 0;
 
     this.#runBoard();
   }
@@ -334,11 +481,27 @@ export class Preview extends LitElement {
       case "end":
         if (!this.#hasOutputs) {
           this.uiElement = html`<div class="output">All done!</div>`;
+        } else {
+          this.uiElement = nothing;
         }
         return Promise.resolve(void 0);
 
       case "nodestart":
-        this.uiElement = html`<div class="working">Working...</div>`;
+        this.#nodesVisited.push(result);
+        this.uiElement = html`<div class="working">
+          <h1>Working...</h1>
+          <div class="container">
+            <ol>
+              ${this.#nodesVisited.map((node) => {
+                return html`<li
+                  class="${classMap({ [node.data.node.type]: true })}"
+                >
+                  ${node.data.node.id}
+                </li>`;
+              })}
+            </ol>
+          </div>
+        </div> `;
         return Promise.resolve(void 0);
     }
   }
@@ -366,7 +529,8 @@ export class Preview extends LitElement {
 
     return html`<main>
         <h1>${this.boardInfo?.title}</h1>
-        ${this.uiElement} ${this.#outputs}
+        <section class="ui">${this.uiElement}</section>
+        <section class="outputs">${this.#outputs}</section>
       </main>
       <footer>Made with Breadboard</footer>`;
   }
