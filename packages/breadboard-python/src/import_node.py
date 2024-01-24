cdf61b3e1cb1eaf0c8f493938e43963bc9e43bf2
Kevin Xiao <xiaok10003@gmail.com>
2024-01-24 10:53:30 -0800

    Add breadboard-python package (#419)
    Adds the breadboard-python package, which allows for breadboards to be
    defined in Python.
    
    > It's a good idea to open an issue first for discussion.
    
    - [ ] Tests pass
    - [ ] Appropriate changes to documentation are included in the PR
    
    ---------
    
    Co-authored-by: Kevin Xiao <kevxiao@google.com>

---
+++ packages/breadboard-python/src/import_node.py
@@ -0,0 +1,55 @@
+from main import AttrDict, Board, convert_from_json_to_pydantic
+from javascript import require
+import javascript
+import json
+
+
+
+class ImportedBoard(Board):
+  def __init__(self, name, schema):
+    self.type = name
+    self.input_schema = schema["inputSchema"]
+    self.output_schema = schema["outputSchema"]
+
+def import_breadboard_js(package_name):
+  
+  if package_name == "@google-labs/llm-starter":
+    kit_package = require("../../llm-starter/dist/src/index.js")
+  elif package_name == "@google-labs/node-nursery-web":
+    kit_package = require("../../node-nursery-web/dist/src/index.js")
+  else:
+    kit_package = require(package_name)
+  a = kit_package()
+  handlers = a.handlers
+
+  output = AttrDict()
+  for handler_name in handlers:
+    if handlers[handler_name].describe is None:
+      continue
+    b = handlers[handler_name].describe()
+    res = javascript.eval_js('''JSON.stringify(await b)''')
+    schema = json.loads(res)
+    input_schema = schema["inputSchema"]
+    output_schema = schema["outputSchema"]
+    print(schema)
+    print(f"KEX: received a handler: {handler_name}")
+    input_schema, input_field = convert_from_json_to_pydantic("Input" + handler_name, input_schema)
+    output_schema, output_field = convert_from_json_to_pydantic("Output" + handler_name, output_schema)
+
+    # TODO: Convert input_schema and output_schema from dicts into actual schemas.
+    class ImportedClass(Board[input_schema, output_schema]):
+      type = handler_name
+      title = f"Auto-imported {handler_name}"
+      description = f"This board is auto-imported from {package_name}"
+      version = "0.?"
+      def describe(self, input):
+        self.output = AttrDict(output_schema)
+        return self.output
+      def get_configuration(self):
+        config = super().get_configuration()
+        config.pop("schema")
+        return config
+
+    output[handler_name] = ImportedClass
+  print(output)
+  return output
