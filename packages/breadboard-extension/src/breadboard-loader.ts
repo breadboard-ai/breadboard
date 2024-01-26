1bcc516da38eac551781444a99097848cdf2f60e
Paul Lewis <617438+paullewis@users.noreply.github.com>
2024-01-26 14:04:52 +0000

    [breadboard-extension] Support JS boards (#491)
    cc @PaulKinlan

--- packages/breadboard-extension/src/breadboard-loader.ts
+++ packages/breadboard-extension/src/breadboard-loader.ts
@@ -69,6 +69,7 @@ export class BreadboardLoader {
       compilerOptions: {
         module: ts.ModuleKind.CommonJS,
         target: ts.ScriptTarget.ESNext,
+        allowJs: true,
       },
     });
 
