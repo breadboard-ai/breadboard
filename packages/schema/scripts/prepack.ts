6321f5fad56933eace06e7c9770e111d0182a5b2
Joseph Mearman <joseph@mearman.co.uk>
2024-01-30 20:41:02 +0000

    Refactor schema URL generation in prepack.ts

--- packages/schema/scripts/prepack.ts
+++ packages/schema/scripts/prepack.ts
@@ -6,17 +6,23 @@
 
 import fs from "fs";
 import path from "path";
-import schema from "../breadboard.schema.json";
 import packageJson from "../package.json";
+
+const PACKGE_ROOT = process.cwd();
+const SCHEMA_PATH = path.relative(PACKGE_ROOT, "breadboard.schema.json");
+
+const schema = JSON.parse(fs.readFileSync(SCHEMA_PATH, "utf8"))
 
 console.log(`current schema id: ${schema.$id}`);
 console.log(`package.json version: ${packageJson.version}`);
-schema.$id = `https://raw.githubusercontent.com/breadboard-ai/breadboard/@google-labs/breadboard-schema/${packageJson.version}/breadboard.schema.json`;
+
+const GITHUB_OWNER = "breadboard-ai";
+const GITHUB_REPO = "breadboard";
+const GITHUB_REF = `@google-labs/breadboard-schema/${packageJson.version}`;
+
+schema.$id = `https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/${GITHUB_REF}/${SCHEMA_PATH}`;
 console.log(`updating schema id to: ${schema.$id}`);
 
-fs.writeFileSync(
-  path.join(process.cwd(), "breadboard.schema.json"),
-  JSON.stringify(schema, null, 2)
-);
+fs.writeFileSync(SCHEMA_PATH, JSON.stringify(schema, null, 2));
 
 console.log("done");
