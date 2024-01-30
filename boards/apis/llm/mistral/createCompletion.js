1c5ea682dcab2954cfb21ca6475de8b4c999ecbe
Paul Kinlan <paul.kinlan@gmail.com>
2024-01-30 22:10:15 +0000

    [recipe] updating mistral
    createCompletion - for simple prompt.
    openapi.yaml - added security information so the library creates the correct code.
    moves to ./api

--- boards/apis/llm/mistral/createCompletion.js
+++ boards/apis/llm/mistral/createCompletion.js
@@ -1,28 +1,14 @@
 /*
 To run this:
 
-1. Create an environment variable called MISTRAL_API_KEY with your API key
-2. npx breadboard run createChatCompletion.js --kit @google-labs/core-kit --kit @google-labs/llm-starter -i "{ \"api_inputs\": {
-      \"authentication\": {
-        \"bearer\": \"MISTRAL_API_KEY\"
-      },
-      \"application/json\": {
-        \"messages\": [
-          {
-            \"role\": \"user\",
-            \"content\": \"What is the best French cheese?\"
-          }
-        ],
-        \"model\": \"mistral-small\"
-      }
-    }
-}"
+1. Create an environment variable called MISTRAL_AI_API_KEY with your API key
+2. npx breadboard run createChatCompletion.js --input-file=./tests/createChatCompletionRequest.json --kit=@google-labs/core-kit
 */
 
 /*
  Note currently the tool to generate the OpenAPI spec does not support the security Scheme, therefore secrets will not work, you have to use an input called key.
 */
-import { board, code, base } from "@google-labs/breadboard";
+import { board, code } from "@google-labs/breadboard";
 import { core } from "@google-labs/core-kit";
 
 const metaData = {
@@ -41,7 +27,6 @@ export default await board(
     model
       .title("model")
       .description("The model to interact with")
-      .optional()
       .default("mistral-small");
     temperature
       .title("temperature")
@@ -56,7 +41,7 @@ export default await board(
       .description("The model topP")
       .isNumber()
       .optional()
-      .default(1); // https://docs.mistral.ai/api/#operation/createChatCompletion
+      .default(1); //https://docs.mistral.ai/api/#operation/createChatCompletion
 
     const convertRequest = code(
       ({ model, prompt, temperature, topP, authentication }) => {
