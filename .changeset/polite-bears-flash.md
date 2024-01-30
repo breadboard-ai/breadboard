c71339fe23dd04766439b646ef919c02b9bd9347
Dimitri Glazkov <dglazkov@google.com>
2024-01-30 15:18:26 -0600

    [breadboard-web] Stop generating mermaid files automatically.
    - Stop generating mermaid files.
    - docs(changeset): Stop generating mermaid files automatically.

---
+++ .changeset/polite-bears-flash.md
@@ -0,0 +1,5 @@
+---
+"@google-labs/breadboard-web": patch
+---
+
+Stop generating mermaid files automatically.
--- packages/breadboard-web/src/make-graphs.ts
+++ packages/breadboard-web/src/make-graphs.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { toMermaid } from "@google-labs/breadboard";
 import { Dirent } from "fs";
 import { mkdir, readdir, writeFile } from "fs/promises";
 import path from "path";
@@ -14,10 +13,8 @@ const MODULE_DIR: string = path.dirname(fileURLToPath(import.meta.url));
 const PATH: string = path.join(MODULE_DIR, "boards");
 const MANIFEST_PATH: string = path.join(MODULE_DIR, "../public");
 const GRAPH_PATH: string = path.join(MODULE_DIR, "../public/graphs");
-const DIAGRAM_PATH: string = path.join(MODULE_DIR, "../docs/graphs");
 
 await mkdir(GRAPH_PATH, { recursive: true });
-await mkdir(DIAGRAM_PATH, { recursive: true });
 
 type ManifestItem = {
   title: string;
@@ -51,17 +48,12 @@ async function saveBoard(filePath: string): Promise<ManifestItem | undefined> {
     const relativePath: string = path.relative(PATH, filePath);
     const baseName: string = path.basename(filePath);
     const jsonFile: string = baseName.replace(".ts", ".json");
-    const diagramFile: string = baseName.replace(".ts", ".md");
 
     // Create corresponding directories based on the relative path
     const graphDir: string = path.dirname(path.join(GRAPH_PATH, relativePath));
-    const diagramDir: string = path.dirname(
-      path.join(DIAGRAM_PATH, relativePath)
-    );
 
     // Make sure the directories exist
     await mkdir(graphDir, { recursive: true });
-    await mkdir(diagramDir, { recursive: true });
 
     const manifestEntry: ManifestItem = {
       title: board.default.title ?? "Untitled",
@@ -72,10 +64,6 @@ async function saveBoard(filePath: string): Promise<ManifestItem | undefined> {
     await writeFile(
       path.join(graphDir, jsonFile),
       JSON.stringify(board.default, null, 2)
-    );
-    await writeFile(
-      path.join(diagramDir, diagramFile),
-      `## ${baseName}\n\n\`\`\`mermaid\n${toMermaid(board.default)}\n\`\`\``
     );
     return manifestEntry;
   } catch (e) {
