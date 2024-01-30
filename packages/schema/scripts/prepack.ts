99523b06f3f6920855cdefcc609299583fcc524c
Joseph Mearman <joseph@mearman.co.uk>
2024-01-30 16:16:29 +0000

    Refactor file paths and update schema references
    - Renamed `packages/schema/board.json` to `packages/schema/scripts/data/board.json`
    - Updated import paths for `breadboard.schema.json` and `package.json`
    - Updated schema ID in the code
    - Updated file write path for `breadboard.schema.json`
    - Removed unnecessary file read operations

--- packages/schema/scripts/prepack.ts
+++ packages/schema/scripts/prepack.ts
@@ -6,20 +6,17 @@
 
 import fs from "fs";
 import path from "path";
+import schema from "~/breadboard.schema.json";
+import packageJson from "~/package.json";
 
-const packageJsonPath = path.join(__dirname, "package.json");
-const schemaPath = path.join(__dirname, "breadboard.schema.json");
-
-const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
-console.log(`schema id: ${schema.$id}`);
-
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+console.log(`current schema id: ${schema.$id}`);
 console.log(`package.json version: ${packageJson.version}`);
-
 schema.$id = `https://raw.githubusercontent.com/breadboard-ai/breadboard/@google-labs/breadboard-schema/${packageJson.version}/breadboard.schema.json`;
-
 console.log(`updating schema id to: ${schema.$id}`);
 
-fs.writeFileSync(schemaPath, JSON.stringify(schema, null, 2));
+fs.writeFileSync(
+  path.join(process.cwd(), "breadboard.schema.json"),
+  JSON.stringify(schema, null, 2)
+);
 
 console.log("done");
--- packages/schema/scripts/validate.ts
+++ packages/schema/scripts/validate.ts
@@ -6,25 +6,20 @@
 
 import addFormats from "ajv-formats";
 import Ajv2020 from "ajv/dist/2020";
-import fs from "fs";
-import path from "path";
+import testData from "~/scripts/data/board.json";
 
 const ajv = new Ajv2020({ allErrors: true });
 addFormats(ajv);
 
-const schema = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "breadboard.schema.json"), "utf8")
-);
+import schema from "~/breadboard.schema.json";
+
 console.log("\x1b[32m", "Successfully read schema", "\x1b[0m");
 
-// Validate the schema
 const validate = ajv.compile(schema);
 console.log("\x1b[32m", "Successfully compiled schema", "\x1b[0m");
 
 console.log("Validating test data...");
-const testData = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "board.json"), "utf8")
-);
+
 const valid = validate(testData);
 
 if (valid) {
