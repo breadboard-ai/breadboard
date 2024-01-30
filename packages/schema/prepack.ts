418379d574a2362449d635f7aa88045eb83ad187
Joseph Mearman <joseph@mearman.co.uk>
2024-01-30 14:59:11 +0000

    Refactor schema file paths and add prepack script
    - Renamed `breadboard.schema.json` to `breadboard.schema.json` in the `packages/breadboard` directory.
    - Updated `$id` property in the schema file to point to a new location: `https://raw.githubusercontent.com/breadboard-ai/breadboard/@google-labs/breadboard-schema/1.0.0/breadboard.schema.json`.
    - Added a new `package.json` file in the `packages/schema` directory with necessary configurations.
    - Added a new `prepack.ts` script in the `packages/schema` directory that updates the `$id` property of the schema file based on the version specified in the package.json file.

---
+++ packages/schema/prepack.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
+const packageJsonPath = path.join(__dirname, 'package.json');
+const schemaPath = path.join(__dirname, 'breadboard.schema.json');
+
+const schema = JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+console.log(`schema id: ${schema.$id}`);
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+console.log(`package.json version: ${packageJson.version}`);
+
+schema.$id = `https://raw.githubusercontent.com/breadboard-ai/breadboard/@google-labs/breadboard-schema/${packageJson.version}/breadboard.schema.json`;
+
+console.log(`updating schema id to: ${schema.$id}`);
+
+fs.writeFileSync(schemaPath, JSON.stringify(schema, null, 2));
+
+console.log('done');
