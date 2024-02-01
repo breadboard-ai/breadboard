fd2c9e2c071e32e6165c2de9e14afb7483507c79
Joseph Mearman <joseph@mearman.co.uk>
2024-02-01 14:36:08 +0000

    Add Jest configuration for ESM support
    This commit adds a new file `jest.config.ts` which contains the Jest configuration for supporting ECMAScript modules (ESM). The configuration includes using the `ts-jest` preset, treating `.ts` files as ESM, mapping JavaScript imports to their corresponding paths, and ignoring certain test path patterns.

---
+++ packages/breadboard/jest.config.ts
@@ -0,0 +1,29 @@
+// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+
+import { JestConfigWithTsJest } from "ts-jest";
+import preset from "ts-jest/presets/index.js";
+
+const jestConfig: JestConfigWithTsJest = {
+  ...preset.defaultsESM,
+  extensionsToTreatAsEsm: [
+    ".ts"
+  ],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        useESM: true,
+      },
+    ],
+  },
+  testPathIgnorePatterns: [
+    ".*/node_modules/.*",
+    ".*/dist/.*",
+  ],
+};
+
+export default jestConfig;
