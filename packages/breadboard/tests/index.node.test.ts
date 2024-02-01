626da221cebd9f740d64d0d30765e41a112cba49
Joseph Mearman <joseph@mearman.co.uk>
2024-02-01 21:38:53 +0000

    Add schema validation tests
    - Added tests to validate using the schema from a board, validate using a schema that is missing a required property, validate using an empty schema, and validate using a schema that requires extra properties.

--- packages/breadboard/tests/index.node.test.ts
+++ packages/breadboard/tests/index.node.test.ts
@@ -102,4 +102,116 @@ describe("schema tests", async (t) => {
     assert.ok(result.valid);
     assert.ok(result.errors.length === 0);
   });
+
+  await test("validate using the schema from a board", async (t) => {
+    const board = new Board();
+    const boardJson = JSON.parse(JSON.stringify(board));
+    const schemaId = board.$schema;
+
+    // assert that schemaId is defined
+    assert.ok(schemaId);
+    const schema = await (await fetch(schemaId)).json();
+
+    // assert that schema is defined
+    assert.ok(schema);
+
+    // assert that it's an object
+    assert.equal(typeof schema, "object");
+
+    const validator = new Validator(schema, draft, shortCircuit);
+    const result: ValidationResult = validator.validate(boardJson);
+    // validation should pass
+    assert.ok(result.valid);
+    // there should be no errors
+    assert.ok(result.errors.length === 0);
+  });
+
+  await test("validate using a schema that is missing a required property", async (t) => {
+    const property = "edges";
+
+    const schema = JSON.parse(JSON.stringify(breadboardSchema));
+    // assert that edges property is defined
+    assert.ok(schema.properties[property]);
+
+    // assert that edges is required
+    assert.ok(schema.required?.includes(property));
+
+    delete schema.properties[property];
+    // assert that edges is missing
+    assert.ok(!schema.properties[property]);
+
+    const board = new Board();
+    const boardJson = JSON.parse(JSON.stringify(board));
+
+    const validator = new Validator(schema, draft, shortCircuit);
+    const result: ValidationResult = validator.validate(boardJson);
+
+    // validation should fail because edges is required
+    assert.ok(!result.valid);
+    assert.ok(result.errors.length > 0);
+  });
+
+  await test("validate using an empty schema", async (t) => {
+    const schema: Schema = {
+      type: "object",
+      maxProperties: 0,
+    };
+
+    const board: Board = new Board();
+    const boardJson = JSON.parse(JSON.stringify(board));
+
+    const validator: Validator = new Validator(schema, draft, shortCircuit);
+    const result: ValidationResult = validator.validate(boardJson);
+
+    // validation should fail because boardJson has more than 0 properties
+    assert.ok(!result.valid);
+    // there should be errors
+    assert.ok(result.errors.length > 0);
+  });
+
+  await test("validate using a schema that requires extra properties", async (t) => {
+    const potatoes = "potatoes";
+    const schema = {
+      ...breadboardSchema,
+      required: breadboardSchema.required.concat([potatoes]),
+      properties: {
+        ...breadboardSchema.properties,
+        [potatoes]: {
+          type: "string",
+        },
+      }
+    };
+
+    // assert the schema has the required additional property
+    assert.ok(schema.required.includes(potatoes));
+    assert.ok(schema.properties[potatoes]);
+
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const board: any = new Board();
+    const boardJson = JSON.parse(JSON.stringify(board));
+
+    const validator: Validator = new Validator(schema as Schema, draft, shortCircuit);
+    const result: ValidationResult = validator.validate(boardJson);
+
+    // validation should fail because nodes is required
+    assert.ok(!result.valid);
+    // there should be errors
+    assert.ok(result.errors.length > 0);
+
+
+    const boardWithAdditionalProperty = {
+      ...board,
+      [potatoes]: "yummy"
+    };
+
+    const boardWithAdditionalPropertyJson = JSON.parse(JSON.stringify(boardWithAdditionalProperty));
+    const result2: ValidationResult = validator.validate(boardWithAdditionalPropertyJson);
+
+    console.log(result2);
+    // validation should pass because boardWithAdditionalPropertyJson has the required additional property
+    assert.ok(result2.valid);
+    // there should be no errors
+    assert.ok(result2.errors.length === 0);
+  });
 });
