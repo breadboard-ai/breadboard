63b65fba4985d9175831f5ebaa108cfac3e52024
Paul Lewis <617438+paullewis@users.noreply.github.com>
2024-02-02 13:06:20 +0000

    [website] Add syntax highlighting, copy-to-clipboard, styling (#597)

--- packages/website/eleventy.config.cjs
+++ packages/website/eleventy.config.cjs
@@ -9,9 +9,11 @@
 
 const markdownItGitHubAlerts = require("markdown-it-github-alerts");
 const markdownItGitHubHeadings = require("markdown-it-github-headings");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("toc", require("./filter-toc.cjs"));
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.amendLibrary("md", (mdLib) => {
     mdLib.use(markdownItGitHubAlerts, { icons: "" });
