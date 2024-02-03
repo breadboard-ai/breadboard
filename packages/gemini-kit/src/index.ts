85edbbd33830371aa770f82d0f3f615a7023d5cb
Dimitri Glazkov <dimitri@glazkov.com>
2024-02-03 13:55:53 -0800

    A working kit!

--- packages/gemini-kit/src/index.ts
+++ packages/gemini-kit/src/index.ts
@@ -5,9 +5,9 @@
  */
 
 import { GraphToKitAdapter, KitBuilder } from "@google-labs/breadboard/kits";
-import { NewNodeFactory, NewNodeValue, addKit } from "@google-labs/breadboard";
 
 import kit from "./kit.js";
+import { NewNodeFactory, NewNodeValue, addKit } from "@google-labs/breadboard";
 
 // TODO: Replace with the actual URL.
 const KIT_BASE_URL =
@@ -18,16 +18,22 @@ const adapter = await GraphToKitAdapter.create(kit, KIT_BASE_URL, []);
 const builder = new KitBuilder(
   adapter.populateDescriptor({
     url: "npm:@google-labs/gemini-kit",
-    namespacePrefix: "gemini-api-",
+    // TODO: This currently doesn't work, because "addKit" below translates
+    // handler id directly into the node name. We need to make it work without
+    // prefix.
+    // namespacePrefix: "gemini-api-",
   })
 );
 
-export const gemini = addKit(
-  builder.build({
-    text: adapter.handlerForNode("text"),
-    vision: adapter.handlerForNode("vision"),
-  })
-) as unknown as {
+const GeminiKit = builder.build({
+  text: adapter.handlerForNode("text"),
+  vision: adapter.handlerForNode("vision"),
+});
+
+export type GeminiKit = InstanceType<typeof GeminiKit>;
+
+export default GeminiKit;
+export const gemini = addKit(GeminiKit) as unknown as {
   text: NewNodeFactory<{ text: string }, { text: string }>;
-  vision: NewNodeFactory<{ parts: NewNodeValue }, { result: string }>;
+  vision: NewNodeFactory<{ parts: NewNodeValue[] }, { result: string }>;
 };
