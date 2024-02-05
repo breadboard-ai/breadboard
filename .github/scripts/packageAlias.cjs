1de9fd47d6d247407d49e23b53a878391776c177
Joseph Mearman <joseph@mearman.co.uk>
2024-02-05 14:56:53 +0000

    Refactor packageAlias.cjs and isolated.yml
    - Refactored packageAlias.cjs to include the "inputs" parameter in the exported function.
    - Updated isolated.yml to pass the "inputs" value from setup.outputs.matrix to packageAlias.cjs.

--- .github/scripts/packageAlias.cjs
+++ .github/scripts/packageAlias.cjs
@@ -1,11 +1,12 @@
-module.exports = async ({ github, context, core, glob, io, exec, require }) => {
-  for (const [key, value] of Object.entries(core)) {
-    console.log(`core.${key}: ${value}`);
-  }
-  for (const [key, value] of Object.entries(github)) {
-    console.log(`github.${key}: ${value}`);
-  }
-  for (const [key, value] of Object.entries(context)) {
-    console.log(`context.${key}: ${value}`);
-  }
+module.exports = async ({
+  github,
+  context,
+  core,
+  glob,
+  io,
+  exec,
+  require,
+  inputs,
+}) => {
+  console.log(inputs);
 };
