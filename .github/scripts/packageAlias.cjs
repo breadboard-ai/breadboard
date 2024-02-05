93b790e8644790499828f6eeffd596bfe2eafc9c
Joseph Mearman <joseph@mearman.co.uk>
2024-02-05 15:11:03 +0000

    Update package.json with new scope aliases

--- .github/scripts/packageAlias.cjs
+++ .github/scripts/packageAlias.cjs
@@ -9,4 +9,45 @@ module.exports = async ({
   inputs,
 }) => {
   console.log(inputs);
+  const packageNames = inputs.package;
+
+  const existingScope = "@google-labs";
+  const newScope = process.env.OWNER_LC;
+  console.log({ existingScope, newScope });
+  if (!newScope) {
+    throw new Error("OWNER_LC is not set");
+  }
+  const depTypes = ["dependencies", "devDependencies"];
+
+  const fs = require("fs");
+  console.log({ cwd: process.cwd() });
+  const basePackageDir = "./packages";
+
+  for (const p of packageNames) {
+    const packageDir = `${basePackageDir}/${p}`;
+    const packageJsonPath = `${packageDir}/package.json`;
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+    console.log({ "package.json": packageJsonPath, name: packageJson.name });
+
+    for (const dtype of depTypes) {
+      const depsOfType = packageJson[dtype];
+      if (depsOfType) {
+        console.log({ dtype, depsOfType });
+        const keys = Object.entries(depsOfType);
+        for (const [key, value] of keys) {
+          if (key.startsWith(existingScope)) {
+            const alias = `npm:@${newScope}/${key.replace(
+              existingScope,
+              ""
+            )}@*`;
+            console.log(`${key} -> ${alias}`);
+            depsOfType[alias] = value;
+          }
+        }
+        packageJson[dtype] = depsOfType;
+      }
+    }
+    fs.writeFileSync(`${p}/package.json`, JSON.stringify(packageJson, null, 2));
+  }
 };
