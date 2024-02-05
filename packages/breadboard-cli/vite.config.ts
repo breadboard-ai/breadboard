53ee67126b651b6d6750102dd29689560c29ffe5
Joseph Mearman <joseph@mearman.co.uk>
2024-02-05 12:24:56 +0000

    drop vite bundling config

--- packages/breadboard-cli/vite.config.ts
+++ packages/breadboard-cli/vite.config.ts
@@ -18,25 +18,7 @@ if (import.meta.resolve) {
 }
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      "@google-labs/breadboard",
-      "@google-labs/breadboard-web",
-      "@google-labs/core-kit",
-      "@google-labs/template-kit",
-      "commander",
-      "esbuild",
-      "serve",
-      "vite",
-      "yaml",
-    ],
-  },
   build: {
-    commonjsOptions: {
-      include: [
-        /node_modules/
-      ]
-    },
     lib: {
       entry: {
         // TODO: These are just variants of the breadboard-web entry points; we
