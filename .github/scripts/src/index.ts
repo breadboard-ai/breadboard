5d3fd4554ea57c4c7de1f819208a92ac28e4119b
Joseph Mearman <joseph@mearman.co.uk>
2024-02-06 01:12:11 +0000

    Add initial script and tsconfig files

---
+++ .github/scripts/src/index.ts
@@ -0,0 +1,27 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as github from "@actions/github";
+import * as glob from "@actions/glob";
+import * as io from "@actions/io";
+import * as fs from "fs";
+const __original_require__ = require;
+
+const globals = {
+  require,
+  __original_require__,
+  github,
+  core,
+  exec,
+  glob,
+  io,
+  fetch,
+};
+
+Object.assign(global, globals);
+
+module.exports = () => {
+  console.log("Hello world");
+  fs.readdirSync(".").forEach((file) => {
+    console.log(file);
+  });
+};
