e9a53ec4112600bfe1533b3dd84f1df13b136c5e
Dimitri Glazkov <dimitri@glazkov.com>
2024-02-07 21:04:18 -0800

    Massage describe method to actually work.

--- packages/breadboard/src/inspector/graph.ts
+++ packages/breadboard/src/inspector/graph.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { SchemaBuilder } from "../schema.js";
+import { combineSchemas } from "../schema.js";
 import {
   GraphDescriptor,
   NodeDescriberResult,
@@ -95,29 +95,41 @@ class Graph implements InspectableGraph {
   }
 
   async describe(): Promise<NodeDescriberResult> {
-    const inputs = this.nodesByType("input").filter((n) => n.isEntry());
-    const outputs = this.nodesByType("output").filter((n) => n.isExit());
-    // TODO: Handle explicitly defined input/output schemas.
-    const inputSchema = new SchemaBuilder();
-    inputs
-      .flatMap((n) => n.outgoing())
-      .forEach((edge) => {
-        inputSchema
-          .addProperty(edge.out, { type: "string" })
-          .addRequired(edge.out);
-      });
-    const outputSchema = new SchemaBuilder();
-    outputs
-      .flatMap((n) => n.incoming())
-      .forEach((edge) => {
-        outputSchema
-          .addProperty(edge.in, { type: "string" })
-          .addRequired(edge.in);
-      });
+    const inputSchemas = (
+      await Promise.all(
+        this.nodesByType("input")
+          .filter((n) => n.isEntry())
+          .map((input) => input.describe())
+      )
+    ).map((result) => result.inputSchema);
+    const outputSchemas = (
+      await Promise.all(
+        this.nodesByType("output")
+          .filter((n) => n.isExit())
+          .map((output) => output.describe())
+      )
+    ).map((result) => result.outputSchema);
+    // // TODO: Handle explicitly defined input/output schemas.
+    // const inputSchema = new SchemaBuilder();
+    // inputs
+    //   .flatMap((n) => n.outgoing())
+    //   .forEach((edge) => {
+    //     inputSchema
+    //       .addProperty(edge.out, { type: "string" })
+    //       .addRequired(edge.out);
+    //   });
+    // const outputSchema = new SchemaBuilder();
+    // outputs
+    //   .flatMap((n) => n.incoming())
+    //   .forEach((edge) => {
+    //     outputSchema
+    //       .addProperty(edge.in, { type: "string" })
+    //       .addRequired(edge.in);
+    //   });
 
     return {
-      inputSchema: inputSchema.build(),
-      outputSchema: outputSchema.build(),
+      inputSchema: combineSchemas(inputSchemas),
+      outputSchema: combineSchemas(outputSchemas),
     };
   }
 }
