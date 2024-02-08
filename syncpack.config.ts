738f9a8b1293abef6e2624bbb1a32243c52fb61e
Joseph Mearman <joseph@mearman.co.uk>
2024-02-08 19:51:57 +0000

    Add semverGroups configuration for @google-labs packages
    This commit adds a new configuration option `semverGroups` to the `syncpack.config.ts` file. The `semverGroups` array now includes a group for `@google-labs/*` packages with a range of `^`.

--- syncpack.config.ts
+++ syncpack.config.ts
@@ -3,6 +3,12 @@ import type { RcFile } from "syncpack";
 // https://jamiemason.github.io/syncpack
 
 const config: RcFile = {
+  semverGroups: [
+    {
+      packages: ["@google-labs/*"],
+      range: "^",
+    },
+  ],
 };
 
 export default config;
