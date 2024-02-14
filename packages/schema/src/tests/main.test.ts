36d7574ab5e2a4d3cb75c6b43e3ae003a77f3859
Joseph Mearman <joseph@mearman.co.uk>
2024-02-14 20:18:59 +0000

    Move test files

--- packages/schema/src/tests/main.test.ts
+++ packages/schema/src/tests/main.test.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { ascendToPackageDir } from "./util/ascendToPackageDir.js";
 import { getBoardFiles } from "./util/getBoardFiles.js";
 
-const ajv = new Ajv.default();
+const ajv = new Ajv();
 let validate: ValidateFunction;
 
 test.before(() => {
