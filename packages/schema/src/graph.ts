6296864becdebe7e8b69c402073d48983f27a144
Joseph Mearman <joseph@mearman.co.uk>
2024-02-14 11:47:48 +0000

    add pattern validation for kit version
    This commit adds a pattern validation for the `version` property in the `KitDescriptor` type. The pattern follows the [semver](https://semver.org/) format and ensures that the version string is valid.

--- packages/schema/src/graph.ts
+++ packages/schema/src/graph.ts
@@ -148,6 +148,7 @@ export type KitDescriptor = KitReference & {
   /**
    * Version of the kit.
    * [semver](https://semver.org/) format is encouraged.
+   * @pattern ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$
    */
   version?: string;
 };
