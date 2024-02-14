5221586c8b115180159523b5ab7c77134324c5bf
Dimitri Glazkov <dglazkov@google.com>
2024-02-14 15:30:25 -0800

    [agent-kit] Introduce Structured Worker node.
    - Update serialized repeater.
    - Add skip logging for debugging cycles.
    - Add Structured Worker board.
    - Start using structured workers.
    - docs(changeset): Add "Structured Worker" node to Agent Kit.
    
    Progress on #690

---
+++ packages/agent-kit/src/boards/structured-worker.ts
@@ -0,0 +1,222 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  NewNodeFactory,
+  NewNodeValue,
+  Schema,
+  base,
+  board,
+  code,
+} from "@google-labs/breadboard";
+import { core } from "@google-labs/core-kit";
+import { gemini } from "@google-labs/gemini-kit";
+import { json } from "@google-labs/json-kit";
+import { templates } from "@google-labs/template-kit";
+
+export type StructuredWorkerType = NewNodeFactory<
+  {
+    /**
+     * The context to use for the worker.
+     */
+    context?: NewNodeValue;
+    /**
+     * The instruction we want to give to the worker so that shapes its
+     * character and orients it a bit toward the task we want to give it.
+     */
+    instruction: NewNodeValue;
+    /**
+     * The JSON schema to use for the worker.
+     */
+    schema?: NewNodeValue;
+  },
+  {
+    /**
+     * The context after generation. Pass this to the next agent when chaining
+     * them together.
+     */
+    context: NewNodeValue;
+    /**
+     * The output from the agent. Use this to just get the output without any
+     * previous context.
+     */
+    json: NewNodeValue;
+  }
+>;
+
+const sampleSchema = JSON.stringify(
+  {
+    type: "object",
+    properties: {
+      poems: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            inspiration: {
+              type: "string",
+              description: "The inspiration behind the poem",
+            },
+            poem: {
+              type: "string",
+              description: "The poem itself",
+            },
+          },
+        },
+      },
+    },
+  } satisfies Schema,
+  null,
+  2
+);
+
+const sampleInstruction = `You are a brilliant poet who specializes in two-line rhyming poems.
+Given any topic, you can quickly whip up three two-line rhyming poems about it.
+Look at the topic below and do your magic`;
+
+const sampleContext = `the universe within us`;
+
+type ContextItem = {
+  role: string;
+  parts: { text: string }[];
+};
+
+const contextAssembler = code(({ context, json }) => {
+  if (!context) throw new Error("Context is required");
+  return {
+    context: [
+      ...(context as ContextItem[]),
+      { role: "model", parts: { text: JSON.stringify(json) } },
+    ],
+  };
+});
+
+const contextBuilder = code(({ context, format, instruction }) => {
+  if (!Array.isArray(context)) {
+    // A clever trick. Let's see if this works
+    // A user can supply context as either ContextItem[] or as non-array.
+    // When it's not an array, let's just conjure up the proper ContextItem[]
+    // from that.
+    const text =
+      typeof context === "string" ? context : JSON.stringify(context);
+    context = [{ role: "user", parts: [{ text }] }];
+  }
+  const list = (context as unknown[]) || [];
+  if (list.length > 0) {
+    const last = list[list.length - 1] as ContextItem;
+    if (last.role === "user") {
+      // A trick: the instruction typically sits in front of the actual task
+      // that the user requests. So do just that -- add it at the front of the
+      // user part list, rather than at the end.
+      last.parts.unshift(
+        { text: instruction as string },
+        { text: format as string }
+      );
+      return { context: list };
+    }
+  }
+  return {
+    context: [
+      ...list,
+      { role: "user", parts: [{ text: instruction }, { text: format }] },
+    ],
+  };
+});
+
+const counter = code(({ context, error, count }) => {
+  const num = (count as number) - 1;
+  if (num != 0) {
+    return { continue: context, count: num };
+  }
+  return { stop: error };
+});
+
+export default await board(({ context, instruction, schema }) => {
+  context.title("Context").isArray().examples(sampleContext);
+  instruction
+    .title("Instruction")
+    .format("multiline")
+    .examples(sampleInstruction);
+  schema
+    .title("Schema")
+    .examples(sampleSchema)
+    .isObject()
+    .format("multiline")
+    .optional()
+    .default("{}");
+
+  const schemish = json.schemish({ $id: "schemish", schema });
+
+  const format = templates.promptTemplate({
+    $id: "format",
+    template: `Reply as valid JSON of the following format:
+
+\`\`\`json
+{{schemish}}
+\`\`\`
+`,
+    schemish: schemish,
+  });
+
+  const buildContext = contextBuilder({
+    $id: "buildContext",
+    context,
+    instruction,
+    format: format.prompt,
+  });
+
+  const initialValues = core.passthrough({
+    $id: "initialValues",
+    count: 5,
+    error: "stub",
+  });
+
+  const count = counter({
+    $id: "count",
+    count: initialValues.count,
+    error: initialValues.error,
+    context: buildContext.context.memoize(),
+  });
+  count.count.to(count);
+
+  const generate = gemini.text({
+    $id: "generate",
+    context: count.continue,
+    text: "unused", // A gross hack (see TODO in gemini-generator.ts)
+  });
+
+  const validate = json.validateJson({
+    $id: "validate",
+    json: generate.text.isString(),
+    schema: schema.memoize(),
+  });
+
+  validate.$error.as("error").to(count);
+
+  base.output({
+    $id: "validationError",
+    $error: validate.$error,
+    context: count.stop,
+  });
+
+  const assembleContext = contextAssembler({
+    $id: "assembleContext",
+    context: buildContext.context,
+    json: validate.json,
+  });
+
+  assembleContext.context
+    .title("Context")
+    .isObject()
+    .description("Agent context after generation");
+  generate.text.title("Output").isString().description("Agent's output");
+
+  return { context: assembleContext.context, json: validate.json };
+}).serialize({
+  title: "Structured Worker",
+  description: "A worker who outputs structure data (JSON) provided a schema.",
+  version: "0.0.1",
+});
--- packages/breadboard-web/src/boards/ad-writer.ts
+++ packages/breadboard-web/src/boards/ad-writer.ts
@@ -5,11 +5,7 @@
  */
 
 import { Schema, board } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
-import { json } from "@google-labs/json-kit";
-
-const jsonAgent = "json-agent.json";
+import { agents } from "@google-labs/agent-kit";
 
 const adSchema = {
   type: "object",
@@ -34,86 +30,52 @@ const requirementsSchema = {
   },
 } satisfies Schema;
 
-export default await board(({ text }) => {
-  text
+export default await board(({ context }) => {
+  context
     .title("Ad specs")
     .format("multiline")
     .examples(
       `This ad is for my lawn care company that will fit into an inch of newspaper copy. It's called "Max's Lawn Care" and it should use the slogan "I care about your lawn." Emphasize the folksiness of it being a local, sole proprietorship that I started after graduating from high school.`
     );
 
-  const requirementsExtractor = core.invoke({
+  const requirementsExtractor = agents.structuredWorker({
     $id: "requiremenstExtractor",
-    path: jsonAgent,
-    text: templates.promptTemplate({
-      template: `Given the following specs, extract requirements for writing an ad copy:
-
-      {{text}}`,
-      text,
-    }).prompt,
-    context: [],
+    context,
+    instruction: `Given the following specs, extract requirements for writing an ad copy`,
     schema: requirementsSchema,
   });
 
-  const adWriter = core.invoke({
+  const adWriter = agents.structuredWorker({
     $id: "adWriter",
-    path: jsonAgent,
-    text: `Write ad copy that conforms to the requirements above`,
+    instruction: `Write ad copy that conforms to the requirements above`,
     context: requirementsExtractor,
     schema: adSchema,
   });
 
-  const customer = core.invoke({
+  const customer = agents.structuredWorker({
     $id: "customer",
-    path: jsonAgent,
-    text: `Imagine you are a customer. You are a middle-aged homeowner from rural Midwest. You are overrun with ads and are weary of being scammed. You just want to work with someone local and trustworty. Review this and offer three improvements that would increase the likelihood of you trusting the ad.`,
+    instruction: `Imagine you are a customer. You are a middle-aged homeowner from rural Midwest. You are overrun with ads and are weary of being scammed. You just want to work with someone local and trustworty. Review this and offer three improvements that would increase the likelihood of you trusting the ad.`,
     context: adWriter,
     schema: requirementsSchema,
   });
 
-  const requirementsExtractor2 = core.invoke({
+  const requirementsExtractor2 = agents.structuredWorker({
     $id: "requirementsExtractor2",
-    path: jsonAgent,
-    text: `Incorporate all feedback above into new, improved requirements`,
+    instruction: `Incorporate all feedback above into new, improved requirements`,
     context: customer,
-    schema: {
-      type: "object",
-      properties: {
-        requirements: {
-          type: "array",
-          items: {
-            type: "string",
-            description: "a prompt requirement",
-          },
-        },
-      },
-    } satisfies Schema,
+    schema: requirementsSchema,
   });
 
-  // TODO: Do something more componenty here. Is restarting context a board?
-  const contextRestart = json.jsonata({
-    $id: "contextRestart",
-    expression: `$`,
-    json: requirementsExtractor2.json.isString(),
-  });
-
-  const adWriter2 = core.invoke({
+  const adWriter2 = agents.structuredWorker({
     $id: "adWriter2",
-    path: jsonAgent,
-    context: [],
-    text: templates.promptTemplate({
-      template: `Write ad copy that conforms to the requirements below
-
-      {{requirements}}`,
-      requirements: contextRestart.result,
-    }),
+    context: requirementsExtractor2.context,
+    instruction: `Write ad copy that conforms to the specified requirements`,
     schema: adSchema,
   });
 
-  const adExec = core.invoke({
+  const adExec = agents.structuredWorker({
     $id: "adExec",
-    path: jsonAgent,
-    text: `You are a Google Ads Search Professional. Given the above prompt and response, generate 3 point constructive critique of the response that I can action to make the output even better and more effective given the prompt.`,
+    instruction: `You are a Google Ads Search Professional. Given the above prompt and response, generate 3 point constructive critique of the response that I can action to make the output even better and more effective given the prompt.`,
     context: adWriter2,
     schema: {
       type: "object",
@@ -129,68 +91,50 @@ export default await board(({ text }) => {
     } satisfies Schema,
   });
 
-  const improver = core.invoke({
+  const improver = agents.structuredWorker({
     $id: "improver",
-    path: jsonAgent,
-    text: `Given the 3 point critique try to generate a new response.`,
+    instruction: `Given the 3 point critique try to generate a new response.`,
     context: adExec,
     schema: adSchema,
   });
 
-  // const promptImprover = core.invoke({
-  //   text: `Given the original prompt and the extracted requirements, rewrite and enrich the prompt to be more expressive and likely to generate a really engaging and interesting ad.`,
-  //   context: requirementsExtractor,
-  //   path: jsonAgent,
-  //   schema: {
-  //     type: "object",
-  //     properties: {
-  //       prompt: {
-  //         type: "string",
-  //         description: "new rewritten prompt",
-  //       },
-  //     },
-  //   } satisfies Schema,
-  //   generator,
-  // });
-  // const assessor = core.invoke({
-  //   text: `Given the list of requirements assess how well the newest response conforms to the requirements.`,
-  //   context: requirementsExtractor,
-  //   path: jsonAgent,
-  //   schema: {
-  //     type: "object",
-  //     properties: {
-  //       assessment: {
-  //         type: "array",
-  //         items: {
-  //           type: "object",
-  //           properties: {
-  //             summary: {
-  //               type: "string",
-  //               description: "summary of an point in the assessment",
-  //             },
-  //             details: {
-  //               type: "string",
-  //               description: "details of the the assessment point",
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   } satisfies Schema,
-  //   generator,
-  // });
+  const assessor = agents.structuredWorker({
+    $id: "assessor",
+    instruction: `Given the list of requirements assess how well the newest response conforms to the requirements.`,
+    context: improver.context,
+    schema: {
+      type: "object",
+      properties: {
+        assessment: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              summary: {
+                type: "string",
+                description: "summary of an point in the assessment",
+              },
+              details: {
+                type: "string",
+                description: "reasoning behind the point in an assessment",
+              },
+            },
+          },
+        },
+      },
+    } satisfies Schema,
+  });
 
-  // const improver2 = core.invoke({
-  //   text: `Use the new prompt to write the final ad:`,
-  //   context: promptImprover,
-  //   path: jsonAgent,
-  //   schema: adSchema,
-  //   generator,
-  // });
+  const improver2 = agents.structuredWorker({
+    $id: "improver2",
+    instruction: `You are a Google Ads Professional. Write the ad copy that satisfies the requirements and is improved based on the assessment`,
+    context: assessor.context,
+    schema: adSchema,
+  });
 
-  return { ...improver };
+  return { ...improver2 };
 }).serialize({
   title: "Ad Writer",
   description: "An example of chain of agents working on writing an ad",
-  version: "0.0.2",
+  version: "0.0.3",
 });
