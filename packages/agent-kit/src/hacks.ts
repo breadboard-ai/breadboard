e9696dfee5a9c451ecda20d3b9b47c23ea23e696
Dimitri Glazkov <dglazkov@google.com>
2024-02-16 15:32:35 -0800

    [agent-kit] Describe nodes automatically.
    - Add describer for the Structured Worker.
    - Add Repeater describer.
    - Use Inspector API to describe graphs in Agent Kit.
    - docs(changeset): Teach Agent Kit to describe its nodes automatically.
    
    Fixes #748

--- packages/agent-kit/src/hacks.ts
+++ packages/agent-kit/src/hacks.ts
@@ -4,57 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { NodeDescriberFunction, NodeHandler } from "@google-labs/breadboard";
+import { GraphDescriptor, NodeHandler, inspect } from "@google-labs/breadboard";
 
 // This is where we hack on making kit do the thing we want
 // TODO: Shape this and move it to the right place
 
-export const workerDescriber: NodeDescriberFunction = async () => {
-  return {
-    inputSchema: {
-      type: "object",
-      properties: {
-        context: {
-          type: "string",
-          title: "Context",
-          description: "The context to use for the worker",
-        },
-        instruction: {
-          type: "string",
-          title: "Instruction",
-          description:
-            "The instruction we want to give to the worker so that shapes its character and orients it a bit toward the task we want to give it.",
-        },
-      },
-      additionalProperties: false,
-    },
-    outputSchema: {
-      type: "object",
-      properties: {
-        context: {
-          type: "string",
-          title: "Context",
-          description:
-            "The context after generation. Pass this to the next agent when chaining them together.",
-        },
-        text: {
-          type: "string",
-          title: "Text",
-          description:
-            "The output from the agent. Use this to just get the output without any previous context.",
-        },
-      },
-      additionalProperties: false,
-    },
-  };
-};
-
-export const addDescriber = (
+export const addDescriber = async (
   handler: NodeHandler,
-  describer: NodeDescriberFunction
+  graph: GraphDescriptor
 ) => {
   if (typeof handler === "function") {
     handler = { invoke: handler };
   }
-  return { invoke: handler.invoke, describe: describer } as NodeHandler;
+  const describerResult = await inspect(graph).describe();
+  return {
+    invoke: handler.invoke,
+    describe: async () => describerResult,
+  } as NodeHandler;
 };
