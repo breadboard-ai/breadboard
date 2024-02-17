ec75cd674c2db778e8d48f31f3ccdff275bccdfe
Dimitri Glazkov <dglazkov@google.com>
2024-02-17 12:34:32 -0800

    [breadboard-web] Update the Chat Bot 2.0 to ask for bot instruction up front.
    - **Update the Chat Bot 2.0 to ask for bot instruction up front.**
    - **docs(changeset):**

--- packages/breadboard-web/src/boards/chat-bot-2.ts
+++ packages/breadboard-web/src/boards/chat-bot-2.ts
@@ -4,10 +4,17 @@ import { core } from "@google-labs/core-kit";
 
 import askUserBoard from "./ask-user";
 
-export default await board(() => {
+export default await board(({ instruction }) => {
+  instruction.title("Chat Bot Instructions").isString().format("multiline")
+    .examples(`
+As a friendly assistant bot, reply to request below in a helpful, delighted, and brief manner to assist the user as quickly as possible.
+
+Pretend you have access to ordering food, booking a table, and other useful services. You can also ask for more information if needed.
+
+You are also a huge fan of Breadboard, which is the open source project that made you possible, so you subtly weave the references to Breadboard and various baking factoids into your answers.`);
   return agents.repeater({
     $id: "repeat",
-    worker: board(({ context }) => {
+    worker: board(({ context, instruction }) => {
       const askUser = core.invoke({
         $id: "askUser",
         // The path is currently resolved relative to the calling board, which is
@@ -20,11 +27,10 @@ export default await board(() => {
       const bot = agents.worker({
         $id: "bot",
         context: askUser.context,
-        instruction:
-          "As a friendlly assistant bot, reply to the request below in a friendly, helpful, and brief manner to assist the user as quickly as possible. Pretend you have access to ordering food, booking a table, or other services. You can also ask for more information if needed. If you don't understand the request, ask for clarification. If you can't help, apologize and explain why you can't help",
+        instruction,
       });
       return { context: bot.context, text: bot.text };
-    }),
+    }).in({ instruction }),
   });
 }).serialize({
   title: "Chat bot 2.0",
