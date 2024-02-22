fd703bac96844dedd19b80c8299f51d7069a3105
Joseph Mearman <joseph@mearman.co.uk>
2024-02-22 10:34:54 +0000

    Add exampleIgnoredVersionGroup to syncpack.config.ts

--- syncpack.config.ts
+++ syncpack.config.ts
@@ -3,17 +3,21 @@ import type { RcFile } from "syncpack";
 // https://jamiemason.github.io/syncpack
 // https://jamiemason.github.io/syncpack/config/syncpackrc/
 
+const exampleIgnoredVersionGroup = {
+  // this is an example of a dependency that will be ignored in a specific package if added to the version groups array
+  packages: ["@google-labs/agent-kit"],
+  dependencies: ["@types/node"],
+  isIgnored: true,
+};
+
 const config: RcFile = {
-  semverGroups: [
+  versionGroups: [
     {
-      dependencies: ["@google-labs/**"],
+      label: "Ignore local dependencies as they are managed by changesets",
+      packages: ["**"],
+      // dependencies: ["@google-labs/**"], // it would also be possible to ignore all dependencies from a specific scope, but that would not catch any local dependencies that are not scoped
       isIgnored: true,
-    },
-    {
-      // this can be used to ignore a given dependency in a given package
-      packages: [],
-      dependencies: [],
-      isIgnored: true,
+      dependencyTypes: ["local"]
     }
   ],
 };
