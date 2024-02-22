df1a5fa1dfba64dc658936af88a029589e3ff679
Joseph Mearman <joseph@mearman.co.uk>
2024-02-22 10:00:49 +0000

    Update semverGroups configuration in syncpack.config.ts
    - Change packages to dependencies and update range for @google-labs/**.
    - Set isIgnored to true for the specified dependencies.

--- syncpack.config.ts
+++ syncpack.config.ts
@@ -1,13 +1,20 @@
 import type { RcFile } from "syncpack";
 
 // https://jamiemason.github.io/syncpack
+// https://jamiemason.github.io/syncpack/config/syncpackrc/
 
 const config: RcFile = {
   semverGroups: [
     {
-      packages: ["@google-labs/*"],
-      range: "^",
+      dependencies: ["@google-labs/**"],
+      isIgnored: true,
     },
+    {
+      // this can be used to ignore a given dependency in a given package
+      packages: [],
+      dependencies: [],
+      isIgnored: true,
+    }
   ],
 };
 
