63baed71a804ea2f14cc19e915d2047da1637cdd
Dimitri Glazkov <dimitri@glazkov.com>
2024-03-02 13:05:51 -0800

    Make `kits` part of `bb-main` state.

--- packages/breadboard-ui/src/elements/editor/editor.ts
+++ packages/breadboard-ui/src/elements/editor/editor.ts
@@ -301,6 +301,7 @@ export class Editor extends LitElement {
     }
     this.#lastGraphUrl = descriptor.url || null;
 
+    console.log("Processing graph", this.kits);
     const breadboardGraph = inspect(descriptor, { kits: this.kits });
 
     const ports = new Map<string, InspectableNodePorts>();
