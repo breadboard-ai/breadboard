cba6e69204e49282d601d9a418dab6bd8752a19a
Paul Kinlan <paul.kinlan@gmail.com>
2024-03-04 22:26:22 +0000

    [breadboard-cli] Extract Routes from Debug Server (#946)
    Clean up the routing logic for the debug server.
    
    + Make routes
    + Make globals
    + Use URLPattern to make routing easier.

--- packages/breadboard-cli/src/commands/lib/debug-server.ts
+++ packages/breadboard-cli/src/commands/lib/debug-server.ts
@@ -4,28 +4,80 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import http from "http";
-import { extname, join, relative } from "path";
+import http, { IncomingMessage, ServerResponse } from "http";
+import { join, relative } from "path";
 import handler from "serve-handler";
-import { pathToFileURL } from "url";
-import { BoardMetaData, defaultKits, loadBoards } from "./utils.js";
+import { pathToFileURL, URL } from "url";
+import { BoardMetaData, defaultKits } from "./utils.js";
 import { stat } from "fs/promises";
-import { createReadStream } from "fs";
+import { URLPattern } from "urlpattern-polyfill";
+
 import { DebugOptions } from "../commandTypes.js";
 import { __dirname } from "../debug.js";
-import { getKits } from "./kits.js";
+import { KitData, getKits } from "./kits.js";
+
+import { boards } from "./debug-routes/boards.js";
+import { kits } from "./debug-routes/kits.js";
+import { kit } from "./debug-routes/kit.js";
+import { debug } from "./debug-routes/debug.js";
+import { board } from "./debug-routes/board.js";
+import { index } from "./debug-routes/index.js";
+
+export type Routes = Record<
+  string,
+  (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+    match: URLPatternResult,
+    globals: ServerGlobals
+  ) => Promise<ServerResponse<IncomingMessage> | void>
+>;
+
+export type ServerGlobals = {
+  distDir: string;
+  fileUrl: URL;
+  isDirectory: boolean;
+  boards: Array<BoardMetaData>;
+  file: string;
+  options: DebugOptions;
+  kits: Array<KitData>;
+  base: URL;
+  clients: Record<string, http.ServerResponse>;
+};
+
+// This is the main routing table for the debug server.
+const routes: Routes = {
+  "/": index,
+  "/index.html": index,
+  "/boards.js": boards,
+  "/kits.json": kits,
+  "/kits/:kitName(.*)": kit,
+  "/~~debug": debug,
+  "/*.json": board, // after kits.json
+};
 
 export const startServer = async (file: string, options: DebugOptions) => {
   const distDir = join(__dirname, "..", "..", "debugger");
   const fileStat = await stat(file);
   const fileUrl = pathToFileURL(file);
   const isDirectory = fileStat.isDirectory();
-
+  const boards: Array<BoardMetaData> = []; // Boards are dynamically loaded based on the "/boards.js" request.
   const kits = await getKits(defaultKits, options.kit);
-
-  let boards: Array<BoardMetaData> = []; // Boards are dynamically loaded based on the "/boards.js" request.
-
   const clients: Record<string, http.ServerResponse> = {};
+  const base = new URL("http://localhost:3000");
+
+  const globals: ServerGlobals = {
+    distDir,
+    fileUrl,
+    isDirectory,
+    boards,
+    file,
+    options,
+    kits,
+    clients,
+    base,
+  };
+
   const notifyClients = () => {
     Object.values(clients).forEach((clientResponse) => {
       clientResponse.write(`event: update\ndata:na\nid:${Date.now()}\n\n`);
@@ -33,124 +85,24 @@ export const startServer = async (file: string, options: DebugOptions) => {
   };
 
   const server = http.createServer(async (request, response) => {
-    const requestURL = new URL(request.url ?? "", "http://localhost:3000");
+    const requestURL = new URL(request.url ?? "", base.origin);
 
-    if (requestURL.pathname === "/boards.js") {
-      // Generate a list of boards that are valid at runtime.
-      // Cache until things change.
-      boards = await loadBoards(file, options);
-
-      const boardsData = JSON.stringify(
-        boards.map((board) => ({
-          url: board.url,
-          version: board.version,
-          title: board.title,
-        }))
-      );
-
-      const responseText = `const r = ${boardsData}; export default { Boards: r };`;
-
-      response.writeHead(200, {
-        "Content-Type": "application/javascript",
-        "Content-Length": responseText.length,
-      });
-
-      return response.end(responseText);
-    }
-
-    if (requestURL.pathname === "/kits.json") {
-      const responseText = JSON.stringify(
-        kits.map((kit) => `/kits/${kit.file}`) || []
-      );
-      response.writeHead(200, {
-        "Content-Type": "application/javascript",
-        "Content-Length": responseText.length,
-      });
-
-      return response.end(responseText);
-    }
-
-    if (requestURL.pathname.startsWith("/kits/")) {
-      const kitName = requestURL.pathname.replace("/kits/", "");
-      const kit = kits.find((kit) => kit.file === kitName);
-      if (kit) {
-        if ("data" in kit) {
-          response.writeHead(200, {
-            "Content-Type": "application/javascript",
-          });
-
-          return response.end(kit.data);
+    for (const route in routes) {
+      const routePattern = new URLPattern(route, base.origin);
+      const match = routePattern.exec(requestURL);
+      if (match) {
+        const routeResponse = await routes[route](
+          request,
+          response,
+          match,
+          globals
+        );
+        if (routeResponse == undefined) {
+          continue;
         } else {
-          response.writeHead(302, {
-            Location: kit.url,
-          });
-          return response.end();
+          return routeResponse;
         }
       }
-    }
-
-    if ("watch" in options) {
-      if (
-        requestURL.pathname === "/" ||
-        requestURL.pathname === "/index.html"
-      ) {
-        response.writeHead(200, {
-          "Content-Type": "text/html",
-        });
-
-        // Need to check this doesn't include "../" and other escape characters.
-        const fileStream = createReadStream(`${distDir}/index.html`);
-        fileStream.pipe(response, { end: false });
-        fileStream.on("end", () => {
-          response.write(`<!-- Added by Debug command --><script>
-const evtSource = new EventSource("/~~debug");
-evtSource.addEventListener("update", () => { window.location.reload(); });</script>`);
-          response.end();
-        });
-        return;
-      }
-
-      if (requestURL.pathname === "/~~debug") {
-        response.writeHead(200, {
-          "Content-Type": "text/event-stream",
-          "Cache-Control": "no-cache",
-          Connection: "keep-alive",
-        });
-        const clientId =
-          Date.now() +
-          Buffer.from(crypto.getRandomValues(new Uint32Array(10))).toString(
-            "hex"
-          );
-        response.on("close", () => {
-          delete clients[clientId];
-        });
-        clients[clientId] = response;
-        return;
-      }
-    }
-
-    let board = boards.find((board) => board.url == requestURL.pathname);
-    if (!board && extname(requestURL.pathname) === ".json") {
-      // Attempt to load the board and append it to the list of boards.
-      const possibleBoardPath = join(process.cwd(), requestURL.pathname);
-      try {
-        const newBoards = await loadBoards(possibleBoardPath, options);
-        const [newBoard] = newBoards;
-        boards.push(newBoard);
-        board = newBoard;
-      } catch (err) {
-        // This board was not found.
-      }
-    }
-
-    // We only want to serve the file that is being debugged... nothing else.
-    if (board) {
-      const boardData = JSON.stringify(board);
-      response.writeHead(200, {
-        "Content-Type": "application/json",
-      });
-
-      return response.end(boardData);
     }
 
     return handler(request, response, {
