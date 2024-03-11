b291aacbfa0e52f8427542514d1fdac5be6b575a
Al Marks <aomarks@google.com>
2024-03-11 13:31:20 -0700

    Add invoke and describe functions so that defineNodeType returns a NodeHandler

--- packages/build/src/test/define_test.ts
+++ packages/build/src/test/define_test.ts
@@ -6,6 +6,13 @@
 
 import { defineNodeType } from "@breadboard-ai/build";
 import { test } from "node:test";
+import type {
+  NodeDescriberFunction,
+  NodeHandler,
+  NodeHandlerContext,
+  NodeHandlerFunction,
+} from "@google-labs/breadboard";
+import assert from "node:assert/strict";
 
 test("expect types: 0 in, 0 out", () => {
   // $ExpectType NodeDefinition<{}, {}>
@@ -238,4 +245,143 @@ test.skip("expect type error: unknown return port", () => {
       };
     }
   );
+});
+
+test("expect types: definitions are NodeHandlers", () => {
+  const definition = defineNodeType(
+    {
+      in1: {
+        type: "string",
+      },
+    },
+    {
+      out1: {
+        type: "string",
+      },
+    },
+    () => {
+      return {
+        out1: "foo",
+      };
+    }
+  );
+  definition satisfies NodeHandler;
+  definition.invoke satisfies NodeHandlerFunction;
+  definition.describe satisfies NodeDescriberFunction;
+});
+
+test("describe function generates JSON schema", async () => {
+  const definition = defineNodeType(
+    {
+      in1: {
+        type: "string",
+        description: "Description of in1",
+      },
+      in2: {
+        type: "number",
+      },
+    },
+    {
+      out1: {
+        type: "boolean",
+        description: "Description of out1",
+      },
+    },
+    () => {
+      return {
+        out1: true,
+      };
+    }
+  );
+  assert.deepEqual(await definition.describe(), {
+    inputSchema: {
+      type: "object",
+      properties: {
+        in1: {
+          title: "in1",
+          description: "Description of in1",
+          type: "string",
+        },
+        in2: {
+          title: "in2",
+          description: undefined,
+          type: "number",
+        },
+      },
+      required: ["in1", "in2"],
+    },
+    outputSchema: {
+      type: "object",
+      properties: {
+        out1: {
+          title: "out1",
+          description: "Description of out1",
+          type: "boolean",
+        },
+      },
+      required: ["out1"],
+    },
+  });
+});
+
+test("invoke returns value from sync function", async () => {
+  const definition = defineNodeType(
+    {
+      a: {
+        type: "string",
+      },
+      b: {
+        type: "string",
+      },
+    },
+    {
+      concat: {
+        type: "string",
+      },
+    },
+    // Synchronous
+    ({ a, b }) => {
+      return {
+        concat: a + b,
+      };
+    }
+  );
+  const result = definition.invoke(
+    { a: "foo", b: "bar" },
+    // Not currently used.
+    null as unknown as NodeHandlerContext
+  );
+  assert(result instanceof Promise);
+  assert.deepEqual(await result, { concat: "foobar" });
+});
+
+test("invoke returns value from async function", async () => {
+  const definition = defineNodeType(
+    {
+      a: {
+        type: "string",
+      },
+      b: {
+        type: "string",
+      },
+    },
+    {
+      concat: {
+        type: "string",
+      },
+    },
+    async ({ a, b }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1));
+      return {
+        concat: a + b,
+      };
+    }
+  );
+  const result = definition.invoke(
+    { a: "foo", b: "bar" },
+    // Not currently used.
+    null as unknown as NodeHandlerContext
+  );
+  assert(result instanceof Promise);
+  assert.deepEqual(await result, { concat: "foobar" });
 });
