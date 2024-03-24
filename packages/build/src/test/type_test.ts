76af583e874d3faeb48ac99e1b821d79a2ec50bf
Joseph Mearman <joseph@mearman.co.uk>
2024-03-24 22:41:02 +0000

    1198 [build] Add array utility to type system (#1199)
    closes https://github.com/breadboard-ai/breadboard/issues/1198

--- packages/build/src/test/type_test.ts
+++ packages/build/src/test/type_test.ts
@@ -4,9 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { anyOf } from "@breadboard-ai/build";
-import { unsafeType } from "@breadboard-ai/build";
-import { object } from "@breadboard-ai/build";
+import { anyOf, array, object, unsafeType } from "@breadboard-ai/build";
 
 import {
   toJSONSchema,
@@ -14,8 +12,8 @@ import {
   type ConvertBreadboardType,
 } from "../internal/type-system/type.js";
 
-import { test } from "node:test";
 import assert from "node:assert/strict";
+import { describe, test } from "node:test";
 
 test("string", () => {
   "string" satisfies BreadboardType;
@@ -184,4 +182,47 @@ test("unsafeType", () => {
   assert.deepEqual(toJSONSchema(strOrNum), {
     anyOf: [{ type: "string" }, { type: "number" }],
   });
+});
+
+describe("array", () => {
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  test("no arguments", () => {
+    // @ts-expect-error no arguments
+    assert.throws(() => array());
+  });
+
+  test("array of numbers", () => {
+    const arr1 = array("number");
+    // $ExpectType number[]
+    type t1 = ConvertBreadboardType<typeof arr1>;
+    assert.deepEqual(toJSONSchema(arr1), {
+      type: "array",
+      items: { type: "number" },
+    });
+  });
+
+  test("array of objects", () => {
+    const arr2 = array(object({ foo: "string" }));
+    // $ExpectType { foo: string; }[]
+    type t2 = ConvertBreadboardType<typeof arr2>;
+    assert.deepEqual(toJSONSchema(arr2), {
+      type: "array",
+      items: {
+        type: "object",
+        properties: { foo: { type: "string" } },
+        required: ["foo"],
+      },
+    });
+  });
+
+  test("array of anyOf types", () => {
+    const arr3 = array(anyOf("string", "number"));
+    // $ExpectType (string | number)[]
+    type t3 = ConvertBreadboardType<typeof arr3>;
+    assert.deepEqual(toJSONSchema(arr3), {
+      type: "array",
+      items: { anyOf: [{ type: "string" }, { type: "number" }] },
+    });
+  });
+  /* eslint-enable @typescript-eslint/no-unused-vars */
 });
