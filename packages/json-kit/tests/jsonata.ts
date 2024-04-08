8b7359eb063ea8e2bf3967ce6a2eb413644f2d41
Joseph Mearman <joseph@mearman.co.uk>
2024-04-08 14:41:03 +0100

    wrap tests in describe blocks

--- packages/json-kit/tests/jsonata.ts
+++ packages/json-kit/tests/jsonata.ts
@@ -5,125 +5,126 @@
  */
 
 import { deepEqual } from "node:assert";
-import test from "node:test";
+import test, { describe } from "node:test";
 import { jsonataDescriber } from "../src/nodes/jsonata.js";
-
-test("`jsonataDescriber` correctly reacts to `raw = false`", async () => {
-  const result = await jsonataDescriber();
-  const expected = {
-    inputSchema: {
-      type: "object",
-      properties: {
-        expression: {
-          title: "expression",
-          description: "The Jsonata expression to evaluate",
-          type: "string",
+describe("jsonata", () => {
+  test("`jsonataDescriber` correctly reacts to `raw = false`", async () => {
+    const result = await jsonataDescriber();
+    const expected = {
+      inputSchema: {
+        type: "object",
+        properties: {
+          expression: {
+            title: "expression",
+            description: "The Jsonata expression to evaluate",
+            type: "string",
+          },
+          raw: {
+            title: "raw",
+            description:
+              "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
+            type: "boolean",
+          },
+          json: {
+            title: "json",
+            description: "The JSON object to evaluate",
+            type: ["object", "string"],
+          },
         },
-        raw: {
-          title: "raw",
-          description:
-            "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
-          type: "boolean",
-        },
-        json: {
-          title: "json",
-          description: "The JSON object to evaluate",
-          type: ["object", "string"],
-        },
+        additionalProperties: false,
+        required: ["expression"],
       },
-      additionalProperties: false,
-      required: ["expression"],
-    },
-    outputSchema: {
-      type: "object",
-      properties: {
-        result: {
-          title: "result",
-          description: "The result of the Jsonata expression",
-          type: "string",
+      outputSchema: {
+        type: "object",
+        properties: {
+          result: {
+            title: "result",
+            description: "The result of the Jsonata expression",
+            type: "string",
+          },
         },
+        required: ["result"],
       },
-      required: ["result"],
-    },
-  };
-  deepEqual(result, expected);
-});
-
-test("`jsonataDescriber` correctly reacts to `raw = true`", async () => {
-  const result = await jsonataDescriber({
-    expression: "foo",
-    json: { foo: { bar: "baz" } },
-    raw: true,
+    };
+    deepEqual(result, expected);
   });
-  const expected = {
-    inputSchema: {
-      type: "object",
-      properties: {
-        expression: {
-          title: "expression",
-          description: "The Jsonata expression to evaluate",
-          type: "string",
-        },
-        raw: {
-          title: "raw",
-          description:
-            "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
-          type: "boolean",
-        },
-        json: {
-          title: "json",
-          description: "The JSON object to evaluate",
-          type: ["object", "string"],
-        },
-      },
-      additionalProperties: false,
-      required: ["expression"],
-    },
-    outputSchema: {
-      type: "object",
-      properties: {
-        bar: {
-          type: "string",
-          title: "bar",
-        },
-      },
-    },
-  };
-  deepEqual(result, expected);
-});
 
-test("`jsonataDescriber` correctly reacts to invalid input", async () => {
-  const result = await jsonataDescriber({
-    raw: true,
-  });
-  const expected = {
-    inputSchema: {
-      type: "object",
-      properties: {
-        expression: {
-          title: "expression",
-          description: "The Jsonata expression to evaluate",
-          type: "string",
+  test("`jsonataDescriber` correctly reacts to `raw = true`", async () => {
+    const result = await jsonataDescriber({
+      expression: "foo",
+      json: { foo: { bar: "baz" } },
+      raw: true,
+    });
+    const expected = {
+      inputSchema: {
+        type: "object",
+        properties: {
+          expression: {
+            title: "expression",
+            description: "The Jsonata expression to evaluate",
+            type: "string",
+          },
+          raw: {
+            title: "raw",
+            description:
+              "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
+            type: "boolean",
+          },
+          json: {
+            title: "json",
+            description: "The JSON object to evaluate",
+            type: ["object", "string"],
+          },
         },
-        raw: {
-          title: "raw",
-          description:
-            "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
-          type: "boolean",
-        },
-        json: {
-          title: "json",
-          description: "The JSON object to evaluate",
-          type: ["object", "string"],
+        additionalProperties: false,
+        required: ["expression"],
+      },
+      outputSchema: {
+        type: "object",
+        properties: {
+          bar: {
+            type: "string",
+            title: "bar",
+          },
         },
       },
-      additionalProperties: false,
-      required: ["expression"],
-    },
-    outputSchema: {
-      type: "object",
-      properties: {},
-    },
-  };
-  deepEqual(result, expected);
+    };
+    deepEqual(result, expected);
+  });
+
+  test("`jsonataDescriber` correctly reacts to invalid input", async () => {
+    const result = await jsonataDescriber({
+      raw: true,
+    });
+    const expected = {
+      inputSchema: {
+        type: "object",
+        properties: {
+          expression: {
+            title: "expression",
+            description: "The Jsonata expression to evaluate",
+            type: "string",
+          },
+          raw: {
+            title: "raw",
+            description:
+              "Whether or not to return use the evaluation result as raw output (true) or as a port called `result` (false). Default is false.",
+            type: "boolean",
+          },
+          json: {
+            title: "json",
+            description: "The JSON object to evaluate",
+            type: ["object", "string"],
+          },
+        },
+        additionalProperties: false,
+        required: ["expression"],
+      },
+      outputSchema: {
+        type: "object",
+        properties: {},
+      },
+    };
+    deepEqual(result, expected);
+  });
 });
--- packages/json-kit/tests/validate-json.ts
+++ packages/json-kit/tests/validate-json.ts
@@ -4,84 +4,86 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import test from "node:test";
+import { deepStrictEqual } from "node:assert";
+import test, { describe } from "node:test";
 import {
+  stripCodeBlock,
   tryParseJson,
   validateJson,
-  stripCodeBlock,
 } from "../src/nodes/validate-json.js";
-import { deepStrictEqual } from "node:assert";
 
-test("tryParseJson correctly parses JSON", (t) => {
-  const json = `{"foo": "bar"}`;
-  const result = tryParseJson(json);
-  deepStrictEqual(result, { foo: "bar" });
-});
-
-test("tryParseJson correctly returns an error for unparsable JSON", (t) => {
-  const json = `{"foo": bar}`;
-  const result = tryParseJson(json);
-  deepStrictEqual(result, {
-    $error: {
-      kind: "error",
-      error: {
-        type: "parsing",
-        message: 'Unexpected token \'b\', "{"foo": bar}" is not valid JSON',
-      },
-    },
-  });
-});
-
-test("validateJson correctly validates JSON", (t) => {
-  const parsed = { foo: "bar" };
-  const schema = {
-    type: "object",
-    properties: { foo: { type: "string" } },
-  };
-  const result = validateJson(parsed, schema);
-  deepStrictEqual(result, { json: { foo: "bar" } });
-});
-
-test("validateJson correctly returns an error for invalid JSON", (t) => {
-  const parsed = { foo: "bar" };
-  const schema = {
-    type: "object",
-    properties: { foo: { type: "number" } },
-  };
-  const result = validateJson(parsed, schema);
-  deepStrictEqual(result, {
-    $error: {
-      kind: "error",
-      error: {
-        type: "validation",
-        message: "data/foo must be number",
-      },
-    },
-  });
-});
-
-test("stripCodeBlock correctly strips Markdown only if present", (t) => {
-  deepStrictEqual(stripCodeBlock('```json\n"json"\n```'), '"json"');
-  deepStrictEqual(stripCodeBlock('```\n"json"\n```'), '"json"');
-  deepStrictEqual(stripCodeBlock('"json"'), '"json"');
-});
-
-test("tryParseJson correctly parses JSON with Markdown code block", (t) => {
-  const json = '```json\n{"foo": "bar"}\n```';
-  const result = tryParseJson(json);
-  deepStrictEqual(result, { foo: "bar" });
-});
-
-test("tryParseJson correctly strips stuff outside of the Markdown code block", (t) => {
-  {
-    const json = 'bar```json\n{"foo": "bar"}\n```\nfooo';
+describe("validate-json", () => {
+  test("tryParseJson correctly parses JSON", (t) => {
+    const json = `{"foo": "bar"}`;
     const result = tryParseJson(json);
     deepStrictEqual(result, { foo: "bar" });
-  }
-  {
-    const json =
-      'sure, here is the JSON you requested:\n\n```json\n{"foo": "bar"}\n```\n\nAdditionally, here is an alternative version of this JSON:\n\n```json\n{"bar": "baz"}\n```\n\nI hope this helps!';
+  });
+
+  test("tryParseJson correctly returns an error for unparsable JSON", (t) => {
+    const json = `{"foo": bar}`;
     const result = tryParseJson(json);
-    deepStrictEqual(result, { bar: "baz" });
-  }
+    deepStrictEqual(result, {
+      $error: {
+        kind: "error",
+        error: {
+          type: "parsing",
+          message: 'Unexpected token \'b\', "{"foo": bar}" is not valid JSON',
+        },
+      },
+    });
+  });
+
+  test("validateJson correctly validates JSON", (t) => {
+    const parsed = { foo: "bar" };
+    const schema = {
+      type: "object",
+      properties: { foo: { type: "string" } },
+    };
+    const result = validateJson(parsed, schema);
+    deepStrictEqual(result, { json: { foo: "bar" } });
+  });
+
+  test("validateJson correctly returns an error for invalid JSON", (t) => {
+    const parsed = { foo: "bar" };
+    const schema = {
+      type: "object",
+      properties: { foo: { type: "number" } },
+    };
+    const result = validateJson(parsed, schema);
+    deepStrictEqual(result, {
+      $error: {
+        kind: "error",
+        error: {
+          type: "validation",
+          message: "data/foo must be number",
+        },
+      },
+    });
+  });
+
+  test("stripCodeBlock correctly strips Markdown only if present", (t) => {
+    deepStrictEqual(stripCodeBlock('```json\n"json"\n```'), '"json"');
+    deepStrictEqual(stripCodeBlock('```\n"json"\n```'), '"json"');
+    deepStrictEqual(stripCodeBlock('"json"'), '"json"');
+  });
+
+  test("tryParseJson correctly parses JSON with Markdown code block", (t) => {
+    const json = '```json\n{"foo": "bar"}\n```';
+    const result = tryParseJson(json);
+    deepStrictEqual(result, { foo: "bar" });
+  });
+
+  test("tryParseJson correctly strips stuff outside of the Markdown code block", (t) => {
+    {
+      const json = 'bar```json\n{"foo": "bar"}\n```\nfooo';
+      const result = tryParseJson(json);
+      deepStrictEqual(result, { foo: "bar" });
+    }
+    {
+      const json =
+        'sure, here is the JSON you requested:\n\n```json\n{"foo": "bar"}\n```\n\nAdditionally, here is an alternative version of this JSON:\n\n```json\n{"bar": "baz"}\n```\n\nI hope this helps!';
+      const result = tryParseJson(json);
+      deepStrictEqual(result, { bar: "baz" });
+    }
+  });
 });
