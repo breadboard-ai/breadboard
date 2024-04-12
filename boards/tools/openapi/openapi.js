fde82dc049dde9f40fb85445c46fc6cd7df77d20
Paul Kinlan <paul.kinlan@gmail.com>
2024-04-12 19:44:37 +0100

    cleanip

--- boards/tools/openapi/openapi.js
+++ boards/tools/openapi/openapi.js
@@ -455,10 +455,6 @@ const createSpecBoard = board((apiSpec) => {
 const convertBoardListToObject = code(({ list }) => {
   const operations = list
     .map((item) => {
-      item.board.title = item.item.operationId;
-      item.board.description = item.item.description;
-      item.board.version = "0.0.3";
-
       item.board.board.title = item.item.operationId;
       item.board.board.description = item.item.description;
       item.board.board.version = "0.0.3";
