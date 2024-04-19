b11cf10640cdaf2fd9b89ec54d0d6e0ef07c9e58
Paul Lewis <617438+paullewis@users.noreply.github.com>
2024-04-19 17:41:11 +0100

    [breadboard-web] Fix audio board

--- packages/breadboard-web/src/boards/audio.ts
+++ packages/breadboard-web/src/boards/audio.ts
@@ -7,11 +7,11 @@
 import { board, code } from "@google-labs/breadboard";
 import { gemini } from "@google-labs/gemini-kit";
 
-// A node that appends the prompt to the picture.
+// A node that appends the prompt to the audio.
 // Note, this one is a bit "in the weeds": it literally formats the Gemini Pro
-// API request to include the picture as part of the prompt.
+// API request to include the audio as part of the prompt.
 const partsMaker = code(({ audio, prompt }) => {
-  return { parts: [audio, { text: prompt }] };
+  return { context: { parts: [audio, { text: prompt }] } };
 });
 
 export default await board(({ audio, prompt }) => {
@@ -22,7 +22,7 @@ export default await board(({ audio, prompt }) => {
     .examples(
       "Describe what you hear in the audio. Please respond in markdown"
     );
-  const { parts } = partsMaker({
+  const { context } = partsMaker({
     $id: "combineAudioAndPrompt",
     audio,
     prompt,
@@ -31,7 +31,7 @@ export default await board(({ audio, prompt }) => {
     $id: "describeAudio",
     model: "gemini-1.5-pro-latest",
     text: "unused",
-    context: parts,
+    context,
   });
   return { text: describeAudio.text };
 }).serialize({
