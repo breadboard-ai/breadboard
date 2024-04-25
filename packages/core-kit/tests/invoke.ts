2097358f792bb5917fec89b3598b616733a536c7
Al Marks <aomarks@google.com>
2024-04-25 15:09:47 -0700

    Add tests for invoke describe

---
+++ packages/core-kit/tests/invoke.ts
@@ -0,0 +1,182 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import test from "ava";
+import handler from "../src/nodes/invoke.js";
+
+test("describe with no values and no context.base", async (t) => {
+  t.deepEqual(await handler.describe(), {
+    inputSchema: {
+      additionalProperties: false,
+      properties: {
+        $board: {
+          behavior: ["board"],
+          description:
+            "The board to invoke. Can be a BoardCapability, a graph or a URL",
+          title: "board",
+          type: "object",
+        },
+        path: {
+          behavior: ["deprecated"],
+          description: "The path to the board to invoke.",
+          title: "path",
+          type: "string",
+        },
+      },
+      type: "object",
+    },
+    outputSchema: {
+      additionalProperties: false,
+      properties: {},
+      type: "object",
+    },
+  });
+});
+
+test("describe with context.base and no values", async (t) => {
+  t.deepEqual(
+    await handler.describe(undefined, undefined, undefined, {
+      base: new URL("http://example.com/"),
+      outerGraph: { nodes: [], edges: [] },
+    }),
+    {
+      inputSchema: {
+        properties: {
+          $board: {
+            behavior: ["board"],
+            description:
+              "The board to invoke. Can be a BoardCapability, a graph or a URL",
+            title: "board",
+            type: "object",
+          },
+          path: {
+            behavior: ["deprecated"],
+            description: "The path to the board to invoke.",
+            title: "path",
+            type: "string",
+          },
+        },
+        type: "object",
+      },
+      outputSchema: {
+        properties: {},
+        type: "object",
+      },
+    }
+  );
+});
+
+test("describe with context.base and invalid $board", async (t) => {
+  t.deepEqual(
+    await handler.describe({ $board: "invalid.json" }, undefined, undefined, {
+      base: new URL("file://invalid/base"),
+      outerGraph: { nodes: [], edges: [] },
+    }),
+    {
+      inputSchema: {
+        properties: {
+          $board: {
+            behavior: ["board"],
+            description:
+              "The board to invoke. Can be a BoardCapability, a graph or a URL",
+            title: "board",
+            type: "object",
+          },
+          path: {
+            behavior: ["deprecated"],
+            description: "The path to the board to invoke.",
+            title: "path",
+            type: "string",
+          },
+        },
+        type: "object",
+      },
+      outputSchema: {
+        properties: {},
+        type: "object",
+      },
+    }
+  );
+});
+
+test("describe with context.base and valid $board", async (t) => {
+  t.deepEqual(
+    await handler.describe(
+      {
+        $board: {
+          nodes: [
+            {
+              type: "input",
+              id: "input-1",
+              configuration: {
+                schema: {
+                  type: "object",
+                  properties: {
+                    invokedBoardInput: { type: "string" },
+                  },
+                  required: ["invokedBoardInput"],
+                },
+              },
+            },
+            {
+              type: "output",
+              id: "output-1",
+              configuration: {
+                schema: {
+                  type: "object",
+                  properties: {
+                    invokedBoardOutput: { type: "number" },
+                  },
+                  required: ["invokedBoardOutput"],
+                },
+              },
+            },
+          ],
+          edges: [],
+        },
+      },
+      undefined,
+      undefined,
+      {
+        base: new URL("http://example.com/"),
+        outerGraph: { nodes: [], edges: [] },
+      }
+    ),
+    {
+      inputSchema: {
+        type: "object",
+        properties: {
+          $board: {
+            behavior: ["board"],
+            description:
+              "The board to invoke. Can be a BoardCapability, a graph or a URL",
+            title: "board",
+            type: "object",
+          },
+          path: {
+            behavior: ["deprecated"],
+            description: "The path to the board to invoke.",
+            title: "path",
+            type: "string",
+          },
+          invokedBoardInput: {
+            type: "string",
+          },
+        },
+        required: ["invokedBoardInput"],
+      },
+      outputSchema: {
+        type: "object",
+        properties: {
+          invokedBoardOutput: {
+            type: "number",
+          },
+        },
+        additionalProperties: false,
+      },
+    }
+  );
+});
