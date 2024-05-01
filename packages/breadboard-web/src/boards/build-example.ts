c409b8de189511069f123da02a374beb66e21d37
Al Marks <aomarks@google.com>
2024-05-01 12:20:20 -0700

    Update the build example to demonstrate code

--- packages/breadboard-web/src/boards/build-example.ts
+++ packages/breadboard-web/src/boards/build-example.ts
@@ -5,17 +5,24 @@
  */
 
 import { board, input } from "@breadboard-ai/build";
+import { code } from "@google-labs/core-kit";
 import { prompt } from "@google-labs/template-kit";
 import { reverseString } from "../build-example-kit.js";
 
 const word = input({ description: "The word to reverse" });
 const reversed = reverseString({ forwards: word });
-const result = prompt`The word "${word}" is "${reversed}" in reverse`;
+const capitalizer = code(
+  { str: word },
+  { capitalized: "string" },
+  ({ str }) => ({ capitalized: str.toUpperCase() })
+);
+const result = prompt`The word "${word}" is "${reversed}" in reverse
+and "${capitalizer.outputs.capitalized}" when capitalized.`;
 
 export default board({
   title: "Example of @breadboard-ai/build",
   description: "A simple example of using the @breadboard-ai/build API",
-  version: "1.0.0",
+  version: "1.1.0",
   inputs: { word },
   outputs: { result },
 });
