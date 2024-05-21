4511418c9577c5f83e89028b7845f0003b9ac365
Dimitri Glazkov <dglazkov@google.com>
2024-05-21 15:11:57 -0700

    Simplest possible server.

--- packages/board-server/src/index.ts
+++ packages/board-server/src/index.ts
@@ -1,7 +1,20 @@
 /**
  * @license
- * Copyright 2023 Google LLC
+ * Copyright 2024 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 
-console.log("code goes here");
+import { createServer } from "http";
+import { env } from "process";
+
+const PORT = env.PORT || 3000;
+const HOST = env.HOST || "localhost";
+
+const server = createServer(async (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, world!");
+});
+
+server.listen(PORT, () => {
+  console.info(`Running on "http://${HOST}:${PORT}"...`);
+});
