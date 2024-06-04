335c8aa7eff711fcd55e2ec23d198d7b1e4395c3
Dimitri Glazkov <dimitri@glazkov.com>
2024-06-04 16:33:53 -0700

    add `api.html` to vitejs build config.

--- packages/board-server/vite.config.ts
+++ packages/board-server/vite.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   optimizeDeps: { esbuildOptions: { target: "esnext" } },
-  build: { target: "esnext", outDir: "./dist/client" },
+  build: {
+    target: "esnext",
+    outDir: "./dist/client",
+    lib: {
+      entry: {
+        index: "./index.html",
+        api: "./api.html",
+      },
+      formats: ["es"],
+    },
+  },
 });
