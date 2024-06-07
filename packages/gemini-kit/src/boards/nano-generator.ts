adef1018025c6862b86e20101756df5b4180e8dc
Dimitri Glazkov <dglazkov@google.com>
2024-06-07 15:34:12 -0700

    Throw an error when Prompt API is not available.

--- packages/gemini-kit/src/boards/nano-generator.ts
+++ packages/gemini-kit/src/boards/nano-generator.ts
@@ -4,19 +4,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { board, input } from "@breadboard-ai/build";
+import { board, input, output } from "@breadboard-ai/build";
 import { code } from "@google-labs/core-kit";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var ai: {
+    createTextSession: () => Promise<{
+      prompt: (text: string) => Promise<string>;
+    }>;
+    canCreateTextSession: () => Promise<boolean>;
+  };
+}
 
 const prompt = input({
   title: "Prompt",
+  description: "The prompt to generate text from",
 });
 
-const { text } = code({ prompt }, { text: "string" }, async ({ prompt }) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const session = await (globalThis as any).ai.createTextSession();
-  const text = (await session.prompt(prompt)) as string;
-  return { text };
-}).outputs;
+const { text } = code(
+  {
+    $metadata: {
+      title: "Call Prompt API",
+      description: "Invoking the Prompt API to generate text from a prompt",
+    },
+    prompt,
+  },
+  { text: "string" },
+  async ({ prompt }) => {
+    const ERROR_MESSAGE =
+      "Prompt API is not available. For more information, see https://developer.chrome.com/docs/ai/built-in.";
+
+    const ai = globalThis.ai;
+    if (!ai) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    const canAI = await ai.canCreateTextSession();
+    if (!canAI) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    const session = await ai.createTextSession();
+    const text = (await session.prompt(prompt)) as string;
+    return { text };
+  }
+).outputs;
 
 export default board({
   title: "Gemini Nano",
@@ -25,5 +56,10 @@ export default board({
     icon: "nano",
   },
   inputs: { prompt },
-  outputs: { text },
+  outputs: {
+    text: output(text, {
+      title: "Text",
+      description: "The generated text",
+    }),
+  },
 });
