de8edd86f726d17163446999c215a67b832a9233
Joseph Mearman <joseph@mearman.co.uk>
2024-06-14 11:16:06 +0100

    feat: Update Hacker News Simplified Algolia Search board to manipulate elements

--- packages/breadboard-web/src/boards/hacker-news-simplified-algolia-search.ts
+++ packages/breadboard-web/src/boards/hacker-news-simplified-algolia-search.ts
@@ -7,14 +7,15 @@
  * see: https://hn.algolia.com/api
  */
 
-import { base } from "@google-labs/breadboard";
+import { base, board, code } from "@google-labs/breadboard";
 import { core } from "@google-labs/core-kit";
+import { graph as forEach } from "./board-for-each";
 import {
-  HackerNewsSearchResultsSchema,
   graph as search,
   searchQuerySchema,
   searchTagsSchema,
 } from "./hacker-news-algolia-search";
+import { graph as manipulator } from "./object-manipulator";
 
 const input = base.input({
   schema: {
@@ -36,10 +37,145 @@ const invocation = core.invoke({
 
 const output = base.output({
   $metadata: { title: "Output" },
-  schema: HackerNewsSearchResultsSchema,
+  schema: {
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        author: {
+          type: "string",
+        },
+        created_at: {
+          type: "string",
+        },
+        num_comments: {
+          type: "number",
+        },
+        objectID: {
+          type: "string",
+        },
+        points: {
+          type: "number",
+        },
+        story_id: {
+          type: "number",
+        },
+        title: {
+          type: "string",
+        },
+        updated_at: {
+          type: "string",
+        },
+        url: {
+          type: "string",
+        },
+        type: {
+          type: "string",
+        },
+      },
+      required: [
+        "author",
+        "created_at",
+        "num_comments",
+        "objectID",
+        "points",
+        "story_id",
+        "title",
+        "updated_at",
+        "url",
+        "objectType",
+      ],
+    },
+  },
 });
 
-invocation.output.to(output);
+export interface SearchResult {
+  _highlightResult: HighlightResult;
+  _tags: string[];
+  author: string;
+  children: number[];
+  created_at: string;
+  created_at_i: number;
+  num_comments: number;
+  objectID: string;
+  points: number;
+  story_id: number;
+  title: string;
+  updated_at: string;
+  url: string;
+}
+
+export interface HighlightResult {
+  author: Author;
+  title: Title;
+  url: Url;
+}
+
+export interface Author {
+  matchLevel: string;
+  matchedWords: any[];
+  value: string;
+}
+
+export interface Title {
+  fullyHighlighted: boolean;
+  matchLevel: string;
+  matchedWords: string[];
+  value: string;
+}
+
+export interface Url {
+  matchLevel: string;
+  matchedWords: string[];
+  value: string;
+  fullyHighlighted?: boolean;
+}
+
+const invokeForEach = core.invoke({
+  $board: forEach,
+  board: board(() => {
+    const input = base.input({});
+    const output = base.output({});
+    const manipulate = core.invoke({
+      $board: manipulator,
+      mode: "pick",
+      keys: [
+        // "_highlightResult",
+        // "_tags",
+        "author",
+        // "children",
+        "created_at",
+        // "created_at_i",
+        "num_comments",
+        "objectID",
+        "points",
+        "story_id",
+        "title",
+        "updated_at",
+        "url",
+        "objectType",
+      ],
+    });
+    const convertTagsToType = code(({ item }: { item: SearchResult }) => {
+      return {
+        item: {
+          ...item,
+          objectType: item["_tags"][0],
+        },
+      };
+    });
+
+    input.item.to(convertTagsToType({})).item.as("object").to(manipulate);
+
+    manipulate.object.as("item").to(output);
+
+    return output;
+  }),
+  array: invocation.output,
+  $metadata: { title: "Manipulate elements" },
+});
+
+invokeForEach.array.as("output").to(output);
 
 const serialised = await output.serialize({
   title: "Hacker News Simplified Algolia Search",
