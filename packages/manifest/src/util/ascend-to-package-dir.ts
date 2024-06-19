9e7c6fc0f91afc2c62df946ab1d2514692a25e75
Joseph Mearman <joseph@mearman.co.uk>
2024-06-19 19:36:23 +0100

    2215 Create board manifest specification (#2216)
    https://github.com/breadboard-ai/breadboard/issues/2215

---
+++ packages/manifest/src/util/ascend-to-package-dir.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+
+
+export function ascendToPackageDir(packageName: string = "breadboard-ai") {
+  let directory = import.meta.dirname;
+  while (directory !== "/") {
+    const packageJsonPath = path.join(directory, "package.json");
+    if (fs.existsSync(packageJsonPath)) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+      if (packageJson.name === packageName) {
+        return directory;
+      }
+    }
+    directory = path.dirname(directory);
+  }
+  throw new Error("Could not find breadboard-ai directory.");
+}
