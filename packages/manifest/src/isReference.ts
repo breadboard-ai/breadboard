4a36b24df09606e5d320d31ff9ecf534c9513a68
Joseph Mearman <joseph@mearman.co.uk>
2024-07-01 13:38:19 +0100

    add copyright header

--- packages/manifest/src/isReference.ts
+++ packages/manifest/src/isReference.ts
@@ -1,5 +1,10 @@
-import { ResourceReference } from ".";
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+import { ResourceReference } from ".";
 
 export const isReference = (value: unknown): value is ResourceReference => {
   return (
