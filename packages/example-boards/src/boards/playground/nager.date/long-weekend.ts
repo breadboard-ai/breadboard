3c857a319ae99bffe5ed0cc71328c90727f60cb8
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-16 14:34:57 +0100

    Migrated Long Weekend board

--- packages/example-boards/src/boards/playground/nager.date/long-weekend.ts
+++ packages/example-boards/src/boards/playground/nager.date/long-weekend.ts
@@ -1,58 +1,44 @@
-import { base, code } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
+import { board, enumeration, input, output } from "@breadboard-ai/build";
+import { fetch } from "@google-labs/core-kit";
+import { urlTemplate } from "@google-labs/template-kit";
 import { countryCodes } from "../../../utils/countryCodes";
 
-const getCurrentYear = code((): { year: number } => {
-  return {
-    year: new Date().getFullYear(),
-  };
+const countryCode = input({
+  title: "countryCode",
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type: enumeration(...countryCodes as any),
+  description: "The data for countryCode",
 });
 
-const input = base.input({
-  $id: "query",
-  year: getCurrentYear({ $id: "getCurrentYear" }).year,
-  schema: {
-    type: "object",
-    properties: {
-      year: {
-        title: "year",
-        type: "number",
-        description: "The data for year",
-        default: new Date().getFullYear().toString(),
-      },
-      countryCode: {
-        title: "countryCode",
-        type: "string",
-        description: "The data for countryCode",
-        enum: countryCodes,
-        default: "US",
-      },
-    },
-    required: ["year", "countryCode"],
-  },
+const year = input({
+  title: "year",
+  type: "string", // TODO: Change this to "number" type when the Template Kit's `urlTemplate` node has been updated to accept number type wildcard inputs.
+  description: "The data for year",
+  default: new Date().getFullYear().toString(),
 });
 
-const urlTemplate = templates.urlTemplate({
-  $id: "urlTemplate",
+const url = urlTemplate({
+  $id: "url",
   template: "https://date.nager.at/api/v3/LongWeekend/{year}/{countryCode}",
-  year: input.year,
-  countryCode: input.countryCode,
+  year: year,
+  countryCode: countryCode,
 });
 
-const fetchUrl = core.fetch({
-  $id: "fetch",
+const fetchResult = fetch({
+  $id: "fetchResult",
   method: "GET",
-  url: urlTemplate.url,
+  url: url.outputs.url,
 });
 
-const output = base.output({
-  $id: "output",
-  dates: fetchUrl.response,
+const info = output(fetchResult.outputs.response, {
+  title: "Long Weekend Info",
+  description: "The long weeekend info for the selected country code for the given year from the Nager Date API"
 });
 
-export default await output.serialize({
+export default board({
   title: "Nager Date Long Weekend API",
   description: "API for long weekends",
-  version: "0.0.1",
+  version: "0.1.0",
+  inputs: { countryCode, year },
+  outputs: { info }
 });
