9c2308b095b8f2b1a3c685f12e1e2cc86657ad06
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-16 15:29:00 +0100

    Migrated 'is today a public holiday' board

--- packages/example-boards/src/boards/playground/nager.date/is-today-public-holiday.ts
+++ packages/example-boards/src/boards/playground/nager.date/is-today-public-holiday.ts
@@ -1,54 +1,57 @@
-import { base, code } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
+import { board, enumeration, input, output } from "@breadboard-ai/build";
+import { fetch, code } from "@google-labs/core-kit";
+import { urlTemplate } from "@google-labs/template-kit";
 import { countryCodes } from "../../../utils/countryCodes";
 
-const inputs = base.input({
-  $id: "query",
-  schema: {
-    type: "object",
-    properties: {
-      countryCode: {
-        title: "countryCode",
-        type: "string",
-        description: "The data for countryCode",
-        enum: countryCodes,
-        default: "US",
-      },
-      offset: {
-        title: "offset",
-        type: "number",
-        description: "utc timezone offset",
-        maximum: 12,
-        minimum: -12,
-        default: "0",
-      },
-    },
-    required: ["year", "countryCode"],
+const countryCode = input({
+  title: "countryCode",
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type: enumeration(...countryCodes as any),
+  description: "The data for countryCode",
+});
+
+const offset = input({
+  title: "offset",
+  type: "string", // TODO: Change this to "number" type when the Template Kit's `urlTemplate` node has been updated to accept number type wildcard inputs.
+  description: "utc timezone offset",
+  default: "0",
+});
+
+const validatedOffset = code(
+  {
+    $id: "validatedOffset",
+    offsetString: offset
   },
-});
+  { offset: "string" },
+  ({ offsetString }) => {
+    const offset = Number(offsetString);
+    if (isNaN(offset) || offset > 12 || offset < -12) {
+      throw new Error(`Invalid offset input: ${offsetString}. Offset must be maximum 12 and minimum -12.`);
+    }
+    return { offset: offsetString }
+  });
 
-const urlTemplate = templates.urlTemplate({
+const url = urlTemplate({
   $id: "urlTemplate",
-  template:
-    "https://date.nager.at/Api/v3/IsTodayPublicHoliday/{countryCode}?{&offset}",
-  countryCode: inputs.countryCode,
-  offset: inputs.offset,
+  template: "https://date.nager.at/Api/v3/IsTodayPublicHoliday/{countryCode}?{&offset}",
+  countryCode: countryCode,
+  offset: validatedOffset.outputs.offset,
 });
 
-const fetchUrl = core.fetch({
+const fetchResult = fetch({
   $id: "fetch",
   raw: true,
   method: "GET",
-  url: urlTemplate.url,
+  url: url.outputs.url,
 });
 
 const statusCodeToResult = code(
-  ({
-    status,
-  }: {
-    status: number;
-  }): {
+  {
+    $id: "statusCodeToResult",
+    status: fetchResult.outputs.status
+  },
+  { isPublicHoliday: "boolean" },
+  ({ status }: { status: number; }): {
     isPublicHoliday: boolean;
   } => {
     if (status === 200) {
@@ -63,26 +66,16 @@ const statusCodeToResult = code(
     throw new Error(`Unexpected status code: ${status}`);
   }
 );
-const result = statusCodeToResult({
-  $id: "statusCodeToResult",
-  status: fetchUrl.status,
-});
-const output = base.output({
-  $id: "output",
-  isPublicHoliday: result.isPublicHoliday,
-  schema: {
-    type: "object",
-    properties: {
-      isPublicHoliday: {
-        type: "boolean",
-      },
-    },
-    required: ["isPublicHoliday"],
-  },
+
+const isPublicHoliday = output(statusCodeToResult.outputs.isPublicHoliday, {
+  title: "Public Holiday Result",
+  description: "A boolean indicating if today is a bank holiday for the given country code from the Nager Date API",
 });
 
-export default await output.serialize({
+export default await board({
   title: "Is Today a Public Holiday",
   description: "Get the public holidays for today for the Nager Date API",
-  version: "0.0.1",
+  version: "0.1.0",
+  inputs: { countryCode, offset },
+  outputs: { isPublicHoliday }
 });
