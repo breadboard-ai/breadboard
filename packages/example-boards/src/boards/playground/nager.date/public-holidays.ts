0ed9d217f78536f77cb13af63c3a325e07e630af
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-16 14:43:42 +0100

    Migrated public holidays board

--- packages/example-boards/src/boards/playground/nager.date/public-holidays.ts
+++ packages/example-boards/src/boards/playground/nager.date/public-holidays.ts
@@ -1,93 +1,77 @@
-import { base } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
+import { anyOf, array, board, enumeration, input, object, output } from "@breadboard-ai/build";
+import { code, fetch } from "@google-labs/core-kit";
+import { urlTemplate } from "@google-labs/template-kit";
 import { countryCodes } from "../../../utils/countryCodes";
 
-const inputs = base.input({
-  $id: "query",
-  schema: {
-    type: "object",
-    properties: {
-      year: {
-        title: "year",
-        type: "number",
-        description: "The data for year",
-        default: new Date().getFullYear().toString(),
-      },
-      countryCode: {
-        title: "countryCode",
-        type: "string",
-        description: "The data for countryCode",
-        enum: countryCodes,
-        default: "US",
-      },
-    },
-    required: ["year", "countryCode"],
-  },
+const countryCode = input({
+  title: "countryCode",
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type: enumeration(...countryCodes as any),
+  description: "The data for countryCode",
 });
 
-const urlTemplate = templates.urlTemplate({
-  $id: "urlTemplate",
+const year = input({
+  title: "year",
+  type: "string", // TODO: Change this to "number" type when the Template Kit's `urlTemplate` node has been updated to accept number type wildcard inputs.
+  description: "The data for year",
+  default: new Date().getFullYear().toString(),
+});
+
+const url = urlTemplate({
+  $id: "url",
   template: "https://date.nager.at/api/v3/PublicHolidays/{year}/{countryCode}",
-  year: inputs.year,
-  countryCode: inputs.countryCode,
+  year: year,
+  countryCode: countryCode,
 });
 
-const fetchUrl = core.fetch({
-  $id: "fetch",
+const fetchResult = fetch({
+  $id: "fetchResult",
   method: "GET",
-  url: urlTemplate.url,
+  url: url.outputs.url,
 });
 
-const output = base.output({
-  $id: "output",
-  dates: fetchUrl.response,
-  schema: {
-    type: "array",
-    items: {
-      type: "object",
-      properties: {
-        date: {
-          type: "string",
-          format: "date",
-        },
-        localName: {
-          type: "string",
-        },
-        name: {
-          type: "string",
-        },
-        countryCode: {
-          type: "string",
-        },
-        fixed: {
-          type: "boolean",
-        },
-        global: {
-          type: "boolean",
-        },
-        counties: {
-          type: "array",
-          items: {
-            type: "string",
-          },
-        },
-        launchYear: {
-          type: "integer",
-        },
-        type: {
-          type: "array",
-          items: {
-            type: "string",
-          },
-        },
-      },
+const spreadNagerDateResponse = code(
+  {
+    $id: "spreadResponse",
+    $metadata: {
+      title: "Spread",
+      description: "Spread the properties of the Nager Date response",
     },
+    obj: fetchResult.outputs.response
   },
+  {
+    results: array(
+      object({
+        date: "string",
+        localName: "string",
+        name: "string",
+        countryCode: "string",
+        fixed: "boolean",
+        global: "boolean",
+        counties: anyOf(array("string"), "null"),
+        launchYear: anyOf("number", "null"),
+        types: array("string"),
+      }))
+  },
+  ({ obj }) => {
+    if (typeof obj !== "object") {
+      throw new Error(`object is of type ${typeof obj} not object`);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return { results: { ...obj } } as any;
+  }
+);
+
+const dates = output(
+  spreadNagerDateResponse.outputs.results, {
+  title: "Public Holidays",
+  description: "A list of public holidays for the selected country code and for the given year from the Nager Date API",
 });
 
-export default await output.serialize({
+export default board({
   title: "Nager Date Public Holidays API",
   description: "Get the public holidays for the Nager Date API",
-  version: "0.0.1",
+  version: "0.1.0",
+  inputs: { countryCode, year },
+  outputs: { dates }
 });
