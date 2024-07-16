55d30b432ccb8faa0c624e4ae39982a10aa912e7
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-16 10:42:20 +0100

    Migrated Available Countries

--- packages/example-boards/src/boards/playground/nager.date/available-countries.ts
+++ packages/example-boards/src/boards/playground/nager.date/available-countries.ts
@@ -1,19 +1,25 @@
-import { base } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
+import { board, output } from "@breadboard-ai/build";
+import { fetch } from "@google-labs/core-kit";
 
-const fetchUrl = core.fetch({
-  $id: "fetch",
-  method: "GET",
+const fetchResult = fetch({
+  $metadata: {
+    $id: "fetch",
+    title: "API Fetch Results",
+    description: "Calling the Nager Date API to get the the available countries",
+  },
   url: "https://date.nager.at/api/v3/AvailableCountries",
+  method: "GET"
 });
 
-const output = base.output({
-  $id: "output",
-  dates: fetchUrl.response,
+const countries = output(fetchResult.outputs.response, {
+  title: "Available Countries",
+  description: "A list of available countries",
 });
 
-export default await output.serialize({
+export default board({
   title: "Nager Date Available Countries API",
   description: "Get the available countries for the Nager Date API",
-  version: "0.0.1",
+  version: "0.1.0",
+  inputs: { },
+  outputs: { countries },
 });
