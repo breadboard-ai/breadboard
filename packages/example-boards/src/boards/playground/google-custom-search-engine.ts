f87b57614e371bfbb0efb763dce6f443ca62b89e
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-19 15:29:33 +0100

    WIP migration to build api

--- packages/example-boards/src/boards/playground/google-custom-search-engine.ts
+++ packages/example-boards/src/boards/playground/google-custom-search-engine.ts
@@ -3,12 +3,9 @@
  * Copyright 2024 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-
-import { GraphInlineMetadata, Schema, base } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
-import { spread } from "../../utils/spread";
-import { pop } from "../../utils/pop";
+import { anyOf, array, board, enumeration, input, object, optional, output } from "@breadboard-ai/build";
+import { code, fetch, secret } from "@google-labs/core-kit";
+import { urlTemplate } from "@google-labs/template-kit";
 
 const PARAM = {
   QUERY: "query",
@@ -22,292 +19,246 @@ const PARAM = {
   },
 } as const;
 
-const input = base.input({
-  $metadata: {
-    title: "Input Parameters",
-  },
-  $id: "input",
-  schema: {
-    type: "object",
-    properties: {
-      [PARAM.QUERY]: {
-        type: "string",
-        title: "Query",
-        description: "What would you like to search for?",
-        default: "Google Breadboard",
-        examples: [
-          "Google Breadboard",
-          "Google Custom Search Engine",
-          "Google Gemini",
-        ],
-      },
-      [PARAM.CSE.NUM]: {
-        description: [
-          `Number of search results to return`,
-          `Valid values are integers between 1 and 10, inclusive.`,
-        ].join("\n"),
-        title: "Number of results",
-        type: "integer",
-        default: "3",
-      },
-      [PARAM.CSE.LANG]: {
-        title: "Language",
-        description: "Search language",
-        type: "string",
-        enum: [
-          "lang_ar",
-          "lang_bg",
-          "lang_ca",
-          "lang_cs",
-          "lang_da",
-          "lang_de",
-          "lang_el",
-          "lang_en",
-          "lang_es",
-          "lang_et",
-          "lang_fi",
-          "lang_fr",
-          "lang_hr",
-          "lang_hu",
-          "lang_id",
-          "lang_is",
-          "lang_it",
-          "lang_iw",
-          "lang_ja",
-          "lang_ko",
-          "lang_lt",
-          "lang_lv",
-          "lang_nl",
-          "lang_no",
-          "lang_pl",
-          "lang_pt",
-          "lang_ro",
-          "lang_ru",
-          "lang_sk",
-          "lang_sl",
-          "lang_sr",
-          "lang_sv",
-          "lang_tr",
-          "lang_zh-CN",
-          "lang_zh-TW",
-        ],
-        default: "lang_en",
-      },
-      [PARAM.CSE.SAFE]: {
-        title: "Safe search",
-        description: "Search safety level",
-        type: "string",
-        enum: ["active", "off"],
-        default: "active",
-      },
-      [PARAM.CSE.START]: {
-        title: "Start index",
-        description: [
-          "The index of the first result to return.",
-          "The default number of results per page is 10, so &start=11 would start at the top of the second page of results.",
-          "Note: The JSON API will never return more than 100 results, even if more than 100 documents match the query, so setting the sum of start + num to a number greater than 100 will produce an error.",
-        ].join("\n"),
-        type: "integer",
-        default: "1",
-      },
-    },
-  } satisfies Schema,
+const query = input({
+  type: "string",
+  title: "Query",
+  description: "What would you like to search for?",
+  default: "Google Breadboard",
+  examples: [
+    "Google Breadboard",
+    "Google Custom Search Engine",
+    "Google Gemini",
+  ],
 });
 
-const secrets = core.secrets({
-  $metadata: {
-    title: "Secrets",
-  },
-  $id: "secrets",
-  keys: [PARAM.CSE.ID, PARAM.CSE.API_KEY],
+const numberOfResults = input({
+  description: [
+    `Number of search results to return`,
+    `Valid values are integers between 1 and 10, inclusive.`,
+  ].join("\n"),
+  title: "Number of results",
+  type: "number",
+  default: 3,
 });
-const urlTemplate = templates.urlTemplate({
+
+const language = input({
+  title: "Language",
+  description: "Search language",
+  type: enumeration(
+    "lang_ar",
+    "lang_bg",
+    "lang_ca",
+    "lang_cs",
+    "lang_da",
+    "lang_de",
+    "lang_el",
+    "lang_en",
+    "lang_es",
+    "lang_et",
+    "lang_fi",
+    "lang_fr",
+    "lang_hr",
+    "lang_hu",
+    "lang_id",
+    "lang_is",
+    "lang_it",
+    "lang_iw",
+    "lang_ja",
+    "lang_ko",
+    "lang_lt",
+    "lang_lv",
+    "lang_nl",
+    "lang_no",
+    "lang_pl",
+    "lang_pt",
+    "lang_ro",
+    "lang_ru",
+    "lang_sk",
+    "lang_sl",
+    "lang_sr",
+    "lang_sv",
+    "lang_tr",
+    "lang_zh-CN",
+    "lang_zh-TW",
+  ),
+  default: "lang_en",
+});
+
+const safeSearch = input({
+  title: "Safe search",
+  description: "Search safety level",
+  type: enumeration("active", "off"),
+  default: "active",
+});
+
+const startIndex = input({
+  title: "Start index",
+  description: [
+    "The index of the first result to return.",
+    "The default number of results per page is 10, so &start=11 would start at the top of the second page of results.",
+    "Note: The JSON API will never return more than 100 results, even if more than 100 documents match the query, so setting the sum of start + num to a number greater than 100 will produce an error.",
+  ].join("\n"),
+  type: "number",
+  default: 1,
+});
+
+const secretId = secret(PARAM.CSE.ID);
+
+const secretApiKey = secret(PARAM.CSE.API_KEY)
+
+const url = urlTemplate({
   $metadata: {
     title: "CSE URL Template",
   },
   $id: "customSearchURL",
   template: `https://www.googleapis.com/customsearch/v1?key={${PARAM.CSE.API_KEY}}&cx={${PARAM.CSE.ID}}&q={${PARAM.QUERY}}&lr={${PARAM.CSE.LANG}}&safe={${PARAM.CSE.SAFE}}&num={${PARAM.CSE.NUM}}&start={${PARAM.CSE.START}}`,
-  [PARAM.CSE.API_KEY]: secrets[PARAM.CSE.API_KEY],
-  [PARAM.CSE.ID]: secrets[PARAM.CSE.ID],
-  [PARAM.QUERY]: input[PARAM.QUERY],
-  [PARAM.CSE.LANG]: input[PARAM.CSE.LANG],
-  [PARAM.CSE.SAFE]: input[PARAM.CSE.SAFE],
-  [PARAM.CSE.NUM]: input[PARAM.CSE.NUM],
-  [PARAM.CSE.START]: input[PARAM.CSE.START],
+  [PARAM.CSE.API_KEY]: secretApiKey,
+  [PARAM.CSE.ID]: secretId,
+  [PARAM.QUERY]: query,
+  [PARAM.CSE.LANG]: language,
+  [PARAM.CSE.SAFE]: safeSearch,
+  [PARAM.CSE.NUM]: numberOfResults,
+  [PARAM.CSE.START]: startIndex,
 });
 
-const fetch = core.fetch({
+const fetchResult = fetch({
   $metadata: {
     title: "Fetch search results",
   },
   $id: "search",
-  url: urlTemplate.url,
+  url: url.outputs.url,
 });
 
-const spreadResponse = spread({
-  $metadata: {
-    title: "Spread search response",
-  },
-  $id: "spreadResponse",
-  object: fetch.response,
-});
-
-const popSearchResult = pop({
-  $metadata: {
-    title: "Pop search result",
-  },
-  array: spreadResponse.items as unknown as object[],
-  $id: "popSearchResult",
-});
-
-popSearchResult.array.to(popSearchResult);
-
-const spreadSearchResult = spread({
-  $metadata: {
-    title: "Spread search result",
-  },
-  $id: "spreadSearchResult",
-  object: popSearchResult.item,
-});
-
-const output = base.output({
-  $metadata: {
-    title: "Output",
-  },
-  $id: "output",
-  schema: {
-    type: "object",
-    properties: {
-      title: {
-        type: "string",
-        title: "Page Title",
-      },
-      htmlTitle: {
-        type: "string",
-        title: "HTML Title",
-      },
-      link: {
-        type: "string",
-        title: "Link",
-      },
-      displayLink: {
-        type: "string",
-        title: "Display Link",
-      },
-      snippet: {
-        type: "string",
-        title: "Snippet",
-      },
-      htmlSnippet: {
-        type: "string",
-        title: "HTML Snippet",
-      },
-      formattedUrl: {
-        type: "string",
-        title: "Formatted URL",
-      },
-      htmlFormattedUrl: {
-        type: "string",
-        title: "HTML Formatted URL",
-      },
-      pagemap: {
-        title: "Page Map",
-        type: "object",
-        properties: {
-          cse_thumbnail: {
-            type: "array",
-            items: [
-              {
-                type: "object",
-                properties: {
-                  src: {
-                    type: "string",
-                  },
-                  width: {
-                    type: "string",
-                  },
-                  height: {
-                    type: "string",
-                  },
-                },
-                required: ["src", "width", "height"],
-              },
-            ],
-          },
-          softwaresourcecode: {
-            type: "array",
-            items: [
-              {
-                type: "object",
-                properties: {
-                  author: {
-                    type: "string",
-                  },
-                  name: {
-                    type: "string",
-                  },
-                  text: {
-                    type: "string",
-                  },
-                },
-                required: ["author", "name", "text"],
-              },
-            ],
-          },
-          metatags: {
-            type: "array",
-            items: [
-              {
-                type: "object",
-              },
-            ],
-          },
-          cse_image: {
-            type: "array",
-            items: [
-              {
-                type: "object",
-                properties: {
-                  src: {
-                    type: "string",
-                  },
-                },
-                required: ["src"],
-              },
-            ],
-          },
-        },
-        required: [
-          "cse_thumbnail",
-          "softwaresourcecode",
-          "metatags",
-          "cse_image",
-        ],
-      },
+const spreadResponse = code(
+  {
+    $id: "spread",
+    $metadata: {
+      title: "Spread",
+      description: "Spread the properties of an object into a new object",
     },
-    required: [
-      "kind",
-      "title",
-      "htmlTitle",
-      "link",
-      "displayLink",
-      "snippet",
-      "htmlSnippet",
-      "formattedUrl",
-      "htmlFormattedUrl",
-      "pagemap",
-    ],
-  } satisfies Schema,
-});
+    obj: fetchResult.outputs.response
+  },
+  {
+    items: array(object({}))
+  },
+  ({ obj }) => {
+    if (typeof obj !== "object") {
+      throw new Error(`object is of type ${typeof obj} not object`);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return { ...obj } as any;
+  }
+);
 
-spreadSearchResult.to(output);
+const popSearchResult = code(
+  {
+    $id: "pop",
+    $metadata: {
+      title: "Pop",
+      description: "Pop the array",
+    },
+    arr: spreadResponse.outputs.items
+  },
+  {
+    array: optional(array(anyOf("string", "number", object({}), "unknown", "boolean"))),
+    item: optional(anyOf("string", "number", object({}), "unknown", "boolean"))
+  },
+  ({ arr }) => {
+    const [item, ...rest] = arr
+    if (item) {
+      console.log("ITEM EXISTS 1")
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return { array: rest, item } as any
+    }
+    return {}
+  }
+);
 
-export default await input.serialize({
+const repop = code(
+  {
+    $id: "repop",
+    $metadata: {
+      title: "RePop",
+      description: "Pop the array",
+    },
+    arr: popSearchResult.outputs.array
+  },
+  {
+    array: optional(array(anyOf("string", "number", object({}), "unknown", "boolean"))),
+    item: optional(anyOf("string", "number", object({}), "unknown", "boolean"))
+  },
+  ({ arr }) => {
+    const [item, ...rest] = arr
+    if (item) {
+      console.log("ITEM EXISTS 2")
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return { array: rest, item } as any
+    }
+    return {}
+  }
+);
+
+const spreadSearchResult = code(
+  {
+    $id: "spreadFinal",
+    $metadata: {
+      title: "Spread Final",
+      description: "Spread the properties of an object into a new object",
+    },
+    obj: repop.outputs.item
+  },
+  {
+    title: "string",
+    htmlTitle: "string",
+    link: "string",
+    displayLink: "string",
+    snippet: "string",
+    htmlSnippet: "string",
+    formattedUrl: "string",
+    htmlFormattedUrl: "string",
+    pagemap: object({
+      cse_thumbnail: array(object({
+        src: "string",
+        height: "string",
+        width: "string",
+      })),
+      softwaresourcecode: array(object({
+        author: "string",
+        name: "string",
+        text: "string",
+      })),
+      metatags: array(object({})),
+      cse_image: array(object({
+        src: "string",
+      })),
+    })
+  },
+  ({ obj }) => {
+    if (typeof obj !== "object") {
+      throw new Error(`object is of type ${typeof obj} not object`);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return { ...obj } as any;
+  }
+);
+
+export default board({
   title: "Google Custom Search Engine Tool",
   description: [
     "A tool to search for information using the Google Custom Search Engine",
     "For more information, see the Google Custom Search Engine documentation.",
     "https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list",
   ].join("\n"),
-  version: "0.1.0",
-} satisfies GraphInlineMetadata);
+  version: "0.2.0",
+  inputs: { query, numberOfResults, language, safeSearch, startIndex },
+  outputs: {
+    title: output(spreadSearchResult.outputs.title),
+    htmlTitle: output(spreadSearchResult.outputs.htmlTitle),
+    link: output(spreadSearchResult.outputs.link),
+    displayLink: output(spreadSearchResult.outputs.displayLink),
+    snippet: output(spreadSearchResult.outputs.snippet),
+    htmlSnippet: output(spreadSearchResult.outputs.htmlSnippet),
+    formattedUrl: output(spreadSearchResult.outputs.formattedUrl),
+    pagemap: output(spreadSearchResult.outputs.pagemap),
+  },
+});
