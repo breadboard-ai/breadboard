487d03b588f1f0281d70294a1f8ee18a8bad8584
Al Marks <aomarks@google.com>
2024-07-19 14:10:50 -0700

    Add google-drive-query element

---
+++ packages/visual-editor/src/ui/elements/google-drive/google-drive-query.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("bb-google-drive-query")
+export class GoogleDriveQuery extends LitElement {
+  override render() {
+    return html`TODO(aomarks)`;
+  }
+}
