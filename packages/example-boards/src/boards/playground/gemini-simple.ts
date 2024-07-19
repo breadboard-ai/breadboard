6c63b12b1195a9fb2ad2a31a78880c07c2fc18e3
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-19 10:37:56 +0100

    Converted to Build API

--- packages/example-boards/src/boards/playground/gemini-simple.ts
+++ packages/example-boards/src/boards/playground/gemini-simple.ts
@@ -1,18 +1,31 @@
-import { board } from "@google-labs/breadboard";
-import { gemini } from "@google-labs/gemini-kit";
+import { annotate, board, input, object, output } from "@breadboard-ai/build";
+import { invoke } from "@google-labs/core-kit";
 
-export default await board(({ text }) => {
-  const llm = gemini.text({
-    $id: "llm",
-    text: text
-      .isString()
-      .title("Prompt")
-      .examples("Write a rhyming poem about breadboards."),
-  });
+const prompt = input({
+  title: "Prompt",
+  type: "string",
+  examples: ["Write a rhyming poem about breadboards"]
+});
 
-  return { text: llm.text };
-}).serialize({
+const generator = input({
+  type: annotate(object({}), {
+    behavior: ["board"],
+  }),
+  default: { kind: "board", path: "gemini-generator.json" },
+});
+
+const llmResponse = invoke({
+  $board: generator,
+  text: prompt,
+}).unsafeOutput("text");
+
+
+const text = output(llmResponse)
+
+export default board({
   title: "Gemini Simple",
   description: "The simplest possible example of using Gemini Kit.",
-  version: "0.0.1",
+  version: "0.1.0",
+  inputs: { prompt, generator },
+  outputs: { text },
 });
