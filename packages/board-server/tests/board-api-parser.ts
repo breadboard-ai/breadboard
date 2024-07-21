386e595c44413979e3ca9ac0ea1924cbfe4aa330
Dimitri Glazkov <dimitri@glazkov.com>
2024-07-21 10:30:10 -0700

    Scaffold up testing.

---
+++ packages/board-server/tests/board-api-parser.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import test, { describe } from "node:test";
+
+describe("Board API Parser", () => {
+  test("parses a simple board URL", (t) => {
+    t.todo("Implement this!");
+  });
+});
