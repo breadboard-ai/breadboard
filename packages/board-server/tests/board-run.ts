6f50802d22489e70285407f22b89263818ddeb01
Dimitri Glazkov <dglazkov@google.com>
2024-07-24 16:34:45 -0700

    Stub out board run tests.

---
+++ packages/board-server/tests/board-run.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import test, { describe } from "node:test";
+import { deepStrictEqual, ok } from "assert";
+
+describe("Board Server Runs Boards", () => {
+  test("can run a simple board", (t) => {
+    ok(true);
+  });
+});
