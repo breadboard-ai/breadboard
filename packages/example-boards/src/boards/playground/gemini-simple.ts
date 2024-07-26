f8865d8c736c30fb14ba472439b53cc8893b018f
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-07-26 10:11:34 +0100

    Updated gemini-simple board to directly import `geminiText` board from Gemini Kit

--- packages/example-boards/src/boards/playground/gemini-simple.ts
+++ packages/example-boards/src/boards/playground/gemini-simple.ts
@@ -1,31 +1,23 @@
-import { annotate, board, input, object, output } from "@breadboard-ai/build";
-import { invoke } from "@google-labs/core-kit";
+import { board, enumeration, input, output } from "@breadboard-ai/build";
+import { geminiText } from "@google-labs/gemini-kit";
 
 const prompt = input({
   title: "Prompt",
   type: "string",
   examples: ["Write a rhyming poem about breadboards"]
 });
-
-const generator = input({
-  type: annotate(object({}), {
-    behavior: ["board"],
-  }),
-  default: { kind: "board", path: "gemini-generator.json" },
+const modelInput = input({
+  type: enumeration("gemini-1.5-flash-latest", "gemini-1.5-pro-latest")
 });
 
-const llmResponse = invoke({
-  $board: generator,
-  text: prompt,
-}).unsafeOutput("text");
+const llmResponse = geminiText({ model: modelInput, text: prompt });
 
-
-const text = output(llmResponse)
+const text = output(llmResponse.outputs.text)
 
 export default board({
   title: "Gemini Simple",
   description: "The simplest possible example of using Gemini Kit.",
   version: "0.1.0",
-  inputs: { prompt, generator },
+  inputs: { prompt },
   outputs: { text },
 });
