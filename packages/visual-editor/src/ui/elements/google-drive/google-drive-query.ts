a1c9849fe88d32ef1f649714b97dd34b5af058ab
Al Marks <aomarks@google.com>
2024-07-26 10:29:49 -0700

    [visual-editor] Button that launches Google Drive Picker (#2657)
    <img width="1109" alt="Screenshot 2024-07-26 at 10 25 39 AM"
    src="https://github.com/user-attachments/assets/65d3cfdd-7ee1-40c9-ab73-42ab8c0ea544">

--- packages/visual-editor/src/ui/elements/google-drive/google-drive-query.ts
+++ packages/visual-editor/src/ui/elements/google-drive/google-drive-query.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Task } from "@lit/task";
 import { LitElement, html } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import { type InputEnterEvent } from "../../events/events.js";
@@ -16,26 +15,35 @@ export class GoogleDriveQuery extends LitElement {
   @state()
   private _authorization?: { clientId: string; secret: string };
 
-  #loadPickerLib = new Task(
-    this,
-    () => loadDrivePicker(),
-    () => []
-  );
+  @state()
+  private _pickerLib?: typeof google.picker;
+
+  #picker?: google.picker.Picker;
+
+  override async connectedCallback(): Promise<void> {
+    super.connectedCallback();
+    this._pickerLib ??= await loadDrivePicker();
+  }
 
   override render() {
-    if (!this._authorization) {
+    if (this._authorization === undefined) {
       return html`<bb-connection-input
         @bbinputenter=${this.#onToken}
         connectionId="google-drive"
       ></bb-connection-input>`;
     }
-
-    return this.#loadPickerLib.render({
-      pending: () => html`<p>Loading Google Drive Picker ...</p>`,
-      error: () => html`<p>Error Loading Google Drive Picker</p>`,
-      complete: (lib: typeof google.picker) =>
-        html`<em>Not yet implemented (${Object.keys(lib).length})</em>`,
-    });
+    if (this._pickerLib === undefined) {
+      return html`<p>Loading Google Drive Picker ...</p>`;
+    }
+    // TODO(aomarks) The actual input element.
+    return html`
+      <p>
+        Only files that you choose to share with Breadboard will be matched by
+        this query. Sharing a file is permanent and applies to all future
+        queries from your signed-in account.
+      </p>
+      <button @click=${this.#onClickPickFiles}>Share Google Drive Files</button>
+    `;
   }
 
   #onToken(event: InputEnterEvent) {
@@ -52,5 +60,47 @@ export class GoogleDriveQuery extends LitElement {
     if (clientId && secret) {
       this._authorization = { clientId, secret };
     }
+  }
+
+  #onClickPickFiles() {
+    if (this._authorization === undefined || this._pickerLib === undefined) {
+      return;
+    }
+    this.#destroyPicker();
+    // See https://developers.google.com/drive/picker/reference
+    this.#picker = new this._pickerLib.PickerBuilder()
+      .setAppId(this._authorization.clientId)
+      .setOAuthToken(this._authorization.secret)
+      .setCallback(this.#pickerCallback.bind(this))
+      .addView(google.picker.ViewId.DOCS)
+      .enableFeature(google.picker.Feature.NAV_HIDDEN)
+      .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+      .build();
+    this.#picker.setVisible(true);
+  }
+
+  #pickerCallback(result: google.picker.ResponseObject): void {
+    switch (result.action) {
+      case "cancel": {
+        this.#destroyPicker();
+        return;
+      }
+      case "picked": {
+        this.#destroyPicker();
+        // TODO(aomarks) Show this as a snackbar
+        console.log(
+          `Shared ${result.docs.length} Google Drive files with Breadboard`
+        );
+      }
+    }
+  }
+
+  #destroyPicker() {
+    if (this.#picker === undefined) {
+      return;
+    }
+    this.#picker.setVisible(false);
+    this.#picker.dispose();
+    this.#picker = undefined;
   }
 }
