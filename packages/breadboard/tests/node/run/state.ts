c4046f2b9ee8d1c07e0f63ad53961277d99a581f
Dimitri Glazkov <dimitri@glazkov.com>
2024-07-27 11:52:02 -0700

    Stub out node testing for breadboard package.

---
+++ packages/breadboard/tests/node/run/state.ts
@@ -0,0 +1,13 @@
+import { ok } from "node:assert";
+import test, { describe } from "node:test";
+
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+describe("managed run state", () => {
+  test("stack manager", async (t) => {
+    ok(true);
+  });
+});
