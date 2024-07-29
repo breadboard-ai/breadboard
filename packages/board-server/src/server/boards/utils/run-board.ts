9f6ddb99bd92ebb80d90bcfd4f455c781545787d
Dimitri Glazkov <dglazkov@google.com>
2024-07-29 11:59:38 -0700

    Fix build error in board server.

--- packages/board-server/src/server/boards/utils/run-board.ts
+++ packages/board-server/src/server/boards/utils/run-board.ts
@@ -48,13 +48,13 @@ export const runBoard = async ({
 
   const resumeFrom = fromNextToState(next, inputs);
 
-  if (resumeFrom.state?.length > 1) {
-    const { url: subgraphUrl } = resumeFrom.state[resumeFrom.state.length - 1]!;
-    if (!subgraphUrl) {
-      return { $error: "Invalid state to resume from." };
-    }
-    url = subgraphUrl;
-  }
+  // if (resumeFrom.state?.length > 1) {
+  //   const { url: subgraphUrl } = resumeFrom.state[resumeFrom.state.length - 1]!;
+  //   if (!subgraphUrl) {
+  //     return { $error: "Invalid state to resume from." };
+  //   }
+  //   url = subgraphUrl;
+  // }
 
   const runner = run({
     url,
