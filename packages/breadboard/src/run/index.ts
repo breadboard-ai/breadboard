7c9e87c203c9bc0dc66759e33f7bdabfcc1eddc5
Dimitri Glazkov <dglazkov@google.com>
2024-08-01 12:23:47 -0700

    Remove tests that rely on GraphDescriptor actually being a BoardRunner.

--- packages/breadboard/src/run/index.ts
+++ packages/breadboard/src/run/index.ts
@@ -4,15 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { InputValues } from "../types.js";
 import { RunStateManager } from "./manager.js";
-import { emptyEntry } from "./registry.js";
 import type {
-  LifecyclePathRegistryEntry,
   ManagedRunState,
   ReanimationInputs,
   ReanimationState,
-  RunStackEntry,
 } from "./types.js";
 
 export const createRunStateManager = (
