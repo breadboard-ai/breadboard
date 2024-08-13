e51727624256899a9e17f459ef344af6029481e2
Dimitri Glazkov <dglazkov@google.com>
2024-08-12 20:54:04 -0700

    Sculpt LightObserver just a bit. It works now!

--- packages/board-server/src/app/utils/light-observer.ts
+++ packages/board-server/src/app/utils/light-observer.ts
@@ -11,23 +11,22 @@ import type {
   InspectableNodePorts,
   InspectableNodeType,
   InspectableRun,
-  InspectableRunLoadResult,
   InspectableRunNodeEvent,
-  InspectableRunObserver,
+  InspectableRunSecretEvent,
   NodeDescriberResult,
   NodeDescriptor,
-  SerializedRunLoadingOptions,
+  NodeHandlerMetadata,
 } from "@google-labs/breadboard";
 import type {
   InputValues,
   NodeConfiguration,
   NodeMetadata,
+  NodeTypeIdentifier,
   OutputValues,
   StartLabel,
 } from "@google-labs/breadboard-schema/graph.js";
 import type {
   HarnessRunner,
-  HarnessRunResult,
   RunEdgeEvent,
   RunGraphEndEvent,
   RunGraphStartEvent,
@@ -35,14 +34,32 @@ import type {
   RunNodeEndEvent,
   RunNodeStartEvent,
   RunOutputEvent,
+  RunSecretEvent,
 } from "@google-labs/breadboard/harness";
 
+const idFromPath = (path: number[]): string => {
+  return `e-${path.join("-")}`;
+};
+
 /**
- * It's like an InspectableRunObserver, but works with "top" diagnostics flag.
+ * A lightweight rewrite of the `InspectableRunObserver` that
+ * only captures the events that are necessary to drive the app UI.
  */
-export class LightObserver implements InspectableRunObserver {
+export class LightObserver {
   #run: InspectableRun | null = null;
   #currentNode: InspectableRunNodeEvent | null = null;
+  /**
+   * Need to keep track of input separately, because
+   * bubbled inputs appear as coming from inside of the
+   * node.
+   */
+  #currentInput: InspectableRunNodeEvent | null = null;
+  /**
+   * Need to keep track of secret separately, because
+   * bubbled secrets may appear as coming from inside of the
+   * node.
+   */
+  #currentSecret: InspectableRunSecretEvent | null = null;
 
   constructor(runner: HarnessRunner) {
     runner.addEventListener("nodestart", this.#nodeStart.bind(this));
@@ -52,24 +69,32 @@ export class LightObserver implements InspectableRunObserver {
     runner.addEventListener("input", this.#input.bind(this));
     runner.addEventListener("output", this.#output.bind(this));
     runner.addEventListener("edge", this.#edge.bind(this));
+    runner.addEventListener("secret", this.#secret.bind(this));
+    runner.addEventListener("resume", (event) => {
+      if (this.#currentInput) {
+        this.#currentInput.end = globalThis.performance.now();
+        this.#currentInput.outputs = event.data.inputs!;
+        this.#currentInput = null;
+      } else if (this.#currentSecret) {
+        this.#currentSecret.end = globalThis.performance.now();
+        this.#currentSecret = null;
+      } else {
+        return;
+      }
+      if (this.#run) {
+        this.#run.events = [...this.#run.events];
+      }
+    });
   }
 
-  async runs(): Promise<InspectableRun[]> {
+  runs(): InspectableRun[] {
     return this.#run ? [this.#run] : [];
   }
 
-  observe(result: HarnessRunResult): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-
-  load(
-    o: unknown,
-    options?: SerializedRunLoadingOptions
-  ): Promise<InspectableRunLoadResult> {
-    throw new Error("Method not implemented.");
-  }
-
   #graphStart(event: RunGraphStartEvent) {
+    if (event.data.path.length > 0) {
+      return;
+    }
     if (this.#run) {
       throw new Error("Graph already started");
     }
@@ -99,69 +124,116 @@ export class LightObserver implements InspectableRunObserver {
   }
 
   #graphEnd(event: RunGraphEndEvent) {
-    this.#run = null;
+    if (event.data.path.length > 0) {
+      return;
+    }
     this.#currentNode = null;
   }
 
   #nodeStart(event: RunNodeStartEvent) {
+    if (event.data.path.length > 1) {
+      return;
+    }
+
     this.#currentNode = {
       type: "node",
-      id: event.data.node.id,
+      id: idFromPath(event.data.path),
       graph: null as unknown as InspectableGraph,
       node: new Node(event.data.node),
       start: event.data.timestamp,
-      end: 0,
+      end: null,
       inputs: event.data.inputs,
       outputs: null,
       bubbled: false,
       hidden: false,
       runs: [],
     };
-    this.#run?.events.push(this.#currentNode);
+    if (!this.#run) {
+      throw new Error("Node started without a graph");
+    }
+    this.#run.events = [...this.#run.events, this.#currentNode];
   }
 
   #nodeEnd(event: RunNodeEndEvent) {
+    if (event.data.path.length > 1) {
+      return;
+    }
+
     this.#currentNode!.end = event.data.timestamp;
     this.#currentNode!.outputs = event.data.outputs;
     this.#currentNode = null;
+    if (!this.#run) {
+      throw new Error("Node end without a graph");
+    }
+
+    this.#run.events = [...this.#run.events];
+  }
+
+  #secret(event: RunSecretEvent) {
+    this.#currentSecret = {
+      type: "secret",
+      start: event.data.timestamp,
+      id: "secret",
+      keys: event.data.keys,
+      end: null,
+    };
+    if (!this.#run) {
+      throw new Error("Node started without a graph");
+    }
+    console.log("Secret", this.#currentSecret);
+    this.#run.events = [...this.#run.events, this.#currentSecret];
   }
 
   #input(event: RunInputEvent) {
-    this.#currentNode = {
+    if (!event.data.bubbled) {
+      // Non-bubbled events will present themselves as node starts.
+      return;
+    }
+    this.#currentInput = {
       type: "node",
-      id: event.data.node.id,
+      id: idFromPath(event.data.path),
       graph: null as unknown as InspectableGraph,
       node: new Node(event.data.node),
       start: event.data.timestamp,
-      end: 0,
+      end: null,
       inputs: event.data.inputArguments,
       outputs: null,
       bubbled: event.data.bubbled,
       hidden: false,
       runs: [],
     };
-    this.#run?.events.push(this.#currentNode);
+    if (!this.#run) {
+      throw new Error("Node started without a graph");
+    }
+    this.#run.events = [...this.#run.events, this.#currentInput];
   }
 
   #output(event: RunOutputEvent) {
-    this.#currentNode = {
+    if (!event.data.bubbled) {
+      // Non-bubbled events will present themselves as node ends.
+      return;
+    }
+    const output: InspectableRunNodeEvent = {
       type: "node",
-      id: event.data.node.id,
+      id: idFromPath(event.data.path),
       graph: null as unknown as InspectableGraph,
       node: new Node(event.data.node),
       start: event.data.timestamp,
-      end: 0,
+      end: event.data.timestamp,
       inputs: event.data.outputs,
       outputs: null,
       bubbled: event.data.bubbled,
       hidden: false,
       runs: [],
     };
-    this.#run?.events.push(this.#currentNode);
+    if (!this.#run) {
+      throw new Error("Node started without a graph");
+    }
+    this.#run.events = [...this.#run.events, output];
   }
 
   #edge(event: RunEdgeEvent) {
-    console.log("Edge", event);
+    //console.log("Edge", event);
   }
 }
 
@@ -201,7 +273,7 @@ class Node implements InspectableNode {
   }
 
   type(): InspectableNodeType {
-    throw new Error("Method not implemented.");
+    return new NodeType(this.descriptor.type);
   }
 
   configuration(): NodeConfiguration {
@@ -220,6 +292,27 @@ class Node implements InspectableNode {
     inputValues?: InputValues,
     outputValues?: OutputValues
   ): Promise<InspectableNodePorts> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class NodeType implements InspectableNodeType {
+  #type: string;
+
+  constructor(type: string) {
+    this.#type = type;
+  }
+
+  metadata(): NodeHandlerMetadata {
+    // TODO: Figure this out.
+    return {};
+  }
+
+  type(): NodeTypeIdentifier {
+    return this.#type;
+  }
+
+  ports(): Promise<InspectableNodePorts> {
     throw new Error("Method not implemented.");
   }
 }
