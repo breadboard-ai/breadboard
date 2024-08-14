92db1c07eca93348fbab580c73d414b869fc8553
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-08-14 10:44:03 +0100

    Converted gpt 4 board without streaming

--- packages/example-boards/src/boards/playground/openai-gpt-4-vision.ts
+++ packages/example-boards/src/boards/playground/openai-gpt-4-vision.ts
@@ -4,136 +4,116 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  GraphInlineMetadata,
-  Schema,
-  base,
-  board,
-} from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { json } from "@google-labs/json-kit";
-import { nursery } from "@google-labs/node-nursery-web";
-import { chunkTransformer } from "./openai-chunk-transformer";
+import { annotate, anyOf, array, board, enumeration, input, object, output } from "@breadboard-ai/build";
+import { secret, fetch } from "@google-labs/core-kit";
+import { jsonata } from "@google-labs/json-kit";
 
-const metadata = {
-  title: "OpenAI GPT 4 Vision Preview",
-  description:
-    "This board is the simplest possible invocation of OpenAI's GPT 4 Vision Preview API to generate text from multipart inputs.",
-  version: "0.0.2",
-} satisfies GraphInlineMetadata;
+const textPartType = object({ text: "string" });
 
-const inputSchema = {
-  type: "object",
-  properties: {
-    content: {
-      type: "array",
-      title: "Content",
-      description: "Add content here",
-      minItems: 1,
-      items: {
-        type: "object",
-        behavior: ["llm-content"],
-        format: "image-file,image-webcam",
-      },
-    },
-    useStreaming: {
-      type: "boolean",
-      title: "Stream",
-      description: "Whether to stream the output",
-      default: "false",
-    },
-  },
-} satisfies Schema;
+const imagePartType = object({
+  inlineData: object({
+    mimeType: enumeration(
+      "image/png",
+      "image/jpeg",
+      "image/heic",
+      "image/heif",
+      "image/webp"
+    ),
+    data: "string",
+  }),
+});
 
-const textOutputSchema = {
-  type: "object",
-  properties: {
-    text: {
-      type: "string",
-      title: "Text",
-      description: "The generated text",
-    },
-  },
-} satisfies Schema;
+const functionCallPartType = object({
+  function_call: object({
+    name: "string",
+    args: object({}, "string"),
+  }),
+});
 
-const streamOutputSchema = {
-  type: "object",
-  properties: {
-    stream: {
-      type: "object",
-      title: "Stream",
-      format: "stream",
-      description: "The generated text",
-    },
-  },
-} satisfies Schema;
+const functionResponsePartType = object({
+  function_response: object({
+    name: "string",
+    response: "unknown",
+  }),
+});
 
-export default await board(() => {
-  const input = base.input({ $id: "input", schema: inputSchema });
+const partType = anyOf(
+  textPartType,
+  imagePartType,
+  functionCallPartType,
+  functionResponsePartType
+);
 
-  const textOutput = base.output({
-    $id: "textOutput",
-    schema: textOutputSchema,
-  });
+const generateContentContentsType = object({
+  role: enumeration("model", "user", "tool", "$metadata"),
+  parts: array(partType),
+});
 
-  const streamOutput = base.output({
-    $id: "streamOutput",
-    schema: streamOutputSchema,
-  });
+const content = input({
+  $id: "Content",
+  type: array(
+    annotate(generateContentContentsType, {
+      behavior: ["llm-content"],
+    })
+  ),
+  title: "Context",
+  description: "An array of messages to use as conversation context",
+});
 
-  const fetch = core.fetch({
-    $id: "openai",
-    url: "https://api.openai.com/v1/chat/completions",
-    method: "POST",
-    stream: input.useStreaming,
-    headers: json.jsonata({
-      $id: "makeHeaders",
-      expression: `{
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " & $.OPENAI_API_KEY
-      }`,
-      OPENAI_API_KEY: core.secrets({ keys: ["OPENAI_API_KEY"] }),
-    }).result,
-    body: input.to(
-      json.jsonata({
-        $id: "makeBody",
-        expression: `{
-          "model": "gpt-4-vision-preview",
-          "messages": [
-            {
-              "role": "user",
-              "content": $.content.{
-                "type": $keys($) = "text" ? "text" : "image_url" ,
-                "text": $.text,
-                "image_url": "data:" & $.inline_data.mime_type & ";base64," & $.inline_data.data
-              }
-            }
-          ],
-          "stream": $.useStreaming,
-          "temperature": 1,
-          "max_tokens": 256,
-          "top_p": 1,
-          "frequency_penalty": 0,
-          "presence_penalty": 0
+const fetchResult = fetch({
+  $id: "openai",
+  url: "https://api.openai.com/v1/chat/completions",
+  method: "POST",
+  stream: false,
+  headers: jsonata({
+    $id: "makeHeaders",
+    expression: `{
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " & $.OPENAI_API_KEY
     }`,
-      })
-    ).result,
-  });
+    OPENAI_API_KEY: secret("OPENAI_API_KEY"),
+  }).unsafeOutput("result"),
+  body: jsonata({
+    $id: "makeBody",
+    expression: `{
+      "model": "gpt-4-turbo",
+      "messages": [
+        {
+            "role": "user",
+            "content": $.content.parts.({
+              "type": $exists($.text) ? "text" : "image_url",
+              "text": $exists($.text) ? $.text : undefined,
+              "image_url": $exists($.inlineData) ? {
+                  "url": "data:" & $.inlineData.mimeType & ";base64," & $.inlineData.data
+              } : undefined
+            })
+        }
+      ],
+      "stream": $.useStreaming,
+      "temperature": 1,
+      "max_tokens": 256,
+      "top_p": 1,
+      "frequency_penalty": 0,
+      "presence_penalty": 0
+    }`,
+    content,
+    useStreaming: false
+  }).unsafeOutput("result")
+});
 
-  json
-    .jsonata({
-      $id: "getResponse",
-      expression: `choices[0].message.content`,
-      json: fetch.response,
-    })
-    .result.as("text")
-    .to(textOutput);
+const formattedResponse = jsonata({
+  $id: "getResponse",
+  expression: `choices[0].message.content`,
+  json: fetchResult.outputs.response,
+});
 
-  return nursery
-    .transformStream({
-      board: chunkTransformer,
-      $id: "streamTransform",
-      stream: fetch,
-    })
-    .to(streamOutput);
-}).serialize(metadata);
+export default board({
+  title: "OpenAI GPT 4 Turbo Vision",
+  description:
+    "This board is the simplest possible invocation of OpenAI's GPT 4 Turbo API, using its vision capabilities to generate text from multipart inputs.",
+  version: "0.1.0",
+  inputs: { content },
+  outputs: {
+    text: output(formattedResponse.unsafeOutput("result")),
+  }
+});
