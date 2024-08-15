27124d3bd2810aab185bb034b7e7f1718f918626
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-08-15 17:25:38 +0100

    Add OpenAI support back into Text Generator

--- packages/example-boards/src/boards/playground/text-generator.ts
+++ packages/example-boards/src/boards/playground/text-generator.ts
@@ -3,14 +3,14 @@
  * Copyright 2023 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import {
   board,
   enumeration,
   input,
+  object,
   output,
 } from "@breadboard-ai/build";
-import { geminiText } from "@google-labs/gemini-kit";
+import { code, invoke } from "@google-labs/core-kit";
 
 const text = input({
   type: "string",
@@ -19,12 +19,34 @@ const text = input({
 });
 
 const model = input({
-  type: enumeration("gemini-1.5-flash-latest", "gemini-1.5-pro-latest")
+  type: enumeration("Gemini Pro", "GPT 3.5 Turbo")
 });
 
-const llmResponse = geminiText({ model, text });
+const switchModel = code(
+  { model },
+  { board: object({ kind: "string", path: "string"}), model: "string" },
+  ({ model }) => {
+    const models: Record<string, string> = {
+      "Gemini Pro": "https://raw.githubusercontent.com/breadboard-ai/breadboard/main/packages/gemini-kit/graphs/gemini-generator.json",
+      "GPT 3.5 Turbo": "https://raw.githubusercontent.com/breadboard-ai/breadboard/main/packages/visual-editor/public/example-boards/playground/openai-gpt-35-turbo.json",
+    };
+    const path = models[model];
+    if (model == "Gemini Pro") {
+      return  { board: { kind: "board", path }, model: "gemini-1.5-pro-latest" }
+    } else {
+      return  { board: { kind: "board", path }, model: "N/A" } // The OpenAI board only supports one model
+    }
+  }
+);
 
-const textOutput = output(llmResponse.outputs.text, {
+const llmResponse = invoke({
+  $id: "llm-response",
+  $board: switchModel.outputs.board,
+  text,
+  model: switchModel.outputs.model,
+}).unsafeOutput("text");
+
+const textOutput = output(llmResponse, {
   title: "Text",
   description: "The generated text",
 });
