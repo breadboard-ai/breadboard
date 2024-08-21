0e2bd67a68a704dfae878d31407a40028cf405eb
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-08-21 13:23:37 +0100

    Converted text classification board

--- packages/example-boards/src/boards/playground/hugging-face-text-classification.ts
+++ packages/example-boards/src/boards/playground/hugging-face-text-classification.ts
@@ -1,90 +1,70 @@
-import { board, base, code } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
+import { board, input, object, output } from "@breadboard-ai/build";
+import { code, secret, fetch } from "@google-labs/core-kit";
 
-
-const inputsSchema = {
+const inputs = input({
     type: "string",
     title: "inputs",
     default: "I like you. I love you",
     description: "The data to send to the hugging face api text classification endpoint"
-};
-
-const keySchema = {
-    type: "string",
-    title: "apiKey",
-    default: "myKey",
-    description: "The hugging face api key"
-};
-
-const useCacheSchema = {
-    type: "boolean",
-    title: "use_cache",
-    default: "true",
-    description: "Boolean. There is a cache layer on the inference API to speedup requests we have already seen. Most models can use those results as is as models are deterministic (meaning the results will be the same anyway). However if you use a non deterministic model, you can set this parameter to prevent the caching mechanism from being used resulting in a real new query"
-};
-
-const waitForModelSchema = {
-    type: "boolean",
-    title: "wait_for_model",
-    default: "false",
-    description: " Boolean. If the model is not ready, wait for it instead of receiving 503. It limits the number of requests required to get your inference done. It is advised to only set this flag to true after receiving a 503 error as it will limit hanging in your application to known places"
-};
-
-export type HuggingFaceTextClassificationRawParams = {
-    inputs: string
-};
-
-const authenticate = code<{ key: string }>((inputs) => {
-    const key = inputs.key
-    const auth = { Authorization: `Bearer ${key}` };
-
-    return { auth };
 });
 
-const handleParams = code<{ inputs: string }>((input) => {
-    const { inputs } = input
+const apiKey = secret("HUGGING_FACE_API_KEY");
 
-    const request: HuggingFaceTextClassificationRawParams = { inputs: inputs };
-    const payload = JSON.stringify(request);
+const useCache = input({
+    type: "boolean",
+    title: "use_cache",
+    default: true,
+    description: "Boolean. There is a cache layer on the inference API to speedup requests we have already seen. Most models can use those results as is as models are deterministic (meaning the results will be the same anyway). However if you use a non deterministic model, you can set this parameter to prevent the caching mechanism from being used resulting in a real new query"
+});
 
-    return { payload }
-})
+const waitForModel = input({
+    type: "boolean",
+    title: "wait_for_model",
+    default: false,
+    description: " Boolean. If the model is not ready, wait for it instead of receiving 503. It limits the number of requests required to get your inference done. It is advised to only set this flag to true after receiving a 503 error as it will limit hanging in your application to known places"
+});
 
-export default await board(() => {
-    const inputs = base.input({
-        $id: "query",
-        schema: {
-            title: "Hugging Face Schema For Text Classification",
-            properties: {
-                inputs: inputsSchema,
-                apiKey: keySchema,
-                use_cache: useCacheSchema,
-                wait_for_model: waitForModelSchema
-            },
-        },
-        type: "string",
-    });
+const makeHeaders = code(
+    { apiKey },
+    { headers: object({ Authorization: "string" }) },
+    ({ apiKey }) => {
+        return { headers: { Authorization: `Bearer ${apiKey}` } };
+    }
+);
 
-    const task = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
-    const output = base.output({ $id: "main" });
+const makePayload = code(
+    { inputs, useCache, waitForModel },
+    {
+        payload: object({
+            inputs: "string", 
+            options: object({
+                use_cache: "boolean",
+                wait_for_model: "boolean"
+            })
+        })
+    },
+    ({ inputs, useCache, waitForModel }) => {
+        const request = {
+            inputs: inputs,
+            options: {
+                use_cache: useCache,
+                wait_for_model: waitForModel
+            }
+        }
+        return { payload: request };
+    }
+);
 
-    const { auth } = authenticate({ key: inputs.apiKey.isString() });
-    const { payload } = handleParams({
-        inputs: inputs.inputs.isString(),
-        use_cache: inputs.use_cache.isBoolean(),
-        wait_for_model: inputs.wait_for_model.isBoolean()
-    });
+const fetchResult = fetch({
+    headers: makeHeaders.outputs.headers,
+    method: "POST",
+    body: makePayload.outputs.payload,
+    url: "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+});
 
-    const response = core.fetch({
-        headers: auth,
-        method: "POST",
-        body: payload,
-        url: task
-    });
-
-    response.to(output);
-    return { output }
-}).serialize({
+export default board({
     title: "Hugging Face Text Classification",
-    description: "Board which calls the Hugging Face Text Classification Endpoint"
+    description: "Board which calls the Hugging Face Text Classification Endpoint",
+    inputs: { inputs, useCache, waitForModel },
+    outputs: { result: output(fetchResult.outputs.response)},
 });
