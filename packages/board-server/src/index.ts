193abae04b701b8e644f3905b67453d30307eba6
Tim Swanson <152931212+timswanson-google@users.noreply.github.com>
2024-08-21 10:49:32 -0600

    [board-server] Create a ServerConfig object (#2901)
    For now, the config contains the value already being used by the server
    to route
    and serve requests. It wille eventually contain allowed origins (just an
    empty
    set for now).
    
    Pass the config object down to whatever handlers require it. As a
    followup, we
    may also want to revisit this routing logic to handle config-specific
    tasks
    before delegating.

--- packages/board-server/src/index.ts
+++ packages/board-server/src/index.ts
@@ -9,21 +9,27 @@ import { createServer as createViteServer } from "vite";
 import { env } from "process";
 
 import { makeRouter } from "./router.js";
+import type { ServerConfig } from "./server/config.js";
 
 const PORT = env.PORT || 3000;
 const HOST = env.HOST || "localhost";
 const HOSTNAME = `http://${HOST}:${PORT}`;
 const IS_PROD = env.NODE_ENV === "production";
 
-const vite = IS_PROD
-  ? null
-  : await createViteServer({
-      server: { middlewareMode: true },
-      appType: "custom",
-      optimizeDeps: { esbuildOptions: { target: "esnext" } },
-    });
+const serverConfig: ServerConfig = {
+  // TODO: #2869 - Get allowed origins from environment var
+  allowedOrigins: new Set(),
+  hostname: HOSTNAME,
+  viteDevServer: IS_PROD
+    ? null
+    : await createViteServer({
+        server: { middlewareMode: true },
+        appType: "custom",
+        optimizeDeps: { esbuildOptions: { target: "esnext" } },
+      }),
+};
 
-const server = createServer(makeRouter(HOSTNAME, vite));
+const server = createServer(makeRouter(serverConfig));
 
 server.listen(PORT, () => {
   console.info(`Running on "${HOSTNAME}"...`);
