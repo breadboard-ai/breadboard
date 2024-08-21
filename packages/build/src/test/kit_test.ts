4b15fe8ca350f2cc0b0adcab8f7b04f39dcd6ac0
Al Marks <aomarks@google.com>
2024-08-21 14:03:25 -0700

    Make type system for kit happy to receieve discrete or board components

---
+++ packages/build/src/test/kit_test.ts
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { test } from "node:test";
+import { kit } from "../internal/kit.js";
+import { defineNodeType } from "../internal/define/define.js";
+import { board } from "../internal/board/board.js";
+
+const discreteComponent = defineNodeType({
+  name: "discreteComponent",
+  inputs: {},
+  outputs: {},
+  invoke: () => ({}),
+});
+
+const boardComponent = board({ id: "foo", inputs: {}, outputs: {} });
+
+test("kit takes discrete component", () => {
+  // $ExpectType KitConstructor<Kit>
+  kit({
+    title: "",
+    url: "",
+    version: "",
+    description: "",
+    components: [discreteComponent],
+  });
+});
+
+test("kit takes board component", () => {
+  // $ExpectType KitConstructor<Kit>
+  kit({
+    title: "",
+    url: "",
+    version: "",
+    description: "",
+    components: [boardComponent],
+  });
+});
