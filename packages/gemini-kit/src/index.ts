13c27cbf407adeb7f453bda6a5cb04887c991860
Al Marks <aomarks@google.com>
2024-08-27 16:03:51 -0700

    [gemini-kit] Fully migrate to Build API (#2951)
    Migrates Gemini Kit to be fully on the Build API, including use of the
    new `legacy()` function which generates a backwards-compatible API for
    use with the old syntax.

--- packages/gemini-kit/src/index.ts
+++ packages/gemini-kit/src/index.ts
@@ -4,61 +4,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { GraphToKitAdapter, KitBuilder } from "@google-labs/breadboard/kits";
+import { kit } from "@breadboard-ai/build";
+import text from "./boards/gemini-generator.js";
+import nano from "./boards/nano-generator.js";
 
-import kit from "./kit.js";
-import { NewNodeFactory, NewNodeValue, addKit } from "@google-labs/breadboard";
-
-// TODO: Replace with the actual URL.
-const KIT_BASE_URL =
-  "https://raw.githubusercontent.com/breadboard-ai/breadboard/main/packages/gemini-kit/graphs/kit.json";
-
-const adapter = await GraphToKitAdapter.create(kit, KIT_BASE_URL, []);
-
-const builder = new KitBuilder(
-  adapter.populateDescriptor({
-    url: "npm:@google-labs/gemini-kit",
-    // TODO: This currently doesn't work, because "addKit" below translates
-    // handler id directly into the node name. We need to make it work without
-    // prefix.
-    // namespacePrefix: "gemini-api-",
-  })
-);
-
-const GeminiKit = builder.build({
-  text: adapter.handlerForNode("text"),
-  nano: adapter.handlerForNode("nano"),
+const geminiKit = kit({
+  title: "Gemini Kit",
+  description: "Nodes for calling Google Gemini APIs",
+  version: "0.1.0",
+  url: "npm:@google-labs/gemini-kit",
+  components: { text, nano },
 });
 
-export type GeminiKit = InstanceType<typeof GeminiKit>;
-
-export type GeminiKitType = {
-  /**
-   * Creates a node that calls the `Gemini Pro` model to generate a response.
-   */
-  text: NewNodeFactory<
-    {
-      /**
-       * The input text that will be used to generate the response.
-       */
-      text: NewNodeValue;
-    },
-    {
-      /**
-       * The generated response from the `Gemini Pro` model.
-       */
-      text: NewNodeValue;
-    }
-  >;
-};
-
-export default GeminiKit;
-
-/**
- * The Gemini Kit. Use members of this object to create nodes to interact with
- * the Gemini language model. The `text` member is used to generate text from
- * the Gemini Pro model.
- */
-export const gemini = addKit(GeminiKit) as unknown as GeminiKitType;
-
-export { default as geminiText } from "./boards/gemini-generator.js";
+export default geminiKit;
+export const gemini = await geminiKit.legacy();
+export { text as geminiText };
