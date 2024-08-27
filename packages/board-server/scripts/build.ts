7807afd99c0aa47614b23ca95f2610f8d34f1f92
Will Faithfull <wfaithfull@users.noreply.github.com>
2024-08-27 23:54:43 +0100

    [board-server] improve board server dockerfile (#2945) (#2946)
    Fixes #2945
    
    - [x] Lightweight image not containing the whole repo.
    - [x] Uses production build, e.g. node dist/server/index.js.
    - [x] Still allows use of the create-account.ts script from inside the
    container to add users.
    
    ```
    $ docker run -d -p 3000:3000 --name board-server board-server
    d6aa812d042f2fc98a67bedb124d80153d1429fda30e54f54fff3fc989a7c208
    $ docker exec -it board-server /bin/bash
    root@d6aa812d042f:/app# node dist/scripts/create-account.js wfaithfull
    Created account for wfaithfull with API key:
    bb-6e52o2r546q6cmu424m544d2p2195t3b444a4m6s2y5q2n2r61
    ```

--- packages/board-server/scripts/build.ts
+++ packages/board-server/scripts/build.ts
@@ -6,12 +6,23 @@
 
 import * as esbuild from "esbuild";
 
-await esbuild.build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  platform: "node",
-  external: ["@google-cloud", "import.meta", "vite", "better-sqlite3"],
-  format: "esm",
-  outfile: "dist/server/index.js",
-  sourcemap: true
-});
+await Promise.all([
+  esbuild.build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    platform: "node",
+    external: ["@google-cloud", "import.meta", "vite", "better-sqlite3"],
+    format: "esm",
+    outfile: "dist/server/index.js",
+    sourcemap: true
+  }),
+  esbuild.build({
+    entryPoints: ["scripts/create-account.ts"],
+    bundle: true,
+    platform: "node",
+    format: "esm",
+    outfile: "dist/scripts/create-account.js",
+    sourcemap: true,
+    external: ["@google-cloud", "import.meta", "vite", "better-sqlite3"],
+  })
+]);
