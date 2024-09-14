8711b105fa2c9c017b26c2438582090d6f595ce3
Dimitri Glazkov <dimitri@glazkov.com>
2024-09-13 21:47:41 -0700

    Add hash util

---
+++ packages/breadboard/tests/node/utils/hash.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import test, { describe } from "node:test";
+import { hash } from "../../../src/utils/hash.js";
+import { deepStrictEqual } from "node:assert";
+
+describe("hash", () => {
+  test("hashes a string", () => {
+    deepStrictEqual(hash("hello"), 181380007);
+  });
+
+  test("hashes an object", () => {
+    deepStrictEqual(hash({ a: "hello" }), 186885731);
+  });
+
+  test("hashes an array", () => {
+    deepStrictEqual(hash(["hello"]), 1327990477);
+  });
+
+  test("hashes a nested object", () => {
+    deepStrictEqual(hash({ a: { b: "hello" } }), 357563268);
+  });
+});
