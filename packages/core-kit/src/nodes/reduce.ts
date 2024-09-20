20a8dd6f830117d535d2d091c8a64707f710d24b
Al Marks <aomarks@google.com>
2024-09-20 11:36:01 -0700

    Convert reduce

--- packages/core-kit/src/nodes/reduce.ts
+++ packages/core-kit/src/nodes/reduce.ts
@@ -4,116 +4,66 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  InputValues,
-  invokeGraph,
-  NodeHandlerContext,
-  NodeHandlerMetadata,
-  NodeValue,
-  OutputValues,
-  SchemaBuilder,
-} from "@google-labs/breadboard";
+import { array, defineNodeType, object } from "@breadboard-ai/build";
+import { JsonSerializable } from "@breadboard-ai/build/internal/type-system/type.js";
+import { invokeGraph } from "@google-labs/breadboard";
 import { getRunner } from "../utils.js";
 
-export type ReduceInputs = {
-  /**
-   * The list to iterate over.
-   */
-  list: unknown[];
-
-  /**
-   * The board to run for each element of the list.
-   */
-  board?: unknown;
-
-  /**
-   * The initial value for the accumulator.
-   */
-  accumulator?: unknown;
-};
-
-export type ReduceOutputs = {
-  /**
-   * The final value of the accumulator.
-   */
-  accumulator: unknown;
-};
-
-export type ReduceFunctionInputs = {
-  /**
-   * The current value of the accumulator.
-   */
-  accumulator: NodeValue;
-
-  /**
-   * The current item from the list.
-   */
-  item: NodeValue;
-};
-
-const invoke = async (
-  inputs: InputValues,
-  context: NodeHandlerContext
-): Promise<OutputValues> => {
-  const { list, board, accumulator } = inputs;
-  if (!Array.isArray(list)) {
-    throw new Error(`Expected list to be an array, but got ${list}`);
-  }
-  const runnableBoard = await getRunner(board, context);
-  if (!runnableBoard) return { accumulator };
-  let result = accumulator;
-  let index = 0;
-  for (const item of list) {
-    const newContext = {
-      ...context,
-      invocationPath: [...(context?.invocationPath || []), index++],
-    };
-    const { accumulator } = await invokeGraph(
-      runnableBoard,
-      { item, accumulator: result },
-      newContext
-    );
-    result = accumulator;
-  }
-  return { accumulator: result };
-};
-
-const describe = async () => {
-  const inputSchema = new SchemaBuilder()
-    .addProperty("list", {
+export default defineNodeType({
+  name: "reduce",
+  metadata: {
+    title: "Reduce",
+    description:
+      "Given a list, an initial accumulator value, and a board, invokes a board (runOnce) for each item and accumulator in the list and returns the final accumulator value. Loosely, same logic as the `reduce` function in JavaScript.",
+    help: {
+      url: "https://breadboard-ai.github.io/breadboard/docs/kits/core/#the-reduce-component",
+    },
+  },
+  inputs: {
+    list: {
       title: "List",
-      type: "array",
+      type: array("unknown"),
       description: "The list to iterate over.",
-    })
-    .addProperty("board", {
+    },
+    board: {
       title: "Board",
-      type: "object",
+      type: object({}, "unknown"),
       behavior: ["board"],
       description: "The board to run for each element of the list.",
-    })
-    .addProperty("accumulator", {
+    },
+    accumulator: {
       title: "Accumulator",
-      type: "object",
+      type: "unknown",
       description: "The initial value for the accumulator.",
-    })
-    .build();
-  const outputSchema = new SchemaBuilder()
-    .addProperty("accumulator", {
-      title: "Accumulator",
-      type: "object",
-      description: "The final value of the accumulator.",
-    })
-    .build();
-  return { inputSchema, outputSchema };
-};
-
-const metadata = {
-  title: "Reduce",
-  description:
-    "Given a list, an initial accumulator value, and a board, invokes a board (runOnce) for each item and accumulator in the list and returns the final accumulator value. Loosely, same logic as the `reduce` function in JavaScript.",
-  help: {
-    url: "https://breadboard-ai.github.io/breadboard/docs/kits/core/#the-reduce-component",
+    },
   },
-} satisfies NodeHandlerMetadata;
-
-export default { metadata, invoke, describe };
+  outputs: {
+    accumulator: {
+      title: "Accumulator",
+      type: "unknown",
+      description: "The final value of the accumulator.",
+    },
+  },
+  invoke: async ({ list, board, accumulator }, _, context) => {
+    if (!Array.isArray(list)) {
+      throw new Error(`Expected list to be an array, but got ${list}`);
+    }
+    const runnableBoard = await getRunner(board, context);
+    if (!runnableBoard) return { accumulator };
+    let result = accumulator;
+    let index = 0;
+    for (const item of list) {
+      const newContext = {
+        ...context,
+        invocationPath: [...(context?.invocationPath || []), index++],
+      };
+      const { accumulator } = await invokeGraph(
+        runnableBoard,
+        { item, accumulator: result },
+        newContext
+      );
+      result = accumulator as JsonSerializable;
+    }
+    return { accumulator: result };
+  },
+});
