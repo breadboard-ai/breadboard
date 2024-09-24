93d0302665242ab71a8b787ba7dbcd7ffd87975b
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-09-24 15:32:55 +0100

    Building url

--- packages/board-server/src/express/controllers/handlers/invoke.ts
+++ packages/board-server/src/express/controllers/handlers/invoke.ts
@@ -3,24 +3,40 @@ import { verifyKey } from '../../../server/boards/utils/verify-key.js';
 import { secretsKit } from '../../../server/proxy/secrets.js';
 import { loadFromStore } from '../../../server/boards/utils/board-server-provider.js';
 import { invokeBoard } from '../../../server/boards/utils/invoke-board.js';
+import { getStore } from '../../../server/store.js';
 
 const invoke = async (req: Request, res: Response) => {
     const { user, boardName } = req.params;
-    const url = req.url || ""; // TODO(Tina): I don't think this is a full URL. Also, in the old implementation, the url from the board-api-parser is used (see #getAdjustedBoardURL)
     const { inputs } = req.body as Record<string, any>;
     const keyVerificationResult = await verifyKey(user!, boardName!, inputs);
     if (!keyVerificationResult.success) {
-        res.status(200).json({ $error: keyVerificationResult.error }); // TODO(Tina): The original status code was 200, but I feel like it should be 
+        res.status(403).json({ $error: keyVerificationResult.error }); // TODO(Tina): The original status code was 200
         return;
     }
+    const store = getStore();
+
+    const board = await store.get(user!, boardName!);
+
+    if (!board) {
+        res.status(404).json({ error: 'Board not found' });
+        return;
+    }
+
+    const API_ENTRY = "/boards";
+    const userAndBoardName = `@${user}/${boardName}`;
+    const url = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    url.pathname = `${API_ENTRY}/${userAndBoardName}`;
+    url.search = "";
+    const href = url.href.endsWith(".json") ? url.href : `${url.href}.json`;
+
     const result = await invokeBoard({
-        url,
+        url: href,
         path: boardName!,
         inputs,
         loader: loadFromStore,
         kitOverrides: [secretsKit],
-      });
-      res.json({ result })
+    });
+    res.json({ result })
 };
 
 export default invoke;
--- packages/board-server/src/express/controllers/handlers/run.ts
+++ packages/board-server/src/express/controllers/handlers/run.ts
@@ -7,78 +7,75 @@ import { secretsKit } from '../../../server/proxy/secrets.js';
 import type { RemoteMessage } from '@google-labs/breadboard/remote';
 
 const run = async (req: Request, res: Response) => {
-    const { user, boardName } = req.params;
-    // TODO(Tina): The first part of the condition will always be true
-    // const url = `${req.protocol}://${req.get('host')}${req.originalUrl}` || ""; // TODO(Tina): In the old implementation, the url from the board-api-parser is used (see #getAdjustedBoardURL)
-    const API_ENTRY = "/boards";
-    const userAndBoardName = `@${user}/${boardName}`;
-    const url = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`); // Check this
-    url.pathname = `${API_ENTRY}/${userAndBoardName}`;
-    url.search = "";
-    console.log("href:" + url.href);
+  const { user, boardName } = req.params;
+  const API_ENTRY = "/boards";
+  const userAndBoardName = `@${user}/${boardName}`;
+  const url = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  url.pathname = `${API_ENTRY}/${userAndBoardName}`;
+  url.search = "";
+  const href = url.href.endsWith(".json") ? url.href : `${url.href}.json`;
 
-    const {
-        $next: next,
-        $diagnostics: diagnostics,
-        ...inputs
-    } = req.body as Record<string, any>;
-    const writer = new WritableStream<RemoteMessage>({
-        write(chunk) {
-          res.write(`data: ${JSON.stringify(chunk)}\n\n`);
-        },
-      }).getWriter();
-    res.setHeader("Content-Type", "text/event-stream");
-    res.statusCode = 200;
+  const {
+    $next: next,
+    $diagnostics: diagnostics,
+    ...inputs
+  } = req.body as Record<string, any>;
+  const writer = new WritableStream<RemoteMessage>({
+    write(chunk) {
+      res.write(`data: ${JSON.stringify(chunk)}\n\n`);
+    },
+  }).getWriter();
+  res.setHeader("Content-Type", "text/event-stream");
+  res.statusCode = 200;
 
-    const store = getStore();
+  const store = getStore();
 
-    const board = await store.get(user!, boardName!);
+  const board = await store.get(user!, boardName!);
 
-    if (!board) {
-        res.status(404).json({ error: 'Board not found' });
-        return;
-    }
+  if (!board) {
+    res.status(404).json({ error: 'Board not found' });
+    return;
+  }
 
-    const keyVerificationResult = await verifyKey(user!, boardName!, inputs);
+  const keyVerificationResult = await verifyKey(user!, boardName!, inputs);
 
-    if (!keyVerificationResult.success) {
-        await writer.write([
-          "graphstart",
-          {
-            path: [],
-            timestamp: timestamp(),
-            graph: { nodes: [], edges: [] },
-          },
-        ]);
-        await writer.write([
-          "error",
-          { error: "Invalid or missing key", code: 403, timestamp: timestamp() },
-        ]);
-        await writer.write([
-          "graphend",
-          {
-            path: [],
-            timestamp: timestamp(),
-          },
-        ]);
-        await writer.close();
-        res.end();
-        return;
-      }
-      console.log("path:" + board)
-    await runBoard({
-        url: url.href,
-        path: board,
-        user: keyVerificationResult.user!,
-        inputs,
-        loader: loadFromStore,
-        kitOverrides: [secretsKit],
-        writer,
-        next,
-        runStateStore: store,
-        diagnostics,
-    });
+  if (!keyVerificationResult.success) {
+    await writer.write([
+      "graphstart",
+      {
+        path: [],
+        timestamp: timestamp(),
+        graph: { nodes: [], edges: [] },
+      },
+    ]);
+    await writer.write([
+      "error",
+      { error: "Invalid or missing key", code: 403, timestamp: timestamp() },
+    ]);
+    await writer.write([
+      "graphend",
+      {
+        path: [],
+        timestamp: timestamp(),
+      },
+    ]);
+    await writer.close();
     res.end();
+    return;
+  }
+  await runBoard({
+    url: href,
+    path: board,
+    user: keyVerificationResult.user!,
+    inputs,
+    loader: loadFromStore,
+    kitOverrides: [secretsKit],
+    writer,
+    next,
+    runStateStore: store,
+    diagnostics,
+  });
+  res.end();
 };
 
 export default run;
