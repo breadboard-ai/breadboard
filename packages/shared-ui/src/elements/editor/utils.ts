66ab01beb2c40ffdc9b85a0306f181ecf2b19f99
Dimitri Glazkov <dglazkov@google.com>
2024-10-03 13:51:16 -0700

    Make constant edge pasteable.

--- packages/shared-ui/src/elements/editor/utils.ts
+++ packages/shared-ui/src/elements/editor/utils.ts
@@ -4,7 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { InspectableEdgeType, InspectablePort } from "@google-labs/breadboard";
+import {
+  Edge,
+  InspectableEdgeType,
+  InspectablePort,
+} from "@google-labs/breadboard";
 import type { EdgeData } from "../../types/types.js";
 import { ComponentExpansionState, VisualMetadata } from "./types.js";
 
@@ -28,12 +32,7 @@ export function inspectableEdgeToString(edge: EdgeData): string {
   return `${edge.from.descriptor.id}:${edge.out}->${edge.to.descriptor.id}:${edge.in}`;
 }
 
-export function edgeToString(edge: {
-  from: string;
-  to: string;
-  out: string;
-  in: string;
-}): string {
+export function edgeToString(edge: Edge): string {
   const fakeEdge = {
     from: {
       descriptor: {
@@ -49,7 +48,7 @@ export function edgeToString(edge: {
     in: edge.in,
     type: InspectableEdgeType.Ordinary,
   };
-  return inspectableEdgeToString(fakeEdge);
+  return inspectableEdgeToString(fakeEdge as EdgeData);
 }
 
 export const DBL_CLICK_DELTA = 450;
