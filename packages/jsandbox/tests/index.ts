63f2f1073c651272cc03e52d3f6c973ad7c0cf41
Dimitri Glazkov <dimitri@glazkov.com>
2024-10-26 15:36:28 -0700

    One more test.

--- packages/jsandbox/tests/index.ts
+++ packages/jsandbox/tests/index.ts
@@ -8,17 +8,36 @@ import test, { describe } from "node:test";
 import { loadRuntime, RunModuleManager } from "../src/node.js";
 import { deepStrictEqual } from "node:assert";
 
+async function run(
+  code: string,
+  inputs: Record<string, unknown> = {}
+): Promise<Record<string, unknown>> {
+  const wasm = await loadRuntime();
+  const manager = new RunModuleManager(wasm);
+  return manager.runModule(code, inputs);
+}
+
 describe("runtime", () => {
   test("can run a simple module", async () => {
-    const wasm = await loadRuntime();
-    const manager = new RunModuleManager(wasm);
-    const result = await manager.runModule(
-      `
-export default function() {
-  return { result: "HELLO" }
-}`,
-      {}
+    deepStrictEqual(
+      await run(
+        `export default function() {
+        return { result: "HELLO" }
+      }`
+      ),
+      { result: "HELLO" }
     );
-    deepStrictEqual(result, { result: "HELLO" });
+  });
+
+  test("can accept arguments", async () => {
+    deepStrictEqual(
+      await run(
+        `export default function({test}) {
+        return { result: test }
+      }`,
+        { test: "HELLO" }
+      ),
+      { result: "HELLO" }
+    );
   });
 });
