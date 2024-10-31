e6e7f5e3f870ae75fce59c8870bd0bc7d7bbfa9d
Tim Swanson <timswanson@google.com>
2024-10-30 21:41:27 -0600

    Update comment

--- packages/connection-server/src/server.ts
+++ packages/connection-server/src/server.ts
@@ -20,7 +20,7 @@ export function startServer(port: number, config: ServerConfig) {
     cors({
       credentials: true,
       // Different browsers allow different max values for max age. The highest
-      // seems to be Chromium at 24 hours.
+      // seems to be 24 hours.
       // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
       maxAge: 24 * 60 * 60,
       methods: "GET",
