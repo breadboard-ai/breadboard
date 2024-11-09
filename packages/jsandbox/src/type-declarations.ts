89ae9baa191c3090461778d6af05cd713301205b
Dimitri Glazkov <dimitri@glazkov.com>
2024-11-09 10:10:59 -0800

    Write out fetch types.

--- packages/jsandbox/src/type-declarations.ts
+++ packages/jsandbox/src/type-declarations.ts
@@ -24,13 +24,51 @@ declare function encodeURIComponent(
 
 declare module "@fetch" {
   export type FetchInputs = {
+    /**
+     * The URL to fetch
+     */
     url: string;
+    /**
+     * The HTTP method to use. "GET is default.
+     */
+    method?: "GET" | "POST" | "PUT" | "DELETE";
+    /**
+     * Headers to send with request
+     */
+    headers?: Record<string, string>;
+    /**
+     * The body of the request
+     */
+    body?: unknown;
+  };
+
+  export type FetchOutputs = {
+    /**
+     * The response from the fetch request
+     */
+    response: unknown;
+    /**
+     * The HTTP status code of the response
+     */
+    status: number;
+    /**
+     * The status text of the response
+     */
+    statusText: string;
+    /**
+     * The content type of the response
+     */
+    contentType: string;
+    /**
+     * The headers of the response
+     */
+    responseHeaders: Record<string, string>;
   };
 
   /**
    * A built-in capability of Breadboard to fetch data.
    */
-  export default function fetch(url: FetchInputs): Promise<Response>;
+  export default function fetch(url: FetchInputs): Promise<FetchOutputs>;
 }
 
 declare module "@secrets" {
