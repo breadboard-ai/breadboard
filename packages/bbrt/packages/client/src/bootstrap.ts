c47256135c6a4b4c8af131f65aac2576d4dd798e
Al Marks <aomarks@google.com>
2024-11-16 11:50:11 -0800

    [bbrt] Some basic layout and components

---
+++ packages/bbrt/packages/client/src/bootstrap.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import './components/main.js';
+import type {Config} from './config.js';
+
+const config: Config = {
+  url: 'potato',
+};
+const main = document.querySelector('bbrt-main');
+if (main !== null) {
+  main.config = config;
+} else {
+  throw new Error('could not find top-level <bbrt-main> element');
+}
--- packages/bbrt/packages/client/src/config.ts
+++ packages/bbrt/packages/client/src/config.ts
@@ -4,4 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import './component.js';
+export interface Config {
+  url: string;
+}
