a0d9115f25bc0c08231a246cb3c1d92df0c33391
Al Marks <aomarks@google.com>
2024-12-05 14:44:54 -0800

    Initial app state serialize/restore methods

--- packages/bbrt/src/app-state.ts
+++ packages/bbrt/src/app-state.ts
@@ -15,6 +15,13 @@ import { IndexedDBSettingsSecrets } from "./secrets/indexed-db-secrets.js";
 import type { ToolProvider } from "./tools/tool-provider.js";
 import type { BBRTTool } from "./tools/tool.js";
 
+export interface SerializedBBRTAppState {
+  activeDriverName: string;
+  activeToolIds: string[];
+  sidePanelOpen: boolean;
+  // TODO(aomarks) Conversation turns.
+}
+
 export class BBRTAppState {
   readonly secrets = new IndexedDBSettingsSecrets();
   readonly drivers = [
@@ -24,9 +31,45 @@ export class BBRTAppState {
   readonly activeDriver = new Signal.State<BBRTDriver>(this.drivers[0]!);
   readonly toolProviders = new SignalArray<ToolProvider>();
   readonly activeTools = new SignalSet<BBRTTool>();
+  readonly sidePanelOpen = new Signal.State<boolean>(true);
   readonly conversation = new BBRTConversation(
     this.activeDriver,
     this.activeTools
   );
-  readonly sidePanelOpen = new Signal.State<boolean>(true);
+
+  serialize(): SerializedBBRTAppState {
+    return {
+      activeDriverName: this.activeDriver.get().name,
+      activeToolIds: Array.from(this.activeTools).map(
+        (tool) => tool.metadata.id
+      ),
+      sidePanelOpen: this.sidePanelOpen.get(),
+    };
+  }
+
+  restore(serialized: SerializedBBRTAppState) {
+    // TODO(aomarks) Inefficient. Drivers should be in a map.
+    const driver = this.drivers.find(
+      (driver) => driver.name === serialized.activeDriverName
+    );
+    if (driver !== undefined) {
+      this.activeDriver.set(driver);
+    } else {
+      console.error(
+        `Could not find driver ${JSON.stringify(serialized.activeDriverName)}`
+      );
+    }
+
+    // TODO(aomarks) Inefficient. All available tools should be in a map.
+    const activeTools = new Set(serialized.activeToolIds);
+    for (const toolProvider of this.toolProviders) {
+      for (const tool of toolProvider.tools()) {
+        if (activeTools.has(tool.metadata.id)) {
+          this.activeTools.add(tool);
+        }
+      }
+    }
+
+    this.sidePanelOpen.set(serialized.sidePanelOpen);
+  }
 }
