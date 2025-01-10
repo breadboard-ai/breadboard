078bec85ca3cf2ba021ab4ca2e25663e3edcdfb7
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-01-10 11:52:02 +0000

    [board-server] Teach App View about Language Packs (#4091)
    Fixes #4088
    
    Possibly more to do in the next few weeks, but this at least unbreaks
    it.

--- packages/board-server/vite.config.ts
+++ packages/board-server/vite.config.ts
@@ -4,24 +4,46 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 
-export default defineConfig({
-  optimizeDeps: { esbuildOptions: { target: "esnext" } },
-  build: {
-    target: "esnext",
-    outDir: "./dist/client",
-    lib: {
-      entry: {
-        index: "./index.html",
-        api: "./api.html",
-        oauth: "./oauth/index.html",
-        editor: "./experimental/editor.html",
+export default defineConfig(async ({ mode }): Promise<UserConfig> => {
+  const envConfig = { ...loadEnv(mode, process.cwd()) };
+  if (!envConfig.VITE_LANGUAGE_PACK) {
+    throw new Error("Language Pack not specified");
+  }
+
+  const languagePackUrl = await import.meta.resolve(
+    envConfig.VITE_LANGUAGE_PACK
+  );
+
+  let languagePack;
+  try {
+    languagePack = (await import(languagePackUrl)).default;
+  } catch (err) {
+    throw new Error("Unable to import language pack");
+  }
+
+  return {
+    optimizeDeps: { esbuildOptions: { target: "esnext" } },
+    build: {
+      target: "esnext",
+      outDir: "./dist/client",
+      lib: {
+        entry: {
+          index: "./index.html",
+          api: "./api.html",
+          oauth: "./oauth/index.html",
+          editor: "./experimental/editor.html",
+        },
+        formats: ["es"],
       },
-      formats: ["es"],
     },
-  },
-  resolve: {
-    dedupe: ["lit"],
-  },
+    define: {
+      LANGUAGE_PACK: JSON.stringify(languagePack),
+      ASSET_PACK: "{}",
+    },
+    resolve: {
+      dedupe: ["lit"],
+    },
+  };
 });
