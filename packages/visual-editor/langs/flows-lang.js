97f51b06e3a8365c0de9ef627341b601f780aa18
Dimitri Glazkov <dglazkov@google.com>
2025-01-16 12:18:23 -0800

    [flows] Add Flows Lang Pack

---
+++ packages/visual-editor/langs/flows-lang.js
@@ -0,0 +1,462 @@
+export default {
+  ActivityLog: {
+    COMMAND_CONTINUE: {
+      str: "Continue",
+    },
+    COMMAND_RE_RUN: {
+      str: "Re-run",
+    },
+    COMMAND_STEP_TO_NEXT: {
+      str: "Step to next",
+    },
+    COMMAND_CLEAR: {
+      str: "Clear",
+    },
+    COMMAND_DOWNLOAD: {
+      str: "Download",
+    },
+    COMMAND_CREATE_EXPORT: {
+      str: "Create export",
+    },
+    STATUS_GENERATING_EXPORT: {
+      str: "Generating export...",
+    },
+    STATUS_RETRIEVING_VALUES: {
+      str: "Retrieving values...",
+    },
+    LABEL_WAITING_MESSAGE: {
+      str: "Tap Test Flow to begin",
+    },
+    LABEL_DETAILS: {
+      str: "Details",
+    },
+  },
+  AppPreview: {
+    COMMAND_CONTINUE: {
+      str: "Continue",
+    },
+    STATUS_GENERATING_EXPORT: {
+      str: "Generating export...",
+    },
+    STATUS_RETRIEVING_VALUES: {
+      str: "Retrieving values...",
+    },
+    LABEL_WAITING_MESSAGE: {
+      str: "Tap Start to begin",
+    },
+    LABEL_DETAILS: {
+      str: "Details",
+    },
+    LABEL_START: {
+      str: "Start",
+    },
+    LABEL_INITIAL_MESSAGE: {
+      str: "Tap Start to begin",
+    },
+    LABEL_FOOTER: {
+      str: "Made with",
+    },
+    LABEL_UNTITLED_APP: {
+      str: "Untitled App",
+    },
+    QUERY_RESTART: {
+      str: "Would you like to restart?",
+    },
+  },
+  AssetOrganizer: {
+    COMMAND_TOGGLE_EXPAND: {
+      str: "Toggle Expand",
+    },
+    COMMAND_TOGGLE_VIEWER: {
+      str: "Viewer",
+    },
+    LABEL_WAITING_MESSAGE: {
+      str: "No value selected",
+    },
+    LABEL_TITLE: {
+      str: "Assets",
+    },
+    LABEL_NO_ASSETS: {
+      str: "There are no assets yet",
+    },
+  },
+  CommandPalette: {
+    LABEL_NO_OPTIONS: {
+      str: "No options available",
+    },
+  },
+  ComponentSelector: {
+    STATUS_NO_ITEMS: {
+      str: "No items",
+    },
+    LABEL_TITLE: {
+      str: "Steps",
+    },
+    LABEL_BUTTON_COMPONENTS: {
+      str: "Steps",
+    },
+    LABEL_BUTTON_KITS: {
+      str: "Library",
+    },
+    LABEL_SEARCH: {
+      str: "Search Steps",
+    },
+  },
+  Editor: {
+    COMMAND_ADD_MISSING_ITEM: {
+      str: "Add",
+    },
+    COMMAND_SHOW_LIBRARY: {
+      str: "Show Library",
+    },
+    COMMAND_LIBRARY_GROUP_1: {
+      str: "Show AI Steps",
+    },
+    COMMAND_LIBRARY_GROUP_2: {
+      str: "Show Control Flow Steps",
+    },
+    COMMAND_LIBRARY_GROUP_3: {
+      str: "Show Tools",
+    },
+    COMMAND_ZOOM_TO_FIT: {
+      str: "Fit to Screen",
+    },
+    LABEL_COMPONENT_LIBRARY: {
+      str: "Step Library",
+    },
+    LABEL_SHOW_LIST: {
+      str: "Show List",
+    },
+    LABEL_NO_COMPONENTS: {
+      str: "No Steps available",
+    },
+    LABEL_ADD_ITEM: {
+      str: "Add Step",
+    },
+    LABEL_MISSING_ITEM: {
+      str: "Missing details for this Step",
+    },
+    LABEL_NO_MISSING_ITEM_INFO: {
+      str: "There is no additional information needed for this Step.",
+    },
+  },
+  Global: {
+    APP_NAME: {
+      str: "Gemini Flows",
+    },
+    SUB_APP_NAME: {
+      str: "Visual Editor",
+    },
+    COMMAND_OPEN_PROJECT: {
+      str: "Open Flow...",
+    },
+    COMMAND_SAVE_PROJECT: {
+      str: "Save Flow",
+    },
+    COMMAND_COPY_PROJECT_URL: {
+      str: "Copy Flow URL",
+    },
+    COMMAND_SAVE_PROJECT_AS: {
+      str: "Save Flow as...",
+    },
+    COMMAND_COPY_FULL_URL: {
+      str: "Copy full URL",
+    },
+    COMMAND_COPY_APP_PREVIEW_URL: {
+      str: "Copy App Preview URL",
+    },
+    COMMAND_EDIT_PROJECT_INFORMATION: {
+      str: "Edit Flow information...",
+    },
+    COMMAND_OPEN_MODULE: {
+      str: "Open Module...",
+    },
+    COMMAND_OPEN: {
+      str: "Open",
+    },
+    COMMAND_CREATE_MODULE: {
+      str: "Create Module...",
+    },
+    COMMAND_COPY_PROJECT_CONTENTS: {
+      str: "Copy Flow Contents",
+    },
+    COMMAND_DELETE_PROJECT: {
+      str: "Delete Flow",
+    },
+    COMMAND_EXPORT_PROJECT: {
+      str: "Export Flow",
+    },
+    COMMAND_EDIT_SETTINGS: {
+      str: "Edit Settings",
+    },
+    COMMAND_ADDITIONAL_ITEMS: {
+      str: "See additional items",
+    },
+    LABEL_RUN: {
+      str: "Start",
+    },
+    LABEL_STOP: {
+      str: "Stop",
+    },
+    LABEL_RUN_PROJECT: {
+      str: "Run this Flow",
+    },
+    LABEL_UNDO: {
+      str: "Undo",
+    },
+    LABEL_REDO: {
+      str: "Redo",
+    },
+    LABEL_SAVE_STATUS_SAVED: {
+      str: "Saved",
+    },
+    LABEL_SAVE_STATUS_SAVING: {
+      str: "Saving",
+    },
+    LABEL_SAVE_STATUS_READ_ONLY: {
+      str: "Read Only",
+    },
+    LABEL_SAVE_ERROR: {
+      str: "Error",
+    },
+    LABEL_SAVE_UNSAVED: {
+      str: "Unsaved",
+    },
+    LABEL_MAIN_PROJECT: {
+      str: "Main Flow...",
+    },
+    STATUS_SAVING_PROJECT: {
+      str: "Saving Flow",
+    },
+    STATUS_DELETING_PROJECT: {
+      str: "Deleting Flow",
+    },
+    STATUS_GENERIC_WORKING: {
+      str: "Working...",
+    },
+    STATUS_GENERIC_LOADING: {
+      str: "Loading...",
+    },
+    STATUS_GENERIC_DONE: {
+      str: "Done",
+    },
+    STATUS_GENERIC_RUN_STOPPED: {
+      str: "Run stopped",
+    },
+    STATUS_PROJECT_SAVED: {
+      str: "Flow saved",
+    },
+    STATUS_PROJECT_CONFIGURATION_SAVED: {
+      str: "Flow and configuration saved",
+    },
+    STATUS_PROJECT_DELETED: {
+      str: "Flow deleted",
+    },
+    STATUS_SAVED_SETTINGS: {
+      str: "Saved settings",
+    },
+    STATUS_PROJECTS_REFRESHED: {
+      str: "Flows refreshed",
+    },
+    STATUS_PROJECT_CONTENTS_COPIED: {
+      str: "Flow contents copied to clipboard",
+    },
+    STATUS_PROJECT_URL_COPIED: {
+      str: "Flow URL copied to clipboard",
+    },
+    STATUS_FULL_URL_COPIED: {
+      str: "Full URL copied to clipboard",
+    },
+    STATUS_APP_PREVIEW_URL_COPIED: {
+      str: "App Preview URL copied to clipboard",
+    },
+    ERROR_UNABLE_TO_CREATE_PROJECT: {
+      str: "Unable to create Flow",
+    },
+    ERROR_UNABLE_TO_LOAD_PROJECT: {
+      str: "Unable to load Flow",
+    },
+    ERROR_NO_PROJECT: {
+      str: "Unable to edit; no Flow found",
+    },
+    ERROR_GENERIC: {
+      str: "An error occurred",
+    },
+    ERROR_RUN_LOAD_DATA_FAILED: {
+      str: "Unable to load run data",
+    },
+    ERROR_LOAD_FAILED: {
+      str: "Unable to load data",
+    },
+    ERROR_SAVE_SETTINGS: {
+      str: "Unable to save settings",
+    },
+    ERROR_UNABLE_TO_REFRESH_PROJECTS: {
+      str: "Unable to refresh Flows",
+    },
+    ERROR_UNABLE_TO_RETRIEVE_TYPE_INFO: {
+      str: "Error retrieving type information; try adding it again",
+    },
+    TITLE_CREATE_PROJECT: {
+      str: "Create new Flow",
+    },
+    TITLE_UNTITLED_PROJECT: {
+      str: "Untitled Flow",
+    },
+    QUERY_DELETE_PROJECT: {
+      str: "Are you sure you want to delete this Flow? This cannot be undone",
+    },
+    QUERY_SAVE_PROJECT: {
+      str: "The current Flow isn't saved - would you like to save first?",
+    },
+  },
+  KitSelector: {
+    LABEL_TITLE: {
+      str: "Library",
+    },
+  },
+  ProjectListing: {
+    COMMAND_NEW_PROJECT: {
+      str: "New Flow",
+    },
+    COMMAND_ADD_NEW_PROJECT_SERVER: {
+      str: "Add new Flow Server",
+    },
+    COMMAND_REFRESH_PROJECT_SERVER: {
+      str: "Refresh Flow Server",
+    },
+    COMMAND_REMOVE_PROJECT_SERVER: {
+      str: "Remove Flow Server",
+    },
+    COMMAND_RENEW_ACCESS: {
+      str: "Renew Access",
+    },
+    STATUS_LOADING: {
+      str: "Loading...",
+    },
+    LABEL_FEATURED_GUIDES: {
+      str: "Featured Guides",
+    },
+    LABEL_PROJECT_SERVER_SETTINGS: {
+      str: "Flow Server Settings",
+    },
+    LABEL_LIST_OTHERS_PROJECTS: {
+      str: "List others' Flows",
+    },
+    LABEL_NO_VERSION: {
+      str: "(no version)",
+    },
+    LABEL_NO_DESCRIPTION: {
+      str: "(no description)",
+    },
+    LABEL_NO_OWNER: {
+      str: "(no owner)",
+    },
+    LABEL_TABLE_HEADER_NAME: {
+      str: "Name",
+    },
+    LABEL_TABLE_HEADER_VERSION: {
+      str: "Version",
+    },
+    LABEL_TABLE_HEADER_DESCRIPTION: {
+      str: "Description",
+    },
+    LABEL_TABLE_HEADER_TAGS: {
+      str: "Tags",
+    },
+    LABEL_TABLE_HEADER_OWNER: {
+      str: "Owner",
+    },
+    LABEL_TABLE_DESCRIPTION_YOUR_PROJECTS: {
+      str: "Your Flow",
+    },
+    LABEL_TABLE_DESCRIPTION_OTHER_PEOPLES_PROJECTS: {
+      str: "Other people's Flows",
+    },
+    LABEL_NO_PROJECTS_FOUND: {
+      str: "No Flows found",
+    },
+    LABEL_ACCESS_EXPIRED_PROJECT_SERVER: {
+      str: "Access has expired for this Flow Server",
+    },
+    LABEL_APP_VERSION: {
+      str: "Version",
+    },
+    LABEL_SEARCH_BOARDS: {
+      str: "Search Flows",
+    },
+    QUERY_CONFIRM_REMOVE_SERVER: {
+      str: "Are you sure you want to remove this Flow Server?",
+    },
+    ERROR_LOADING_PROJECTS: {
+      str: "Error loading Flow Server",
+    },
+  },
+  UIController: {
+    COMMAND_BACK_TO_CONSOLE: {
+      str: "Back to Console",
+    },
+    COMMAND_CONTINUE: {
+      str: "Continue",
+    },
+    COMMAND_START: {
+      str: "Test Flow",
+    },
+    LABEL_SECTION_NAV_CHAT: {
+      str: "Chat",
+    },
+    LABEL_SECTION_NAV_CONSOLE: {
+      str: "Console",
+    },
+    LABEL_SECTION_NAV_PROJECT: {
+      str: "Flow",
+    },
+    LABEL_SECTION_NAV_CAPABILITIES: {
+      str: "Capabilities",
+    },
+    LABEL_SECTION_NAV_COMPONENTS: {
+      str: "Steps",
+    },
+    LABEL_PROJECT: {
+      str: "Flow",
+    },
+    LABEL_APP_LAYOUT: {
+      str: "App Layout",
+    },
+    LABEL_APP_LAYOUT_DESCRIPTION: {
+      str: "Choose a layout to style your app",
+    },
+    LABEL_TEMPLATE_1: {
+      str: "Default",
+    },
+    LABEL_TEMPLATE_2: {
+      str: "Dark",
+    },
+    LABEL_TEMPLATE_3: {
+      str: "Light",
+    },
+    LABEL_TEST: {
+      str: "Test Flow",
+    },
+    LABEL_PUBLIC: {
+      str: "Public",
+    },
+    LABEL_SHARE: {
+      str: "Share",
+    },
+    LABEL_SHARE_DESCRIPTION: {
+      str: "Share this URL with friends",
+    },
+    LABEL_TOGGLE_EXPAND: {
+      str: "Toggle expand",
+    },
+  },
+  WorkspaceOutline: {
+    COMMAND_CREATE: {
+      str: "Create",
+    },
+    LABEL_SEARCH: {
+      str: "Search",
+    },
+  },
+};
