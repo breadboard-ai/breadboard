abe4580456fe8de3c052da29534b03e812e843a2
Tim Swanson <timswanson@google.com>
2025-01-16 14:43:29 -0700

    [unified-server] Add endpoints from connection server
    Part of #3913

--- packages/unified-server/src/server/main.ts
+++ packages/unified-server/src/server/main.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+import { createServer } from "@breadboard-ai/connection-server/server.js";
+
 const app = express();
+
+app.use("/connection", createServer());
 
 ViteExpress.listen(app, 3000, () => {
   console.log("Server is listening on port 3000");
