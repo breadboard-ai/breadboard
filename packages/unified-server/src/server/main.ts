c34dadb8ce9f1584360f110b1e3fd72df79c005f
Tim Swanson <152931212+timswanson-google@users.noreply.github.com>
2025-01-17 11:24:51 -0700

    Revert "[unified-server] Add board server to unified server (#4145)" (#4152)
    This reverts commit 9600786936f96091ecfac60668c69a2aa1151d75.
    
    Broke tests because unified server wouldn't start up
    
    Part of #3913

--- packages/unified-server/src/server/main.ts
+++ packages/unified-server/src/server/main.ts
@@ -2,7 +2,6 @@ import express from "express";
 import ViteExpress from "vite-express";
 
 import { createServer } from "@breadboard-ai/connection-server/server.js";
-import { makeRouter } from "@breadboard-ai/board-server/router";
 
 const app = express();
 
@@ -11,18 +10,6 @@ app.use(
   createServer({
     connections: new Map(),
     allowedOrigins: [],
-  })
-);
-
-app.use(
-  "/board",
-  makeRouter({
-    allowedOrigins: new Set(),
-    hostname: "",
-    serverUrl: "",
-    viteDevServer: null,
-    rootPath: "",
-    storageBucket: "",
   })
 );
 
