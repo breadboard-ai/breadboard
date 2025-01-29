77c6a67e86bb4593c262497d7c14478aca9aac28
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-01-29 16:03:57 +0000

    [shared-ui] Fast Access Improvements (#4262)
    #4207 vibes
    
    - Fixes a few issues around undo/redo, cut and paste, etc
    - Enables keyboard navigation for selecting the item from the Fast
    Access menu
    - Cleans the output

---
+++ .changeset/curly-flies-shout.md
@@ -0,0 +1,5 @@
+---
+"@breadboard-ai/shared-ui": patch
+---
+
+Fast Access Improvements
--- packages/shared-ui/src/elements/fast-access-menu/fast-access-menu.ts
+++ packages/shared-ui/src/elements/fast-access-menu/fast-access-menu.ts
@@ -11,10 +11,15 @@
  */
 import { SignalWatcher } from "@lit-labs/signals";
 import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { FastAccess } from "../../state";
+import { customElement, property, state } from "lit/decorators.js";
+import { Component, FastAccess, GraphAsset, Tool } from "../../state";
 import { GraphIdentifier, NodeIdentifier } from "@breadboard-ai/types";
-import { FastAccessSelectEvent } from "../../events/events";
+import {
+  FastAccessDismissedEvent,
+  FastAccessSelectEvent,
+} from "../../events/events";
+import { classMap } from "lit/directives/class-map.js";
+import { createRef, ref, Ref } from "lit/directives/ref.js";
 
 @customElement("bb-fast-access-menu")
 export class FastAccessMenu extends SignalWatcher(LitElement) {
@@ -26,6 +31,9 @@ export class FastAccessMenu extends SignalWatcher(LitElement) {
 
   @property()
   accessor nodeId: NodeIdentifier | null = null;
+
+  @state()
+  accessor selectedIndex = 0;
 
   static styles = css`
     :host {
@@ -40,6 +48,12 @@ export class FastAccessMenu extends SignalWatcher(LitElement) {
       box-shadow: var(--bb-elevation-6);
       padding: var(--bb-grid-size-2) var(--bb-grid-size-3);
       white-space: normal;
+    }
+
+    .no-items {
+      font: 400 var(--bb-body-small) / var(--bb-body-line-height-small)
+        var(--bb-font-family);
+      margin-bottom: var(--bb-grid-size-2);
     }
 
     #assets,
@@ -78,8 +92,9 @@ export class FastAccessMenu extends SignalWatcher(LitElement) {
           &:not([disabled]) {
             cursor: pointer;
 
+            &.active,
             &:hover {
-              background-color: var(--bb-neutral-50);
+              background-color: var(--bb-neutral-100);
             }
           }
         }
@@ -99,6 +114,161 @@ export class FastAccessMenu extends SignalWatcher(LitElement) {
     }
   `;
 
+  #itemContainerRef: Ref<HTMLDivElement> = createRef();
+  #onKeyDownBound = this.#onKeyDown.bind(this);
+  #onEscapeBound = this.#onEscape.bind(this);
+
+  #items: {
+    assets: GraphAsset[];
+    tools: Tool[];
+    components: Component[];
+  } = { assets: [], tools: [], components: [] };
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    window.addEventListener("keydown", this.#onEscapeBound, { capture: true });
+    window.addEventListener("keydown", this.#onKeyDownBound);
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+
+    window.addEventListener("keydown", this.#onEscapeBound, { capture: true });
+    window.removeEventListener("keydown", this.#onKeyDownBound);
+  }
+
+  protected willUpdate(): void {
+    const graphId = this.graphId || "";
+    const assets = [...(this.state?.graphAssets.values() || [])];
+    const tools = [...(this.state?.tools.values() || [])];
+    const components = [
+      ...(this.state?.components.get(graphId)?.values() || []),
+    ];
+
+    this.#items = {
+      assets,
+      tools,
+      components,
+    };
+  }
+
+  updated() {
+    if (!this.#itemContainerRef.value) {
+      return;
+    }
+
+    const button = this.#itemContainerRef.value.querySelector("button.active");
+    if (!button) {
+      return;
+    }
+
+    button.scrollIntoView({ block: "nearest" });
+  }
+
+  #onEscape(evt: KeyboardEvent): void {
+    if (!this.classList.contains("active")) {
+      return;
+    }
+
+    if (evt.key !== "Escape") {
+      return;
+    }
+
+    this.dispatchEvent(new FastAccessDismissedEvent());
+    evt.stopImmediatePropagation();
+  }
+
+  #onKeyDown(evt: KeyboardEvent): void {
+    if (!this.classList.contains("active")) {
+      return;
+    }
+
+    const totalSize =
+      this.#items.assets.length +
+      this.#items.tools.length +
+      this.#items.components.length;
+
+    switch (evt.key) {
+      case "Enter": {
+        evt.stopImmediatePropagation();
+        this.#emitCurrentItem();
+        break;
+      }
+
+      case "ArrowUp": {
+        this.selectedIndex = this.#clamp(
+          this.selectedIndex - 1,
+          0,
+          totalSize - 1
+        );
+        break;
+      }
+
+      case "Tab":
+      case "ArrowDown": {
+        evt.preventDefault();
+
+        this.selectedIndex = this.#clamp(
+          this.selectedIndex + 1,
+          0,
+          totalSize - 1
+        );
+        break;
+      }
+    }
+  }
+
+  #clamp(value: number, min: number, max: number) {
+    if (value < min) {
+      value = min;
+    }
+
+    if (value > max) {
+      value = max;
+    }
+
+    return value;
+  }
+
+  #emitCurrentItem() {
+    let idx = this.selectedIndex;
+    if (idx < this.#items.assets.length) {
+      const asset = this.#items.assets[idx];
+      this.dispatchEvent(
+        new FastAccessSelectEvent(
+          asset.path,
+          asset.metadata?.title ?? "Untitled asset",
+          "asset"
+        )
+      );
+      return;
+    }
+
+    idx -= this.#items.assets.length;
+    if (idx < this.#items.tools.length) {
+      const tool = this.#items.tools[idx];
+      this.dispatchEvent(
+        new FastAccessSelectEvent(
+          tool.url,
+          tool.title ?? "Untitled tool",
+          "tool"
+        )
+      );
+      return;
+    }
+
+    idx -= this.#items.tools.length;
+    if (idx < this.#items.tools.length) {
+      const component = this.#items.components[idx];
+      this.dispatchEvent(
+        new FastAccessSelectEvent(component.id, component.title, "output")
+      );
+      return;
+    }
+    console.warn("Index out of bounds for fast access selection");
+  }
+
   render() {
     const graphId = this.graphId || "";
     const assets = [...(this.state?.graphAssets.values() || [])];
@@ -106,73 +276,85 @@ export class FastAccessMenu extends SignalWatcher(LitElement) {
     const components = [
       ...(this.state?.components.get(graphId)?.values() || []),
     ];
-    return html` <section id="assets">
+    let idx = 0;
+    return html` <div ${ref(this.#itemContainerRef)}>
+      <section id="assets">
         <h3>Assets</h3>
-        <menu>
-          ${assets.map((asset) => {
-            return html`<li>
-              <button
-                @click=${() => {
-                  this.dispatchEvent(
-                    new FastAccessSelectEvent(
-                      asset.path,
-                      asset.metadata?.title ?? "Untitled asset",
-                      "asset"
-                    )
-                  );
-                }}
-              >
-                ${asset.metadata?.title}
-              </button>
-            </li>`;
-          })}
-        </menu>
+        ${assets.length
+          ? html` <menu>
+              ${assets.map((asset) => {
+                const active = idx === this.selectedIndex;
+                const globalIndex = idx;
+                idx++;
+                return html`<li>
+                  <button
+                    class=${classMap({ active })}
+                    @pointerover=${() => {
+                      this.selectedIndex = globalIndex;
+                    }}
+                    @click=${() => {
+                      this.#emitCurrentItem();
+                    }}
+                  >
+                    ${asset.metadata?.title}
+                  </button>
+                </li>`;
+              })}
+            </menu>`
+          : html`<div class="no-items">No assets</div>`}
       </section>
 
       <section id="tools">
         <h3>Tools</h3>
-        <menu>
-          ${tools.map((tool) => {
-            return html`<li>
-              <button
-                @click=${() => {
-                  this.dispatchEvent(
-                    new FastAccessSelectEvent(
-                      tool.url,
-                      tool.title ?? "Untitled tool",
-                      "tool"
-                    )
-                  );
-                }}
-              >
-                ${tool.title}
-              </button>
-            </li>`;
-          })}
-        </menu>
+        ${tools.length
+          ? html` <menu>
+              ${tools.map((tool) => {
+                const active = idx === this.selectedIndex;
+                const globalIndex = idx;
+                idx++;
+                return html`<li>
+                  <button
+                    class=${classMap({ active })}
+                    @pointerover=${() => {
+                      this.selectedIndex = globalIndex;
+                    }}
+                    @click=${() => {
+                      this.#emitCurrentItem();
+                    }}
+                  >
+                    ${tool.title}
+                  </button>
+                </li>`;
+              })}
+            </menu>`
+          : html`<div class="no-items">No tools</div>`}
       </section>
 
       <section id="outputs">
         <h3>Outputs</h3>
-        <menu>
-          ${components.map((component) => {
-            return html`<li>
-              <button
-                @click=${() => {
-                  this.dispatchEvent(
-                    new FastAccessSelectEvent(
-                      component.id,
-                      component.title,
-                      "output"
-                    )
-                  );
-                }}
-              >
-                ${component.title}
-              </button>
-            </li>`;
-          })}
-        </menu>
-      </section>`;
+        ${components.length
+          ? html` <menu>
+              ${components.map((component) => {
+                const active = idx === this.selectedIndex;
+                const globalIndex = idx;
+                idx++;
+                return html`<li>
+                  <button
+                    class=${classMap({ active })}
+                    @pointerover=${() => {
+                      this.selectedIndex = globalIndex;
+                    }}
+                    @click=${() => {
+                      this.#emitCurrentItem();
+                    }}
+                  >
+                    ${component.title}
+                  </button>
+                </li>`;
+              })}
+            </menu>`
+          : html`<div class="no-items">No components</div>`}
+      </section>
+    </div>`;
   }
 }
