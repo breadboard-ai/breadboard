7efdb2d77fcd1e89611f01ff4fd13eca31e4514c
Al Marks <aomarks@google.com>
2025-01-29 17:05:48 -0800

    Allow app-preview to take EdgeLogEntry instead of InspectableRunNodeEvent as a pending input

--- packages/shared-ui/src/elements/app-preview/app-preview.ts
+++ packages/shared-ui/src/elements/app-preview/app-preview.ts
@@ -30,7 +30,11 @@ import { InputEnterEvent, RunEvent, StopEvent } from "../../events/events.js";
 import { until } from "lit/directives/until.js";
 import { DirectiveResult } from "lit/directive.js";
 import { cache, CacheDirective } from "lit/directives/cache.js";
-import { UserInputConfiguration } from "../../types/types.js";
+import {
+  EdgeLogEntry,
+  LogEntry,
+  UserInputConfiguration,
+} from "../../types/types.js";
 import {
   isLLMContentArrayBehavior,
   isLLMContentBehavior,
@@ -56,7 +60,7 @@ export class AppPreview extends LitElement {
   accessor inputsFromLastRun: InspectableRunInputs | null = null;
 
   @property({ reflect: false })
-  accessor events: InspectableRunEvent[] | null = null;
+  accessor events: InspectableRunEvent[] | LogEntry[] | null = null;
 
   @property()
   accessor boardServers: BoardServer[] = [];
@@ -87,112 +91,129 @@ export class AppPreview extends LitElement {
     });
   }
 
-  async #renderPendingInput(event: InspectableRunNodeEvent | null) {
+  async #renderPendingInput(
+    event: InspectableRunNodeEvent | EdgeLogEntry | null
+  ) {
     let userInput: HTMLTemplateResult | DirectiveResult<typeof CacheDirective> =
       cache(html`<div class="no-input-needed"></div>`);
     let continueRun: (() => void) | null = null;
 
     if (event !== null) {
-      const { inputs, node } = event;
-      const nodeSchema = await node.describe(inputs);
-      const descriptor = node.descriptor;
-      const schema = nodeSchema?.outputSchema || inputs.schema;
-      const requiredFields = schema.required ?? [];
-
-      if (!schema.properties || Object.keys(schema.properties).length === 0) {
-        this.dispatchEvent(
-          new InputEnterEvent(descriptor.id, {}, /* allowSavingIfSecret */ true)
-        );
+      let descriptor, schema;
+      if ("inputs" in event) {
+        event satisfies InspectableRunNodeEvent;
+        const { inputs, node } = event;
+        const nodeSchema = await node.describe(inputs);
+        descriptor = node.descriptor;
+        schema = nodeSchema?.outputSchema || inputs.schema;
+      } else {
+        event satisfies EdgeLogEntry;
+        descriptor = event.descriptor;
+        schema = event.schema;
       }
 
-      // TODO: Implement support for multiple iterations over the
-      // same input over a run. Currently, we will only grab the
-      // first value.
-      const values = this.inputsFromLastRun?.get(descriptor.id)?.[0];
-      const userInputs: UserInputConfiguration[] = Object.entries(
-        schema.properties ?? {}
-      ).reduce((prev, [name, schema]) => {
-        let value = values ? values[name] : undefined;
-        if (schema.type === "object") {
-          if (isLLMContentBehavior(schema)) {
-            if (!isLLMContent(value)) {
-              value = undefined;
+      if (schema && descriptor) {
+        const requiredFields = schema.required ?? [];
+
+        if (!schema.properties || Object.keys(schema.properties).length === 0) {
+          this.dispatchEvent(
+            new InputEnterEvent(
+              descriptor.id,
+              {},
+              /* allowSavingIfSecret */ true
+            )
+          );
+        }
+
+        // TODO: Implement support for multiple iterations over the
+        // same input over a run. Currently, we will only grab the
+        // first value.
+        const values = this.inputsFromLastRun?.get(descriptor.id)?.[0];
+        const userInputs: UserInputConfiguration[] = Object.entries(
+          schema.properties ?? {}
+        ).reduce((prev, [name, schema]) => {
+          let value = values ? values[name] : undefined;
+          if (schema.type === "object") {
+            if (isLLMContentBehavior(schema)) {
+              if (!isLLMContent(value)) {
+                value = undefined;
+              }
+            } else {
+              value = JSON.stringify(value, null, 2);
             }
-          } else {
-            value = JSON.stringify(value, null, 2);
           }
-        }
 
-        if (schema.type === "array") {
-          if (isLLMContentArrayBehavior(schema)) {
-            if (!isLLMContentArray(value)) {
-              value = undefined;
+          if (schema.type === "array") {
+            if (isLLMContentArrayBehavior(schema)) {
+              if (!isLLMContentArray(value)) {
+                value = undefined;
+              }
+            } else {
+              value = JSON.stringify(value, null, 2);
             }
-          } else {
-            value = JSON.stringify(value, null, 2);
           }
-        }
 
-        if (schema.type === "string" && typeof value === "object") {
-          value = undefined;
-        }
+          if (schema.type === "string" && typeof value === "object") {
+            value = undefined;
+          }
 
-        prev.push({
-          name,
-          title: schema.title ?? name,
-          secret: false,
-          schema,
-          configured: false,
-          required: requiredFields.includes(name),
-          value,
-        });
+          prev.push({
+            name,
+            title: schema.title ?? name,
+            secret: false,
+            schema,
+            configured: false,
+            required: requiredFields.includes(name),
+            value,
+          });
 
-        return prev;
-      }, [] as UserInputConfiguration[]);
+          return prev;
+        }, [] as UserInputConfiguration[]);
 
-      continueRun = () => {
-        if (!this.#userInputRef.value) {
-          return;
-        }
-
-        const outputs = this.#userInputRef.value.processData(true);
-        if (!outputs) {
-          return;
-        }
-
-        this.dispatchEvent(
-          new InputEnterEvent(
-            descriptor.id,
-            outputs,
-            /* allowSavingIfSecret */ true
-          )
-        );
-      };
-
-      userInput = html`<bb-user-input
-        .boardServers=${this.boardServers}
-        .showTypes=${false}
-        .showTitleInfo=${false}
-        .inputs=${userInputs}
-        .inlineControls=${true}
-        .llmInputShowEntrySelector=${false}
-        .useChatInput=${true}
-        ${ref(this.#userInputRef)}
-        @keydown=${(evt: KeyboardEvent) => {
-          const isMac = navigator.platform.indexOf("Mac") === 0;
-          const isCtrlCommand = isMac ? evt.metaKey : evt.ctrlKey;
-
-          if (!(evt.key === "Enter" && isCtrlCommand)) {
+        continueRun = () => {
+          if (!this.#userInputRef.value) {
             return;
           }
 
-          if (!continueRun) {
+          const outputs = this.#userInputRef.value.processData(true);
+          if (!outputs) {
             return;
           }
 
-          continueRun();
-        }}
-      ></bb-user-input>`;
+          this.dispatchEvent(
+            new InputEnterEvent(
+              descriptor.id,
+              outputs,
+              /* allowSavingIfSecret */ true
+            )
+          );
+        };
+
+        userInput = html`<bb-user-input
+          .boardServers=${this.boardServers}
+          .showTypes=${false}
+          .showTitleInfo=${false}
+          .inputs=${userInputs}
+          .inlineControls=${true}
+          .llmInputShowEntrySelector=${false}
+          .useChatInput=${true}
+          ${ref(this.#userInputRef)}
+          @keydown=${(evt: KeyboardEvent) => {
+            const isMac = navigator.platform.indexOf("Mac") === 0;
+            const isCtrlCommand = isMac ? evt.metaKey : evt.ctrlKey;
+
+            if (!(evt.key === "Enter" && isCtrlCommand)) {
+              return;
+            }
+
+            if (!continueRun) {
+              return;
+            }
+
+            continueRun();
+          }}
+        ></bb-user-input>`;
+      }
     }
 
     return html`${userInput}
@@ -213,6 +234,22 @@ export class AppPreview extends LitElement {
   render() {
     const isRunning = this.state?.status === "running";
     const newestEvent = this.events?.at(-1);
+
+    const pendingInput: InspectableRunNodeEvent | EdgeLogEntry | null = (() => {
+      if (newestEvent) {
+        if (
+          newestEvent.type === "node" &&
+          "node" in newestEvent &&
+          newestEvent.node.descriptor.type === "input"
+        ) {
+          return newestEvent satisfies InspectableRunNodeEvent;
+        }
+        if (newestEvent.type === "edge") {
+          return newestEvent satisfies EdgeLogEntry;
+        }
+      }
+      return null;
+    })();
 
     return html` <section
       id="content"
@@ -280,16 +317,7 @@ export class AppPreview extends LitElement {
         ></bb-chat>
       </div>
 
-      <div id="input">
-        ${until(
-          this.#renderPendingInput(
-            newestEvent?.type === "node" &&
-              newestEvent.node.descriptor.type === "input"
-              ? newestEvent
-              : null
-          )
-        )}
-      </div>
+      <div id="input">${until(this.#renderPendingInput(pendingInput))}</div>
       <footer>
         ${Strings.from("LABEL_FOOTER")} ${GlobalStrings.from("APP_NAME")}
       </footer>
