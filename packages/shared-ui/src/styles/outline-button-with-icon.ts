2151a9f9fc278fe539326d6ed6e81d61254605cb
Al Marks <aomarks@google.com>
2025-02-25 15:33:18 -0800

    Add outline-button-with-icon shared class

---
+++ packages/shared-ui/src/styles/outline-button-with-icon.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { css } from "lit";
+
+export const outlineButtonWithIcon = css`
+  .bb-outline-button-with-icon {
+    background: var(--bb-icon, var(--bb-icon-help)) 16px center / 20px 20px
+      no-repeat;
+    color: #3271ea;
+    border: 1px solid currentColor;
+    border-radius: 100px;
+    padding: 10px 24px 10px 42px;
+    font-size: 16px;
+
+    &:not([disabled]) {
+      cursor: pointer;
+      transition: opacity 0.2s cubic-bezier(0, 0, 0.3, 1);
+
+      &:focus,
+      &:hover {
+        filter: brightness(0.8);
+      }
+    }
+  }
+`;
