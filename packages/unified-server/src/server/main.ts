648b54a592f735f82b29b8a8bbfe8dbe9665626c
Tim Swanson <152931212+timswanson-google@users.noreply.github.com>
2025-02-26 14:09:00 -0700

    [board-server unified-server] Refactor board server base to use Express (#4585)
    This change reworks the initialization logic for board server so that it
    is created as an Express app, even in the board-server package. This
    gets rid of the need to export `makeRouter` from the board-server
    package, so now only the single module is exported. The package.json is
    updated accordingly.
    
    Everything needed outside of the board server package should be exported
    by the `server` module, which is the default export of the package. At
    the moment this includes `createServer`, `createServerConfig`, and the
    config type.
    
    Part of #3172 and #4353

--- packages/unified-server/src/server/main.ts
+++ packages/unified-server/src/server/main.ts
@@ -1,35 +1,33 @@
 import express from "express";
-import ViteExpress from "vite-express";
-
-import {
-  createServer,
-  loadConnections,
-} from "@breadboard-ai/connection-server/server.js";
-import {
-  makeRouter,
-  createServerConfig,
-} from "@breadboard-ai/board-server/router";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import ViteExpress from "vite-express";
+
+import * as connectionServer from "@breadboard-ai/connection-server/server.js";
+import * as boardServer from "@breadboard-ai/board-server";
 
 const MODULE_PATH = dirname(fileURLToPath(import.meta.url));
 const ROOT_PATH = resolve(MODULE_PATH, "../../");
 
-const app = express();
+const server = express();
 
 const configPath = process.env.CONNECTIONS_FILE;
-const connections = configPath ? await loadConnections(configPath) : new Map();
+const connections = configPath
+  ? await connectionServer.loadConnections(configPath)
+  : new Map();
 
-app.use("/connection", createServer({ connections, allowedOrigins: [] }));
+const boardServerConfig = boardServer.createServerConfig(ROOT_PATH);
 
-const config = createServerConfig(ROOT_PATH, null);
-
-app.use("/board", makeRouter(config));
+server.use("/board", boardServer.createServer(boardServerConfig));
+server.use(
+  "/connection",
+  connectionServer.createServer({ connections, allowedOrigins: [] })
+);
 
 ViteExpress.static({
   enableBrotli: true,
 });
 
-ViteExpress.listen(app, config.port, () => {
-  console.log(`Unified server at: http://localhost:${config.port}`);
+ViteExpress.listen(server, boardServerConfig.port, () => {
+  console.log(`Unified server at: http://localhost:${boardServerConfig.port}`);
 });
