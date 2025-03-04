7ec02d8ecbb4ea012a2efabe03a3ee629c758830
Tim Swanson <timswanson@google.com>
2025-03-04 12:06:19 -0700

    [unified-server] Create commit script

---
+++ packages/board-server/scripts/deploy-to-gcp.sh
@@ -0,0 +1,14 @@
+set -x
+
+project="$(gcloud config get project)"
+location="$(gcloud config get artifacts/location)"
+
+domain="${location}-docker.pkg.dev"
+image_url="${domain}/${project}/breadboard/unified-server"
+
+docker build --build-context=breadboard=../.. --tag=unified-server .
+docker tag unified-server ${image_url}
+docker push ${image_url}
+
+gcloud run deploy unified-server --image=${image_url}
+
