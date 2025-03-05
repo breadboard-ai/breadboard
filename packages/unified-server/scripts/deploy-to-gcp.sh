d492b2468dfff3240d70af3440960684dfd1a448
Joseph Mearman <joseph@mearman.co.uk>
2025-03-05 08:38:17 +0000

    chore(scripts): enhance deploy script with error handling and directory navigation
    - Added error handling options to improve script robustness.
    - Implemented directory navigation to locate package.json.

--- packages/unified-server/scripts/deploy-to-gcp.sh
+++ packages/unified-server/scripts/deploy-to-gcp.sh
@@ -1,5 +1,19 @@
+#!/usr/bin/env bash
+
 set -e
-set -x
+# -e: Exit immediately if a command exits with a non-zero status.
+set -u
+# -u: Treat unset variables as an error when substituting.
+set -o pipefail
+# -o pipefail: The return value of a pipeline is the status of the last command to exit with a non-zero status, or zero if no command exited with a non-zero status.
+
+# CD to script directory
+cd "$(dirname "$0")"
+
+# Ascend until package.json is found (project root)
+while [ ! -f package.json ]; do
+	cd ..
+done
 
 project="$(gcloud config get project)"
 location="$(gcloud config get artifacts/location)"
@@ -12,4 +26,3 @@ docker tag unified-server ${image_url}
 docker push ${image_url}
 
 gcloud run deploy unified-server --image=${image_url}
-
