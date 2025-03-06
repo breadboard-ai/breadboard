655611bf5f7b9fcdd21ad86fff46ed7581045360
Al Marks <aomarks@google.com>
2025-03-06 14:32:28 -0800

    [board generation] Fix the replace graph operation (#4681)
    It was updating the UI, but not the underlying graph.

--- packages/breadboard/src/editor/operations/replace-graph.ts
+++ packages/breadboard/src/editor/operations/replace-graph.ts
@@ -21,9 +21,27 @@ export class ReplaceGraph implements EditOperation {
         `Editor API integrity error: expected type "replacegraph", received "${edit.type}" instead.`
       );
     }
+
+    // Do a full in-place replacement of the graph. Re-assigning onto context
+    // won't work because the object itself is captured and used elsewhere.
+    const originalUrl = context.graph.url;
+    for (const key in context.graph) {
+      if (Object.prototype.hasOwnProperty.call(context.graph, key)) {
+        delete context.graph[key as keyof typeof context.graph];
+      }
+    }
+    Object.assign(context.graph, edit.replacement);
+    // Ensure the URL doesn't change; it's very important.
+    context.graph.url = originalUrl;
+
+    // Also update the "mutable" graph, which is actually a bunch of caches for
+    // the UI that are not automatically synchronized with the actual graph.
     context.mutable.rebuild(edit.replacement);
+
     return {
       success: true,
+      // We don't need to include any affected things, because rebuild replaces
+      // them anyway.
       affectedModules: [],
       affectedNodes: [],
       affectedGraphs: [],
