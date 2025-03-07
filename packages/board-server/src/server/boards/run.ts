88ff41af6153f95e80fa6c979aaab6e0bd47be74
Tim Swanson <timswanson@google.com>
2025-03-06 21:27:16 -0700

    [board-server] Use express.json
    Instead of manually parsing the body

--- packages/board-server/src/server/boards/run.ts
+++ packages/board-server/src/server/boards/run.ts
@@ -8,7 +8,6 @@ import type { Request, Response } from "express";
 
 import type { RemoteMessage } from "@google-labs/breadboard/remote";
 
-import { getBody } from "../common.js";
 import type { ServerConfig } from "../config.js";
 import { secretsKit } from "../proxy/secrets.js";
 import { getStore } from "../store.js";
@@ -28,12 +27,11 @@ async function runHandler(
   url.pathname = `boards/${fullPath}`;
   url.search = "";
 
-  const body = await getBody(req);
   const {
     $next: next,
     $diagnostics: diagnostics,
     ...inputs
-  } = body as Record<string, any>;
+  } = req.body as Record<string, any>;
   const writer = new WritableStream<RemoteMessage>({
     write(chunk) {
       res.write(`data: ${JSON.stringify(chunk)}\n\n`);
