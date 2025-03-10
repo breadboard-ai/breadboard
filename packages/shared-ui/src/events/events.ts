15b96a9016367dd7a1006235f6bb0a25f0ad2ad8
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-03-10 21:34:32 +0000

    [shared-ui] Bring back renaming & exporting of graph items (#4758)

--- packages/shared-ui/src/events/events.ts
+++ packages/shared-ui/src/events/events.ts
@@ -129,7 +129,8 @@ export class BoardInfoUpdateEvent extends Event {
     public readonly isTool: boolean | null = null,
     public readonly isComponent: boolean | null = null,
     public readonly subGraphId: string | null = null,
-    public readonly moduleId: string | null = null
+    public readonly moduleId: string | null = null,
+    public readonly exported: boolean | null = null
   ) {
     super(BoardInfoUpdateEvent.eventName, { ...eventInit });
   }
