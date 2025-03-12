ec937f69cfcf00c5568345d5b90629e53c3264ab
Al Marks <aomarks@google.com>
2025-03-12 16:49:42 -0700

    Add g-icon class for Google Symbols

--- packages/shared-ui/src/styles/icons.ts
+++ packages/shared-ui/src/styles/icons.ts
@@ -6,6 +6,16 @@
 
 import { css } from "lit";
 
+/**
+ * CSS classes for Material Icons and Google Symbols.
+ *
+ * Usage:
+ *
+ * ```html
+ * <span class="m-icon">play_arrow</span>
+ * <span class="g-icon">pen_spark</span>
+ * ```
+ */
 export const icons = css`
   .m-icon {
     font-family: "Material Symbols Outlined";
@@ -21,9 +31,37 @@ export const icons = css`
     direction: ltr;
     -webkit-font-feature-settings: "liga";
     -webkit-font-smoothing: antialiased;
+
+    font-variation-settings:
+      "FILL" 0,
+      "wght" 300,
+      "GRAD" 0,
+      "opsz" 48;
+
+    &.filled {
+      font-variation-settings:
+        "FILL" 1,
+        "wght" 300,
+        "GRAD" 0,
+        "opsz" 48;
+    }
   }
 
-  .m-icon {
+  .g-icon {
+    font-family: "Google Symbols";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: "liga";
+    -webkit-font-smoothing: antialiased;
+
     font-variation-settings:
       "FILL" 0,
       "wght" 300,
