21403d9af51b420a3eecd25d6e9d54b26207d73f
Tim Swanson <152931212+timswanson-google@users.noreply.github.com>
2025-03-12 15:25:32 -0600

    [board-server] Delete all UI (#4801)
    Fixes #4798

--- packages/board-server/src/index.ts
+++ packages/board-server/src/index.ts
@@ -6,7 +6,6 @@
 
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
-import { createServer as createViteServer } from "vite";
 
 import { createServer, createServerConfig } from "./server.js";
 
@@ -14,17 +13,7 @@ const MODULE_PATH = dirname(fileURLToPath(import.meta.url));
 const ROOT_PATH = resolve(MODULE_PATH, "../../");
 
 try {
-  // TODO move this into createServerConfig?
-  const viteDevServer =
-    process.env.NODE_ENV === "production"
-      ? undefined
-      : await createViteServer({
-          server: { middlewareMode: true },
-          appType: "custom",
-          optimizeDeps: { esbuildOptions: { target: "esnext" } },
-        });
-
-  const config = createServerConfig(ROOT_PATH, viteDevServer);
+  const config = createServerConfig(ROOT_PATH);
   const server = createServer(config);
 
   server.listen(config.port);
