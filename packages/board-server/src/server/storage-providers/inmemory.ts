c7179a3809883f8793da736a0dcacecfe638c5e5
Tim Swanson <152931212+timswanson-google@users.noreply.github.com>
2025-03-18 16:57:29 -0600

    [board-server] Create in-memory storage provider (#4948)
    And add tests
    
    Part of #4933

---
+++ packages/board-server/src/server/storage-providers/inmemory.ts
@@ -0,0 +1,74 @@
+import type { ReanimationState } from "@google-labs/breadboard";
+import type { BoardServerStore, ServerInfo, StorageBoard } from "../store.js";
+
+export const IN_MEMORY_SERVER_INFO: ServerInfo = {
+  title: "In-memory board server",
+  description: "Stores boards in memory",
+  url: "https://example.com/board-server",
+};
+
+export class InMemoryStorageProvider implements BoardServerStore {
+  /** API key -> user ID */
+  #users: Record<string, string> = {};
+
+  /** board name -> boards */
+  #boards: Record<string, StorageBoard> = {};
+
+  async getServerInfo(): Promise<ServerInfo | null> {
+    return IN_MEMORY_SERVER_INFO;
+  }
+
+  async createUser(userId: string, apiKey: string): Promise<void> {
+    if (Object.values(this.#users).includes(userId)) {
+      throw Error("user exists");
+    }
+    this.#users[apiKey] = userId;
+  }
+
+  async findUserIdByApiKey(apiKey: string): Promise<string> {
+    return this.#users[apiKey] ?? "";
+  }
+
+  async loadBoard(_userId: string, name: string): Promise<StorageBoard | null> {
+    return this.#boards[name] ?? null;
+  }
+
+  async listBoards(userId: string): Promise<StorageBoard[]> {
+    return Object.values(this.#boards).filter((board) => {
+      return board.owner === userId || board.tags.includes("published");
+    });
+  }
+
+  async createBoard(userId: string, name: string): Promise<void> {
+    this.#boards[name] = {
+      name,
+      owner: userId,
+      displayName: name,
+      description: "",
+      tags: [],
+      thumbnail: "",
+    };
+  }
+
+  async updateBoard(board: StorageBoard): Promise<void> {
+    this.#boards[board.name] = board;
+  }
+
+  async deleteBoard(_userId: string, boardName: string): Promise<void> {
+    delete this.#boards[boardName];
+  }
+
+  async loadReanimationState(
+    _user: string,
+    _ticket: string
+  ): Promise<ReanimationState | undefined> {
+    throw Error("unimplemented");
+  }
+
+  saveReanimationState(
+    _user: string,
+    _state: ReanimationState
+  ): Promise<string> {
+    throw Error("unimplemented");
+  }
+}
