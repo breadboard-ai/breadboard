ce35bb27dd81ae50766cadb65b84b83ba6b15c2d
Tim Swanson <timswanson@google.com>
2025-03-18 09:16:49 -0600

    [board-server] Change update to take StorageBoard

--- packages/board-server/src/server/boards/post.ts
+++ packages/board-server/src/server/boards/post.ts
@@ -12,7 +12,7 @@ import { getBody } from "../common.js";
 
 import del from "./delete.js";
 import type { BoardId, BoardServerStore } from "../types.js";
-import { asPath } from "../store.js";
+import { asPath, type StorageBoard } from "../store.js";
 
 async function post(
   req: Request,
@@ -47,27 +47,40 @@ async function update(
     res.sendStatus(403);
     return;
   }
-  if (!body) {
+
+  const graph = asGraph(body);
+  if (!graph) {
     // TODO Error body
     res.sendStatus(400);
     return;
   }
 
-  const maybeGraph = body as GraphDescriptor;
-  if (
-    !(("nodes" in maybeGraph && "edges" in maybeGraph) || "main" in maybeGraph)
-  ) {
-    // TODO Error body
-    res.sendStatus(400);
-    return;
-  }
+  const board: StorageBoard = {
+    name: boardId.name,
+    owner: userId,
+    displayName: graph.title || boardId.name,
+    description: graph.description ?? "",
+    tags: graph.metadata?.tags ?? [],
+    thumbnail: "",
+    graph: graph,
+  };
 
   try {
-    await store.update(boardId.user, boardId.name, maybeGraph);
-    res.json({ created: asPath(boardId.user, boardId.name) });
+    await store.updateBoard(board);
+    // TODO what does the client do with this response, and why is the property
+    // called "created"?
+    res.json({ created: asPath(board.owner, board.name) });
   } catch (e) {
     next(e);
   }
+}
+
+function asGraph(body: unknown): GraphDescriptor | null {
+  const graph = body as GraphDescriptor;
+  if (!(("nodes" in graph && "edges" in graph) || "main" in graph)) {
+    return null;
+  }
+  return graph;
 }
 
 export default post;
