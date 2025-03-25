0b12a5d07d6f262fb21601365d5da120e3cc63be
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-03-25 08:19:38 +0000

    Update edges

--- packages/shared-ui/src/elements/step-editor/edge.ts
+++ packages/shared-ui/src/elements/step-editor/edge.ts
@@ -6,23 +6,25 @@
 import { customElement, property } from "lit/decorators.js";
 import { Box } from "./box";
 import { GraphNode } from "./graph-node";
-import { css, html, nothing, svg } from "lit";
+import { css, html, svg } from "lit";
 import { map } from "lit/directives/map.js";
 import { classMap } from "lit/directives/class-map.js";
 import { styleMap } from "lit/directives/style-map.js";
 import { toCSSMatrix } from "./utils/to-css-matrix";
-import { DEGRAD, GRID_SIZE } from "./constants";
-import { intersects } from "./utils/rect-intersection";
+import { GRID_SIZE } from "./constants";
+
+interface Connection {
+  n1: DOMPoint;
+  n2: DOMPoint;
+  s1: "Top" | "Right" | "Bottom" | "Left";
+  s2: "Top" | "Right" | "Bottom" | "Left";
+}
 
 @customElement("bb-graph-edge")
 export class GraphEdge extends Box {
   static styles = [
     Box.styles,
     css`
-      * {
-        box-sizing: border-box;
-      }
-
       :host {
         z-index: 0;
       }
@@ -62,10 +64,6 @@ export class GraphEdge extends Box {
     return new DOMRect(0, 0, width, height);
   }
 
-  /**
-   * Figures out the shortest path from the cardinal points on node 1 to the
-   * cardinal points on node 2.
-   */
   #calculateShortestPath() {
     // Node 1 cardinal points.
     const n1l = new DOMPoint(
@@ -105,21 +103,26 @@ export class GraphEdge extends Box {
 
     // Set up the smallest distance and the candidates.
     let smallestDist = Number.POSITIVE_INFINITY;
-    const candidates = { n1: new DOMPoint(), n2: new DOMPoint() };
+    const candidates: Connection = {
+      n1: new DOMPoint(),
+      n2: new DOMPoint(),
+      s1: "Top",
+      s2: "Top",
+    };
     const cSP = (
       n1: DOMPoint,
       n2: DOMPoint,
       t1: DOMPoint,
       t2: DOMPoint,
-      cardinal = false
+      s1: "Top" | "Right" | "Bottom" | "Left",
+      s2: "Top" | "Right" | "Bottom" | "Left"
     ) => {
       const dist = this.#distanceSq(n1, n2, t1, t2);
       if (dist > smallestDist) {
         return;
       }
 
-      // Prefer cardinal points when there is a contest for shortest distance.
-      if (dist === smallestDist && !cardinal) {
+      if (dist === smallestDist) {
         return;
       }
 
@@ -129,45 +132,48 @@ export class GraphEdge extends Box {
       candidates.n1.y = n1.y + t1.y;
       candidates.n2.x = n2.x + t2.x;
       candidates.n2.y = n2.y + t2.y;
+      candidates.s1 = s1;
+      candidates.s2 = s2;
     };
 
     const t1 = new DOMPoint(
       this.node1.transform.e - this.transform.e,
       this.node1.transform.f - this.transform.f
     );
+
     const t2 = new DOMPoint(
       this.node2.transform.e - this.transform.e,
       this.node2.transform.f - this.transform.f
     );
 
     // Top.
-    cSP(n1t, n2l, t1, t2, true);
-    cSP(n1t, n2b, t1, t2, true);
-    cSP(n1t, n2r, t1, t2, true);
+    cSP(n1t, n2l, t1, t2, "Top", "Left");
+    cSP(n1t, n2b, t1, t2, "Top", "Bottom");
+    cSP(n1t, n2r, t1, t2, "Top", "Right");
 
     // Right.
     if (
       this.node2.transform.e >
       this.node1.transform.e + this.node1.bounds.width
     ) {
-      cSP(n1r, n2t, t1, t2, true);
-      cSP(n1r, n2l, t1, t2, true);
-      cSP(n1r, n2b, t1, t2, true);
+      cSP(n1r, n2t, t1, t2, "Right", "Top");
+      cSP(n1r, n2l, t1, t2, "Right", "Left");
+      cSP(n1r, n2b, t1, t2, "Right", "Bottom");
     }
 
     // Bottom.
-    cSP(n1b, n2r, t1, t2, true);
-    cSP(n1b, n2t, t1, t2, true);
-    cSP(n1b, n2l, t1, t2, true);
+    cSP(n1b, n2r, t1, t2, "Bottom", "Right");
+    cSP(n1b, n2t, t1, t2, "Bottom", "Top");
+    cSP(n1b, n2l, t1, t2, "Bottom", "Left");
 
     // Left.
     if (
       this.node2.transform.e + this.node2.bounds.width <
       this.node1.transform.e
     ) {
-      cSP(n1l, n2t, t1, t2, true);
-      cSP(n1l, n2r, t1, t2, true);
-      cSP(n1l, n2b, t1, t2, true);
+      cSP(n1l, n2t, t1, t2, "Left", "Top");
+      cSP(n1l, n2r, t1, t2, "Left", "Right");
+      cSP(n1l, n2b, t1, t2, "Left", "Bottom");
     }
 
     return candidates;
@@ -182,23 +188,118 @@ export class GraphEdge extends Box {
     return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
   }
 
-  protected renderSelf() {
-    // In the event that there is an intersection we will simply hide the edge.
-    if (intersects(this.node1.worldBounds, this.node2.worldBounds)) {
-      return nothing;
+  #createStepsFromConnectionPoints(connectionPoints: Connection) {
+    const steps: string[] = [];
+
+    const midX =
+      connectionPoints.n1.x +
+      (connectionPoints.n2.x - connectionPoints.n1.x) * 0.5;
+
+    const midY =
+      connectionPoints.n1.y +
+      (connectionPoints.n2.y - connectionPoints.n1.y) * 0.5;
+
+    if (
+      (connectionPoints.s1 === "Right" && connectionPoints.s2 === "Left") ||
+      (connectionPoints.s1 === "Left" && connectionPoints.s2 === "Right")
+    ) {
+      steps.push(
+        `M ${connectionPoints.n1.x} ${connectionPoints.n1.y}`,
+        `L ${midX} ${connectionPoints.n1.y}`,
+        `L ${midX} ${connectionPoints.n2.y}`,
+        `L ${connectionPoints.n2.x} ${connectionPoints.n2.y}`
+      );
+    } else if (
+      (connectionPoints.s1 === "Bottom" && connectionPoints.s2 === "Top") ||
+      (connectionPoints.s1 === "Top" && connectionPoints.s2 === "Bottom")
+    ) {
+      steps.push(
+        `M ${connectionPoints.n1.x} ${connectionPoints.n1.y}`,
+        `L ${connectionPoints.n1.x} ${midY}`,
+        `L ${connectionPoints.n2.x} ${midY}`,
+        `L ${connectionPoints.n2.x} ${connectionPoints.n2.y}`
+      );
+    } else if (
+      (connectionPoints.s1 === "Bottom" && connectionPoints.s2 === "Left") ||
+      (connectionPoints.s1 === "Top" && connectionPoints.s2 === "Right") ||
+      (connectionPoints.s1 === "Bottom" && connectionPoints.s2 === "Right") ||
+      (connectionPoints.s1 === "Top" && connectionPoints.s2 === "Left")
+    ) {
+      // Maybe adjust.
+      if (
+        connectionPoints.s1 === "Bottom" &&
+        connectionPoints.s2 === "Left" &&
+        connectionPoints.n1.x > connectionPoints.n2.x - GRID_SIZE
+      ) {
+        connectionPoints.n1.x = connectionPoints.n2.x - GRID_SIZE;
+      } else if (
+        connectionPoints.s1 === "Bottom" &&
+        connectionPoints.s2 === "Right" &&
+        connectionPoints.n1.x < connectionPoints.n2.x + GRID_SIZE
+      ) {
+        connectionPoints.n1.x = connectionPoints.n2.x + GRID_SIZE;
+      } else if (
+        connectionPoints.s1 === "Top" &&
+        connectionPoints.s2 === "Right" &&
+        connectionPoints.n1.x < connectionPoints.n2.x + GRID_SIZE
+      ) {
+        connectionPoints.n1.x = connectionPoints.n2.x + GRID_SIZE;
+      } else if (
+        connectionPoints.s1 === "Top" &&
+        connectionPoints.s2 === "Left" &&
+        connectionPoints.n1.x > connectionPoints.n2.x - GRID_SIZE
+      ) {
+        connectionPoints.n1.x = connectionPoints.n2.x - GRID_SIZE;
+      }
+
+      steps.push(
+        `M ${connectionPoints.n1.x} ${connectionPoints.n1.y}`,
+        `L ${connectionPoints.n1.x} ${connectionPoints.n2.y}`,
+        `L ${connectionPoints.n2.x} ${connectionPoints.n2.y}`
+      );
+    } else if (
+      (connectionPoints.s1 === "Right" && connectionPoints.s2 === "Top") ||
+      (connectionPoints.s1 === "Left" && connectionPoints.s2 === "Top") ||
+      (connectionPoints.s1 === "Left" && connectionPoints.s2 === "Bottom") ||
+      (connectionPoints.s1 === "Right" && connectionPoints.s2 === "Bottom")
+    ) {
+      steps.push(
+        `M ${connectionPoints.n1.x} ${connectionPoints.n1.y}`,
+        `L ${connectionPoints.n2.x} ${connectionPoints.n1.y}`,
+        `L ${connectionPoints.n2.x} ${connectionPoints.n2.y}`
+      );
     }
 
+    return steps;
+  }
+
+  #createRotationFromConnectionPoints(connectionPoints: Connection) {
+    let rotation = 0;
+    switch (connectionPoints.s2) {
+      case "Top":
+        rotation = 90;
+        break;
+      case "Right":
+        rotation = 180;
+        break;
+      case "Bottom":
+        rotation = 270;
+        break;
+    }
+
+    return rotation;
+  }
+
+  protected renderSelf() {
     const styles: Record<string, string> = {
       transform: toCSSMatrix(this.worldTransform),
     };
 
     const arrowSize = 8;
     const connectionPoints = this.#calculateShortestPath();
-    const rotation =
-      Math.atan2(
-        connectionPoints.n2.y - connectionPoints.n1.y,
-        connectionPoints.n2.x - connectionPoints.n1.x
-      ) / DEGRAD;
+    const rotation = this.#createRotationFromConnectionPoints(connectionPoints);
+    const steps = this.#createStepsFromConnectionPoints(connectionPoints);
+
     return html`<section
         id="container"
         class=${classMap({ bounds: this.showBounds })}
@@ -208,12 +309,9 @@ export class GraphEdge extends Box {
           <svg version="1.1"
                width=${this.bounds.width} height=${this.bounds.height}
                xmlns="http://www.w3.org/2000/svg">
-                <line x1=${connectionPoints.n1.x}
-                  y1=${connectionPoints.n1.y}
-                  x2=${connectionPoints.n2.x}
-                  y2=${connectionPoints.n2.y}
+                <path d=${steps.join(" ")}
                   stroke="#c2c2c2"
-                  stroke-width="2" />
+                  stroke-width="2" fill="none" />
 
                 <line x1=${connectionPoints.n2.x}
                   y1=${connectionPoints.n2.y}
@@ -236,8 +334,7 @@ export class GraphEdge extends Box {
   render() {
     return [
       this.renderSelf(),
-      html`${map(this.entities.values(), (entity) => {
-        entity.showBounds = this.showBounds;
+      html`${map(this.entities, (entity) => {
         return html`${entity}`;
       })}`,
     ];
