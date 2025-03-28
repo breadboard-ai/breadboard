f958cc21149ba65ed356469a33f6b29c2de6ce74
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-03-28 14:55:14 +0000

    [shared-ui] Teach UI controller to recall side nav item (#5110)

--- packages/shared-ui/src/strings/en_US/activity-log.ts
+++ packages/shared-ui/src/strings/en_US/activity-log.ts
@@ -37,7 +37,7 @@ export default {
 
   // Labels.
   LABEL_WAITING_MESSAGE: {
-    str: "Tap Start to begin",
+    str: "Start the Flow to see outputs",
   },
   LABEL_DETAILS: {
     str: "Details",
