b671467bd3eaa8808ee3e8c3d05e9e5bde3ad791
Al Marks <aomarks@google.com>
2025-04-01 14:45:17 -0700

    Add revert to history

--- packages/breadboard/src/editor/history.ts
+++ packages/breadboard/src/editor/history.ts
@@ -44,6 +44,13 @@ export class GraphEditHistory implements EditHistory {
     this.#controller.onHistoryChanged?.(this.#historyIncludingPending);
   }
 
+  revert(index: number) {
+    const revision = this.#history.revert(index);
+    this.#controller.setGraph(revision.graph);
+    this.#controller.onHistoryChanged?.(this.#historyIncludingPending);
+    return revision;
+  }
+
   canUndo(): boolean {
     return this.#history.canGoBack();
   }
@@ -116,6 +123,14 @@ export class EditHistoryManager {
 
   index() {
     return this.#index;
+  }
+
+  revert(index: number) {
+    this.history.splice(index + 1);
+    this.#index = index;
+    this.pending = undefined;
+    this.pauseLabel = null;
+    return this.history[index];
   }
 
   add(
