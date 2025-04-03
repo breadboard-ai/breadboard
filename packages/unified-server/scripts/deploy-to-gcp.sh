80615fbe87cfd6dfc01d7b140ee81bd105116890
Tim Swanson <timswanson@google.com>
2025-04-03 12:13:21 -0600

    [unified-server] Handle organization-bound project names

--- packages/unified-server/scripts/deploy-to-gcp.sh
+++ packages/unified-server/scripts/deploy-to-gcp.sh
@@ -4,6 +4,11 @@ set -x
 project="$(gcloud config get project)"
 location="$(gcloud config get artifacts/location)"
 
+# If the project is in a organization, the full project name will be
+# "org:project". The org and project need to be separate path elements in the
+# repo URL. Replace the colon with a slash.
+project="${project/:/\/}"
+
 domain="${location}-docker.pkg.dev"
 image_url="${domain}/${project}/breadboard/unified-server"
 
