9a20eccb65d59d31621c0ae7456afe1fb523e6c1
Dimitri Glazkov <dglazkov@google.com>
2025-04-07 14:54:46 -0700

    Stub out NotebookLM connector.

---
+++ packages/a2/bgl/src/notebooklm-connector/configurator.ts
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview Add a description for your module here.
+ */
+
+import { err, ok } from "./a2/utils";
+import { createConfigurator } from "./a2/connector-manager";
+
+import read from "@read";
+import write from "@write";
+
+export { invoke as default, describe };
+
+const CONNECTOR_TITLE = "NotebookLM";
+
+const { invoke, describe } = createConfigurator({
+  title: CONNECTOR_TITLE,
+  initialize: async () => {
+    return { title: "Untitled NotebookLM", configuration: {} };
+  },
+});
