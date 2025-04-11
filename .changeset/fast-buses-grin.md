fa670bca156f0f60afc91b0f8bedaead05e5ebfd
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-04-11 13:38:50 +0100

    [shared-ui] Teach MegaStep about subtypes in graph (#5326)
    Fixes #5318

---
+++ .changeset/fast-buses-grin.md
@@ -0,0 +1,5 @@
+---
+"@breadboard-ai/shared-ui": patch
+---
+
+Teach MegaStep about subtypes in graph
--- packages/shared-ui/src/elements/step-editor/graph.ts
+++ packages/shared-ui/src/elements/step-editor/graph.ts
@@ -41,6 +41,7 @@ import { toGridSize } from "./utils/to-grid-size";
 import { MOVE_GRAPH_ID } from "./constants";
 import { GraphAsset } from "./graph-asset";
 import { AssetPath } from "@breadboard-ai/types";
+import { isControllerBehavior } from "../../utils/behaviors";
 
 @customElement("bb-graph")
 export class Graph extends Box {
@@ -145,6 +146,32 @@ export class Graph extends Box {
         graphNode.updating = ports.updating ?? false;
         graphNode.icon = metadata.icon ?? null;
         graphNode.ports = ports;
+        for (const port of ports.inputs.ports) {
+          if (isControllerBehavior(port.schema) && port.schema.enum) {
+            const selectedControllerType = port.schema.enum.find((v) => {
+              if (typeof v === "string") {
+                return false;
+              }
+
+              return v.id === port.value;
+            });
+
+            if (
+              !selectedControllerType ||
+              typeof selectedControllerType === "string"
+            ) {
+              continue;
+            }
+
+            if (!selectedControllerType.icon) {
+              continue;
+            }
+
+            graphNode.icon = selectedControllerType.icon;
+            break;
+          }
+        }
+
         if (metadata.tags) {
           for (const tag of metadata.tags ?? []) {
             graphNode.classList.add(tag);
