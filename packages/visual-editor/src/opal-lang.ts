15da5723df9903f9b120c3f37e55caa9a50af80c
Al Marks <aomarks@google.com>
2025-04-14 15:04:49 -0700

    Add Opal strings and logo

---
+++ packages/visual-editor/src/opal-lang.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import DefaultLangPack from "@breadboard-ai/shared-ui/strings/en_US";
+
+export default {
+  ...DefaultLangPack,
+  Global: {
+    ...DefaultLangPack.Global,
+    APP_NAME: "Opal",
+  },
+  ProjectListing: {
+    ...DefaultLangPack.ProjectListing,
+    LABEL_WELCOME_MESSAGE_A: {
+      str: "Opals",
+    },
+    LABEL_NO_PROJECTS_FOUND: {
+      str: "You don't have any Opals yet.",
+    },
+  },
+};
