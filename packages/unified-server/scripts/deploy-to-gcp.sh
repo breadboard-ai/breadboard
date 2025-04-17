4c758097dbe92fbacc1455b4bbce1d58d1394308
Dimitri Glazkov <dglazkov@google.com>
2025-04-16 19:31:27 -0700

    Make it possible to deploy to another service.

--- packages/unified-server/scripts/deploy-to-gcp.sh
+++ packages/unified-server/scripts/deploy-to-gcp.sh
@@ -3,6 +3,7 @@ set -x
 
 project="$(gcloud config get project)"
 location="$(gcloud config get artifacts/location)"
+service_name="${1:-unified-server}"
 
 # If the project is in a organization, the full project name will be
 # "org:project". The org and project need to be separate path elements in the
@@ -10,11 +11,11 @@ location="$(gcloud config get artifacts/location)"
 project="${project/:/\/}"
 
 domain="${location}-docker.pkg.dev"
-image_url="${domain}/${project}/breadboard/unified-server"
+image_url="${domain}/${project}/breadboard/${service_name}"
 
-docker build --build-context=breadboard=../.. --tag=unified-server .
-docker tag unified-server ${image_url}
+docker build --build-context=breadboard=../.. --tag=${service_name} .
+docker tag ${service_name} ${image_url}
 docker push ${image_url}
 
-gcloud run deploy unified-server --image=${image_url}
+gcloud run deploy ${service_name} --image=${image_url}
 
