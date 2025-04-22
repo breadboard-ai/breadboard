36ef1cc38b9505e8d1274d345b6e5802a700c230
Al Marks <aomarks@google.com>
2025-04-22 16:15:30 -0700

    Automatically find or create a root folder in user's Google Drive (#5433)
    We now find or create a top-level Google Drive folder called
    "Breadboard" at initialization. We'll make that a VITE_ configurable env
    var later.
    
    Other fixes:
    
    - Don't list files or folders that are in the trash.
    - Fix the board URL we return when creating a board in Drive; it only
    have the file ID, not the parent folder ID.

--- packages/google-drive-kit/src/board-server/server.ts
+++ packages/google-drive-kit/src/board-server/server.ts
@@ -39,6 +39,10 @@ interface DriveFileQuery {
   files: DriveFile[];
 }
 
+// TODO(aomarks) Make this configurable via a VITE_ env variable.
+const GOOGLE_DRIVE_FOLDER_NAME = "Breadboard";
+const GOOGLE_DRIVE_FOLDER_MIME_TYPE = "application/vnd.google-apps.folder";
+
 // This whole package should probably be called
 // "@breadboard-ai/google-drive-board-server".
 // But it's good that we have both components and the board server here:
@@ -138,9 +142,12 @@ class GoogleDriveBoardServer extends EventTarget implements BoardServer {
   }
 
   async #refreshProjects(): Promise<BoardServerProject[]> {
-    const folderId = this.url.hostname;
+    const folderId = await this.#findOrCreateUserRootFolder();
     const accessToken = await getAccessToken(this.vendor);
-    const query = `"${folderId}" in parents and mimeType = "application/json"`;
+    const query =
+      `"${folderId}" in parents` +
+      ` and mimeType = "application/json"` +
+      ` and trashed=false`;
 
     if (!folderId || !accessToken) {
       throw new Error("No folder ID or access token");
@@ -290,7 +297,7 @@ class GoogleDriveBoardServer extends EventTarget implements BoardServer {
   ): Promise<{ result: boolean; error?: string; url?: string }> {
     // First create the file, then save.
 
-    const parent = this.url.hostname;
+    const parent = await this.#findOrCreateUserRootFolder();
     const fileName = url.href.replace(`${this.url.href}/`, "");
     const accessToken = await getAccessToken(this.vendor);
 
@@ -309,10 +316,11 @@ class GoogleDriveBoardServer extends EventTarget implements BoardServer {
       );
 
       const file: DriveFile = await response.json();
-      const updatedUrl = `${GoogleDriveBoardServer.PROTOCOL}//${parent}/${file.id}`;
+      const updatedUrl = `${GoogleDriveBoardServer.PROTOCOL}/${file.id}`;
 
       this.projects = this.#refreshProjects();
 
+      console.log("Google Drive: Created new board", updatedUrl);
       return { result: true, url: updatedUrl };
     } catch (err) {
       console.warn(err);
@@ -409,6 +417,45 @@ class GoogleDriveBoardServer extends EventTarget implements BoardServer {
 
   async preview(_url: URL): Promise<URL> {
     throw new Error("Method not implemented.");
+  }
+
+  async #findOrCreateUserRootFolder(): Promise<string> {
+    const accessToken = await getAccessToken(this.vendor);
+    if (!accessToken) {
+      throw new Error("No access token");
+    }
+    const api = new Files(accessToken);
+
+    const findRequest = api.makeQueryRequest(
+      `name="${GOOGLE_DRIVE_FOLDER_NAME}"` +
+        ` and mimeType="${GOOGLE_DRIVE_FOLDER_MIME_TYPE}"` +
+        ` and trashed=false`
+    );
+    const { files } = (await (
+      await fetch(findRequest)
+    ).json()) as DriveFileQuery;
+    if (files.length > 0) {
+      if (files.length > 1) {
+        console.warn(
+          "Google Drive: Multiple candidate root folders found," +
+            " picking the first one arbitrarily:",
+          files
+        );
+      }
+      const id = files[0]!.id;
+      console.log("Google Drive: Found existing root folder", id);
+      return id;
+    }
+
+    const createRequest = api.makeCreateRequest({
+      name: GOOGLE_DRIVE_FOLDER_NAME,
+      mimeType: GOOGLE_DRIVE_FOLDER_MIME_TYPE,
+    });
+    const { id } = (await (await fetch(createRequest)).json()) as {
+      id: string;
+    };
+    console.log("Google Drive: Created new root folder", id);
+    return id;
   }
 }
 
