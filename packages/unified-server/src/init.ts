637d443644115317b548ed5705a9751ae47394ae
Dimitri Glazkov <dimitri@glazkov.com>
2025-04-26 14:26:09 -0700

    Hide functionality behind env flag.

--- packages/unified-server/src/init.ts
+++ packages/unified-server/src/init.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/// <reference types="vite/client" />
+
 import { bootstrap } from "@breadboard-ai/visual-editor/bootstrap";
 import { asRuntimeKit, err } from "@google-labs/breadboard";
 import Core from "@google-labs/core-kit";
@@ -15,6 +17,7 @@ bootstrap({
   requiresSignin: true,
   kits: [asRuntimeKit(Core)],
   moduleInvocationFilter: (context) => {
+    if (!import.meta.env.VITE_NO_3P_MODULES) return;
     if (!isA2(baseURLFromContext(context))) {
       return err(`This module is not allowed to run in this configuration`);
     }
