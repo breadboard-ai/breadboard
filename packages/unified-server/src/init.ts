9f4d46f9b49158c14d9680e0c99fdac51693d6d1
Dimitri Glazkov <dimitri@glazkov.com>
2025-04-26 12:41:02 -0700

    Load Core kit only for unified server.

--- packages/unified-server/src/init.ts
+++ packages/unified-server/src/init.ts
@@ -5,8 +5,11 @@
  */
 
 import { bootstrap } from "@breadboard-ai/visual-editor/bootstrap";
+import { asRuntimeKit } from "@google-labs/breadboard";
+import Core from "@google-labs/core-kit";
 
 bootstrap({
   connectionServerUrl: new URL("/connection/", window.location.href),
   requiresSignin: true,
+  kits: [asRuntimeKit(Core)],
 });
