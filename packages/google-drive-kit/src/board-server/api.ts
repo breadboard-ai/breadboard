87427f7ea2128d8674fac19a5496d655ecaa0934
Dimitri Glazkov <dimitri@glazkov.com>
2025-05-20 10:25:01 -0700

    Revert "Revert "[gdrive] Showing board descriptions in the list." (#5801)"
    This reverts commit 35aa29aba658e07ea451d17379a8bf57433dea54.

--- packages/google-drive-kit/src/board-server/api.ts
+++ packages/google-drive-kit/src/board-server/api.ts
@@ -30,6 +30,7 @@ export type Properties = {
 
 export type AppProperties = {
   title: string;
+  /** A truncated copy of the board description for listing. */
   description: string;
   tags: GraphTag[];
   thumbnailUrl?: string;
