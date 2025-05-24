330389bf4ea9cf0d243a9777eb6f26dfc69341eb
Amy <amy.skerry@gmail.com>
2025-05-24 08:31:15 -0700

    Add some not-released-to-users tools for more specific search types (hotels, events, jobs).
    We will explore quality tradeoff of these vs the
    generic mega-search.

---
+++ packages/a2/bgl/src/tools/tool-search-jobs.ts
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview The guts of the Job Search tool.
+ */
+
+import { ok, err } from "./a2/utils";
+import { executeTool } from "./a2/step-executor";
+
+export { invoke as default, describe };
+
+export type SearchInputs = {
+  query: string;
+};
+
+export type SearchOutputs = {
+  results: string;
+};
+
+async function invoke({
+  query,
+}: SearchInputs): Promise<Outcome<SearchOutputs>> {
+  const executing = await executeTool<string>("google_search_jobs.jobs", {
+    query,
+  });
+  if (!ok(executing)) return executing;
+
+  return { results: executing };
+}
+
+async function describe() {
+  return {
+    inputSchema: {
+      type: "object",
+      properties: {
+        query: {
+          type: "string",
+          title: "Query",
+          description: "The query to Job Search",
+        },
+      },
+    } satisfies Schema,
+    outputSchema: {
+      type: "object",
+      properties: {
+        results: {
+          type: "string",
+          title: "Search Results",
+        },
+      },
+    } satisfies Schema,
+  };
+}
