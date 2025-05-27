05bc15801a2da7b91b8ef3fafeb5b47f5ed1dfa5
Al Marks <aomarks@google.com>
2025-05-27 14:21:08 -0700

    Discover storedData drive assets

--- packages/shared-ui/src/elements/google-drive/find-google-drive-assets-in-graph.ts
+++ packages/shared-ui/src/elements/google-drive/find-google-drive-assets-in-graph.ts
@@ -12,11 +12,17 @@ export function findGoogleDriveAssetsInGraph(graph: GraphDescriptor): string[] {
 
   if (graph.assets) {
     for (const asset of Object.values(graph.assets)) {
-      if (asset.metadata?.subType === "gdrive") {
-        // Cast needed because `data` is very broadly typed as `NodeValue`.
-        const firstPart = (asset.data as LLMContent[])[0]?.parts[0];
-        if (firstPart && "fileData" in firstPart) {
+      // Cast needed because `data` is very broadly typed as `NodeValue`.
+      const firstPart = (asset.data as LLMContent[])[0]?.parts[0];
+      if (firstPart) {
+        if ("fileData" in firstPart && asset.metadata?.subType === "gdrive") {
           const fileId = firstPart.fileData?.fileUri;
+          if (fileId) {
+            fileIds.add(fileId);
+          }
+        }
+        if ("storedData" in firstPart) {
+          const fileId = extractDriveFileId(firstPart.storedData.handle);
           if (fileId) {
             fileIds.add(fileId);
           }
@@ -30,12 +36,18 @@ export function findGoogleDriveAssetsInGraph(graph: GraphDescriptor): string[] {
   if (themes) {
     for (const theme of Object.values(themes)) {
       const splashHandle = theme.splashScreen?.storedData?.handle;
-      const fileIdMatch = splashHandle?.match(/^drive:\/?(.+)/);
-      if (fileIdMatch?.[1]) {
-        fileIds.add(fileIdMatch[1]);
+      if (splashHandle) {
+        const fileId = extractDriveFileId(splashHandle);
+        if (fileId) {
+          fileIds.add(fileId);
+        }
       }
     }
   }
 
   return [...fileIds];
+}
+
+function extractDriveFileId(str: string): string | null {
+  return str.match(/^drive:\/?(.+)/)?.[1] ?? null;
 }
