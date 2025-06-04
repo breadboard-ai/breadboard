acede35577e456f8aa230a2971c6971549e1c449
Al Marks <aomarks@google.com>
2025-06-04 15:12:37 -0700

    [sharing] Inline generated assets in generated html (#5931)

--- packages/shared-ui/src/app-templates/basic/index.ts
+++ packages/shared-ui/src/app-templates/basic/index.ts
@@ -27,6 +27,7 @@ import Animations from "../shared/styles/animations.js";
 
 import { classMap } from "lit/directives/class-map.js";
 import {
+  asBase64,
   BoardServer,
   GraphDescriptor,
   InspectableRun,
@@ -74,7 +75,7 @@ import { findFinalOutputValues } from "../../utils/save-results.js";
 import { consume } from "@lit/context";
 import { boardServerContext } from "../../contexts/board-server.js";
 import { GoogleDriveBoardServer } from "@breadboard-ai/google-drive-kit";
-import { NodeValue } from "@breadboard-ai/types";
+import { LLMContent, NodeValue } from "@breadboard-ai/types";
 
 function keyFromGraphUrl(url: string) {
   return `cw-${url.replace(/\W/gi, "-")}`;
@@ -1177,19 +1178,37 @@ export class Template extends LitElement implements AppTemplate {
     // Inline all content.
     await Promise.all(
       Object.entries(finalOutputValues).map(async ([key, value]) => {
-        if (isLLMContentArray(value)) {
-          const inlined = await transformDataParts(
-            new URL(graphUrl),
-            value,
-            "inline",
-            boardServer.dataPartTransformer(new URL(graphUrl))
-          );
-          if (ok(inlined)) {
-            finalOutputValues[key] = inlined as NodeValue;
-          } else {
-            console.error(`Error inlining results content for ${key}`, inlined);
+        if (!isLLMContentArray(value)) {
+          return;
+        }
+
+        // Transform any inline data parts.
+        const inlined = await transformDataParts(
+          new URL(graphUrl),
+          value,
+          "inline",
+          boardServer.dataPartTransformer(new URL(graphUrl))
+        );
+        if (!ok(inlined)) {
+          console.error(`Error inlining results content for ${key}`, inlined);
+          return;
+        }
+
+        // Also check for blobs inside of HTML, and inline those too.
+        for (const content of inlined) {
+          for (const part of content.parts) {
+            if (
+              "inlineData" in part &&
+              part.inlineData.mimeType === "text/html" &&
+              part.inlineData.data
+            ) {
+              const html = part.inlineData.data;
+              part.inlineData.data = await inlineHtmlBlobUrls(html);
+            }
           }
         }
+
+        finalOutputValues[key] = inlined as NodeValue;
       })
     );
 
@@ -1498,4 +1517,61 @@ export class Template extends LitElement implements AppTemplate {
         : nothing}
     </section>`;
   }
+}
+
+async function inlineHtmlBlobUrls(html: string): Promise<string> {
+  const blobUrls = findBlobUrlsInHtml(html);
+  if (blobUrls.length === 0) {
+    return html;
+  }
+
+  const replacements = (
+    await Promise.all(
+      blobUrls.map(async ({ start, end, blobId }) => {
+        // Let's not trust the raw URL. We instead extract the blob ID from the
+        // URL if it looks like a blob URL, and then construct a new safe blob
+        // URL from scratch. This way there is no way for generated HTML to
+        // trigger an unsafe fetch.
+        const safeUrl = new URL(
+          `/board/blobs/${encodeURIComponent(blobId)}`,
+          document.location.origin
+        );
+        const response = await fetch(safeUrl);
+        if (!response.ok) {
+          console.error(
+            `${response.status} error fetching blob`,
+            safeUrl,
+            await response.text()
+          );
+          return null;
+        }
+        const blob = await response.blob();
+        const base64 = await asBase64(blob);
+        const dataUrl = `data:${blob.type};base64,${base64}`;
+        return { start, end, replacement: dataUrl };
+      })
+    )
+  ).filter((replacement) => replacement != null);
+
+  // Apply replacements reverse so that indices remain correct.
+  replacements.sort((a, b) => b.start - a.start);
+  for (const { start, end, replacement } of replacements) {
+    html = html.slice(0, start) + replacement + html.slice(end);
+  }
+  return html;
+}
+
+function findBlobUrlsInHtml(
+  str: string
+): Array<{ start: number; end: number; blobId: string }> {
+  const results = [];
+  const matches = str.matchAll(/https?:\/\/[^/]+\/board\/blobs\/([a-z0-9-]+)/g);
+  for (const match of matches) {
+    results.push({
+      start: match.index,
+      end: match.index + match[0].length,
+      blobId: match[1],
+    });
+  }
+  return results;
 }
