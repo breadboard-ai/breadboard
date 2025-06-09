35be988b9dd5f4c6ef8b3642b7f279073ac7c698
Dimitri Glazkov <dglazkov@google.com>
2025-06-09 11:16:39 -0700

    Factor out `getStepIcon`.

---
+++ packages/shared-ui/src/utils/get-step-icon.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { InspectableNodePorts } from "@google-labs/breadboard";
+import { isControllerBehavior } from "./behaviors";
+
+export { getStepIcon };
+
+function getStepIcon(
+  defaultIcon: string | undefined,
+  ports: InspectableNodePorts
+) {
+  let icon = defaultIcon ?? null;
+  for (const port of ports.inputs.ports) {
+    if (isControllerBehavior(port.schema) && port.schema.enum) {
+      const selectedControllerType = port.schema.enum.find((v) => {
+        if (typeof v === "string") {
+          return false;
+        }
+
+        return v.id === port.value;
+      });
+
+      if (
+        !selectedControllerType ||
+        typeof selectedControllerType === "string"
+      ) {
+        continue;
+      }
+
+      if (!selectedControllerType.icon) {
+        continue;
+      }
+
+      icon = selectedControllerType.icon;
+      break;
+    }
+  }
+  return icon;
+}
