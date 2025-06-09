688917047ad48e55e7364032f519251dee7e4e7e
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-06-09 17:57:38 +0100

    [shared-ui] Fix the sidenav height (#5967)

--- packages/shared-ui/src/elements/ui-controller/ui-controller.styles.ts
+++ packages/shared-ui/src/elements/ui-controller/ui-controller.styles.ts
@@ -605,6 +605,7 @@ export const styles = [
         align-items: center;
         justify-content: center;
         background: var(--s-90, var(--bb-neutral-0));
+        flex: 0 0 auto;
 
         height: var(--bb-grid-size-14);
         border-bottom: 1px solid var(--s-80, var(--bb-neutral-300));
