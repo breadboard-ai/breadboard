c882f104200312810d7f161843764be41b598364
Amy <amy.skerry@gmail.com>
2025-06-09 09:22:28 -0700

    Add client method for checking and accepting TOS

--- packages/shared-ui/src/flow-gen/app-catalyst.ts
+++ packages/shared-ui/src/flow-gen/app-catalyst.ts
@@ -17,6 +17,12 @@ export interface AppCatalystChatResponse {
   messages: AppCatalystContentChunk[];
 }
 
+export interface CheckAppAccessResponse {
+  canAccess: boolean;
+  hasAcceptedTos: boolean;
+  termsOfServiceVersion: number;
+}
+
 export interface AppCatalystContentChunk {
   mimetype: "text/plain" | "text/breadboard";
   data: string;
@@ -49,5 +55,52 @@ export class AppCatalystApiClient {
     });
     const result = (await response.json()) as AppCatalystChatResponse;
     return result;
+  }
+
+  async checkTos(tosVersion: number = 0): Promise<boolean> {
+    const token = await this.#signinAdapter.refresh();
+    if (token?.state !== "valid") {
+      throw new Error(`Expected "valid" token, got "${token?.state}"`);
+    }
+    try {
+      const result = (await (
+        await fetch(new URL(`v1beta1/checkAppAccess`, this.#apiBaseUrl), {
+          headers: {
+            Authorization: `Bearer ${token.grant.access_token}`,
+          },
+        })
+      ).json()) as CheckAppAccessResponse;
+      if (
+        result.hasAcceptedTos &&
+        result.termsOfServiceVersion === tosVersion
+      ) {
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+
+    return false;
+  }
+
+  async acceptTos(tosVersion: number = 0): Promise<void> {
+    const token = await this.#signinAdapter.refresh();
+    if (token?.state !== "valid") {
+      throw new Error(`Expected "valid" token, got "${token?.state}"`);
+    }
+    const url = new URL("v1beta1/acceptToS", this.#apiBaseUrl);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${token.grant.access_token}`,
+      },
+      body: JSON.stringify({
+        termsOfServiceVersion: tosVersion,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to accept TOS: ${response.statusText}`);
+    }
   }
 }
