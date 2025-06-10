8dc68f73724839e447df20e3f859624ca43431ac
Volodya Shtenovych <shvolodya@gmail.com>
2025-06-10 19:22:01 +0200

    [gdrive] Storing values in properties instead of appProperties (#5981)
    Reads from both but writes to properties only.
    
    ---------
    
    Co-authored-by: Leksyk <volodya@google.com>

---
+++ packages/google-drive-kit/tests/board-server/operations.ts
@@ -0,0 +1,77 @@
+import { beforeEach, describe, it } from "node:test";
+import { deepEqual } from "node:assert";
+
+import type { DriveFile } from "../../src/board-server/api.js";
+
+import {
+  readProperties,
+  createProperties,
+} from "../../src/board-server/operations.js";
+
+describe("create/readProperties", () => {
+  let properties: Record<string, string>;
+  let file: DriveFile;
+
+  beforeEach(() => {
+    properties = {
+      title: "test title",
+      description: "test desc",
+      tags: `["a","b"]`,
+      thumbnailUrl: "abc",
+    };
+
+    file = {
+      id: "d",
+      kind: "a",
+      mimeType: "text/plain",
+      name: "Test File",
+      resourceKey: "key",
+      properties: {},
+      appProperties: properties,
+    };
+  });
+
+  it("simple convert", () => {
+    const props = readProperties(file);
+    deepEqual(props, {
+      description: "test desc",
+      thumbnailUrl: "abc",
+      title: "test title",
+      tags: ["a", "b"],
+    });
+    const savedProps = createProperties(props);
+    deepEqual(savedProps, properties);
+  });
+
+  it("overrides", () => {
+    file.properties = { title: "overridden" };
+    const props = readProperties(file);
+    deepEqual(props, {
+      description: "test desc",
+      thumbnailUrl: "abc",
+      title: "overridden",
+      tags: ["a", "b"],
+    });
+    const savedProps = createProperties(props);
+    deepEqual(savedProps, { ...properties, title: "overridden" });
+  });
+
+  it("puts both together", () => {
+    file.properties = { description: "from props" };
+    file.appProperties = { title: "from app props" };
+    const props = readProperties(file);
+    deepEqual(props, {
+      title: "from app props",
+      description: "from props",
+      tags: [],
+      thumbnailUrl: undefined,
+    });
+    const savedProps = createProperties(props);
+    deepEqual(savedProps, {
+      title: "from app props",
+      description: "from props",
+      tags: "[]",
+      thumbnailUrl: undefined,
+    });
+  });
+});
