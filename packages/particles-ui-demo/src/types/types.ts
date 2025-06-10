4d2e9feff4dcfec7a8ce91b685e4afe02f8bea3b
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-06-10 11:51:12 -0700

    [particles-ui-demo] Add basic types (#5989)

---
+++ packages/particles-ui-demo/src/types/types.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export interface TodoItem {
+  title: string;
+  done: boolean;
+  description?: string;
+  dueDate?: Date;
+}
+
+export type TodoItemListTitle = string;
+export type TodoItems = Map<TodoItemListTitle, TodoItem>;
