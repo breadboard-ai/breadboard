e57e950cb0fc7ae53ce42173ab2d66f5cb9c9786
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-07-16 12:44:35 +0100

    [shared-ui] Restyle toasts

--- packages/shared-ui/src/elements/toast/toast.ts
+++ packages/shared-ui/src/elements/toast/toast.ts
@@ -7,6 +7,10 @@
 import { ToastRemovedEvent, ToastType } from "../../events/events.js";
 import { LitElement, html, css, PropertyValueMap } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { colorsLight } from "../../styles/host/colors-light.js";
+import { type } from "../../styles/host/type.js";
+import { icons } from "../../styles/icons.js";
+import { classMap } from "lit/directives/class-map.js";
 
 const DEFAULT_TIMEOUT = 8000;
 
@@ -27,81 +31,77 @@ export class Toast extends LitElement {
   @property()
   accessor offset = 0;
 
-  static styles = css`
-    :host {
-      --bb-toast-icon: var(--bb-icon-info);
-
-      position: fixed;
-      bottom: calc(var(--bb-grid-size) * 10);
-      right: calc(var(--bb-grid-size) * 10);
-      display: block;
-      background: var(--bb-neutral-0);
-      border: 1px solid #ccc;
-      box-shadow:
-        0 2px 3px 0 rgba(0, 0, 0, 0.13),
-        0 7px 9px 0 rgba(0, 0, 0, 0.16);
-      border-radius: calc(var(--bb-grid-size) * 8);
-      padding: var(--bb-grid-size-3) var(--bb-grid-size-5) var(--bb-grid-size-3)
-        var(--bb-grid-size-10);
-
-      translate: 0 calc(var(--offset) * var(--bb-grid-size-16) * -1);
-      transition: translate 0.2s cubic-bezier(0, 0, 0.3, 1);
-      animation: slideIn 0.15s cubic-bezier(0, 0, 0.3, 1) forwards;
-      max-width: min(360px, 80vw);
-    }
-
-    :host([type="warning"]) {
-      --bb-toast-icon: var(--bb-icon-warning);
-      color: var(--bb-boards-600);
-    }
-
-    :host([type="error"]) {
-      --bb-toast-icon: var(--bb-icon-error);
-      color: var(--bb-warning-600);
-    }
-
-    :host([type="pending"]) {
-      --bb-toast-icon: url(/images/progress-neutral.svg);
-    }
-
-    :host(.toasted) {
-      animation: slideOut 0.3s cubic-bezier(0, 0, 0.3, 1) forwards;
-    }
-
-    :host::before {
-      content: "";
-      position: absolute;
-      left: 12px;
-      top: 11px;
-      width: 20px;
-      height: 20px;
-      background: var(--bb-toast-icon) center center / 20px 20px no-repeat;
-    }
-
-    @keyframes slideIn {
-      from {
-        transform: translateY(20px);
-        opacity: 0;
+  static styles = [
+    colorsLight,
+    type,
+    icons,
+    css`
+      :host {
+        position: fixed;
+        bottom: calc(var(--bb-grid-size) * 10);
+        right: calc(var(--bb-grid-size) * 10);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: var(--n-0);
+        color: var(--n-100);
+        box-shadow:
+          0 2px 3px 0 rgba(0, 0, 0, 0.13),
+          0 7px 9px 0 rgba(0, 0, 0, 0.16);
+        border-radius: calc(var(--bb-grid-size) * 8);
+        padding: var(--bb-grid-size-3) var(--bb-grid-size-6)
+          var(--bb-grid-size-3) var(--bb-grid-size-5);
+        translate: 0 calc(var(--offset) * var(--bb-grid-size-16) * -1);
+        transition: translate 0.2s cubic-bezier(0, 0, 0.3, 1);
+        animation: slideIn 0.15s cubic-bezier(0, 0, 0.3, 1) forwards;
+        max-width: min(360px, 80vw);
       }
 
-      to {
-        transform: none;
-        opacity: 1;
-      }
-    }
-
-    @keyframes slideOut {
-      from {
-        transform: none;
-        opacity: 1;
+      :host([type="warning"]) {
+        color: var(--e-60);
       }
 
-      to {
-        transform: translateY(-20px);
-        opacity: 0;
+      :host([type="error"]) {
+        color: var(--e-40);
       }
-    }
-  `;
+
+      :host(.toasted) {
+        animation: slideOut 0.3s cubic-bezier(0, 0, 0.3, 1) forwards;
+      }
+
+      p {
+        margin: 0;
+      }
+
+      .g-icon {
+        margin-right: var(--bb-grid-size-2);
+      }
+
+      @keyframes slideIn {
+        from {
+          transform: translateY(20px);
+          opacity: 0;
+        }
+
+        to {
+          transform: none;
+          opacity: 1;
+        }
+      }
+
+      @keyframes slideOut {
+        from {
+          transform: none;
+          opacity: 1;
+        }
+
+        to {
+          transform: translateY(-20px);
+          opacity: 0;
+        }
+      }
+    `,
+  ];
 
   #removalTimeout = -1;
   connectedCallback(): void {
@@ -153,6 +153,31 @@ export class Toast extends LitElement {
       this.style.setProperty("--offset", this.offset.toString());
     }
 
-    return html`<div>${this.message}</div>`;
+    let icon;
+    switch (this.type) {
+      case ToastType.INFORMATION:
+        icon = "info";
+        break;
+      case ToastType.ERROR:
+        icon = "error";
+        break;
+      case ToastType.WARNING:
+        icon = "warning";
+        break;
+      case ToastType.PENDING:
+        icon = "progress_activity";
+        break;
+    }
+
+    return html` <span
+        class=${classMap({
+          "g-icon": true,
+          round: true,
+          filled: true,
+          rotate: this.type === ToastType.PENDING,
+        })}
+        >${icon}</span
+      >
+      <p class="sans-flex round md-title-small">${this.message}</p>`;
   }
 }
