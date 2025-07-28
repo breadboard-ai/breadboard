164791957e7a88d55693972005d28a2adc21236f
Volodya Shtenovych <shvolodya@gmail.com>
2025-07-28 16:46:25 +0200

    [ui-tests] Updated tests after particles redesign (#6414)
    Fixes #<issue_number_goes_here>
    
    > It's a good idea to open an issue first for discussion.
    
    - [ ] Tests pass
    - [ ] Appropriate changes to documentation are included in the PR
    
    Co-authored-by: Volodya <volodya@google.com>

--- packages/ui-tests/smoke.spec.ts
+++ packages/ui-tests/smoke.spec.ts
@@ -5,7 +5,6 @@ import { test, BreadboardTestHarness } from "./test-harness";
 test("smoke test", async ({ harness }) => {
   await verifyMainPage(harness);
   await harness.newFlow();
-  await verifyEditorButtons(harness);
   // Adding 2 simple steps.
   await harness.newUserInput({
     name: "USER_NAME",
@@ -13,7 +12,7 @@ test("smoke test", async ({ harness }) => {
   });
   await harness
     .fromNode("USER_NAME")
-    .newDisplay({ content: ["Hello, ", harness.createRef("USER_NAME")] });
+    .newOutput({ content: ["Hello, ", harness.createRef("USER_NAME")] });
   // Now lets run the flow.
   await harness.run();
   expect(await harness.getOutputs()).toEqual(["What's your name?"]);
@@ -23,39 +22,11 @@ test("smoke test", async ({ harness }) => {
 
 /** Checking the main page headings are present.  */
 async function verifyMainPage(harness: BreadboardTestHarness) {
-  await expect(harness.page).toHaveTitle(/Breadboard - Flows/);
+  await expect(harness.page).toHaveTitle("Breadboard [Dev]");
   expect(await harness.getHeadings()).toEqual([
-    "Breadboards are mini AI apps anyone can build",
+    "Breadboard",
+    "Build, edit and share mini-AI apps using natural language",
     "Your Flows",
     "Gallery",
   ]);
-}
-
-/** Checking for important interface buttons to be present. */
-async function verifyEditorButtons(harness: BreadboardTestHarness) {
-  const buttons = await harness.getButtonsTexts();
-  const executedButtons: Array<string | RegExp> = [
-    "Asset",
-    "User Input",
-    "Generate",
-    "Display Output",
-    "Edit Theme",
-    "Console",
-    "Zoom to fit",
-    "Zoom in",
-    "Zoom out",
-    "history",
-    "URL", // Share button
-    /Start/,
-  ];
-  for (const expectedButton of executedButtons) {
-    if (typeof expectedButton === "string") {
-      expect(buttons, expectedButton).toContain(expectedButton);
-    } else {
-      expect(
-        buttons.findIndex((button) => expectedButton.test(button)),
-        expectedButton.toString()
-      ).toBeGreaterThanOrEqual(0);
-    }
-  }
 }
