6253335ab674c140d17edf4c6460ee1b36accd19
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-08-12 18:02:24 +0100

    Add test

--- packages/utils/tests/node/sanitizer.test.ts
+++ packages/utils/tests/node/sanitizer.test.ts
@@ -7,6 +7,7 @@
 import assert from "node:assert";
 import { suite, test } from "node:test";
 import * as Sanitizer from "../../src/sanitizer.js";
+import { Template } from "../../src/template.js";
 
 suite("escape", () => {
   test("handles empty values", () => {
@@ -32,6 +33,61 @@ suite("escape", () => {
   });
 
   test("double-encodes already-encoded chars", () => {
+    assert.equal(
+      Sanitizer.escape("Morecambe &amp; Wise"),
+      "Morecambe &amp;amp; Wise"
+    );
+
+    assert.equal(Sanitizer.escape("10 &gt; 8"), "10 &amp;gt; 8");
+    assert.equal(Sanitizer.escape("8 &lt; 18"), "8 &amp;lt; 18");
+    assert.equal(
+      Sanitizer.escape("They said &quot;Hello&quot;"),
+      "They said &amp;quot;Hello&amp;quot;"
+    );
+    assert.equal(
+      Sanitizer.escape("They said &#39;Hello&#39;"),
+      "They said &amp;#39;Hello&amp;#39;"
+    );
+    assert.equal(
+      Sanitizer.escape("&amp;&lt;&gt;&#39;&quot;"),
+      "&amp;amp;&amp;lt;&amp;gt;&amp;#39;&amp;quot;"
+    );
+  });
+
+  test("works with templates", () => {
+    const tmpl = new Template(`
+      10 > 20;
+
+      <img src="foo.jpg">
+
+      Initial topic:
+      {{"type": "in", "path": "a6f7367b-66f3-4d1b-9a57-a51f83110bc3", "title": "Get topic"}}
+
+      Research:
+      {{"type": "in", "path": "7a3fd89a-d86d-4aea-98cd-15194027dff1", "title": "Do Research"}}
+
+      <`);
+
+    tmpl.substitute(
+      (part) => part.title,
+      (part) => Sanitizer.escape(part)
+    );
+    assert.strictEqual(
+      tmpl.renderable,
+      `
+      10 &gt; 20;
+
+      &lt;img src=&quot;foo.jpg&quot;&gt;
+
+      Initial topic:
+      Get topic
+
+      Research:
+      Do Research
+
+      &lt;`
+    );
+
     assert.equal(
       Sanitizer.escape("Morecambe &amp; Wise"),
       "Morecambe &amp;amp; Wise"
