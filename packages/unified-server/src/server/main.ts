b8edb15305be5f3b0c642e2a4ee14e18fa4c46af
Al Marks <aomarks@google.com>
2025-08-21 16:05:27 -0700

    Re-enable the drive proxy, clean it up a bunch, and improve error handling

--- packages/unified-server/src/server/main.ts
+++ packages/unified-server/src/server/main.ts
@@ -11,7 +11,7 @@ import * as connectionServer from "@breadboard-ai/connection-server";
 import * as boardServer from "@breadboard-ai/board-server";
 import { InputValues, NodeDescriptor } from "@breadboard-ai/types";
 
-// import { makeDriveProxyMiddleware } from "./drive-proxy.js";
+import { makeDriveProxyMiddleware } from "./drive-proxy.js";
 import { allowListChecker } from "./allow-list-checker.js";
 import { getConfigFromSecretManager } from "./provide-config.js";
 import { makeCspHandler } from "./csp.js";
@@ -50,15 +50,6 @@ server.use(
 
 server.use("/app/@:user/:name", boardServer.middlewares.loadBoard());
 
-// server.use(
-//   "/files",
-//   makeDriveProxyMiddleware({
-//     serverUrl: serverConfig.SERVER_URL,
-//     featuredGalleryFolderId:
-//       serverConfig.GOOGLE_DRIVE_FEATURED_GALLERY_FOLDER_ID,
-//   })
-// );
-
 server.use("/updates", createUpdatesHandler());
 
 server.use("/app", (req, res) => {
@@ -85,6 +76,8 @@ server.use(
     cacheRefreshSeconds: 10 * 60,
   })
 );
+
+server.use("/api/drive-proxy", makeDriveProxyMiddleware());
 
 server.use(
   "/api/mcp-proxy",
