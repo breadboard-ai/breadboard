79df324bbcc97a0ea93ee98e3328c5bb41a1c019
Al Marks <aomarks@google.com>
2025-09-10 10:24:54 -0700

    display contents

--- packages/shared-ui/src/elements/shell/sign-in-modal.ts
+++ packages/shared-ui/src/elements/shell/sign-in-modal.ts
@@ -58,13 +58,7 @@ export class VESignInModal extends LitElement {
     icons,
     css`
       :host {
-        display: block;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 1;
+        display: contents;
       }
 
       bb-modal {
