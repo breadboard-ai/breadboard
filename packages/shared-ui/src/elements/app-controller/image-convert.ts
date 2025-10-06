3187241cc2af54891c32b80e1a0ad52de71c4d2b
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-10-06 13:51:21 +0100

    [su] Add support for file uploads in theming (#6751)
    With the following properties:
    
    1. Rejects images that are less than 256px in dimensions.
    2. Rejects images that are > 5MB in file size.
    3. Resizes images to 2048px via a canvas.
    4. Pushes image files to the same storage layer as generated images.

---
+++ packages/shared-ui/src/elements/app-controller/image-convert.ts
@@ -0,0 +1,76 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { InlineDataCapabilityPart } from "@breadboard-ai/types";
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+const PREAMBLE = "data:image/jpeg;base64,";
+const MIN_IMAGE_SIZE = 256;
+const MAX_IMAGE_SIZE = 2048;
+
+/**
+ * Converts an uploaded image blob to a InlineData part. In so doing it checks
+ * that the image being handled is at least 256px square, and if it is larger
+ * than 2048px square it downscales it.
+ *
+ * N.B. It does not ensure that the image is proportioned appropriately for the
+ * case of a splash screen.
+ *
+ * @param file
+ * @returns
+ */
+export async function convertImageToInlineData(
+  file: Blob
+): Promise<InlineDataCapabilityPart> {
+  if (!ctx) {
+    throw new Error("Unable to handle image");
+  }
+
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("loadend", () => {
+      try {
+        const img = new Image();
+        img.src = reader.result as string;
+        img.onload = () => {
+          if (
+            img.naturalWidth < MIN_IMAGE_SIZE ||
+            img.naturalHeight < MIN_IMAGE_SIZE
+          ) {
+            reject("The uploaded image is too small");
+            return;
+          }
+
+          const ratio = img.naturalWidth / img.naturalHeight;
+          const width = Math.min(img.naturalWidth, MAX_IMAGE_SIZE);
+          const height = width * ratio;
+          canvas.width = width;
+          canvas.height = height;
+
+          ctx.drawImage(img, 0, 0, width, height);
+          const data = canvas
+            .toDataURL("image/jpeg", 85)
+            .substring(PREAMBLE.length);
+
+          resolve({
+            inlineData: {
+              mimeType: "image/jpeg",
+              data,
+            },
+          });
+        };
+        img.onerror = () => {
+          reject("Unable to handle image");
+        };
+      } catch (err) {
+        console.warn(err);
+        reject("Unable to handle image");
+      }
+    });
+    reader.readAsDataURL(file);
+  });
+}
