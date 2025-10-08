283dbeea78d6eb5ae6f8f49e8fca8061255c7a59
aniruddhha <aniruddha.kudalkar@gmail.com>
2025-10-08 21:33:16 +0530

    feat(board-server): include author username in single-board GET via graph.metadata.username

--- packages/board-server/src/server/boards/get.ts
+++ packages/board-server/src/server/boards/get.ts
@@ -7,6 +7,7 @@
 import type { Request, Response } from "express";
 import * as errors from "../errors.js";
 import type { StorageBoard } from "../store.js";
+import type { GraphDescriptor } from "@google-labs/breadboard";
 
 async function get(_req: Request, res: Response) {
   try {
@@ -15,7 +16,23 @@ async function get(_req: Request, res: Response) {
       res.sendStatus(404);
       return;
     }
-    res.json(board.graph);
+        const graph = board.graph;
+    if (!graph) {
+      res.sendStatus(404);
+      return;
+    }
+
+    // Non-breaking enhancement: attach author's username to graph.metadata.
+    const username = board.owner; // StorageBoard.owner is a string
+    const enriched: GraphDescriptor = {
+      ...graph,
+      metadata: {
+        ...(graph.metadata ?? {}),
+        ...(username ? { username } : {}),
+      },
+    };
+
+    res.json(enriched);
   } catch (e) {
     errors.serverError(res, (e as Error).message);
     return;
