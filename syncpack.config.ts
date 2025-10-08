0b4ce196c5d1dda40ae55105749e86215b79b8ed
Al Marks <aomarks@google.com>
2025-10-08 16:09:32 -0700

    make syncpack happy

--- syncpack.config.ts
+++ syncpack.config.ts
@@ -16,6 +16,11 @@ const config: RcFile = {
       dependencies: ["@google-labs/**", "@breadboard-ai/**"],
       isIgnored: true,
     },
+    {
+      label: "Ignore express because we're using both v4 and v5 and that's ok",
+      dependencies: ["express", "@types/express"],
+      isIgnored: true,
+    },
   ],
 };
 
