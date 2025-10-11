0f62c833325bed5d77d6a451a779d64c0f1b6064
Dimitri Glazkov <dimitri@glazkov.com>
2025-10-11 14:07:29 -0700

    Scaffold up the logic experiment

---
+++ experiments/logic/README.md
@@ -0,0 +1,3 @@
+# The Logic Experiment
+
+TODO
---
+++ experiments/logic/src/cases/a-insta-caption.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Case } from "../types";
+
+export const a: Case = {
+  prompt: `For each image in 
+{{"type": "in", "path": "5960f99b-470c-4ef6-bdd2-1864c32086eb", "title": "Images"}}, provide a catchy caption suitable for an Instagram post. Output them collated into pairs: image + caption + image + caption, etc.`,
+};
