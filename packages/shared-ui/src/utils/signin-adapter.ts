001654bfd1321675739abbed20090342003a5096
Al Marks <aomarks@google.com>
2025-10-13 13:40:43 -0700

    fix bad position of log

--- packages/shared-ui/src/utils/signin-adapter.ts
+++ packages/shared-ui/src/utils/signin-adapter.ts
@@ -219,11 +219,10 @@ class SigninAdapter {
       const url = new URL("list", this.#globalConfig.connectionServerUrl);
       const httpRes = await fetch(url, { credentials: "include" });
       if (!httpRes.ok) {
-        return;
-      } else {
-        console.warn(
+        console.error(
           `SigninAdapter: Failed to fetch connections from ${url.href}, status: ${httpRes.status} ${httpRes.statusText}`
         );
+        return;
       }
       const list = (await httpRes.json()) as ListConnectionsResponse;
       const connection = list.connections.find(
