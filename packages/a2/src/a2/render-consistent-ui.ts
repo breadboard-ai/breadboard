35e95f4d7d43b33b72cb265e599c93cecd42c9ef
Dimitri Glazkov <dglazkov@google.com>
2025-10-14 16:38:42 -0700

    Add the simplest possible Gemini invocation.

--- packages/a2/src/a2/render-consistent-ui.ts
+++ packages/a2/src/a2/render-consistent-ui.ts
@@ -5,13 +5,38 @@
  */
 
 import { Capabilities, LLMContent, Outcome } from "@breadboard-ai/types";
+import { GeminiPrompt } from "./gemini-prompt";
+import { ok } from "@breadboard-ai/utils";
+import { GeminiSchema } from "./gemini";
 
 export { renderConsistentUI };
 
+const UI_SCHEMA: GeminiSchema = {
+  type: "object",
+  properties: {
+    data: {
+      type: "string",
+    },
+  },
+};
+
 async function renderConsistentUI(
-  _caps: Capabilities,
+  caps: Capabilities,
   data: LLMContent,
-  _systemInstruction?: LLMContent
+  systemInstruction?: LLMContent
 ): Promise<Outcome<LLMContent[]>> {
-  return [data];
+  const prompt = new GeminiPrompt(caps, {
+    model: "gemini-2.5-flash",
+    body: {
+      contents: [data],
+      systemInstruction,
+      generationConfig: {
+        responseMimeType: "application/json",
+        responseSchema: UI_SCHEMA,
+      },
+    },
+  });
+  const generated = await prompt.invoke();
+  if (!ok(generated)) return generated;
+  return generated.all;
 }
