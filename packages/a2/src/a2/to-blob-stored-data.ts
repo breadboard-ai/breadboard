a672774c6ec81653f9dd2842a30db15d5987dc55
Dimitri Glazkov <dglazkov@google.com>
2025-10-16 17:33:59 -0700

    Convert `callImageGen` to use `fetchWithCreds`.

--- packages/a2/src/a2/to-blob-stored-data.ts
+++ packages/a2/src/a2/to-blob-stored-data.ts
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Outcome, StoredDataCapabilityPart } from "@breadboard-ai/types";
+import { Chunk, Outcome, StoredDataCapabilityPart } from "@breadboard-ai/types";
 import { err } from "@breadboard-ai/utils";
 
-export { toBlobStoredData };
+export { toBlobStoredData, toGcsAwareChunk };
 
 const BLOB_PREFIX = new URL("/board/blobs/", window.location.href).href;
 
@@ -51,4 +51,22 @@ async function toBlobStoredData(
   } catch (e) {
     return err(`Failed to convert Drive file to Blob: ${(e as Error).message}`);
   }
+}
+
+function toGcsAwareChunk(
+  bucketId: string,
+  blobStoreData: BlobStoredData
+): Chunk {
+  const {
+    part: {
+      storedData: { handle },
+    },
+  } = blobStoreData;
+
+  // pluck blobId out
+  const blobId = handle.split("/").slice(-1)[0];
+  const path = `${bucketId}/${blobId}`;
+
+  const data = btoa(String.fromCodePoint(...new TextEncoder().encode(path)));
+  return { data, mimetype: "text/gcs-path" };
 }
