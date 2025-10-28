489fe7433bac65d4072831fb8e39b361296f4cee
Al Marks <aomarks@google.com>
2025-10-28 12:36:23 -0700

    Use URL parameter instead of document.referrer

--- packages/shared-ui/src/utils/opal-shell-guest.ts
+++ packages/shared-ui/src/utils/opal-shell-guest.ts
@@ -4,7 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { OpalShellProtocol } from "@breadboard-ai/types/opal-shell-protocol.js";
+import {
+  SHELL_ORIGIN_URL_PARAMETER,
+  type OpalShellProtocol,
+} from "@breadboard-ai/types/opal-shell-protocol.js";
 import { createContext } from "@lit/context";
 import * as comlink from "comlink";
 import { CLIENT_DEPLOYMENT_CONFIG } from "../config/client-deployment-configuration.js";
@@ -14,6 +17,8 @@ import "./url-pattern-conditional-polyfill.js";
 export const opalShellContext = createContext<OpalShellProtocol | undefined>(
   "OpalShell"
 );
+
+const SHELL_ORIGIN_SESSION_STORAGE_KEY = "shellOrigin";
 
 export async function connectToOpalShellHost(): Promise<OpalShellProtocol> {
   const hostOrigin = await discoverShellHostOrigin();
@@ -52,28 +57,67 @@ async function discoverShellHostOrigin(): Promise<string | undefined> {
   ) {
     return;
   }
-  if (!document.referrer) {
-    console.error("[shell guest] document.referrer was empty");
+
+  // We need to discover the origin of the shell window which is iframing us, so
+  // that we can verify it against our allowlist and use it to secure our
+  // postMessage channel.
+  //
+  // To accomplish this, the shell simply passes its origin to this iframe via a
+  // URL parameter. However if there is a subsequent navigation within the
+  // iframe, then this URL parameter might get dropped, and we don't want the
+  // burden of maintaining that URL parameter for all possible navigations
+  // within the app. So we instead also persist any verified origin to session
+  // storage, and use that as a fallback.
+  //
+  // Alternatives considered:
+  //
+  // - window.parent.origin is not readable cross-origin.
+  //
+  // - document.referrer works somewhat, but because it can change to a
+  //   different origin when a subsequent navigation occurs within the iframe,
+  //   we can only trust it on the very first load of an iframe. We _could_ use
+  //   session storage in a similar way to above to effectively identify this
+  //   first load, _except_ that because session storage is tied to the lifetime
+  //   of the top-level browser tab/window instead of the iframe instance, then
+  //   it would not be resilient against the following scenario:
+  //
+  //   1. User loads shell1.example, which is iframing app.example.
+  //   2. In the same browser tab, user loads shell2.example, which is also
+  //      iframing app.example.
+
+  const passedInShellOrigin = new URL(window.location.href).searchParams.get(
+    SHELL_ORIGIN_URL_PARAMETER
+  );
+  const shellOrigin =
+    passedInShellOrigin ||
+    sessionStorage.getItem(SHELL_ORIGIN_SESSION_STORAGE_KEY);
+  if (!shellOrigin) {
+    console.error(
+      `[shell guest] Could not find shell origin because shell did not set ` +
+        `the ${JSON.stringify(SHELL_ORIGIN_URL_PARAMETER)} URL parameter.`
+    );
     return;
   }
-  const referrerOrigin = new URL(document.referrer).origin;
+
   for (const pattern of allowedOriginPatterns) {
     if (
       pattern.includes("*")
-        ? new URLPattern(pattern).test(referrerOrigin)
-        : referrerOrigin === pattern
+        ? new URLPattern(pattern).test(shellOrigin)
+        : shellOrigin === pattern
     ) {
       console.debug(
-        `[shell guest] Referrer ${document.referrer} ` +
-          `matched allowed origin ${pattern}`
+        `[shell guest] ${shellOrigin} matched allowed origin ${pattern}`
       );
-      return referrerOrigin;
+      if (passedInShellOrigin) {
+        sessionStorage.setItem(
+          SHELL_ORIGIN_SESSION_STORAGE_KEY,
+          passedInShellOrigin
+        );
+      }
+      return shellOrigin;
     }
   }
-  console.error(
-    "[shell guest] Referrer origin was not in allowlist",
-    referrerOrigin
-  );
+  console.error("[shell guest] Shell origin was not in allowlist", shellOrigin);
 }
 
 function beginSyncronizingUrls(host: OpalShellProtocol) {
