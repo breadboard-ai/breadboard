c8e3bc44d66fe0bca60e4e6eb5560901d5a54120
Dimitri Glazkov <dglazkov@google.com>
2025-10-29 16:43:12 -0700

    Add a bunch more examples.

--- packages/a2/src/agent/a2ui/prompt.ts
+++ packages/a2/src/agent/a2ui/prompt.ts
@@ -88,6 +88,165 @@ const singleShotExample = [
   { beginRendering: { root: "root" } },
 ];
 
+const incrementalFullRender = [
+  {
+    surfaceUpdate: {
+      components: [
+        {
+          component: {
+            Column: {
+              children: {
+                explicitList: ["round_heading", "image_row"],
+              },
+            },
+          },
+          id: "root",
+        },
+        {
+          component: {
+            Heading: {
+              level: "2",
+              text: {
+                literalString: "Round 5",
+              },
+            },
+          },
+          id: "round_heading",
+        },
+        {
+          component: {
+            Row: {
+              children: {
+                explicitList: ["image_1_card", "image_2_card"],
+              },
+              distribution: "spaceAround",
+            },
+          },
+          id: "image_row",
+        },
+        {
+          component: {
+            Card: {
+              child: "image_1_button",
+            },
+          },
+          id: "image_1_card",
+        },
+        {
+          component: {
+            Button: {
+              action: {
+                name: "choose_image",
+                context: [
+                  {
+                    value: {
+                      literalString: "/vfs/image3.png",
+                    },
+                    key: "vfs_path",
+                  },
+                ],
+              },
+              child: "image_1",
+            },
+          },
+          id: "image_1_button",
+        },
+        {
+          id: "image_1",
+          component: {
+            Image: {
+              url: {
+                literalString: "/vfs/image3.png",
+              },
+            },
+          },
+        },
+        {
+          id: "image_2_card",
+          component: {
+            Card: {
+              child: "image_2_button",
+            },
+          },
+        },
+        {
+          component: {
+            Button: {
+              child: "image_2",
+              action: {
+                name: "choose_image",
+                context: [
+                  {
+                    value: {
+                      literalString: "/vfs/image6.png",
+                    },
+                    key: "vfs_path",
+                  },
+                ],
+              },
+            },
+          },
+          id: "image_2_button",
+        },
+        {
+          component: {
+            Image: {
+              url: {
+                literalString: "/vfs/image6.png",
+              },
+            },
+          },
+          id: "image_2",
+        },
+      ],
+      surfaceId: "ai_slop_or_not",
+    },
+  },
+  {
+    beginRendering: {
+      surfaceId: "ai_slop_or_not",
+      root: "root",
+    },
+  },
+];
+
+const incrementalUpdate = {
+  surfaceUpdate: {
+    surfaceId: "ai_slop_or_not",
+    components: [
+      {
+        id: "image_2_button",
+        component: {
+          Button: {
+            child: "image_2",
+            action: {
+              context: [
+                {
+                  key: "vfs_path",
+                  value: {
+                    literalString: "/vfs/image7.png",
+                  },
+                },
+              ],
+              name: "choose_image",
+            },
+          },
+        },
+      },
+      {
+        component: {
+          Image: {
+            url: {
+              literalString: "/vfs/image7.png",
+            },
+          },
+        },
+        id: "image_2",
+      },
+    ],
+  },
+};
+
 export const prompt = tr`
 
 Your ONLY means of interacting with the user are these two functions:
@@ -112,7 +271,7 @@ The "ui_render_user_interface" allows four types messages:
 - "deleteSurface -- removes a surface and its contents from the UI.
 - "dataModelUpdate" -- updates the underlying surface data model.
 - "beginRendering" -- signals to perform the initial render of the UI. This message is designed to prevent the "flash of incomplete content", allowing you 
-to send multiple "surfaceUpdate" prior to initial render.
+to send multiple "surfaceUpdate" prior to initial render. The "beginRendering" does not need to be sent more than once. The subsequent invocations are ignored.
 
 There are a couple of typical patterns of using "ui_render_user_interface":
 
@@ -122,10 +281,7 @@ the output, delivered at different times. In this pattern, call the "surfaceUpda
 
 Here's an example of a single shot pattern, rendering an info card:
 
-\`\`\`json
-${JSON.stringify(singleShotExample, null, 2)}
-\`\`\
-
+${makeExample(singleShotExample)}
 
 When designing the UI for the user interaction, see if there's a matching 
 pattern you can follow and use it.
@@ -150,15 +306,85 @@ The non-container components (like Text, Image, etc.) use a similar pattern:
 - "literalString", "literalBoolean", etc. to specify the known, static values
 - "path" to specify the data-bound values.
 
+### Actions and context
+
+When designing the UI tree that contains interactive elements, such as Button, etc. make sure to properly structure the action's "context" definition. The context definition is what allows an interactive element to correctly formulate a response when the user interacts with it.
+
+The context definition is an array that consists of the key-value pair defintions of values that will be sent back as the response of the "ui_await_user_input" function.
+
+The items in the content array can refer to either literal or data model values.
+
+Here's an example of a simple literal definition of an action context:
+
+${makeExample([
+  {
+    key: "vfs_path",
+    value: {
+      literalString: "/vfs/image7.png",
+    },
+  },
+])}
+
+When this action is invoked by the user, the "ui_await_user_input" will receive this value:
+
+${makeExample({
+  vfs_path: "/vfs/image7.png",
+})}
+
+Here's an example of a data model-based action context:
+
+${makeExample([
+  {
+    key: "topic",
+    value: {
+      path: "topic",
+    },
+  },
+])}
+
+When this action is invoked by the user, the UI will look up the "topic" value in the data model, and then return it. So, if we imagine that the "topic" value
+is "Opal", then the "ui_await_user_input" will receive this value:
+
+${makeExample({
+  topic: "Opal",
+})}
+
+### Incremental rendering
+
+Because the UI tree is stored as adjacency lists, you can use it to improve
+performance and efficiency by only sending the components that were updated.
+As long as the structure of the UI tree remains the same, you can safely send 
+incremental updates.
+
+Consider this example. Let's suppose that in this tree:
+
+${makeExample(incrementalFullRender)}
+
+When the user clicks any of the buttons, your task is to replace the image
+that was NOT clicked with another image. The structure remains the same. The
+only thing we're changing is the "literalString" value on either "image_1" or
+"image_2" component.
+
+So, If the user clicks the "image_button_1", then the you can skip re-sending
+the whole tree and only send the update for the "image_2" (the other image) and 
+the corresponding button "image_2_button" to update the action context:
+
+${makeExample(incrementalUpdate)}
+
+NOTE: When designing the UI for the first render, consider the following:
+how might it be structured so that the subsequent updates could be incremental?
+Study the objective and try to discern a modular pattern that allows you to
+create the smallest updates possible. Make incremental rendering work for you.
+
 ### Rendering dynamic UI with "template" and "path"
 
 Rendering dynamic UI consists of two steps:
 
-1. Calling (once) the "ui_surface_update" function to define the overall surface structure that uses the "template" and "path" properties to specify data binding points.
+1. Setting up the UI tree with overall surface structure that uses the "template" and "path" properties to specify data binding points.
 
-2. Calling (multiple times) the "ui_data_model_update" function to populate the data model for that surface. This data is then bound to the surface.
+2. Instead of (or in addition to) sending the "surfaceUpdate", send the "dataModelUpdate" message to populate the data model for that surface. This data is then bound to the surface and resolved to update the UI.
 
-At the first step, all container components rely on the "template" property to render dynamic lists and use thes two properties:
+All container components rely on the "template" property to render dynamic lists and use thes two properties:
 
 - "dataBinding": A path to a list in the data model (e.g., user.posts).
 - "componentId": The id of another component in the buffer to use as a template 
@@ -172,3 +398,9 @@ Whenever you use a dataBinding you must start paths for child items with no    o
 structure on which it is bound.
 
 `;
+
+function makeExample(json: unknown): string {
+  return `\`\`\`json
+${JSON.stringify(json)}
+\`\`\``;
+}
