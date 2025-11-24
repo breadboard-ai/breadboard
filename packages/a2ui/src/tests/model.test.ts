e8a8bd612a91b5f9037e24c25d9d042020b48a5c
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-11-24 17:27:31 +0000

    More path tweaks

--- packages/a2ui/src/tests/model.test.ts
+++ packages/a2ui/src/tests/model.test.ts
@@ -736,12 +736,12 @@ describe("A2UIModelProcessor", () => {
               {
                 id: "title-heading",
                 component: {
-                  Heading: {
-                    level: "1",
+                  Text: {
                     text: {
                       literalString: "Top Restaurants",
                     },
                   },
+                  usageHint: "h1",
                 },
               },
               {
@@ -807,12 +807,12 @@ describe("A2UIModelProcessor", () => {
               {
                 id: "template-name",
                 component: {
-                  Heading: {
-                    level: "3",
+                  Text: {
                     text: {
                       path: "name",
                     },
                   },
+                  usageHint: "h3",
                 },
               },
               {
@@ -1139,7 +1139,9 @@ describe("A2UIModelProcessor", () => {
               },
               {
                 id: "day-title",
-                component: { Heading: { text: { path: "title" } } },
+                component: {
+                  Text: { text: { path: "title" }, usageHint: "h1" },
+                },
               },
               {
                 id: "activity-list",
