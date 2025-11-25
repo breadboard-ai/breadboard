e3ec5479886330c4afae2afe16ee9191bfa447e5
Dimitri Glazkov <dglazkov@google.com>
2025-11-24 20:13:10 -0800

    Teach modal dialog to be undismissable sometimes.

--- packages/visual-editor/src/index-lite.ts
+++ packages/visual-editor/src/index-lite.ts
@@ -542,7 +542,9 @@ export class LiteMain extends MainBase implements LiteEditInputController {
     return [
       this.renderTooltip(),
       this.#renderOnboardingTooltip(),
-      this.uiState.show.has("SignInModal") ? this.renderSignInModal() : nothing,
+      this.uiState.show.has("SignInModal")
+        ? this.renderSignInModal(true)
+        : nothing,
     ];
   }
 
