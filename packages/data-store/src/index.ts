7118ebdd349807bbb3a992498b46cdb25664f85a
Dimitri Glazkov <dglazkov@google.com>
2025-11-27 12:45:47 -0800

    Fix build.

--- packages/data-store/src/index.ts
+++ packages/data-store/src/index.ts
@@ -4,15 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { createDefaultDataStore } from "@breadboard-ai/data";
+import { RuntimeFlags } from "@breadboard-ai/types";
 import {
   type DataStore,
-  createDefaultDataStore,
-  PersistentBackend,
   EphemeralBlobStore,
+  PersistentBackend,
 } from "@google-labs/breadboard";
 import { IDBBackend } from "./file-system/idb-backend.js";
 import { IdbFlagManager } from "./flags/idb-flag-manager.js";
-import { RuntimeFlags } from "@breadboard-ai/types";
 
 // TODO: Allow for other data stores.
 export function getDataStore(): DataStore {
