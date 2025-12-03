7da254daa9f174ea05f2d7b6801c862fe3dab601
Dimitri Glazkov <dglazkov@google.com>
2025-12-03 13:47:25 -0800

    refactor: Move client-side build configuration and assets from unified-server to visual-editor.

--- packages/visual-editor/src/init.ts
+++ packages/visual-editor/src/init.ts
@@ -5,8 +5,35 @@
  */
 
 import { CLIENT_DEPLOYMENT_CONFIG } from "@breadboard-ai/shared-ui/config/client-deployment-configuration.js";
-import { bootstrap } from "./bootstrap";
+import { bootstrap } from "./bootstrap.js";
+import { initializeAnalytics } from "@breadboard-ai/shared-ui/utils/action-tracker";
+import type { JsonSerializable, LLMContent } from "@breadboard-ai/types";
+
+const deploymentConfiguration = CLIENT_DEPLOYMENT_CONFIG;
+
+if (!deploymentConfiguration.BACKEND_API_ENDPOINT) {
+  throw new Error(`No BACKEND_API_ENDPOINT was configured`);
+}
+const executeStepEndpoint: string = new URL(
+  "v1beta1/executeStep",
+  deploymentConfiguration.BACKEND_API_ENDPOINT
+).href;
+
+if (deploymentConfiguration?.MEASUREMENT_ID) {
+  initializeAnalytics(deploymentConfiguration.MEASUREMENT_ID, true);
+}
 
 bootstrap({
-  deploymentConfiguration: CLIENT_DEPLOYMENT_CONFIG,
+  deploymentConfiguration,
+  env: [
+    {
+      path: "/env/settings/backend",
+      data: toLLMContent({ endpoint_url: executeStepEndpoint }),
+    },
+  ],
 });
+
+function toLLMContent(json: JsonSerializable): LLMContent[] {
+  // test
+  return [{ parts: [{ json }] }];
+}
---
+++ packages/visual-editor/vite.config.ts
@@ -0,0 +1,70 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { configureAssets } from "./src/configure-assets.js";
+import { tryGetGitHash } from "./src/build-info.js";
+import { defineConfig, loadEnv, UserConfig } from "vite";
+import compression from "vite-plugin-compression2";
+import tsconfigRaw from "./tsconfig.client.json" with { type: "json" };
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(async ({ mode }): Promise<UserConfig> => {
+  const envConfig = { ...loadEnv(mode!, __dirname) };
+
+  const [definedAssets, buildInfo] = await Promise.all([
+    configureAssets(__dirname, envConfig),
+    tryGetGitHash(),
+  ]);
+
+  const define = { ...buildInfo, ...definedAssets };
+
+  const entry: Record<string, string> = {
+    index: "./index.html",
+    oauth: "./oauth/index.html",
+    landing: "./landing/index.html",
+    shell: "./shell/index.html",
+  };
+
+  return {
+    esbuild: {
+      tsconfigRaw,
+    },
+    optimizeDeps: { esbuildOptions: { target: "esnext" } },
+    build: {
+      target: "esnext",
+      outDir: "dist/client",
+      lib: {
+        entry,
+        formats: ["es"],
+      },
+    },
+    define,
+    resolve: {
+      dedupe: ["lit"],
+    },
+    plugins: [
+      compression({
+        include: /\.(js|json|css|html|wasm|svg|woff2)$/,
+        algorithms: ["brotliCompress"],
+        threshold: 1024,
+        deleteOriginalAssets: false,
+      }),
+    ],
+    server: {
+      watch: {
+        ignored: [
+          "**/shared-ui/src/bgl/**",
+          "**/.wireit/**",
+          "**/*.kit.json/**",
+        ],
+      },
+    },
+  } satisfies UserConfig;
+});
