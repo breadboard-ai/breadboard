387a2f100d3459eea137434aa8dee09bb14b5aac
Dimitri Glazkov <dglazkov@google.com>
2025-12-09 15:24:32 -0800

    feat: Introduce `fetchWithRetry` utility to add retry logic for Google Drive API calls. (#7320)

--- packages/visual-editor/src/ui/utils/google-drive-host-operations.ts
+++ packages/visual-editor/src/ui/utils/google-drive-host-operations.ts
@@ -33,7 +33,7 @@ async function findUserOpalFolder(
   url.searchParams.set("orderBy", "createdTime desc");
 
   try {
-    let { files } = (await fetch(url, {
+    let { files } = (await fetchWithRetry(url, {
       // Closure munges the header key so it needs to be quoted.
       // But prettier likes to remove the quotes.
       // prettier-ignore
@@ -88,7 +88,7 @@ and 'me' in owners
   url.searchParams.set("orderBy", "modifiedTime desc");
 
   try {
-    let { files } = (await fetch(url, {
+    let { files } = (await fetchWithRetry(url, {
       // Closure munges the header key so it needs to be quoted.
       // But prettier likes to remove the quotes.
       // prettier-ignore
@@ -118,4 +118,55 @@ and 'me' in owners
 
 function quote(value: string) {
   return `'${value.replace(/'/g, "\\'")}'`;
+}
+
+/** Delay between GDrive API retries. */
+const RETRY_MS = 200;
+
+/** Retries fetch() calls until status is not an internal server error. */
+async function fetchWithRetry(
+  input: string | Request | URL,
+  init?: RequestInit,
+  numAttempts: 1 | 2 | 3 | 4 | 5 = 3
+): Promise<Response> {
+  function shouldRetry(response: Response): boolean {
+    return 500 <= response.status && response.status <= 599;
+  }
+
+  async function recursiveHelper(numAttemptsLeft: number): Promise<Response> {
+    numAttemptsLeft -= 1;
+    let response: Response | null = null;
+    try {
+      response = await fetch(input, init);
+      if (shouldRetry(response)) {
+        console.warn(
+          `Error in fetch(${input}). Attempts left: ${numAttemptsLeft}/${numAttempts}. Response:`,
+          response
+        );
+      } else {
+        return response;
+      }
+    } catch (e) {
+      console.warn(
+        `Exception in fetch(${input}). Attempts left: ${numAttemptsLeft}/${numAttempts}`,
+        e
+      );
+      // return "403 Forbidden" response, as this is likely a CORS error
+      response = new Response(null, {
+        status: 403,
+      });
+    }
+
+    if (numAttemptsLeft <= 0) {
+      return response;
+    }
+
+    return await new Promise((resolve) => {
+      setTimeout(async () => {
+        resolve(await recursiveHelper(numAttemptsLeft));
+      }, RETRY_MS);
+    });
+  }
+
+  return recursiveHelper(numAttempts);
 }
