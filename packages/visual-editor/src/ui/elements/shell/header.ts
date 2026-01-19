ae3392c1457483ff5dcbe10acec61642275b00be
Georgi Genov <ggenov@google.com>
2026-01-19 15:20:22 +0200

    fixed discord path and removed the new svg

--- packages/visual-editor/src/ui/elements/shell/header.ts
+++ packages/visual-editor/src/ui/elements/shell/header.ts
@@ -585,7 +585,7 @@ export class VEHeader extends SignalWatcher(LitElement) {
         id: "chat",
         title: Strings.from("COMMAND_JOIN_CHAT"),
         svgIcon:
-          "var(--bb-icon-discord, url(/third_party/icons/discord.svg))",
+          "var(--bb-icon-discord, url(/styles/landing/images/third_party/discord-logo.svg))",
       },
       ...(hasEnabledGlobalSettings(
         this.#uiState,
