a9e83aa20b98bcb2a5ac33252235cfcb1ba73a6c
Al Marks <aomarks@google.com>
2026-02-03 13:55:54 -0800

    Boilerplate for Share actions

--- packages/visual-editor/src/sca/actions/actions.ts
+++ packages/visual-editor/src/sca/actions/actions.ts
@@ -10,12 +10,14 @@ import * as Board from "./board/board-actions.js";
 import * as Flowgen from "./flowgen/flowgen-actions.js";
 import * as Graph from "./graph/graph-actions.js";
 import * as Run from "./run/run-actions.js";
+import * as Share from "./share/share-actions.js";
 
 export interface AppActions {
   board: typeof Board;
   flowgen: typeof Flowgen;
   graph: typeof Graph;
   run: typeof Run;
+  share: typeof Share;
 }
 
 let instance: AppActions | null = null;
@@ -25,12 +27,14 @@ export function actions(controller: AppController, services: AppServices) {
     Flowgen.bind({ controller, services });
     Graph.bind({ controller, services });
     Run.bind({ controller, services });
+    Share.bind({ controller, services });
 
     instance = {
       board: Board,
       flowgen: Flowgen,
       graph: Graph,
       run: Run,
+      share: Share,
     } satisfies AppActions;
   }
   return instance;
---
+++ packages/visual-editor/src/sca/actions/share/share-actions.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { makeAction } from "../binder.js";
+
+export const bind = makeAction();
