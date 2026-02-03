670c8847d0259a831106a115fd7dad9de7bcdc13
Al Marks <aomarks@google.com>
2026-02-03 13:52:05 -0800

    Add new ShareController

---
+++ packages/visual-editor/src/sca/controller/subcontrollers/editor/share-controller.ts
@@ -0,0 +1,81 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type { NarrowedDriveFile } from "@breadboard-ai/utils/google-drive/google-drive-client.js";
+import { RootController } from "../root-controller.js";
+import { field } from "../../decorators/field.js";
+
+export type ShareState =
+  | { status: "closed" }
+  | { status: "opening" }
+  | { status: "loading" }
+  | {
+    status: "readonly";
+    shareableFile: { id: string; resourceKey: string | undefined };
+  }
+  | {
+    status: "writable";
+    published: true;
+    publishedPermissions: gapi.client.drive.Permission[];
+    granularlyShared: boolean;
+    shareableFile: {
+      id: string;
+      resourceKey: string | undefined;
+      stale: boolean;
+      permissions: gapi.client.drive.Permission[];
+      shareSurface: string | undefined;
+    };
+    latestVersion: string;
+    userDomain: string;
+  }
+  | {
+    status: "writable";
+    published: false;
+    granularlyShared: boolean;
+    shareableFile:
+    | {
+      id: string;
+      resourceKey: string | undefined;
+      stale: boolean;
+      permissions: gapi.client.drive.Permission[];
+      shareSurface: string | undefined;
+    }
+    | undefined;
+    latestVersion: string;
+    userDomain: string;
+  }
+  | {
+    status: "updating";
+    published: boolean;
+    granularlyShared: boolean;
+    shareableFile:
+    | { id: string; resourceKey: string | undefined; stale: boolean }
+    | undefined;
+    userDomain: string;
+  }
+  | {
+    status: "granular";
+    shareableFile: { id: string };
+  }
+  | {
+    status: "unmanaged-assets";
+    problems: UnmanagedAssetProblem[];
+    oldState: ShareState;
+    closed: { promise: Promise<void>; resolve: () => void };
+  };
+
+export type UnmanagedAssetProblem = {
+  asset: NarrowedDriveFile<"id" | "resourceKey" | "name" | "iconLink">;
+} & (
+    | { problem: "cant-share" }
+    | { problem: "missing"; missing: gapi.client.drive.Permission[] }
+  );
+
+
+export class ShareController extends RootController {
+  @field()
+  accessor state: ShareState = { status: "closed" };
+}
