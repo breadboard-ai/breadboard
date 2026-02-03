e56f757d5b34fed304b573ac4d16f777d8a299a6
Paul Lewis <617438+paullewis@users.noreply.github.com>
2026-02-03 17:46:04 +0000

    [ve] Move node config editing nearer to SCA (#7669)

--- packages/visual-editor/src/sca/controller/subcontrollers/editor/editor.ts
+++ packages/visual-editor/src/sca/controller/subcontrollers/editor/editor.ts
@@ -8,3 +8,4 @@ export * as Splitter from "./splitter/splitter-controller.js";
 export * as Selection from "./selection/selection.js";
 export * as Sidebar from "./sidebar/sidebar-controller.js";
 export * as Graph from "./graph/graph.js";
+export * as Step from "./step/step-controller.js";
