5a7d8b2a013aea7f90bba84695c07842ca7d9930
Dimitri Glazkov <dglazkov@google.com>
2026-02-03 16:33:51 -0800

    Remove describe/invoke caps.

--- packages/visual-editor/eval/eval.ts
+++ packages/visual-editor/eval/eval.ts
@@ -249,18 +249,12 @@ class EvalRun implements EvalHarnessRuntimeArgs {
   readonly requestLogger = new Logger();
 
   readonly caps: Capabilities = {
-    invoke() {
-      throw new Error(`Not implemented`);
-    },
     input() {
       throw new Error(`Not implemented`);
     },
     async output(data) {
       console.log(data.$metadata?.title);
       return { delivered: true };
-    },
-    describe() {
-      throw new Error(`Not implemented`);
     },
     query() {
       throw new Error(`Not implemented`);
