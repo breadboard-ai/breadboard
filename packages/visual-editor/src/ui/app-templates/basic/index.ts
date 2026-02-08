d48b83e0f1e1c4478f6773be4690dfecf3de1d2f
Dimitri Glazkov <dglazkov@google.com>
2026-02-07 18:55:00 -0800

    Enable HTML support for A2UI display.

--- packages/visual-editor/src/ui/app-templates/basic/index.ts
+++ packages/visual-editor/src/ui/app-templates/basic/index.ts
@@ -28,6 +28,7 @@ import "./a2ui-custom-elements/a2ui-custom-pdf-viewer.js";
 import "./a2ui-custom-elements/a2ui-custom-media-container.js";
 import "./a2ui-custom-elements/a2ui-custom-video.js";
 import "./a2ui-custom-elements/a2ui-custom-google-drive.js";
+import "./a2ui-custom-elements/a2ui-custom-html.js";
 import "./header/header.js";
 
 import { SignalWatcher } from "@lit-labs/signals";
