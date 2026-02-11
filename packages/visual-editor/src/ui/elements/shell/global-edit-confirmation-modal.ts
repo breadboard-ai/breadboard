1ab81109216b92abd80040314b4614757cd8e28a
Georgi Genov <ggenov@google.com>
2026-02-11 13:33:00 +0200

    modal css fix

--- packages/visual-editor/src/ui/elements/shell/global-edit-confirmation-modal.ts
+++ packages/visual-editor/src/ui/elements/shell/global-edit-confirmation-modal.ts
@@ -3,12 +3,29 @@
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
 import { ModalDismissedEvent } from "../../events/events.js";
 
 @customElement("bb-global-edit-confirmation-modal")
 export class VEGlobalEditConfirmationModal extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 1000;
+      pointer-events: none;
+    }
+
+    bb-modal {
+      pointer-events: auto;
+    }
+  `;
+
   render() {
     return html`<bb-modal
       modalTitle="This may edit your entire Opal. Confirm to continue"
