50ee7a47953d274efe23502503fd428eab2f0b70
Dimitri Glazkov <dglazkov@google.com>
2026-02-12 09:44:38 -0800

    [runner] Make `PlanRunner` testable and add some tests.
    - **refactor: extract `getLatestConfig` function into a new file and
    update imports in `plan-runner.ts`**
    - **Make NodeInvoker a type and teach PlanRunner to DI it.**
    - **Extract `RunStateController`.**
    - **Clean up PlanRunner constructor sig.**
    - **Add tests for `RunStateController`.**

---
+++ packages/visual-editor/src/engine/runtime/harness/get-latest-config.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type {
+  GraphDescriptor,
+  NodeConfiguration,
+  NodeHandlerContext,
+  NodeIdentifier,
+  Outcome,
+} from "@breadboard-ai/types";
+import { err } from "@breadboard-ai/utils";
+
+export { getLatestConfig };
+
+function getLatestConfig(
+  id: NodeIdentifier,
+  graph: GraphDescriptor,
+  context: NodeHandlerContext
+): Outcome<NodeConfiguration> {
+  const gettingMainGraph = context.graphStore?.getByDescriptor(graph);
+  if (!gettingMainGraph?.success) {
+    return err(`Can't to find graph "${graph.url}" in graph store`);
+  }
+  const inspector = context.graphStore?.inspect(gettingMainGraph.result, "");
+  if (!inspector) {
+    return err(`Can't get inspector for graph "${graph.url}"`);
+  }
+  const inspectableNode = inspector.nodeById(id);
+  if (!inspectableNode) {
+    return err(`Unable to find node "${id}`);
+  }
+  return inspectableNode?.configuration();
+}
--- packages/visual-editor/src/engine/types.ts
+++ packages/visual-editor/src/engine/types.ts
@@ -11,6 +11,7 @@ export type {
   GraphDescriptor,
   GraphIdentifier,
   GraphInlineMetadata,
+  GraphToRun,
   InputIdentifier,
   InputResponse,
   InputValues,
@@ -24,8 +25,54 @@ export type {
   OutputIdentifier,
   OutputResponse,
   OutputValues,
+  RunArguments,
   SubGraphs,
 } from "@breadboard-ai/types";
 export type * from "@breadboard-ai/types/legacy.js";
 export type * from "@breadboard-ai/types/node-handler.js";
 export type * from "@breadboard-ai/types/schema.js";
+
+import type {
+  GraphDescriptor,
+  GraphToRun,
+  InputValues,
+  NodeConfiguration,
+  NodeDescriptor,
+  NodeHandlerContext,
+  NodeIdentifier,
+  OrchestrationPlan,
+  Outcome,
+  OutputValues,
+  RunArguments,
+} from "@breadboard-ai/types";
+
+export type { NodeInvoker };
+
+interface NodeInvoker {
+  invokeNode(
+    args: RunArguments,
+    graph: GraphToRun,
+    descriptor: NodeDescriptor,
+    inputs: InputValues,
+    invocationPath: number[]
+  ): Promise<OutputValues>;
+}
+
+export type { PlanCreator };
+
+type PlanCreator = (graph: GraphDescriptor) => OrchestrationPlan;
+
+export type { ConfigProvider };
+
+type ConfigProvider = (
+  id: NodeIdentifier,
+  graph: GraphDescriptor,
+  context: NodeHandlerContext
+) => Outcome<NodeConfiguration>;
+
+export type { RunEventSink };
+
+interface RunEventSink {
+  pause(): void;
+  dispatch(event: Event): void;
+}
