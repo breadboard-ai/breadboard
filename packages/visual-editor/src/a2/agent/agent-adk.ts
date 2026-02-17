d224974dee1733bc4af3686ad406c59d14e5ff51
walker7734 <walker7734@gmail.com>
2026-02-17 16:48:07 +0000

    [Opal ADK] Wires up the ability to send binary data to Opal ADK.  This technically works. However, tuning will need to be done to the prompts provided by the planner and the substituting logic in Breadboard.  The Opal ADK agent gets very confused when an input is referenced but it doesn't see it as part of its input. This is because binary input is stored in artifacts in ADK. I updated some prompts on the backend to indicate it should look in the artifacts to find the uploads and this helps but it has a way to go. This will provide a good place to start as it will still send the data to Opal ADK and you can see the agent bang its head against a wall.

--- packages/visual-editor/src/a2/agent/agent-adk.ts
+++ packages/visual-editor/src/a2/agent/agent-adk.ts
@@ -9,6 +9,7 @@ import {
 } from "@breadboard-ai/types";
 import { ok } from "@breadboard-ai/utils";
 import { Template } from "../a2/template.js";
+import { toLLMContent, isLLMContent } from "../a2/utils.js";
 import { A2ModuleArgs } from "../runnable-module-factory.js";
 import { OpalAdkStream, NODE_AGENT_KEY } from "../a2/opal-adk-stream.js";
 import { AgentInputs, AgentOutputs, toAgentOutputs } from "./main.js"
@@ -27,18 +28,25 @@ export async function invokeAgentAdk(
   const params = Object.fromEntries(
     Object.entries(rest).filter(([key]) => key.startsWith("p-z-"))
   );
-  const opalAdkStream = new OpalAdkStream(moduleArgs);
+
+  const opalAdkStream = new OpalAdkStream(caps, moduleArgs);
   const uiType = enableA2UI ? "a2ui" : "chat";
-  const template = new Template(prompt_template, moduleArgs.context.currentGraph);
+  const template = new Template(caps, prompt_template);
+  // TODO: This is going to need to be updated for Opal ADK subtitution.
   const completed_prompt = await template.substitute(params);
   if (!ok(completed_prompt)) {
     return completed_prompt;
   }
-  console.log("substitutine: ", completed_prompt);
+  
+  const paramsArray = Object.entries(rest)
+    .filter(([key]) => key.startsWith("p-z-"))
+    .map(([_, value]) =>
+      isLLMContent(value) ? value : toLLMContent(String(value))
+    );
   const results = await opalAdkStream.executeOpalAdkStream(
+    completed_prompt,
     NODE_AGENT_KEY,
-    [completed_prompt],
-    "none",
+    paramsArray,
     uiType,
     uiPrompt,
     invocation_id,
